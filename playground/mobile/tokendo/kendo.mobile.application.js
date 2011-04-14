6813e93810192a3ad25b5e75bc6edcf0d851d1b3
Alexander Gyoshev <alexander.gyoshev@telerik.com>
2011-04-14 13:51:48 +0300

    introduced more Application methods, organized user code
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C468986

--- playground/mobile/tokendo/kendo.mobile.application.js
+++ playground/mobile/tokendo/kendo.mobile.application.js
@@ -1,10 +1,11 @@
 function Application(options) {
+    this.views = [];
     $.extend(this, options);
     this.running = false;
 }
 
 Application.prototype = {
-    run: function() {
+    run: function(callback) {
         if (this.running)
             return;
 
@@ -15,14 +16,22 @@ Application.prototype = {
         window.addEventListener("DOMContentLoaded", function() {
             that.root = document.body;
 
+            callback(that);
             that.show(0);
         }, false);
 
         window.scrollTo(0, 1);
     },
 
+    addView: function(view) {
+        this.views.push(view);
+    },
+
     show: function(viewIndex) {
         this.views[viewIndex].element.appendTo(this.root);
+    },
+
+    goToView: function(viewName) {
     }
 };
 
