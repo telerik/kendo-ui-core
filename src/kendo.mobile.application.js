cb5512b60e46c1287bcaa6f660ccb3b8ae59c3de
Alexander Gyoshev <alexander.gyoshev@telerik.com>
2011-04-15 15:12:32 +0300

    moved mobile files, started using loader in index
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C469482

--- src/kendo.mobile.application.js
+++ src/kendo.mobile.application.js
@@ -1,24 +1,14 @@
 function Application(options) {
     this.views = [];
     $.extend(this, options);
-    this.running = false;
 }
 
 Application.prototype = {
     run: function(callback) {
-        if (this.running)
-            return;
+        this.root = document.body;
 
-        this.running = true;
-
-        var that = this;
-
-        window.addEventListener("DOMContentLoaded", function() {
-            that.root = document.body;
-
-            callback(that);
-            that.show(0);
-        }, false);
+        callback(this);
+        this.show(0);
 
         window.scrollTo(0, 1);
     },
