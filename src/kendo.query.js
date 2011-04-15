746dc9c8a9ca83a71854bcd393a319d8742561d7
Nikolay Rusev <Nikolay.Rusev@telerik.com>
2011-04-15 13:01:53 +0300

    filter implementation for kendo.data.Query
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C469360

--- src/kendo.query.js
+++ src/kendo.query.js
@@ -43,6 +43,152 @@
         }
     };
 
+    var Filter = {	               
+        create: function(expressions) {            
+            var indx, 
+                length,
+                expr,
+                selector,
+                operator,
+                desc,
+                descriptors = [],
+                caseSensitive,
+                predicate;
+                
+            expressions = expressions || []; 
+            for(indx = 0, length = expressions.length; indx < length; indx ++) {
+                expr = expressions[indx];
+                if(typeof expr.value === "string" && !expr.caseSensitive) { 
+                     caseSensitive = function(value) {
+                        return value.toLowerCase();
+                     };
+                } else {
+                    caseSensitive = function(value) { 
+                        return value;
+                    };
+                }
+                selector = Filter.selector(expr.field, caseSensitive);
+                operator = Filter.operator(expr.operator);
+                desc = operator(selector, caseSensitive(expr.value));
+                descriptors.push(desc);
+            }
+            predicate = Filter.combine(descriptors);
+
+            return function(data) {                 
+                return Filter.execute(predicate, data);
+            };	
+        },
+        selector: function(field, caseSensitive) {
+            return $.isFunction(field) ? field : function(record) {
+                return caseSensitive(record[field]);
+            };
+        },        
+        execute: function(predicate, data) {
+            var indx, 
+                length = data.length,
+                record,
+                result = [];
+                
+		    for(indx = 0; indx < length; indx ++) {               
+                record = data[indx];
+                
+                if(predicate(record)) {
+				    result.push(record);
+                }
+            }
+            		    
+		    return result;
+        },
+        combine: function(descriptors) {
+            return function(record) {
+                var result = true,
+                indx = 0, 
+                length = descriptors.length;
+
+                while(result && indx < length) {
+                    result = descriptors[indx ++](record);
+                }
+
+                return result;
+            };
+        },
+        operator: function(operator) {
+            if(!operator)
+                return Filter.eq;
+            if($.isFunction(operator))                
+                return operator;
+
+            operator = operator.toLowerCase();
+            operatorStrings = Filter.operatorStrings;
+            for (var op in operatorStrings) {
+                if ($.inArray(operator, operatorStrings[op]) > -1) {
+                    operator = op;
+                    break;
+                }
+            }
+            
+            return Filter[operator];
+        },
+        operatorStrings: {
+            "eq": ["eq", "==", "isequalto", "equals", "equalto", "equal"],
+            "neq": ["neq", "!=", "isnotequalto", "notequals", "notequalto", "notequal", "not"],
+            "lt": ["lt", "<", "islessthan", "lessthan", "less"],
+            "lte": ["lte", "<=", "islessthanorequalto", "lessthanequal"],
+            "gt": ["gt", ">", "isgreaterthan", "greaterthan", "greater"],
+            "gte": ["gte", ">=", "isgreaterthanorequalto", "greaterthanequal"],
+            "startswith": ["startswith"],
+            "endswith": ["endswith"],
+            "contains": ["contains", "substringof"]
+        },        
+        eq: function(selector, value) {                        
+            return function(record){   
+                var item = selector(record);
+                return item > value ? false : (value > item ? false : true);                                    
+            };
+        },
+        neq: function(selector, value) {
+            return function(record){                       
+                return selector(record) != value;
+            };
+        },
+        lt: function(selector, value) {            
+            return function(record){                     
+                return selector(record) < value;
+            };
+        },
+        lte: function(selector, value) {
+            return function(record){                       
+                return selector(record) <= value;
+            };
+        },
+        gt: function(selector, value) {
+            return function(record){                       
+                return selector(record) > value;
+            };
+        },
+        gte: function(selector, value) {
+            return function(record){                       
+                return selector(record) >= value;
+            };
+        },
+        startswith: function(selector, value) {
+            return function(record){                       
+                return selector(record).indexOf(value) == 0;
+            };
+        },
+        endswith: function(selector, value) {
+            return function(record){                       
+                var item = selector(record);
+                return item.lastIndexOf(value) == item.length - 1;
+            };
+        },
+        contains: function(selector, value) {
+            return function(record){                       
+                return selector(record).indexOf(value) > -1;
+            };
+        }               
+    }
+
     function Query(data) {
         this.data = data;
     }
@@ -81,6 +227,11 @@
             }
 
             return this.orderBy({ compare: Comparer.combine(comparers) });
+        },
+        filter: function(expressions) {
+            expressions = $.isArray(expressions) ? expressions : [expressions];
+            var predicate = Filter.create(expressions);                        
+            return new Query(predicate(this.data));
         }
     }
 
