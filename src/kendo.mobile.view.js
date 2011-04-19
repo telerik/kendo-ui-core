5fa3760bfa420e03faba06938909438b7b52692d
Alexander Gyoshev <alexander.gyoshev@telerik.com>
2011-04-19 09:06:58 +0300

    revived application/view switching code
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C470103

--- src/kendo.mobile.view.js
+++ src/kendo.mobile.view.js
@@ -2,12 +2,28 @@ function View(options) {
     $.extend(this, options);
     this.element = $('<div class="data-container"/>');
     this.scroller = new Scroller(this.element);
-    this.scrollElement = this.scroller.scrollElement;
+    this.contentElement = this.scroller.scrollElement || this.element;
+    
+    if (this.onCreate) {
+        this.onCreate();
+    }
 }
 
 View.prototype = {
     bind: function(data) {
-        this.onBind(data, this.scrollElement || this.element);
+        this.onBind(data);
+    },
+
+    show: function(container) {
+        if (this.onShow) {
+            this.onShow();
+        }
+
+        this.element.appendTo(container);
+    },
+
+    hide: function() {
+        this.element.remove();
     }
 };
 
