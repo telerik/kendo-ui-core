ea209abce593d09099553be05eb16d71f268f4d8
Atanas Korchev <akorchev@gmail.com>
2011-04-22 16:48:48 +0300

    added support for nested test suites

---
+++ tests/core/tests.js
@@ -0,0 +1,22 @@
+tests.push(
+    {
+        page: "core/observable.html",
+        title: "observable"
+    },
+    {
+        page: "core/component.html",
+        title: "component"
+    },
+    {
+        page: "core/format.html",
+        title: "format"
+    },
+    {
+        page: "core/json.html",
+        title: "json"
+    },
+    {
+        page: "core/template.html",
+        title: "template"
+    }
+);
---
+++ tests/data/tests.js
@@ -0,0 +1,46 @@
+tests.push(
+    {
+        page: "data/datasource/read.html",
+        title: "datasource.read"
+    },
+    {
+        page: "data/datasource/initialization.html",
+        title: "datasource initialization"
+    },
+    {
+        page: "data/datasource/create.html",
+        title: "datasource.create"
+    },
+    {
+        page: "data/datasource/destroy.html",
+        title: "datasource.destroy"
+    },
+    {
+        page: "data/datasource/update.html",
+        title: "datasource.update"
+    },
+    {
+        page: "data/datasource/find.html",
+        title: "datasource.find / datasource.at"
+    },
+    {
+        page: "data/datasource/query.html",
+        title: "datasource.query"
+    },
+    {
+        page: "data/query.html",
+        title: "query"
+    },
+    {
+        page: "data/localstorage.html",
+        title: "local storage cache"
+    },
+    {
+        page: "data/remotetransport.html",
+        title: "remote transport"
+    },
+    {
+        page: "data/inmemorycache.html",
+        title: "in-memory cache"
+    }
+);
