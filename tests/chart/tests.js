c83050cff5c4c141824717c5f39b3111c92901e5
Tsvetomir Tsonev <Tsvetomir.Tsonev@telerik.com>
2011-04-25 09:19:21 +0000

    Chart tests now use the common test runner
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C471221

---
+++ tests/chart/tests.js
@@ -0,0 +1,18 @@
+tests.push(
+    {
+        page: "chart/viewmodel.html",
+        title: "chart viewmodel"
+    },
+    {
+        page: "chart/svg.html",
+        title: "chart svg renderer"
+    },
+    {
+        page: "chart/barchart.html",
+        title: "barchart"
+    },
+    {
+        page: "chart/numericaxis.html",
+        title: "chart numericaxis"
+    }
+);
