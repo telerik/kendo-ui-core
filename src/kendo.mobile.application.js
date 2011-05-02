29412723701896780332fac274612adcb7dcc3d7
Alexander Gyoshev <alexander.gyoshev@telerik.com>
2011-05-02 15:20:38 +0000

    introduced kendo.mobile.init
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C472993

--- src/kendo.mobile.application.js
+++ src/kendo.mobile.application.js
@@ -1,32 +1,54 @@
-function Application(options) {
-    this.views = [];
-    $.extend(this, options);
-}
+(function($){
+    var mobile = kendo.mobile = kendo.mobile || {};
 
-Application.prototype = {
-    run: function(callback) {
-        this.root = document.body;
+    $.extend(mobile, {
+        init: function(options) {
+            var html = [
+                    "<meta name='viewport' content='width=device-width' />"
+                ];
 
-        callback(this);
-        this.show(0);
+            if (options.icon) {
+                html.push("<link rel='apple-touch-icon' href='");
+                html.push(options.icon);
+                html.push("' />");
+            }
 
-        window.scrollTo(0, 1);
-    },
-
-    addView: function(view) {
-        this.views.push(view);
-    },
-
-    show: function(viewIndex, options) {
-        if (this.currentView) {
-            this.currentView.hide();
+            $(html.join("")).appendTo("head");
         }
+    });
 
-        var view = this.views[viewIndex]; 
-        
-        view.show(this.root);
-
-        this.currentView = view;
+    function Application(options) {
+        $.extend(this, options);
+        this.views = [];
     }
-};
 
+    Application.prototype = {
+        run: function(callback) {
+            this.root = document.body;
+
+            callback(this);
+            this.show(0);
+
+            window.scrollTo(0, 1);
+        },
+
+        addView: function(view) {
+            this.views.push(view);
+        },
+
+        show: function(viewIndex, options) {
+            if (this.currentView) {
+                this.currentView.hide();
+            }
+
+            var view = this.views[viewIndex]; 
+            
+            view.show(this.root);
+
+            this.currentView = view;
+        }
+    };
+
+    // temporary, the class will be removed
+    window.Application = Application;
+})(jQuery);
