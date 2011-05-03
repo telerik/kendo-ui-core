301aa9e754d2e62439afb972e388b86fed5b0036
Georgi Krustev <Georgi.Krustev@telerik.com>
2011-05-03 12:14:48 +0000

    add basic keyboard support to autocomplete
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C473175

--- src/kendo.autocomplete.js
+++ src/kendo.autocomplete.js
@@ -25,6 +25,94 @@
         that._dataSource();
 
         that.dataSource.read();
+
+        that.element.keydown($.proxy(that._keydown, that));
+    }
+
+    function move(direction, ul) {
+        var items = ul.find("li"),
+            selectedItem = items.filter(".selected");
+
+        if(direction == "next") {
+            if(selectedItem.length){
+                selectedItem.removeClass("selected")
+                            .next()
+                            .addClass("selected");
+            } else {
+                items.first().addClass("selected");
+            }
+        } else if(direction == "prev") {
+            if(selectedItem.length){
+                selectedItem.removeClass("selected")
+                            .prev()
+                            .addClass("selected");
+            } else {
+                items.last().addClass("selected");
+            }
+        }
+    }
+
+    function lastIndexOf(value, character) {
+        var characterLength = character.length;
+        for (var i = value.length - 1; i > -1; i--)
+            if (value.substr(i, characterLength) == character) return i;
+        return -1;
+    }
+
+    function caretPos (element) {
+        var pos = -1;
+
+        if (document.selection)
+            pos = Math.abs(element.document.selection.createRange().moveStart('character', -element.value.length));
+        else if (element.selectionStart !== undefined)
+            pos = element.selectionStart;
+
+        return pos;
+    }
+
+    function valueArrayIndex(input, separator) {
+        return input.value.substring(0, $t.caretPos(input)).split(separator).length - 1;
+    }
+
+    function selection(input, start, end) {
+        if (input.createTextRange) {
+            var selRange = input.createTextRange();
+            selRange.collapse(true);
+            selRange.moveStart('character', start);
+            selRange.moveEnd('character', end - start);
+            selRange.select();
+        } else if (input.selectionStart) {
+            input.selectionStart = start;
+            input.selectionEnd = end;
+        }
+    }
+
+    function autoFill(input, text, separator, multiple) {
+        var textBoxValue = input.val(),
+            endIndex = caretPos(input);
+
+        var lastSeparatorIndex = multiple && separator ? $t.lastIndexOf(textBoxValue.substring(0, endIndex), separator) : -1;
+        var startIndex = lastSeparatorIndex != -1 ? lastSeparatorIndex + separator.length : 0;
+
+        var filterString = textBoxValue.substring(startIndex, endIndex);
+        var matchIndex = text.toLowerCase().indexOf(filterString.toLowerCase());
+
+        if (matchIndex != -1) {
+
+            var stringToAppend = text.substring(matchIndex + filterString.length);
+
+            if (multiple) {
+                var split = textBoxValue.split(separator),
+                    wordIndex = valueArrayIndex(input, separator);
+
+                split[wordIndex] = filterString + stringToAppend;
+                input.value = split.join(separator) + (component.multiple && wordIndex != 0 && wordIndex == split.length - 1 ? separator : '');
+            } else {
+                input.val(filterString + stringToAppend);
+            }
+
+            selection(input, endIndex, endIndex + stringToAppend.length);
+        }
     }
 
     AutoComplete.prototype = {
@@ -53,6 +141,33 @@
 
             that.dataSource = DataSource.create(dataSource);
             that.dataSource.bind("change", $.proxy(that.refresh, that));
+        },
+
+        _keydown: function(e){
+            var that = this,
+                key = e.keyCode,
+                keys = kendo.keys;
+
+            switch(key) {
+            case keys.DOWN :
+                 if (!that.popup.element.is(":visible")) {
+                    that.popup.open();
+                 }
+                 move("next", that.ul);
+                 var selectedItem = that.ul.find(".selected");
+                 if (selectedItem.length > 0) {
+                    autoFill(that.element, selectedItem.text(), that.options.separator, that.options.multiple);
+                 }
+                 e.preventDefault();
+                 break;
+            case keys.UP :
+                 if (!that.popup.element.is(":visible")) {
+                    that.popup.open();
+                 }
+                 move("prev", that.ul);
+                 e.preventDefault();
+                 break;
+            }
         }
     }
 
