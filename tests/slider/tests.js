f8ee6410ac9d075bc28044965016b190dba91b8d
Hristo Germanov <Hristo.Germanov@telerik.com>
2011-05-04 15:23:56 +0000

    Modify tests for the slider and window
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C473612

---
+++ tests/slider/tests.js
@@ -0,0 +1,18 @@
+tests.push(
+    {
+        page: "slider/clientsideapi.html",
+        title: "slider client api"
+    },
+    {
+        page: "slider/rangeslider.html",
+        title: "range slider"
+    },
+    {
+        page: "slider/rendering.html",
+        title: "slider rendering"
+    },
+    {
+        page: "slider/slider.html",
+        title: "slider"
+    }
+);
---
+++ tests/window/tests.js
@@ -0,0 +1,10 @@
+tests.push(
+    {
+        page: "window/clientcreation.html",
+        title: "window client creation"
+    },
+    {
+        page: "window/clientsideapi.html",
+        title: "window client side api"
+    }
+);
