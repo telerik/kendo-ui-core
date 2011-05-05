8a1199ae3eeff31a1dd98112e2c181a0b900c929
Nikolay Rusev <Nikolay.Rusev@telerik.com>
2011-05-05 09:18:36 +0000

    added support for shift key and select, change events
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C473815

--- src/kendo.selectable.js
+++ src/kendo.selectable.js
@@ -4,7 +4,11 @@
         proxy = $.proxy,               
         MOUSEDOWN = "mousedown",
         MOUSEUP = "mouseup",
-        MOUSEMOVE = "mousemove"       
+        MOUSEMOVE = "mousemove",
+        SELECTED = "t-state-selected",
+        ACTIVE = "t-state-selecting",
+        SELECTABLE = "t-selectable",
+        UNSELECTING = "t-state-unselecting";       
 
     function Selectable(element, options) {
         var that = this;
@@ -12,24 +16,19 @@
                 	
         that.options = $.extend({}, that.options, options);		       
         that._marquee = $("<div class='t-marquee'></div>");
+        that._lastActive = null;
 
         that._moveDelegate = proxy(that._move, that);
-        that._upDelegate = proxy(that._up, that);
+        that._upDelegate = proxy(that._up, that);        
 
-        that._eventSelector = "." + that.options.selectableClass + " " + that.options.filter;
-
-        that.element.addClass(that.options.selectableClass);                
-        that.element.delegate(that._eventSelector, MOUSEDOWN, proxy(that._down, that));     
+        that.element.addClass(SELECTABLE);                
+        that.element.delegate("." + SELECTABLE + " " + that.options.filter, MOUSEDOWN, proxy(that._down, that));     
     }
     
     Selectable.prototype = {
         options: {
                 filter: ">*",
-                single: true,
-                selectedClass: "t-state-selected",
-                selectingClass: "t-state-selecting",
-                selectableClass: "t-selectable",
-                unselectingClass: "t-unselecting"
+                single: true
             },
             _collide: function(element, marqueePos) {
                 var pos = element.offset();
@@ -71,19 +70,18 @@
             _down: function (event) {                
                 var that = this,                    
                     ctrlKey = event.ctrlKey,
-                    shiftKey = event.shiftKey;
-
-                that._downTarget = $(event.currentTarget)
-
+                    shiftKey = event.shiftKey;                
+                that._downTarget = $(event.currentTarget);
+                that._shiftPressed = shiftKey;                
                 $(document).bind(MOUSEUP, that._upDelegate);     
-                if(!that.options.single) {
-                    $(document).bind(MOUSEMOVE, that._moveDelegate)
-                }           
-
                 that._originalPosition = {
                     x: event.pageX,
                     y: event.pageY
                 };
+
+                if(!that.options.single) {
+                    $(document).bind(MOUSEMOVE, that._moveDelegate)
+                }                           
                 
                 if (!that.options.single) {
                     $("body").append(that._marquee);
@@ -95,94 +93,150 @@
                     });
                 }
                
-                var wasSelected = that._downTarget.hasClass(that.options.selectedClass);                
-                if(that.options.single || !ctrlKey) {
-                    //unselect all selected
+                var selected = that._downTarget.hasClass(SELECTED);                
+                if(that.options.single || !(ctrlKey || shiftKey)) {
                     that.element
-                        .find(that.options.filter + "." + that.options.selectedClass)
-                        .removeClass(that.options.selectedClass);                    
-                }                      
-
-                if(!that._downTarget.hasClass(that.options.selectedClass)) {                    
-                    if(wasSelected && ctrlKey) {
-                        that._downTarget.addClass(that.options.selectedClass);
-                        if(that.options.single) {
-                            that._downTarget.addClass(that.options.unselectingClass);
-                        }
-                    }
-                    else {
-                        that._downTarget.addClass(that.options.selectingClass);
-                    }
+                        .find(that.options.filter + "." + SELECTED)
+                        .removeClass(SELECTED);                    
                 }
+                if(ctrlKey) {
+                    that._lastActive = that._downTarget;
+                }
+                
+                if(selected && (ctrlKey || shiftKey)) {
+                    that._downTarget.addClass(SELECTED);                    
+                    if(!shiftKey)                     
+                        that._downTarget.addClass(UNSELECTING);                    
+                }
+                else {
+                    that._downTarget.addClass(ACTIVE);
+                }                
                 
                 event.preventDefault();
             },
             _move: function (event) {
                 var that = this,
                     pos = that._position(event),
-                    ctrlKey = event.ctrlKey,
-                    selectingClassName = that.options.selectingClass,
-                    selectedClassName = that.options.selectedClass,
-                    unselectingClassName = that.options.unselectingClass;
+                    ctrlKey = event.ctrlKey;
                 
                 that._marquee.css({
                     "left": pos.left,
                     "top": pos.top,
                     "width": pos.right - pos.left,
                     "height": pos.bottom - pos.top
-                });                
+                });                   
+                               
                 that.element.find(that.options.filter).each(function () {                    
                     var selectee = $(this),
                         collide = that._collide(selectee, pos);
                         
                     if (collide) {         
-                        if(selectee.hasClass(selectedClassName)) {
-                            if(ctrlKey && that._downTarget[0] !== selectee[0]) {                                
-                                selectee
-                                    .removeClass(selectedClassName)
-                                    .addClass(unselectingClassName);
+                        if(selectee.hasClass(SELECTED)) {
+                            if(that._downTarget[0] !== selectee[0]) {
+                                if(ctrlKey) {
+                                    selectee
+                                        .removeClass(SELECTED)
+                                        .addClass(UNSELECTING);
+                                }                               
                             }
-                        } else if (!selectee.hasClass(selectingClassName) && !selectee.hasClass(unselectingClassName)) {
-                            selectee.addClass(selectingClassName);
+                        } else if (!selectee.hasClass(ACTIVE) && !selectee.hasClass(UNSELECTING)) {
+                            selectee.addClass(ACTIVE);
                         }
                     }
                     else {
-                        if (selectee.hasClass(selectingClassName)) {                                                   
-                            selectee.removeClass(selectingClassName);
+                        if (selectee.hasClass(ACTIVE)) {                                                   
+                            selectee.removeClass(ACTIVE);
                         }
-                        else if(ctrlKey && selectee.hasClass(unselectingClassName)) {
+                        else if(ctrlKey && selectee.hasClass(UNSELECTING)) {
                             selectee
-                                .removeClass(unselectingClassName)
-                                .addClass(selectedClassName);
-                        }
+                                .removeClass(UNSELECTING)
+                                .addClass(SELECTED);
+                        }                        
                     }
-                });
-            },
+                });                
+            },            
             _up: function (event) {                
-                var that = this;                
+                var that = this,
+                    options = that.options;                
                 $(document)
                     .unbind(MOUSEMOVE, that._moveDelegate)
-			        .unbind(MOUSEUP, that._upDelegate);                
-                that._downTarget = null;
+			        .unbind(MOUSEUP, that._upDelegate);                                
                 if (!that.options.single) {
                     that._marquee.remove();
                 }
+                
+                if(!options.single && that._shiftPressed === true) {
+                    that.selectRange(that._firstSelectee(), that._downTarget[0]);
+                }
+                else {
+                    that.element
+                        .find(options.filter + "." + ACTIVE).each(function() {
+                            var selecee = $(this),
+                                isPrevented = that.trigger("select", { element: this });
 
-                that.element
-                    .find(that.options.filter + "." + that.options.selectingClass)
-                    .removeClass(that.options.selectingClass)
-                    .addClass(that.options.selectedClass);   
+                            selecee.removeClass(ACTIVE);                            
+                            if(!isPrevented) {                            
+                                selecee.addClass(SELECTED);
+                            }
+                    });   
 
-                that.element
-                    .find(that.options.filter + "." + that.options.unselectingClass)
-                    .removeClass(that.options.unselectingClass)
-                    .removeClass(that.options.selectedClass);
+                    that.element
+                        .find(options.filter + "." + UNSELECTING)
+                        .removeClass(UNSELECTING)
+                        .removeClass(SELECTED);
+                }
+                if(!that._shiftPressed) {
+                    that._lastActive = that._downTarget;
+                }
+                that._downTarget = null;
+                that._shiftPressed = false;
+
+                that.trigger("change", {});
             },
             values: function() {
                 var that = this;
                 return that.element
-                    .find(that.options.filter + "." + that.options.selectedClass)
+                    .find(that.options.filter + "." + SELECTED)
                     .toArray();
+            },
+            _firstSelectee: function() {
+                var that = this;                    
+                if(that._lastActive !== null)
+                    return that._lastActive[0];
+
+                var selected = that.values();
+                return selected.length > 0 ? selected[0] : 
+                    that.element.find(that.options.filter)[0];
+            },
+            selectRange: function(start, end) {
+                var that = this,
+                    found = false;
+               
+                that.element.find(that.options.filter).each(function () {                    
+                    if(found) {                        
+                        $(this).addClass(SELECTED);
+                        if(this === end) {
+                            $(this).removeClass(ACTIVE);                            
+                            found = false;
+                        }
+                    }
+                    else if(this === start) {
+                        found = start === end ? false : true;
+                        $(this).addClass(SELECTED);
+                    }
+                    else if(this === end) {
+                        var tmp = start;
+                        start = end;
+                        end = tmp;
+                        found = true;
+                        $(this)                            
+                            .removeClass(ACTIVE)
+                            .addClass(SELECTED);
+                    }
+                    else {
+                        $(this).removeClass(SELECTED);
+                    }
+                });
             }
     }
     kendo.ui.plugin("Selectable", Selectable, kendo.ui.Component);
