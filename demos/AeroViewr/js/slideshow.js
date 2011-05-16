413046ae9bf686542169b6a241b3e88f71f60df8
Nikolay Rusev <Nikolay.Rusev@telerik.com>
2011-05-16 13:46:50 +0000

    slideshow implementations for guest account
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C476334

---
+++ demos/AeroViewr/js/slideshow.js
@@ -0,0 +1,34 @@
+(function($, window){
+    var TIMEOUT = 3000;
+    window.slideshow = {
+        init: function(listView) {
+            this._listView = listView;
+            this.stop();            
+        },
+        play: function() {
+            var that = this,
+                listView = that._listView;          
+            that._timeoutId = setTimeout(function() {                
+                var selected = listView.selected().next();
+                if(selected.length == 0) {
+                    selected = listView.element.find("li:first");                            
+                }
+                listView.selectable.clear();
+                listView.selectable.value(selected);
+                that._timeoutId = setTimeout(arguments.callee, TIMEOUT);                
+            }, TIMEOUT);
+        },
+        stop: function() {           
+            clearTimeout(this._timeoutId);
+            this._timeoutId = null;
+        },        
+        toggle: function() {
+            if(this._timeoutId) {
+                this.stop();
+            }
+            else {
+                this.play();
+            }
+        }
+    }
+})(jQuery, window);
