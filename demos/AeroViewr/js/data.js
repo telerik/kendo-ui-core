fd065e15106fa3253e80db698133c8a5b49a3285
Rosen Konstantinov <Rosen.Konstantinov@telerik.com>
2011-05-17 14:06:06 +0000

    added method to create common dataSource
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C476837

---
+++ demos/AeroViewr/js/data.js
@@ -0,0 +1,24 @@
+(function($, window, undefined) {
+    var data = {
+        dataSource: function(options) {
+            options = $.extend(true, {}, options, {
+                transport: createTransport(options.dialect, options.cache),
+                reader: options.reader
+            });
+            return new kendo.data.DataSource(options);
+        }
+    };
+
+    var createTransport = function(dialect, cache) {
+        var read = $.extend({},
+            !$.support.cors ? { jsonpCallback: "jsonFlickrApi", jsonp: false, dataType: "jsonp" } : { dataType: "json" },
+            { url: flickr.service, cache: true }
+        );
+        return {
+            read: read,
+            cache: cache !== undefined ? cache : "localstorage",
+            dialect: dialect
+        };
+    }
+    window.data = data;
+})(jQuery, window);
