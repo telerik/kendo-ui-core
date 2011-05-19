17a7421ef79740af52095f7f29abd2c36d4e4bfa
Atanas Korchev <akorchev@gmail.com>
2011-05-19 15:30:09 +0300

    copy the demo in the live folder

--- build/aeroviewr.js
+++ build/aeroviewr.js
@@ -1,8 +1,40 @@
 var uglify = require("./uglify-js").uglify;
 var parser = require("./uglify-js").parser;
 var fs = require("fs");
+var path = require("path");
+var sys = require("sys");
 
 var html = fs.readFileSync("demos/aeroviewr/index.html", "utf8");
+
+function copytree(src, dst) {
+    if(!path.existsSync(src)) {
+        throw new Error(src + ' does not exists. Nothing to be copied');
+    }
+
+    if(!fs.statSync(src).isDirectory()) {
+        throw new Error(src + ' must be a directory');
+    }
+
+    var filenames = fs.readdirSync(src);
+    var basedir = src;
+
+    if(!path.existsSync(dst)) {
+        fs.mkdirSync(dst, 0755);
+    }
+
+    for(name in filenames) {
+        var file = basedir + '/' + filenames[name];
+        var newdst = dst + '/' + filenames[name];
+
+        if(fs.statSync(file).isDirectory()) {
+            copytree(file, newdst);
+        } else {
+            var reader = fs.createReadStream(file);
+            var writer = fs.createWriteStream(newdst);
+            sys.pump(reader, writer);
+        }
+    }
+}
 
 function fileGroup(group, path, matcher) {
     var script = "";
@@ -23,7 +55,7 @@ function fileGroup(group, path, matcher) {
         }
     }
 
-    fs.writeFileSync("demos/aeroviewr/js/" + group + ".all.js", script);
+    fs.writeFileSync("live/js/" + group + ".all.js", script);
 
     var ast = parser.parse(script);
 
@@ -31,14 +63,24 @@ function fileGroup(group, path, matcher) {
     ast = uglify.ast_squeeze(ast);
 
     script = uglify.gen_code(ast);
-    fs.writeFileSync("demos/aeroviewr/js/" + group + ".all.min.js", script);
+    fs.writeFileSync("live/js/" + group + ".all.min.js", script);
 }
 
+copytree("demos/aeroviewr", "live");
+
 fileGroup("kendo","", /src="..\/..\/([^"]*)/);
-fileGroup("aeroviewr", "demos/aeroviewr/", /src="([^"]*)/);
+fileGroup("aeroviewr", "live/", /src="([^"]*)/);
 
 var sections = html.split("<!-- kendo -->");
 html = sections[0] + '<script src="js/kendo.all.min.js" ></script>' + sections[2];
 
 sections = html.split("<!-- aeroviewr -->");
-fs.writeFileSync("index.html", sections[0] + '<script src="js/aeroviewr.all.min.js" ></script>' + sections[2]);
+
+html = sections[0] + '<script src="js/aeroviewr.all.min.js" ></script>' + sections[2];
+
+sections = html.split("<!-- jquery -->");
+
+html = sections[0] + '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" ></script>' + sections[2];
+
+fs.unlinkSync("live/index.html");
+fs.writeFileSync("live/index.html", html);
