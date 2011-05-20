bcdf7f49c6739675e0e1a87adfa81a56a22179a0
Alexander Gyoshev <alexander.gyoshev@telerik.com>
2011-05-20 08:21:33 +0000

    fixed build script to not look for live folder
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C477865

--- build/aeroviewr.js
+++ build/aeroviewr.js
@@ -40,9 +40,7 @@ function replaceBlock(block, replace) {
     return sections[0] + replace + sections[2];
 }
 
-wrench.rmdirSyncRecursive("live");
 wrench.copyDirSyncRecursive("demos/aeroviewr", "live");
-wrench.rmdirSyncRecursive("live/research");
 fs.unlinkSync("live/js/aeroviewr.js");
 fs.unlinkSync("live/js/visitor.js");
 fs.unlinkSync("live/js/upload.js");
@@ -54,7 +52,7 @@ fs.unlinkSync("live/js/slideshow.js");
 processFileGroup("kendo","", /src="..\/..\/([^"]*)/);
 processFileGroup("aeroviewr", "demos/aeroviewr/", /src="([^"]*)/);
 
-html = replaceBlock("jquery",  '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>');
+html = replaceBlock("jquery",  '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>');
 html = replaceBlock("kendo",  '<script src="js/kendo.all.min.js"></script>');
 html = replaceBlock("aeroviewr",  '<script src="js/aeroviewr.all.min.js"></script>');
 
