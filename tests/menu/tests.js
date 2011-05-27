44cc0fe7b428623ac21a49d1839d8e76ae3ab0a0
Kamen Bundev <Kamen.Bundev@telerik.com>
2011-05-27 16:23:07 +0000

    Fixed some menu bugs, ported the tests, they seem to pass :D
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C479387

---
+++ tests/menu/tests.js
@@ -0,0 +1,14 @@
+tests.push(
+    {
+        page: "menu/clientAPI.html",
+        title: "menu api"
+    },
+    {
+        page: "menu/clientEvents.html",
+        title: "menu events"
+    },
+    {
+        page: "menu/openOnClick.html",
+        title: "menu open on click"
+    }
+);
