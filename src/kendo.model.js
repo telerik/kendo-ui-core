4a4172e8753f5766b9bc110bb970d94021cdc1e6
Atanas Korchev <akorchev@gmail.com>
2011-05-28 18:00:12 +0300

    added guid to be method of Model

--- src/kendo.model.js
+++ src/kendo.model.js
@@ -36,21 +36,6 @@
         return true;
     }
 
-    function guid() {
-        var id = "", i, random;
-
-        for (i = 0; i < 32; i++) {
-            random = Math.random() * 16 | 0;
-
-            if (i == 8 || i == 12 || i == 16 || i == 20) {
-                id += "-";
-            }
-            id += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
-        }
-
-        return id;
-    }
-
     var Model = Observable.extend({
         init: function(data) {
             var that = this;
@@ -69,8 +54,23 @@
             that.idField =  that.idField || "id";
             if(that.id() === undefined) {
                 that.state = CREATED;
-                that.accessor(that.idField).set(that.data, guid());
+                that.accessor(that.idField).set(that.data, that.guid());
             }
+        },
+
+        guid: function() {
+            var id = "", i, random;
+
+            for (i = 0; i < 32; i++) {
+                random = Math.random() * 16 | 0;
+
+                if (i == 8 || i == 12 || i == 16 || i == 20) {
+                    id += "-";
+                }
+                id += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+            }
+
+            return id;
         },
 
         accessor: function(field) {
