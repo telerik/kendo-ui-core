4f82899112a72d4299002027272e35fedcf5b9d6
Alexander Gyoshev <alexander.gyoshev@telerik.com>
2011-05-30 15:14:24 +0000

    ported expand/collapse functionality of treeview, commented out client-side rendering to give it some thought
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C480021

--- tests/treeview/tests.js
+++ tests/treeview/tests.js
@@ -2,5 +2,9 @@ tests.push(
     {
         page: "treeview/rendering.html",
         title: "treeview rendering"
+    },
+    {
+        page: "treeview/expandcollapse.html",
+        title: "treeview expand/collapse"
     }
 );
