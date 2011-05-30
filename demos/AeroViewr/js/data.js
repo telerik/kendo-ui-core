94301cb23b39d2d0c2f74867b2864f26b5d96abb
Georgi Krustev <Georgi.Krustev@telerik.com>
2011-05-30 12:15:34 +0000

    fix jsonp error in IE
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C479882

--- demos/AeroViewr/js/data.js
+++ demos/AeroViewr/js/data.js
@@ -2,16 +2,16 @@
     var data = {
         dataSource: function(options) {
             options = $.extend(true, {}, options, {
-                transport: createTransport(options.dialect, options.cache),
+                transport: createTransport(options.dialect, options.cache, options.jsoncallback),
                 reader: options.reader
             });
             return new kendo.data.DataSource(options);
         }
     };
 
-    var createTransport = function(dialect, cache) {
+    var createTransport = function(dialect, cache, jsoncallback) {
         var read = $.extend({},
-            !$.support.cors ? { jsonpCallback: "jsonFlickrApi", jsonp: false, dataType: "jsonp" } : { dataType: "json" },
+            !$.support.cors ? { jsonpCallback: jsoncallback || "jsonFlickrApi", jsonp: false, dataType: "jsonp" } : { dataType: "json" },
             { url: flickr.service, cache: true }
         );
         return {
