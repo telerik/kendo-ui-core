be9eded2dca458de4fc5b7a8c41a8510e79a245d
Georgi Krustev <Georgi.Krustev@telerik.com>
2011-06-08 15:47:14 +0000

    initial implementation of combobox
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C482767

---
+++ tests/combobox/tests.js
@@ -0,0 +1,10 @@
+tests.push(
+    {
+        page: "combobox/initialization.html",
+        title: "combobox initialization"
+    },
+    {
+        page: "combobox/navigation.html",
+        title: "combobox navigation"
+    }
+);
