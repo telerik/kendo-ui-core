99e3528adc5935c112f529f85f152136e8df8de6
Georgi Krustev <Georgi.Krustev@telerik.com>
2011-07-13 15:27:04 +0000

    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C496494

--- build/TeamBuild.bat
+++ build/TeamBuild.bat
@@ -1,16 +1,10 @@
 @echo off
 
-echo "windows format path:"
-echo %1
-
 C:\NodeJS\bin\cygpath.exe -u %1 > tmpFile
-
 set /p converted= < tmpFile 
 del tmpFile
 
 set buildPath=%converted%/build/build.js
 set buildPath=%buildPath: =\ %
 
-echo %buildPath%
-
-"C:\NodeJS\bin\node.exe" buildPath
+"C:\NodeJS\bin\node.exe" %buildPath%
