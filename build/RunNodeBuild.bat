ce930df2e713919b8471b4d483eee5ad5478c367
Georgi Krustev <Georgi.Krustev@telerik.com>
2011-07-13 11:24:04 +0000

    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C496275

--- build/RunNodeBuild.bat
+++ build/RunNodeBuild.bat
@@ -4,5 +4,5 @@ echo "start";
 echo "run node";
 echo "error level: ";
 echo ERRORLEVEL;
-C:\NodeJS\bin\node.exe /cygdrive/c/TB/%1/Griffin/Kendo/Sources/build/build.js;
+C:\NodeJS\bin\node.exe "/cygdrive/c/TB/%1/Griffin/Kendo/Sources/build/build.js";
 echo ERRORLEVEL;
