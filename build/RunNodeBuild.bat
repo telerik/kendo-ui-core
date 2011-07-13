f9a2d357eac5be21261d2b82a66b35fdcd81b6e5
Georgi Krustev <Georgi.Krustev@telerik.com>
2011-07-13 11:39:08 +0000

    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C496285

--- build/RunNodeBuild.bat
+++ build/RunNodeBuild.bat
@@ -5,5 +5,5 @@ set PATH=c:\NodeJS\bin;%PATH%;
 
 echo %CD%
 
-"C:\NodeJS\bin\node.exe '/cygdrive/c/TB/%1/Griffin/Kendo/Sources/build/build.js'"
+"C:\NodeJS\bin\node.exe" "/cygdrive/c/TB/%1/Griffin/Kendo/Sources/build/build.js"
 endlocal
