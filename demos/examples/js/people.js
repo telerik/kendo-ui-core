bd4fc8b3a0332c73fa2b4c3051ae94632d76760c
Atanas Korchev <akorchev@gmail.com>
2011-07-21 13:28:36 +0300

    added template expressions example

--- demos/examples/js/people.js
+++ demos/examples/js/people.js
@@ -2,7 +2,8 @@ function createRandomData(count) {
     var firstNames = ["Nancy", "Andrew", "Janet", "Margaret", "Steven", "Michael", "Robert", "Laura", "Anne", "Nige"],
         lastNames = ["Davolio", "Fuller", "Leverling", "Peacock", "Buchanan", "Suyama", "King", "Callahan", "Dodsworth", "White"],
         cities = ["Seattle", "Tacoma", "Kirkland", "Redmond", "London", "Philadelphia", "New York", "Seattle", "London", "Boston"],
-        titles = ["Accountant", "Vice President, Sales", "Sales Representative", "Technical Support", "Sales Manager", "Web Designer", "Sales Representative", "Inside Sales Coordinator", "Sales Manager", "Vice President, Sales"],
+        titles = ["Accountant", "Vice President, Sales", "Sales Representative", "Technical Support", "Sales Manager", "Web Designer",
+        "Software Developer", "Inside Sales Coordinator", "Chief Techical Officer", "Chief Execute Officer"],
         birthDates = [new Date("1948-12-08"), new Date("1952-02-19"), new Date("1963-08-30"), new Date("1937-09-19"), new Date("1955-03-04"), new Date("1963-07-02"), new Date("1960-05-29"), new Date("1958-01-09"), new Date("1966-01-27"), new Date("1966-03-27")];
 
     var data = [],
