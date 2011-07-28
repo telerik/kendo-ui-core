47233b84115cab6d6b7a41756f57eb75f7e4aee4
Nikolay Rusev <Nikolay.Rusev@telerik.com>
2011-07-28 15:31:26 +0000

    fix Date generation for Safari
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C501189

--- demos/examples/js/people.js
+++ demos/examples/js/people.js
@@ -4,7 +4,7 @@ function createRandomData(count) {
         cities = ["Seattle", "Tacoma", "Kirkland", "Redmond", "London", "Philadelphia", "New York", "Seattle", "London", "Boston"],
         titles = ["Accountant", "Vice President, Sales", "Sales Representative", "Technical Support", "Sales Manager", "Web Designer",
         "Software Developer", "Inside Sales Coordinator", "Chief Techical Officer", "Chief Execute Officer"],
-        birthDates = [new Date("1948-12-08"), new Date("1952-02-19"), new Date("1963-08-30"), new Date("1937-09-19"), new Date("1955-03-04"), new Date("1963-07-02"), new Date("1960-05-29"), new Date("1958-01-09"), new Date("1966-01-27"), new Date("1966-03-27")];
+        birthDates = [new Date("1948/12/08"), new Date("1952/02/19"), new Date("1963/08/30"), new Date("1937/09/19"), new Date("1955/03/04"), new Date("1963/07/02"), new Date("1960/05/29"), new Date("1958/01/09"), new Date("1966/01/27"), new Date("1966/03/27")];
 
     var data = [],
         now = new Date();
