e827efb39fdefa856c69eee7d547ac91d26a9d60
Georgi Krustev <Georgi.Krustev@telerik.com>
2011-07-29 13:16:47 +0000

    number formatting prototype
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C501485

---
+++ playground/numerformatting/tests/core/tests.js
@@ -0,0 +1,38 @@
+tests.push(
+    {
+        page: "core/class.html",
+        title: "class"
+    },
+    {
+        page: "core/observable.html",
+        title: "observable"
+    },
+    {
+        page: "core/component.html",
+        title: "component"
+    },
+    {
+        page: "core/animation.html",
+        title: "animation"
+    },
+    {
+        page: "core/format.html",
+        title: "format"
+    },
+    {
+        page: "core/tostring.html",
+        title: "tostring"
+    },
+    {
+        page: "core/dateformatting.html",
+        title: "date formatting"
+    },
+    {
+        page: "core/json.html",
+        title: "json"
+    },
+    {
+        page: "core/template.html",
+        title: "template"
+    }
+);
