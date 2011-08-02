8c47d43cce77707b92fd99f7b668095df25b7c48
Georgi Tunev <Georgi.Tunev@telerik.com>
2011-08-02 08:35:37 +0000

    Updated description according to Todd's document
    git-tfs-id: [http://tfs:8082/DefaultCollection]$/Griffin/Kendo;C502126

--- src/kendo.autocomplete.js
+++ src/kendo.autocomplete.js
@@ -1,18 +1,99 @@
-(function($, undefined) {
+(function ($, undefined) {
     /**
     * @name kendo.ui.AutoComplete.Description
     *
-    * @section The autocomplete component provide suggestions depending on the typed text. Also it allows
-    * to enter multiple values devided by predefined separator.
+    * @section The AutoComplete widget provides suggestions depending on the typed text. It also allows entry of multiple values with by predefined separator. 
+    * The suggestions shown by the AutoComplete widget can come from a local Array or from a remote data service.
     *
-    * @exampleTitle Creating an autocomplete from existing input HTML element
-    * @example
-    * <input id="autocomplete" />
-    *
-    * @exampleTitle AutoComplete initialization
-    * @example
-    * var autocomplete = $("#autocomplete").kendoAutoComplete(["Item1", "Item2"]);
-    */
+    * <h3>Getting Started</h3>
+   * @exampleTitle Create a simple HTML input element
+   * @example
+   * <input id="autocomplete" />
+   *
+   * @exampleTitle Initialize Kendo AutoComplete using a jQuery selector
+   * @example
+   * var autocomplete = $("#autocomplete").kendoAutoComplete(["Item1", "Item2"]);
+   *
+   * @section <h3>AutoComplete Suggestions</h3>
+   * There are two primary ways to provide the AutoComplete suggestions:
+   *   <ol>
+   *       <li>From a local, statically defined JavaScript Array</li>
+   *       <li>From a remote data service</li>
+   *   </ol>
+   * Locally defined values are best for small, fixed sets of suggestions. Remote suggestions should be used for larger data sets. When used with the <a href="../datasource/index.html">Kendo DataSource</a>,
+   * filtering large remote data services can be pushed to the server, too, maximizing client-side performance.
+   * <h3>Local Suggestions</h3>
+   * To configure and provide AutoComplete suggestions locally, you can either pass an Array directly to the AutoComplete constructor, 
+   * or you can set the AutoComplete dataSource property to an Array defined elsewhere in your JavaScript code.
+   * @exampleTitle Directly initialize suggestions in constructor
+   * @example
+   * $("#autocomplete").kendoAutoComplete(["Item1", "Item2", "Item3"]);
+   * 
+   * @exampleTitle Using dataSource property to bind to local Array
+   * @example
+   * var data = ["Item1", "Item2", "Item3"];
+   * $("#autocomplete").kendoAutoComplete({
+   *    dataSource: data
+   * });
+   * @section <h3>Remote Suggestions</h3>
+   * The easiest way to bind to remote AutoComplete suggestions is to use the Kendo DataSource  component. The Kendo DataSource is an abstraction for local and 
+   * remote data, and it can be used to serve data from a variety of data services, such as XML, JSON, and JSONP. 
+   * 
+   * @exampleTitle Using Kendo DataSource to bind to remote suggestions with OData
+   * @example
+   * $("#autocomplete").kendoAutoComplete({
+   *    minLength: 3, 
+   *    dataTextField: "Name", //JSON property name to use
+   *    dataSoruce: new kendo.data.DataSource({
+   *	    type: "odata", //Specifies data protocol 
+   *	    pageSize: 10, //Limits result set
+   *	    transport: {
+   *		    read: "ht	tp://odata.netflix.com/Catalog/Titles"
+   * 	    }
+   *    })
+   * });
+   * 
+   * @exampleTitle Using Kendo DataSource to bind to JSONP suggestions
+   * @example
+   * $(document).ready(function(){
+   *    $("#txtAc").kendoAutoComplete({
+   *	    minLength:6,
+   *	    dataTextField:"title",
+   *	    filter: "contains",
+   *	    dataSource: new kendo.data.DataSource({
+   *		    transport:{
+   *			    read:{
+   *				    url: "http://api.geonames.org/wikipediaSearchJSON",
+   *				    data:{
+   *					    q:function(){
+   *						    var ac = $("#txtAc").data("kendoAutoComplete");
+   *						    return ac.value();
+   *					    },
+   *					    maxRows:10, 
+   *					    username:"demo"
+   *				    }
+   *			    }   
+   *		    },
+   *		    schema:{
+   *			    data:"geonames"
+   *		    }
+   *	    }),
+   *	    change:function(){
+   *		    this.dataSource.read();    
+   *	    } 
+   *    });              
+   * });
+   * 
+   * @section <h3>Accessing an Existing AutoComplete</h3>
+   * To access an existing Kendo UI AutoComplete widget instance, use the jQuery data API. Once a reference to the AutoComplete is established, 
+   * you can use the Kendo UI API to control the widget.
+   * 
+   * @exampleTitle Accessing Existing AutoComplete widget instance
+   * @example
+   * var autocomplete = $("#autocomplete").data("kendoAutoComplete");
+   * 
+   * 
+   */
     var kendo = window.kendo,
         ui = kendo.ui,
         DataSource = kendo.data.DataSource,
@@ -28,7 +109,7 @@
     }
 
     function wordAtCaret(caret, text, separator) {
-        return text.split(separator)[ indexOfWordAtCaret(caret, text, separator) ];
+        return text.split(separator)[indexOfWordAtCaret(caret, text, separator)];
     }
 
     function replaceWordAtCaret(caret, text, word, separator) {
@@ -64,10 +145,10 @@
         * @option {String} [filter] <"startswith"> Defines the type of filtration.
         * @option {Number} [height] <200> Define the height of the drop-down list in pixels.
         */
-        init: function(element, options) {
+        init: function (element, options) {
             var that = this;
 
-            options = $.isArray(options) ? { dataSource: options } : options;
+            options = $.isArray(options) ? { dataSource: options} : options;
 
             List.fn.init.call(that, element, options);
 
@@ -80,24 +161,24 @@
             that.dataSource = DataSource.create(that.options.dataSource || {}).bind(CHANGE, proxy(that.refresh, that));
 
             that.bind([
-                /**
-                * Fires when the drop-down list is opened
-                * @name kendo.ui.AutoComplete#open
-                * @event
-                * @param {Event} e
-                */
-                /**
-                * Fires when the drop-down list is closed
-                * @name kendo.ui.AutoComplete#close
-                * @event
-                * @param {Event} e
-                */
-                /**
-                * Fires when the value has been changed.
-                * @name kendo.ui.AutoComplete#change
-                * @event
-                * @param {Event} e
-                */
+            /**
+            * Fires when the drop-down list is opened
+            * @name kendo.ui.AutoComplete#open
+            * @event
+            * @param {Event} e
+            */
+            /**
+            * Fires when the drop-down list is closed
+            * @name kendo.ui.AutoComplete#close
+            * @event
+            * @param {Event} e
+            */
+            /**
+            * Fires when the value has been changed.
+            * @name kendo.ui.AutoComplete#change
+            * @event
+            * @param {Event} e
+            */
                 CHANGE
             ], that.options);
 
@@ -107,11 +188,11 @@
                 .bind({
                     keydown: proxy(that._keydown, that),
                     paste: proxy(that._search, that),
-                    focus: function() {
+                    focus: function () {
                         that.previous = that.value();
                     },
-                    blur: function() {
-                        that._bluring = setTimeout(function() {
+                    blur: function () {
+                        that._bluring = setTimeout(function () {
                             that._blur();
                         }, 100);
                     }
@@ -134,13 +215,13 @@
         * @example
         * autocomplete.close();
         */
-        close: function() {
+        close: function () {
             var that = this;
             that._current = null;
             that.popup.close();
         },
 
-        refresh: function() {
+        refresh: function () {
             var that = this,
             ul = that.ul[0],
             data = that.dataSource.view(),
@@ -166,7 +247,7 @@
         * // selects by jQuery object
         * autocomplete.select(autocomplete.ul.children().eq(0));
         */
-        select: function(li) {
+        select: function (li) {
             var that = this,
             separator = that.options.separator,
             data = that.dataSource.view(),
@@ -201,7 +282,7 @@
         * // Searches for item which has "Inception" in the name.
         * autocomplete.search("Inception");
         */
-        search: function(word) {
+        search: function (word) {
             var that = this,
             word = word || that.value(),
             options = that.options,
@@ -223,11 +304,11 @@
             if (!length) {
                 that.popup.close();
             } else if (length >= that.options.minLength) {
-                that.dataSource.filter( {field: options.dataTextField, operator: options.filter, value: word } );
+                that.dataSource.filter({ field: options.dataTextField, operator: options.filter, value: word });
             }
         },
 
-        suggest: function(word) {
+        suggest: function (word) {
             var that = this,
                 element = that.element[0],
                 separator = that.options.separator,
@@ -257,7 +338,7 @@
                 word = replaceWordAtCaret(caret, value, word, separator);
             }
 
-            if(word !== value) {
+            if (word !== value) {
                 that.value(word);
 
                 selectionEnd = word.length;
@@ -280,7 +361,7 @@
         * // get the text of the autocomplete.
         * var value = autocomplete.value();
         */
-        value: function(value) {
+        value: function (value) {
             var that = this,
                 element = that.element[0];
 
@@ -291,7 +372,7 @@
             }
         },
 
-        _accept: function(li) {
+        _accept: function (li) {
             var that = this;
 
             that._focus(li);
@@ -299,7 +380,7 @@
             moveCaretAtEnd(that.element[0]);
         },
 
-        _move: function(li) {
+        _move: function (li) {
             var that = this;
 
             li = li[0] ? li : null;
@@ -311,11 +392,11 @@
             }
         },
 
-        _keydown: function(e) {
+        _keydown: function (e) {
             var that = this,
                 key = e.keyCode,
                 keys = kendo.keys
-                visible = that.popup.visible();
+            visible = that.popup.visible();
 
             if (key === keys.DOWN) {
                 if (visible) {
@@ -336,11 +417,11 @@
             }
         },
 
-        _search: function() {
+        _search: function () {
             var that = this;
             clearTimeout(that._typing);
 
-            that._typing = setTimeout(function() {
+            that._typing = setTimeout(function () {
                 if (that.previous !== that.value()) {
                     that.search();
                 }
