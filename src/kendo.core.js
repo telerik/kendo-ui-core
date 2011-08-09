c7164efa6fbe06c7ce690026309922a464f5067b
Nikolay Rusev <nikolay.rusev@telerik.com>
2011-08-09 15:32:17 +0300

    fixed grid grouping when two grid widgets on same page

--- src/kendo.core.js
+++ src/kendo.core.js
@@ -1433,6 +1433,21 @@
                 get: kendo.getter(expression),
                 set: kendo.setter(expression)
             };
+        },
+        /** @ignore */
+        guid: function() {
+            var id = "", i, random;
+
+            for (i = 0; i < 32; i++) {
+                random = Math.random() * 16 | 0;
+
+                if (i == 8 || i == 12 || i == 16 || i == 20) {
+                    id += "-";
+                }
+                id += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+            }
+
+            return id;
         }
     });
 
--- src/kendo.model.js
+++ src/kendo.model.js
@@ -62,23 +62,8 @@
 
             if(that.id() === undefined) {
                 that.state = CREATED;
-                that.data["__id"] = that.guid();
+                that.data["__id"] = kendo.guid();
             }
-        },
-
-        guid: function() {
-            var id = "", i, random;
-
-            for (i = 0; i < 32; i++) {
-                random = Math.random() * 16 | 0;
-
-                if (i == 8 || i == 12 || i == 16 || i == 20) {
-                    id += "-";
-                }
-                id += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
-            }
-
-            return id;
         },
 
         accessor: function(field) {
