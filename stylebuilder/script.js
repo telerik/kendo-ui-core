66cee0033b6babf1184d488e12e44a745a1696e0
Alex Gyoshev <alex@gyoshev.net>
2011-08-17 18:35:50 +0300

    started using iframe to load the stylebuilder interface to prevent obtrusive styles from breaking it

--- stylebuilder/script.js
+++ stylebuilder/script.js
@@ -1,20 +1,34 @@
 (function($, kendo){
-    var template = kendo.template(
-            "<div id='kendo-stylebuilder'>" +
-                "<ul>" +
-                    "<li>Headers</li>" +
-                    "<li>Links</li>" +
-                "</ul>" +
-            "</div>"
-        );
+    if (!kendo) {
+        return;
+    }
 
-    $("<link rel='stylesheet' href='http://gyoshev.telerik.com/kendo/stylebuilder/styles.css' />").appendTo("head");
+    var applicationRoot = "http://localhost/kendo/stylebuilder/", // this should be changed in production
+        StyleBuilder = kendo.Observable.extend({
+            init: function() {
+                $("<link rel='stylesheet' href='" + applicationRoot + "styles.css' />").appendTo("head");
 
-    $(template({}))
-        .css({
-            position: "fixed",
-            top: 20,
-            right: 20
-        })
-        .appendTo(document.body);
+                // use inline styles to be sure that the wrapper won't inherit styles from the page
+                this.element = $("<div id='kendo-stylebuilder' />")
+                    .css({
+                        backgroundColor: "#333",
+                        border: "1px solid #888",
+                        boxShadow: "0 0 4px #ccc",
+                        color: "#f1f1f1",
+                        borderRadius: "5px",
+                        padding: "10px",
+                        position: "fixed",
+                        top: "20px",
+                        right: "20px"
+                    })
+                    .append("<iframe frameborder='0' src='" + applicationRoot + "interface.html' />")
+                    .appendTo(document.body);
+            }
+        });
+
+    new StyleBuilder();
+
+    $.extend(kendo, {
+        StyleBuilder: StyleBuilder
+    });
 })(jQuery, kendo);
