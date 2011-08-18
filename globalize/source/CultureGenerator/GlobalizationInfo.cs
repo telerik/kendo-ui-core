5d62c8e5037c46337d96e2c421b0091dfa898854
George Krystev <ggkrustev@yahoo.com>
2011-08-18 14:01:29 +0300

    add CultureGenerator.exe and the source

---
+++ globalize/source/CultureGenerator/GlobalizationInfo.cs
@@ -0,0 +1,104 @@
+// (c) Copyright 2002-2010 Telerik 
+// This source is subject to the GNU General Public License, version 2
+// See http://www.gnu.org/licenses/gpl-2.0.html. 
+// All other rights reserved.
+
+namespace CultureGenerator
+{
+    using System.Linq;
+    using System.Globalization;
+    using System.Collections.Generic;
+    using System.Web.Script.Serialization;
+    
+    public static class GlobalizationInfo
+    {
+        static string[] numberNegativePatterns = new string[] { "(n)", "-n", "- n", "n-", "n -" };
+
+        static string[] currencyPositivePatterns = new string[] { "$n", "n$", "$ n", "n $" };
+        static string[] currencyNegativePatterns = new string[] { "($n)", "-$n", "$-n", "$n-", "(n$)", "-n$", "n-$", "n$-", "-n $", "-$ n", "n $-", "$ n-", "$ -n", "n- $", "($ n)", "(n $)" };
+
+        static string[] percentPositivePatterns = new string[] { "n %", "n%", "%n", "% n" };
+        static string[] percentNegativePatterns = new string[] { "-n %", "-n%", "-%n", "%-n", "%n-", "n-%", "n%-", "-% n", "n %-", "% n-", "% -n", "n- %" };
+
+        private static IDictionary<string, object> BuildFlatDictionary(CultureInfo cultureInfo)
+        {
+            NumberFormatInfo numberFormats = cultureInfo.NumberFormat;
+            DateTimeFormatInfo dateTimeFormats = cultureInfo.DateTimeFormat;
+
+            IDictionary<string, object> globalization = new Dictionary<string, object>();
+
+            globalization["Name"] = cultureInfo.Name;
+            
+            //number info
+            globalization["NumberPattern"] = new string[] { numberNegativePatterns[cultureInfo.NumberFormat.NumberNegativePattern] };
+            globalization["NumberDecimalDigits"] = cultureInfo.NumberFormat.NumberDecimalDigits;
+            globalization["NumberGroupSeparator"] = numberFormats.NumberGroupSeparator;
+            globalization["NumberDecimalSeparator"] = numberFormats.NumberDecimalSeparator;
+            globalization["NumberGroupSizes"] = numberFormats.NumberGroupSizes;
+
+            //percent info
+            globalization["PercentPattern"] = new string[] { percentNegativePatterns[cultureInfo.NumberFormat.PercentNegativePattern], percentPositivePatterns[cultureInfo.NumberFormat.PercentPositivePattern] };
+            globalization["PercentDecimalDigits"] = cultureInfo.NumberFormat.PercentDecimalDigits;
+            globalization["PercentGroupSeparator"] = numberFormats.PercentGroupSeparator;
+            globalization["PercentDecimalSeparator"] = numberFormats.PercentDecimalSeparator;
+            globalization["PercentGroupSizes"] = numberFormats.PercentGroupSizes;
+            globalization["PercentSymbol"] = numberFormats.PercentSymbol;
+
+            //currency info
+            globalization["CurrencyPattern"] = new string[] { currencyNegativePatterns[cultureInfo.NumberFormat.CurrencyNegativePattern], currencyPositivePatterns[cultureInfo.NumberFormat.CurrencyPositivePattern] };
+            globalization["CurrencyDecimalDigits"] = cultureInfo.NumberFormat.CurrencyDecimalDigits;
+            globalization["CurrencyGroupSeparator"] = numberFormats.CurrencyGroupSeparator;
+            globalization["CurrencyDecimalSeparator"] = numberFormats.CurrencyDecimalSeparator;
+            globalization["CurrencyGroupSizes"] = numberFormats.CurrencyGroupSizes;
+            globalization["CurrencySymbol"] = numberFormats.CurrencySymbol;
+
+            //standard calendar info
+            globalization["DayNames"] = dateTimeFormats.DayNames;
+            globalization["AbbreviatedDayNames"] = dateTimeFormats.AbbreviatedDayNames;
+            globalization["ShortestDayNames"] = dateTimeFormats.ShortestDayNames;
+
+            globalization["MonthNames"] = dateTimeFormats.MonthNames;
+            globalization["AbbreviatedMonthNames"] = dateTimeFormats.AbbreviatedMonthNames;
+
+            globalization["d"] = dateTimeFormats.ShortDatePattern;
+            globalization["D"] = dateTimeFormats.LongDatePattern;
+            globalization["F"] = dateTimeFormats.FullDateTimePattern;
+            globalization["g"] = dateTimeFormats.ShortDatePattern + " " + dateTimeFormats.ShortTimePattern;
+            globalization["G"] = dateTimeFormats.ShortDatePattern + " " + dateTimeFormats.LongTimePattern;
+            globalization["m"] = dateTimeFormats.MonthDayPattern;
+            globalization["M"] = dateTimeFormats.MonthDayPattern;
+            globalization["s"] = dateTimeFormats.SortableDateTimePattern;
+            globalization["t"] = dateTimeFormats.ShortTimePattern;
+            globalization["T"] = dateTimeFormats.LongTimePattern;
+            globalization["u"] = dateTimeFormats.UniversalSortableDateTimePattern;
+            globalization["y"] = dateTimeFormats.YearMonthPattern;
+            globalization["Y"] = dateTimeFormats.YearMonthPattern;
+
+            var am = dateTimeFormats.AMDesignator;
+            var pm = dateTimeFormats.PMDesignator;
+
+            globalization["AM"] = string.IsNullOrEmpty(am) ? new string[] { am } : new string[] { am, am.ToLower(cultureInfo), am.ToUpper(cultureInfo) };
+            globalization["PM"] = string.IsNullOrEmpty(pm) ? new string[] { am } : new string[] { pm, pm.ToLower(cultureInfo), pm.ToUpper(cultureInfo) };
+            globalization["DateSeparator"] = dateTimeFormats.DateSeparator;
+            globalization["TimeSeparator"] = dateTimeFormats.TimeSeparator;
+
+            return globalization;
+        }
+
+        public static string Format(this CultureInfo cultureInfo, string culturePattern) 
+        {
+            var serializer = new JavaScriptSerializer();
+            var cultureDictionary = BuildFlatDictionary(cultureInfo);
+            
+            foreach (KeyValuePair<string, object> pair in cultureDictionary)
+            {
+                var key = pair.Key;
+                var value = pair.Value;
+
+                culturePattern = culturePattern.Replace("{{" + key + "}}", value is System.Array ? serializer.Serialize(value) : System.Convert.ToString(value, cultureInfo));
+            }
+
+            return culturePattern;
+        }
+    }
+}
---
+++ globalize/source/CultureGenerator/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CultureGenerator
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Globalization;
+
+    class Program
+    {
+        static string filePrefix = "kendo.culture";
+        static string outputPath = "C:\\work\\kendo\\cultures";
+        static string culturePatternPath = "kendo.culture.format.txt";
+        static string culturePattern = "";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/*** Culture Generator ***/");
+
+            foreach (string param in args)
+            {
+                if (param.StartsWith("--path="))
+                {
+                    outputPath = param.Substring("--path=".Length);
+                }
+                else if (param.StartsWith("--pattern-path="))
+                {
+                    culturePatternPath = param.Substring("--pattern-path=".Length);
+                }
+                else if (param.StartsWith("--file-prefix="))
+                {
+                    filePrefix = param.Substring("--file-prefix=".Length);
+                }
+                else if (param == "--help")
+                {
+                    Console.Write(@"
+Usage:CultureGenerator [<options>]
+
+options:
+    --path=<PATH> The output path where the file will be created. Default: C:\Work\Kendo\cultures
+
+    --pattern-path=<PATH> Full Path to the culture pattern used to generate culture.js files. Default: current folder\pattern name => .\kendo.culture.format.txt
+
+    --file-prefix=<PREFIX> The prefix of the culture file name. Default: kendo.globalize
+");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Retrieve culture pattern...");
+            try
+            {
+                Console.WriteLine(culturePatternPath);
+                culturePattern = File.ReadAllText(culturePatternPath);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("error: FileNotFoundException!");
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            Console.WriteLine("Start generating culture files...");
+
+            //Generate {filePrefix}.culture.{cultureName}.js for all cultures.
+            CultureInfo.GetCultures(CultureTypes.AllCultures)
+                       .ToList()
+                       .ForEach(WriteCulture);
+
+            Console.WriteLine("End...");
+        }
+
+        private static void WriteCulture(CultureInfo cultureInfo)
+        {
+            var filePath = String.Format(@"{0}\{1}.{2}.js", outputPath, filePrefix, cultureInfo.Name);
+            File.WriteAllText(filePath, cultureInfo.Format(culturePattern));
+            Console.WriteLine(filePath);
+        }
+    }
+}
