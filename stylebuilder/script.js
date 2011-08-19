4f389d4cf6e8cb58f05b84f2674ad6cd61cbb5a7
Dimo Dimov <dimo.dimov@telerik.com>
2011-08-19 17:00:48 +0300

    stylebuilder form

--- stylebuilder/script.js
+++ stylebuilder/script.js
@@ -3,6 +3,27 @@
 
 (function() {
 
+    function addStylesheet(cssText) {
+        var ss = document.createElement('style');
+
+        document.getElementsByTagName("head")[0].appendChild(ss);
+
+        if (ss.styleSheet) {
+            ss.styleSheet.cssText = cssText;
+        } else {
+            ss.appendChild(document.createTextNode(cssText));
+        }
+    }
+
+    var styleBuilderCss = "\
+        #kendo-stylebuilder{text-align:left;font:12px sans-serif;}\
+        #kendo-stylebuilder .styling-options{padding:.4em;color:}\
+        #kendo-stylebuilder label{display:inline-block;width:40%;}\
+        #kendo-stylebuilder .t-style-apply{margin:1em 0 0;}\
+        ";
+
+    addStylesheet(styleBuilderCss);
+
     var startStyleBuilder = function($, kendo){
         var applicationRoot = "http://localhost/kendo/stylebuilder/", // this should be changed in production
             StyleBuilder = kendo.Observable.extend({
@@ -13,14 +34,44 @@
                     // TODO: convert div to window that can be dragged/resized
                     // TODO: use window.contentUrl to load interface
 
-                    $("<div id='kendo-stylebuilder' />").appendTo(document.body);
+                    $('\
+                        <div id="kendo-stylebuilder">\
+                            <ul id="stylable-elements">\
+                                <li>Widget\
+                                    <div><div class="styling-options">\
+                                        <label for="widget-bc">Border color</label> <input id="widget-bc" />\
+                                        <label for="widget-bgc">Background color</label> <input id="widget-bgc" />\
+                                        <label for="widget-c">Text color</label> <input id="heawidgetder-c" />\
+                                    </div></div></li>\
+                                <li>Headers\
+                                    <div><div class="styling-options">\
+                                        <label for="header-bgc">Background color</label> <input id="header-bgc" />\
+                                        <label for="header-c">Text color</label> <input id="header-c" />\
+                                    </div></div></li>\
+                                <li>Links\
+                                    <div><div class="styling-options">\
+                                    <label for="link-c">Text color</label> <input id="link-c" />\
+                                    </div></div></li>\
+                                <li>Content &amp; Template Holders\
+                                    <div><div class="styling-options">\
+                                    <label for="content-bgc">Background color</label> <input id="content-bgc" />\
+                                    </div></div></li>\
+                                <li>Item Group Holders\
+                                    <div><div class="styling-options">\
+                                    <label for="group-bgc">Background color</label> <input id="group-bgc" />\
+                                    </div></div></li>\
+                                    </ul>\
+                            <button type="button" class="t-style-apply t-button">Apply</button>\
+                        </div>').appendTo(document.body);
                     $("#kendo-stylebuilder").kendoWindow({
                         title: "Kendo Stylebuilder",
                         draggable: true,
                         resizable: true,
                         actions: ["Close"],
-                        contentUrl: applicationRoot + "interface.html"
-                    }).closest(".t-window").css({ width: '300px', top: '20px', right: '20px' });
+                        content: "interface.html"
+                    }).closest(".t-window").css({ width: '300px', minHeight: '300px', top: '20px', right: '20px' });
+
+                    $("#stylable-elements").kendoPanelBar();
                 }
             });
 
