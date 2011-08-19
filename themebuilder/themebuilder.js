e88cbf9c2f8f7de77122601ce38424923835cec6
Alex Gyoshev <alex@gyoshev.net>
2011-08-19 19:31:34 +0300

    added todos, moved themebuilder wrapper styles to css, refactored themebuilder.js

--- themebuilder/themebuilder.js
+++ themebuilder/themebuilder.js
@@ -1,58 +1,61 @@
 (function($, kendo) {
-    function addStylesheet(cssText) {
-        var ss = document.createElement('style');
 
-        document.getElementsByTagName("head")[0].appendChild(ss);
-
-        if (ss.styleSheet) {
-            ss.styleSheet.cssText = cssText;
-        } else {
-            ss.appendChild(document.createTextNode(cssText));
-        }
-    }
-
-    var applicationRoot = "http://localhost/kendo/themebuilder/", // this should be changed in production
+    // TODO: split into multiple templates (color picker w/ label, etc.)
+    var template = kendo.template(
+            "<div id='kendo-themebuilder'>" +
+                "<ul id='stylable-elements'>" +
+                    "<li>Widget" +
+                        "<div><div class='styling-options'>" +
+                            "<label for='widget-bc'>Border color</label> <input id='widget-bc' />" +
+                            "<label for='widget-bgc'>Background color</label> <input id='widget-bgc' />" +
+                            "<label for='widget-c'>Text color</label> <input id='widget-c' />" +
+                        "</div></div></li>" +
+                    "<li>Headers" +
+                        "<div><div class='styling-options'>" +
+                            "<label for='header-bgc'>Background color</label> <input id='header-bgc' />" +
+                            "<label for='header-c'>Text color</label> <input id='header-c' />" +
+                        "</div></div></li>" +
+                    "<li>Links" +
+                        "<div><div class='styling-options'>" +
+                        "<label for='link-c'>Text color</label> <input id='link-c' />" +
+                        "</div></div></li>" +
+                    "<li>Content &amp; Template Holders" +
+                        "<div><div class='styling-options'>" +
+                        "<label for='content-bgc'>Background color</label> <input id='content-bgc' />" +
+                        "</div></div></li>" +
+                    "<li>Item Group Holders" +
+                        "<div><div class='styling-options'>" +
+                        "<label for='group-bgc'>Background color</label> <input id='group-bgc' />" +
+                        "</div></div></li>" +
+                "</ul>" +
+                "<button type='button' class='t-style-apply t-button'>Apply</button>" +
+            "</div>"
+        ),
         ThemeBuilder = kendo.Observable.extend({
             init: function() {
-                // use inline styles to be sure that the wrapper won't inherit styles from the page
-                $('\
-                    <div id="kendo-themebuilder">\
-                        <ul id="stylable-elements">\
-                            <li>Widget\
-                                <div><div class="styling-options">\
-                                    <label for="widget-bc">Border color</label> <input id="widget-bc" />\
-                                    <label for="widget-bgc">Background color</label> <input id="widget-bgc" />\
-                                    <label for="widget-c">Text color</label> <input id="widget-c" />\
-                                </div></div></li>\
-                            <li>Headers\
-                                <div><div class="styling-options">\
-                                    <label for="header-bgc">Background color</label> <input id="header-bgc" />\
-                                    <label for="header-c">Text color</label> <input id="header-c" />\
-                                </div></div></li>\
-                            <li>Links\
-                                <div><div class="styling-options">\
-                                <label for="link-c">Text color</label> <input id="link-c" />\
-                                </div></div></li>\
-                            <li>Content &amp; Template Holders\
-                                <div><div class="styling-options">\
-                                <label for="content-bgc">Background color</label> <input id="content-bgc" />\
-                                </div></div></li>\
-                            <li>Item Group Holders\
-                                <div><div class="styling-options">\
-                                <label for="group-bgc">Background color</label> <input id="group-bgc" />\
-                                </div></div></li>\
-                                </ul>\
-                        <button type="button" class="t-style-apply t-button">Apply</button>\
-                    </div>').appendTo(document.body);
-                $("#kendo-themebuilder").kendoWindow({
-                    title: "Kendo ThemeBuilder",
-                    draggable: true,
-                    resizable: true,
-                    actions: ["Close"],
-                    content: "interface.html"
-                }).closest(".t-window").css({ width: '300px', minHeight: '300px', top: '20px', right: '20px' });
+                $(template({})).appendTo(document.body);
+
+                this.element = $("#kendo-themebuilder")
+                    .kendoWindow({
+                        title: "Kendo ThemeBuilder",
+                        draggable: true,
+                        resizable: true
+                    })
+                    .closest(".t-window").attr("id", "kendo-themebuilder-wrapper");
 
                 $("#stylable-elements").kendoPanelBar();
+            },
+            // TODO: test this
+            updateStyleSheet: function(cssText) {
+                var ss = document.createElement("style");
+
+                document.getElementsByTagName("head")[0].appendChild(ss);
+
+                if (ss.styleSheet) {
+                    ss.styleSheet.cssText = cssText;
+                } else {
+                    ss.appendChild(document.createTextNode(cssText));
+                }
             }
         });
 
