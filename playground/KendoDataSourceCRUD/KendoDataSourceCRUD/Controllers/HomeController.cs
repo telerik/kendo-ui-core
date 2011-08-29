73666c7dc16db6d88404089197770b8240eefe66
Atanas Korchev <akorchev@gmail.com>
2011-08-29 16:02:44 +0300

    Add CRUD sample project to the playground

---
+++ playground/KendoDataSourceCRUD/KendoDataSourceCRUD/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using KendoDataSourceCRUD.Models;
+
+namespace KendoDataSourceCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BatchFalseAutoSyncTrue()
+        {
+            return View();
+        }
+
+        public ActionResult BatchMultiple()
+        {
+            return View();
+        }
+
+        public IEnumerable<Product> Products()
+        {
+            var products = (IEnumerable<Product>)Session["products"];
+
+            if (products == null)
+            {
+                Session["products"] = products = new[] {
+                    new Product
+                    {
+                        ID = 0,
+                        Name = "Chai"
+                    },
+                    new Product
+                    {
+                        ID = 1,
+                        Name = "Coffee"
+                    }
+                };
+            }
+
+            return products;
+        }
+				 
+        public ActionResult Read()
+        {
+            return Json(Products(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult UpdateProducts(IEnumerable<Product> products)
+        {
+            return Json(products);
+        }
+
+        public ActionResult Update(Product model)
+        {
+            var products = Products();
+
+            var product = products.FirstOrDefault(p => p.ID == model.ID);
+
+            if (product != null)
+            {
+                product.Name = model.Name;
+            }
+
+            return Json(products);
+        }
+    }
+}
