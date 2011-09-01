c4f352a17eb69301ad30173f95ade2ed623faead
Alex Gyoshev <alex@gyoshev.net>
2011-09-01 13:35:13 +0300

    Build script for LESS-based themes; LESS theme template

---
+++ build/skins.js
@@ -0,0 +1,36 @@
+var less = require("./less-js/lib/less"),
+    fs = require("fs");
+
+function generateSkins(skinsFolder) {
+    var template = fs.readFileSync(skinsFolder + "template.less", "utf8"),
+        themes = fs.readdirSync(skinsFolder)
+                    .filter(function(file) {
+                        return file != "template.less" && /\.less$/i.test(file);
+                    })
+                    .map(function(file) {
+                        return skinsFolder + file;
+                    });
+
+    themes.forEach(function(theme) {
+        var constants = fs.readFileSync(theme, "utf8"),
+            themeName = /\/([^\/]+)\.less/i.exec(theme)[1];
+
+        less.render(constants + template, function (e, css) {
+            if (e) {
+                console.log("ERROR: `" + theme + "` theme generation failed: " + e.message);
+            } else {
+                console.log("theme `" + themeName + "` generated successfully.");
+            }
+        });
+    });
+}
+
+function build() {
+    generateSkins("styles/");
+}
+
+if (require.main === module) {
+    build();
+} else {
+    exports.build = build;
+}
