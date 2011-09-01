843691b654e09d14b64bf70cd1779d59c8adc036
Alex Gyoshev <alex@gyoshev.net>
2011-09-01 17:47:31 +0300

    Remove wrench.js dependency

--- build/kendo-build.js
+++ build/kendo-build.js
@@ -1,9 +1,61 @@
-var parser = require("./lib/parse-js"),
+var fs = require("fs"),
+    parser = require("./lib/parse-js"),
     uglify = require("./lib/process");
 
+function rmdirSyncRecursive(path) {
+    var files = fs.readdirSync(path)
+        .map(function(fileName) { return path + "/" + fileName });
+
+    for (var i = 0; i < files.length; i++) {
+        var fileInfo = fs.statSync(files[i]);
+
+        if (fileInfo.isDirectory()) {
+            rmdirSyncRecursive(files[i]);
+        } else {
+            fs.unlinkSync(files[i]);
+        }
+    }
+
+    return fs.rmdirSync(path);
+}
+
+function copyDirSyncRecursive(sourceDir, newDirLocation, skipClean) {
+    try {
+        if (!skipClean && fs.statSync(newDirLocation).isDirectory()) {
+            exports.rmdirSyncRecursive(newDirLocation);
+        }
+    } catch(e) { }
+
+    var checkDir = fs.statSync(sourceDir);
+
+    if (!skipClean) {
+        fs.mkdirSync(newDirLocation, checkDir.mode);
+    }
+
+    var files = fs.readdirSync(sourceDir);
+
+    for (var i = 0; i < files.length; i++) {
+        var sourcePath = sourceDir + "/" + files[i],
+            destinationPath = newDirLocation + "/" + files[i],
+            fileInfo = fs.statSync(sourcePath);
+
+        if (fileInfo.isDirectory()) {
+            fs.mkdirSync(destinationPath, fileInfo.mode);
+            copyDirSyncRecursive(sourcePath, newDirLocation + "/" + files[i]);
+        } else if (fileInfo.isSymbolicLink()) {
+            fs.symlinkSync(fs.readlinkSync(sourcePath), destinationPath);
+        } else {
+            fs.writeFileSync(destinationPath, fs.readFileSync(sourcePath));
+        }
+    }
+}
+
+exports.rmdirSyncRecursive = rmdirSyncRecursive;
+exports.copyDirSyncRecursive = copyDirSyncRecursive;
 exports.minifyJs = function(source) {
     var ast = parser.parse(source);
     ast = uglify.ast_mangle(ast);
     ast = uglify.ast_squeeze(ast);
     return uglify.gen_code(ast);
 };
+
