a611eddd6650729a406eab95b0ebb2386fdbdfe7
Alex Gyoshev <alex@gyoshev.net>
2011-09-14 15:13:34 +0300

    Enable inferring of constant properties from current page

--- themebuilder/script.js
+++ themebuilder/script.js
@@ -39,7 +39,98 @@
     }
 
     window.lessLoaded = function(lessTemplate) {
-        new kendo.ThemeBuilder(lessTemplate);
+            var properties = {
+                    bgColor: { property: "background-color", label: "Background" },
+                    color: { property: "color", label: "Text color" },
+                    brdColor: { property: "border-color", label: "Border color" },
+                },
+                extend = $.extend;
+
+        // TODO: optimize by generating the properties array more efficiently
+        new kendo.ThemeBuilder(lessTemplate, new kendo.LessConstants([{
+                    prefix: "@widget",
+                    title: "Widgets",
+                    properties: [
+                        extend({}, properties.bgColor),
+                        extend({}, properties.color),
+                        extend({}, properties.brdColor)
+                    ]
+                }, {
+                    prefix: "@header",
+                    title: "Headers",
+                    properties: [
+                        extend({}, properties.bgColor),
+                        extend({}, properties.color)
+                    ]
+                }, {
+                    prefix: "@link",
+                    title: "Links",
+                    properties: [
+                        extend({}, properties.color)
+                    ]
+                }, {
+                    prefix: "@button",
+                    title: "Buttons",
+                    properties: [
+                        extend({}, properties.bgColor),
+                        extend({}, properties.color)
+                    ]
+                }, {
+                    prefix: "@tooltip",
+                    title: "Tooltips",
+                    properties: [
+                        extend({}, properties.bgColor),
+                        extend({}, properties.color)
+                    ]
+                }, {
+                    prefix: "@hover",
+                    title: "Hover state",
+                    properties: [
+                        extend({}, properties.bgColor),
+                        extend({}, properties.color),
+                        extend({}, properties.brdColor)
+                    ]
+                }, {
+                    prefix: "@selected",
+                    title: "Selected state",
+                    properties: [
+                        extend({}, properties.bgColor),
+                        extend({}, properties.color),
+                        extend({}, properties.brdColor)
+                    ]
+                }, {
+                    prefix: "@active",
+                    title: "Active state",
+                    properties: [
+                        extend({}, properties.bgColor),
+                        extend({}, properties.color),
+                        extend({}, properties.brdColor)
+                    ]
+                }, {
+                    prefix: "@disabled",
+                    title: "Disabled state",
+                    properties: [
+                        extend({}, properties.color)
+                    ]
+                }, {
+                    prefix: "@error",
+                    title: "Error state",
+                    properties: [
+                        extend({}, properties.bgColor),
+                        extend({}, properties.color),
+                        extend({}, properties.brdColor)
+                    ]
+                }]));
+                /* missing constants:
+                 * @image-folder: "BlueOpal"; // we should think of something that allows us to change the gradients
+                 * @loading-panel-color: #fff;
+                 * @shadow-color: #aaa;
+                 * @content-background-color: @widget-background-color;
+                 * @group-background-color: #f1f4f5;
+                 * @input-background-color: @widget-background-color;
+                 * @splitbar-background-color: @header-background-color;
+                 * @alt-background-color: @group-background-color;
+                 */
     };
 
     queue.push(function(){
