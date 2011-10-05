66358695686ca633be7f200db8174c533c2ccf64
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2011-10-05 13:49:39 +0300

    Refactor validation rules execution logic

--- src/kendo.validate.js
+++ src/kendo.validate.js
@@ -1,6 +1,7 @@
 ;(function($, undefined) {
     var kendo = window.kendo,
         validation = kendo.support.validation,
+        INVALID = "t-invalid",
         Component = kendo.ui.Component;
 
     var Validator = Component.extend( {
@@ -13,7 +14,24 @@
         },
 
         options: {
-            individualErrorTemplate: "<span>${message}</span>"
+            individualErrorTemplate: "<span>${message}</span>",
+            rules: {
+                required: function(input) {
+                    if (input.filter("[required]").length && input.val() === "") {
+                        return false;
+                    }
+                    return true;
+                },
+                min: function(input) {
+                    if (input.filter("[type=number]").filter("[min]").length && input.val() !== "") {
+                        var min = parseInt(input.attr("min"),10) || 0,
+                            val = parseInt(input.val(), 10);
+
+                        return min < val;
+                    }
+                    return true;
+                }
+            }
         },
 
         validate: function() {
@@ -43,19 +61,19 @@
                 individualErrors = that.options.individualErrors,
                 template = that._individualErrorTemplate,
                 valid = validation ? input[0].checkValidity() :  that._checkValidity(input),
+                lbl,
                 message;
 
             if (!valid) {
-
                 message = kendo.format(input.attr("validationMessage") || input.attr("title") || "", input.attr("name"));
                 that._messages.push(message);
 
                 if (individualErrors) {
-                    $(template({ message: message })).addClass("t-invalid").insertAfter(input);
+                    $(template({ message: message })).addClass(INVALID).insertAfter(input);
                 }
             } else if (individualErrors) {
-                var lbl = input.next();
-                if (lbl.is(".t-invalid")) {
+                lbl = input.next();
+                if (lbl.hasClass(INVALID)) {
                     lbl.remove();
                 }
             }
@@ -64,10 +82,15 @@
         },
 
         _checkValidity: function(input) {
-            if (input.filter("[required]").length && input.val() !== "") {
-                return true;
+            var rules = this.options.rules;
+
+            for (rule in rules) {
+                if (!rules[rule](input)) {
+                    return false;
+                }
             }
-            return false;
+
+            return true;
         },
 
         messages: function() {
