0b0408af17592bda72c870b3db06a435c6f80925
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2011-10-06 18:15:27 +0300

    Support for rule specific error messages

--- src/kendo.validate.js
+++ src/kendo.validate.js
@@ -1,25 +1,25 @@
 ;(function($, undefined) {
     var kendo = window.kendo,
+        Component = kendo.ui.Component,
         INVALIDMSG = "k-invalid-msg",
         INVALIDINPUT = "k-invalid",
         emailRegExp = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
         urlRegExp = /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
-        Component = kendo.ui.Component;
+        INPUTSELECTOR = ":input",
+        patternMatcher = function(value, pattern) {
+            if (typeof pattern === "string") {
+                pattern = new RegExp('^(?:' + pattern + ')$');
+            }
+            return pattern.test(value);
+        },
+        matcher = function(input, selector, pattern) {
+            var value = input.val();
 
-    var patternMatcher = function(value, pattern) {
-        if (typeof pattern === "string") {
-            pattern = new RegExp('^(?:' + pattern + ')$');
+            if (input.filter(selector).length && value !== "") {
+                return patternMatcher(value, pattern);
+            }
+            return true;
         }
-        return pattern.test(value);
-    },
-    matcher = function(input, selector, pattern) {
-        var value = input.val();
-
-        if (input.filter(selector).length && value !== "") {
-            return patternMatcher(value, pattern);
-        }
-        return true;
-    }
 
     var Validator = Component.extend( {
         init: function(element, options) {
@@ -35,6 +35,15 @@
 
         options: {
             errorTemplate: "<span>${message}</span>",
+            messages: {
+                required: "{0} is required.",
+                pattern: "{0} is not valid",
+                min: "{0} should be greater than {1}",
+                max: "{0} should be smaller then {1}",
+                step: "{0} is not valid",
+                email: "{0} is not valid email",
+                url: "{0} is not valid URL"
+            },
             rules: {
                 required: function(input) {
                     if (input.filter("[required]").length && input.val() === "") {
@@ -87,8 +96,8 @@
 
         _attachEvents: function() {
             var that = this;
-            if (!that.element.is(":input")) {
-                that.element.delegate(":input", "blur", function() {
+            if (!that.element.is(INPUTSELECTOR)) {
+                that.element.delegate(INPUTSELECTOR, "blur", function() {
                     that._validateInput($(this));
                 });
             } else {
@@ -107,8 +116,8 @@
 
             that._errors = {};
 
-            if (!that.element.is(":input")) {
-                inputs = that.element.find(":input");
+            if (!that.element.is(INPUTSELECTOR)) {
+                inputs = that.element.find(INPUTSELECTOR);
 
                 for (idx = 0, length = inputs.length; idx < length; idx++) {
                     if (!that._validateInput(inputs.eq(idx))) {
@@ -123,7 +132,9 @@
         _validateInput: function(input) {
             var that = this,
                 template = that._errorTemplate,
-                valid = that._checkValidity(input),
+                customMessages = that.options.messages,
+                result = that._checkValidity(input),
+                valid = result.valid,
                 lbl = input.next(),
                 fieldName = input.attr("name"),
                 message;
@@ -133,11 +144,13 @@
             }
 
             if (!valid) {
-                message = kendo.format(input.attr("validationMessage") || input.attr("title") || "", fieldName);
+                message = kendo.format(input.attr("validationMessage") || input.attr("title") || customMessages[result.key] || "", fieldName);
                 that._errors[fieldName] = message;
                 $(template({ message: message })).addClass(INVALIDMSG).insertAfter(input);
             }
+
             input.toggleClass(INVALIDINPUT, !valid);
+
             return valid;
         },
 
@@ -147,11 +160,11 @@
 
             for (rule in rules) {
                 if (!rules[rule](input)) {
-                    return false;
+                    return { valid: false, key: rule };
                 }
             }
 
-            return true;
+            return { valid: true };
         },
 
         errors: function() {
