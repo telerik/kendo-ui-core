8c5deb47e6d5be6c7fbeda3656109c97d12388a7
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2011-10-12 18:07:16 +0300

    Support for custom validation message containers

--- src/kendo.validate.js
+++ src/kendo.validate.js
@@ -6,6 +6,7 @@
         emailRegExp = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
         urlRegExp = /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
         INPUTSELECTOR = ":input",
+        proxy = $.proxy,
         patternMatcher = function(value, pattern) {
             if (typeof pattern === "string") {
                 pattern = new RegExp('^(?:' + pattern + ')$');
@@ -104,21 +105,24 @@
             }
         },
 
+        _submit: function(e) {
+            if (!this.validate()) {
+                e.stopPropagation();
+                e.stopImmediatePropagation();
+                e.preventDefault();
+                return false;
+            }
+            return true;
+        },
+
         _attachEvents: function() {
             var that = this;
+
+            if (that.element.is("form")) {
+                that.element.submit(proxy(that._submit, that));
+            }
+
             if (!that.element.is(INPUTSELECTOR)) {
-
-                if (that.element.is("form")) {
-                    that.element.submit(function(e) {
-                        if (!that.validate()) {
-                            e.stopPropagation();
-                            e.stopImmediatePropagation();
-                            e.preventDefault();
-                            return false;
-                        }
-                    });
-                }
-
                 that.element.delegate(INPUTSELECTOR, "blur", function() {
                     that._validateInput($(this));
                 });
@@ -157,18 +161,20 @@
                 customMessages = that.options.messages,
                 result = that._checkValidity(input),
                 valid = result.valid,
-                lbl = input.next(),
+                className = "." + INVALIDMSG,
                 fieldName = input.attr("name"),
-                message;
-
-            if (lbl.hasClass(INVALIDMSG)) {
-                lbl.remove();
-            }
+                lbl = that.element.find(className + "[data-for=" + fieldName + "]").add(input.next(className)).hide(),
+                messageText;
 
             if (!valid) {
-                message = that._extractMessage(input, result.key);
-                that._errors[fieldName] = message;
-                $(template({ message: message })).addClass(INVALIDMSG).insertAfter(input);
+                messageText = that._extractMessage(input, result.key);
+                that._errors[fieldName] = messageText;
+
+                var messageLabel = $(template({ message: messageText })).addClass(INVALIDMSG).attr("data-for", fieldName || "");
+                if (!lbl.replaceWith(messageLabel).length) {
+                    messageLabel.insertAfter(input)
+                }
+                messageLabel.show();
             }
 
             input.toggleClass(INVALIDINPUT, !valid);
