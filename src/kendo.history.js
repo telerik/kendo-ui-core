ec1cddc7aa35430025b287d9e2b32ba936725b52
Petyo Ivanov <underlog@gmail.com>
2011-10-14 17:04:34 +0300

    Root, hash to pushstate sync features done

--- src/kendo.history.js
+++ src/kendo.history.js
@@ -1,27 +1,111 @@
 (function($, undefined) {
     var location = window.location,
         history = window.history,
-        pushStateSupported = window.history && window.history.pushState,
+        checkUrlInterval = 50,
+        hashStrip = /^#*/,
         document = window.document;
 
 
-    kendo.History = {
+    var History = kendo.Observable.extend({
         start: function(options) {
-            this._pushState = !!options["pushState"] && pushStateSupported;
-            this.fragment = "";
+            options = options || {};
+
+            var that = this,
+                wantsPushState = !!options["pushState"],
+                checkUrlProxy = $.proxy(that.checkUrl, that),
+                atRoot = location.pathname === that.root;
+
+            that._pushState = wantsPushState && that.pushStateSupported();
+            that.fragment = "";
+            that.root = options["root"] || "/";
+
+
+            if (wantsPushState && !that.pushStateSupported() && !atRoot) {
+                location.replace(that.root + '#' + that.stripRoot(location.pathname));
+                return true;
+            } else if (that._pushState && atRoot && location.hash) {
+                that.fragment = location.hash.replace(hashStrip, '');
+                history.replaceState({}, document.title, that.makePushStateUrl(that.fragment));
+            }
+
+            if (that._pushState) {
+                $(window).bind("popstate", checkUrlProxy);
+            } else if ("onhashchange" in window) {
+                $(window).bind("hashchange", checkUrlProxy);
+            } else {
+                setInterval(checkUrlProxy, checkUrlInterval);
+            }
+        },
+
+        checkUrl: function() {
+            var that = this, current = that.currentLocation();
+
+            if (current != that.fragment) {
+               that.navigate(current);
+            }
+        },
+
+        stripRoot: function(url) {
+            var that = this;
+
+            if (url.indexOf(that.root) === 0) {
+                return ('/' + url.substr(that.root.length)).replace(/\/\//g, '/');
+            } else {
+                return url;
+            }
+        },
+
+        currentLocation: function() {
+            var fragment, that = this;
+
+            if (that._pushState) {
+                fragment = location.pathname;
+
+                if (location.search) {
+                    fragment += location.search;
+                }
+
+                return that.stripRoot(fragment);
+            } else {
+                return location.hash.replace(hashStrip, '');
+            }
+        },
+
+        pushStateSupported: function() {
+            return window.history && window.history.pushState;
+        },
+
+        change: function(callback) {
+            this.bind('change', callback);
+        },
+
+        makePushStateUrl: function(address) {
+            var that = this;
+
+            if (address.indexOf(that.root) != 0) {
+                address = (that.root + address).replace(/\/\//g, '/');
+            }
+
+            return location.protocol + '//' + location.host + address;
         },
 
         navigate: function(to) {
-            if (this.fragment === to || this.fragment === decodeURIComponent(to)) {
+            var that = this;
+
+            if (that.fragment === to || that.fragment === decodeURIComponent(to)) {
                 return;
             }
 
-            if (this._pushState) {
-                history.pushState({}, document.title, location.protocol + '//' + location.host + to);
-                this.fragment = to;
+            if (that._pushState) {
+                history.pushState({}, document.title, that.makePushStateUrl(to));
+                that.fragment = to;
             } else {
-                this.fragment = location.hash = to;
+                that.fragment = location.hash = to;
             }
+
+            that.trigger("change", { location: that.fragment });
         }
-    }
+    });
+
+    kendo.history = new History();
 })(jQuery);
