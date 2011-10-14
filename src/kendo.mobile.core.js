39ca74d79a1d9325b1087da681d7da06af84a5ad
Atanas Korchev <akorchev@gmail.com>
2011-10-14 16:17:25 +0300

    MobileWidget is implemented. Reads configuration options from data attributes

---
+++ src/kendo.mobile.core.js
@@ -0,0 +1,26 @@
+(function($, undefined) {
+    var kendo = window.kendo,
+        extend = $.extend,
+        Observable = kendo.Observable;
+
+    var MobileWidget = Observable.extend({
+        init: function(element, options) {
+            var that = this, option, value;
+
+            Observable.fn.init.call(that);
+
+            that.element = element = $(element);
+            that.options = extend(true, {}, that.options);
+
+            for (option in that.options) {
+                value = element.data("kendo-" + option);
+
+                if (value !== undefined) {
+                    that.options[option] = value;
+                }
+            }
+        }
+    });
+
+    kendo.ui.MobileWidget = MobileWidget;
+})(jQuery);
