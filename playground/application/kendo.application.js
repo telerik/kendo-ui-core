59ad25dd6063b05ac3491e98bac5f3e64b894dc6
Petyo Ivanov <underlog@gmail.com>
2011-10-18 17:38:49 +0300

    Application supports local views

--- playground/application/kendo.application.js
+++ playground/application/kendo.application.js
@@ -3,6 +3,11 @@
         init: function(element, options) {
             this.options = options;
             this.element = element;
+
+            if (element) {
+                element.data("kendoView", this);
+                this._bindEvents();
+            }
         },
 
         render: function(success) {
@@ -12,7 +17,6 @@
 
             if (that.element) {
                 success(that.element);
-                this._bindEvents();
             } else {
                 $.ajax({
                     url: this.options.url,
@@ -24,7 +28,9 @@
 
                         placeholder[0].innerHTML = bodyInnerHTML;
                         that.element = placeholder.find("[data-kendo-role=view]").first();
+                        that.element.hide().attr("data-kendo-url", that.options.url).data("kendoView", that);
 
+                        $(document.body).append(that.element);
                         success(that.element);
                         that._bindEvents();
                     }
@@ -33,8 +39,16 @@
         },
 
         replace: function(view) {
-            view.element.replaceWith(this.element);
+            if (view == this) {
+                return;
+            }
+            this.element.show();
+            view.hide();
             return this;
+        },
+
+        hide: function() {
+            this.element.hide();
         },
 
         _bindEvents: function () {
@@ -46,45 +60,44 @@
     });
 
     var Application = kendo.Observable.extend({
+
         init: function(options) {
             var that = this;
-
             kendo.Observable.fn.init.call(this, options);
         },
 
         start: function(history) {
             var that = this;
 
-            that._view = new kendo.View($("[data-kendo-role=view]").first());
-
             kendo.history.start(history);
+
+            $("[data-kendo-role=view]").not(":first").hide();
+            that._view = new kendo.View($("[data-kendo-role=view]").first(), { url: kendo.history._fragment });
 
             kendo.history.change(function(e) {
                 that.loadView(e.location);
             });
-
-            that._view.render(function() {
-                that._viewInit(that._view);
-            });
-        },
-
-        _viewInit: function(view) {
-            this.trigger("viewinit", {
-                element: view.element
-            });
         },
 
         loadView: function(url) {
-            var that = this;
 
-            var view = new kendo.View(null, { url: url });
+            var that = this, view;
+
+            var element = $(url + ", [data-kendo-url='" + url + "']").first();
+            view = element.data("kendoView");
+
+            if (!view) {
+                if (url.charAt(0) === "#") {
+                    view = new kendo.View($(url), { url: url });
+                } else {
+                    view = new kendo.View(null, { url: url });
+                }
+            }
 
             view.render(function(element) {
                 kendo.history.navigate(url, true);
 
                 that._view = view.replace(that._view);
-
-                that._viewInit(that._view);
             });
         }
     });
@@ -92,7 +105,6 @@
     kendo.application = new Application();
 
     $(function() {
-        kendo.application.start({
-        });
+        kendo.application.start({});
     });
 })(jQuery);
