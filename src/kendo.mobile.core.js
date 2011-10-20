a2ab38dd120b2947f114ec5af17b78cd18f0eccf
Bundyo <bundyo@gmail.com>
2011-10-20 17:12:37 +0300

    Added options to the that.options extend.

--- src/kendo.mobile.core.js
+++ src/kendo.mobile.core.js
@@ -10,7 +10,7 @@
             Observable.fn.init.call(that);
 
             that.element = element = $(element);
-            that.options = extend(true, {}, that.options);
+            that.options = extend(true, {}, that.options, options);
 
             for (option in that.options) {
                 value = element.data("kendo-" + option);
