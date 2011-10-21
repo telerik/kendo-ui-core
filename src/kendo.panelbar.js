161061f6224ff2382ec7bfbf4ce80864b26e9bde
Bundyo <bundyo@gmail.com>
2011-10-21 14:36:16 +0300

    Fix for PanelBar removing all .k-icon elements inside it. Add overrides for touch callouts in common.css Change clicked states to active. Start adding Toggle base class.

--- src/kendo.panelbar.js
+++ src/kendo.panelbar.js
@@ -304,7 +304,7 @@
     function updateArrow (items) {
         items = $(items);
 
-        items.find(".k-icon").remove();
+        items.children(".k-link").children(".k-icon").remove();
 
         items
             .filter(":has(.k-group),:has(.k-content)")
