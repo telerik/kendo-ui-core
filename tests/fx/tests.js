d25acfa63a2e979d301b051bac70c9984b959516
Petyo Ivanov <underlog@gmail.com>
2011-10-21 15:47:19 +0300

    Transition tests

--- tests/fx/tests.js
+++ tests/fx/tests.js
@@ -1,4 +1,10 @@
-tests.push({
+tests.push(
+    {
         page: "fx/animation.html",
         title: "FX animation"
-    });
+    },
+    {
+        page: "fx/transition.html",
+        title: "FX transition"
+    }
+);
