368966dbb42588c5b40f148128f61e57099b496f
George Krystev <ggkrustev@yahoo.com>
2011-10-25 18:25:02 +0300

    Implement enable/disable

--- src/kendo.textbox.js
+++ src/kendo.textbox.js
@@ -2,23 +2,39 @@
 
     var kendo = window.kendo,
         ui = kendo.ui,
+        keys = kendo.keys,
         Component = ui.Component,
         parse = kendo.parseFloat,
+        touch = kendo.support.touch,
+        TOUCHEND = "touchend",
+        MOUSEDOWN = touch ? "touchstart" : "mousedown",
+        MOUSEUP = touch ? "touchmove " + TOUCHEND : "mouseup mouseleave",
+        HOVER = "k-state-hover",
+        HOVEREVENTS = "mouseenter mouseleave",
+        DISABLED = "disabled",
         DIV = "<div />"
         HIDE = "k-hide-text",
         INPUT = "k-input",
         CHANGE = "change",
-        NULL = null;
+        NULL = null,
+        decimals = {
+            190 : ".",
+            188 : ","
+        };
 
     var TextBox = Component.extend(/** @lends kendo.ui.TextBox.prototype */{
         init: function(element, options) {
             var that = this,
-                value;
+                min, max, value;
 
             Component.fn.init.call(that, element, options);
 
-            element = that.element.addClass(INPUT);
             options = that.options;
+            element = that.element.addClass(INPUT)
+                          .bind({
+                              keydown: $.proxy(that._keydown, that)
+                          })
+                          .bind("paste", function(e) { console.log(e, e.target.value) });
 
             that._wrapper();
             that._arrows();
@@ -32,7 +48,7 @@
                 focusin: function() {
                     clearTimeout(that._bluring);
                     that.element.focus();
-                    that._focus();
+                    that._toggleText(false);
                 },
                 focusout: function() {
                     that._bluring = setTimeout(function() {
@@ -43,12 +59,21 @@
 
             that.bind(CHANGE, options);
 
-            value = options.value;
-            if (value === null) {
-                value = element.val();
+            min = parse(element.attr("min"));
+            max = parse(element.attr("max"));
+
+            if (options.min === NULL && min !== NULL) {
+                options.min = min;
             }
 
-            that.value(value);
+            if (options.max === NULL && max !== NULL) {
+                options.max = max;
+            }
+
+            that.enable(!element.is('[disabled]'));
+
+            value = options.value;
+            that.value(value !== NULL ? value : element.val());
 
             that._blur();
             that._old = that._value;
@@ -57,11 +82,49 @@
             value: NULL,
             min: NULL,
             max: NULL,
-            format: "n",
             step: 1,
+            format: "n",
             empty: "Enter value",
             upArrowText: "Increase value",
             downArrowText: "Decrease value"
+        },
+
+        enable: function(enable) {
+            var that = this,
+                upArrow = that._upArrow,
+                downArrow = that._downArrow,
+                wrapper = that.wrapper,
+                element = that.element;
+
+            upArrow.unbind(MOUSEDOWN);
+            downArrow.unbind(MOUSEDOWN);
+
+            that._toggleText(enable);
+
+            if (enable === false) {
+                wrapper
+                    .addClass(DISABLED)
+                    .unbind(HOVEREVENTS);
+
+                element.attr(DISABLED, DISABLED);
+            } else {
+                wrapper
+                    .removeClass(DISABLED)
+                    .bind(HOVEREVENTS, that._toggleHover);
+
+                element
+                    .removeAttr(DISABLED);
+
+                upArrow.bind(MOUSEDOWN, function(e) {
+                    e.preventDefault();
+                    that._spin(1);
+                });
+
+                downArrow.bind(MOUSEDOWN, function(e) {
+                    e.preventDefault();
+                    that._spin(-1);
+                });
+            }
         },
 
         value: function(value) {
@@ -69,17 +132,11 @@
                 options = that.options,
                 format = options.format,
                 decimals = options.decimals,
-                numberFormat = kendo.culture().numberFormat,
+                numberFormat = that._format(format),
                 isNULL;
 
             if (value === undefined) {
                 return that._value;
-            }
-
-            if (format.indexOf("c") > -1) {
-                numberFormat = numberFormat.currency;
-            } else if (format.indexOf("p") > -1) {
-                numberFormat = numberFormat.percent;
             }
 
             if (decimals === undefined) {
@@ -88,7 +145,7 @@
 
             value = parse(value);
 
-            isNULL = value === null;
+            isNULL = value === NULL;
 
             if (!isNULL) {
                 value = parseFloat(value.toFixed(decimals));
@@ -119,10 +176,6 @@
             arrows,
             options = that.options,
             element = that.element,
-            touch = kendo.support.touch,
-            TOUCHEND = "touchend",
-            START = touch ? "touchstart" : "mousedown",
-            END = touch ? "touchmove " + TOUCHEND : "mouseup mouseleave",
             stop = function(e) {
                 if (!touch || kendo.eventTarget(e) != e.currentTarget || e.type === TOUCHEND) {
                     that._stop();
@@ -135,26 +188,15 @@
                 arrows = $(buttonHtml("up", options.upArrowText) + buttonHtml("down", options.downArrowText)).insertAfter(element);
             }
 
-            that._upArrow = arrows.eq(0)
-                                  .bind(END, stop)
-                                  .bind(START, function(e) {
-                                      e.preventDefault();
-                                      that._spin(1);
-                                  });
-
-            that._downArrow = arrows.eq(1)
-                                    .bind(END, stop)
-                                    .bind(START, function(e) {
-                                        e.preventDefault();
-                                        that._spin(-1);
-                                    });
+            that._upArrow = arrows.eq(0).bind(MOUSEUP, stop);
+            that._downArrow = arrows.eq(1).bind(MOUSEUP, stop);
         },
 
         _blur: function() {
             var that = this;
 
-            that._text.show();
-            that._change(that.element.addClass(HIDE).val());
+            that._toggleText(true);
+            that._change(that.element.val());
         },
 
         _change: function(value) {
@@ -185,11 +227,69 @@
             that._text = text.addClass(element.className.replace(INPUT));
         },
 
-        _focus: function() {
-            var that = this;
+        _format: function(format) {
+            var that = this,
+                options = that.options,
+                numberFormat = kendo.culture().numberFormat;
 
-            that.element.removeClass(HIDE);
-            that._text.hide();
+
+            if (format.indexOf("c") > -1) {
+                numberFormat = numberFormat.currency;
+            } else if (format.indexOf("p") > -1) {
+                numberFormat = numberFormat.percent;
+            }
+
+            return numberFormat;
+        },
+
+        _keydown: function(e) {
+            var that = this,
+                key = e.keyCode;
+
+            if (key == keys.DOWN) {
+                that._step(-1);
+            } else if (key == keys.UP) {
+                that._step(1);
+            }
+
+            if (!that._prevent(key) && !e.ctrlKey && !e.shiftKey) {
+                e.preventDefault();
+            }
+        },
+
+        _prevent: function(key) {
+            var that = this,
+                valid = false,
+                min = that.options.min,
+                element = that.element[0],
+                value = element.value,
+                separator = that._format(that.options.format)["."];
+
+            if (key > 47 && key < 58) {
+                valid = true;
+            } else if (key > 95 && key < 106) {
+                valid = true;
+            } else if (key > 32 && key < 37) { //PageUp/PageDown Home/End
+                valid = true;
+            } else if (key == 46 || key == 45 || key == 37 || key == 39) {
+                valid = true;
+            } else if (decimals[key] === separator && value.indexOf(separator) == -1) {
+                valid = true;
+            } else if (key > 16 && key < 21) {
+                valid = true;
+            } else if (key == 8 || key == 9 || key == 13) {
+                valid = true;
+            } else if ((min === NULL || min < 0) && value.indexOf("-") == -1 && (key == 189 || key == 109) && getCaret(element) == 0) { //sign
+                valid = true;
+            } else if (key == 110 && value.indexOf(separator) == -1) {
+                valid = false;
+                var idx = getCaret(element),
+                end = value.substring(idx);
+
+                element.value = value.substring(0, idx) + separator + end;
+            }
+
+            return valid;
         },
 
         _spin: function(step, timeout) {
@@ -224,6 +324,19 @@
             clearTimeout( this._spinning );
         },
 
+        _toggleHover: function(e) {
+            if (!touch) {
+                $(e.currentTarget).toggleClass(HOVER, e.type === "mouseenter");
+            }
+        },
+
+        _toggleText: function(toggle) {
+            var that = this;
+
+            that._text.toggle(toggle);
+            that.element.toggleClass(HIDE, toggle)
+        },
+
         _wrapper: function() {
             var that = this,
                 element = that.element,
@@ -242,6 +355,27 @@
 
     function buttonHtml(className, text) {
         return '<span class="k-link k-icon k-arrow-' + className + '" title="' + text + '">' + text + '</span>'
+    }
+
+    function getCaret(el) {
+        if (el.selectionStart) {
+            return el.selectionStart;
+        } else if (document.selection) {
+            el.focus();
+
+            var r = document.selection.createRange();
+            if (r == NULL) {
+                return 0;
+            }
+
+            var re = el.createTextRange(),
+            rc = re.duplicate();
+            re.moveToBookmark(r.getBookmark());
+            rc.setEndPoint('EndToStart', re);
+
+            return rc.text.length;
+        }
+        return 0;
     }
 
     ui.plugin("TextBox", TextBox);
