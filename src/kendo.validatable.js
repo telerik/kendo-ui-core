1a1d19528858feee3f0e64286e5cf45d649b7dcb
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2011-10-27 15:58:46 +0300

    Rename validatable data-for attrbiute to data-kendo-for

--- src/kendo.validatable.js
+++ src/kendo.validatable.js
@@ -6,6 +6,12 @@
         emailRegExp = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
         urlRegExp = /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
         INPUTSELECTOR = ":input:not(:button,[type=submit],[type=reset])",
+        NUMBERINPUTSELECTOR = "[type=number],[type=range],[data-kendo-type=number]",
+        BLUR = "blur",
+        NAME = "name",
+        FORM = "form",
+        DATAFOR = "data-kendo-for",
+        NOVALIDATE = "novalidate",
         proxy = $.proxy,
         patternMatcher = function(value, pattern) {
             if (typeof pattern === "string") {
@@ -36,8 +42,8 @@
 
             that._errorTemplate = kendo.template(that.options.errorTemplate);
 
-            if (that.element.is("form")) {
-                that.element.attr("novalidate", "novalidate");
+            if (that.element.is(FORM)) {
+                that.element.attr(NOVALIDATE, NOVALIDATE);
             }
 
             that._errors = {};
@@ -71,7 +77,7 @@
                     return true;
                 },
                 min: function(input) {
-                    if (input.filter("[type=number],[type=range],[data-kendo-type=number]").filter("[min]").length && input.val() !== "") {
+                    if (input.filter(NUMBERINPUTSELECTOR).filter("[min]").length && input.val() !== "") {
                         var min = parseInt(input.attr("min"), 10) || 0,
                             val = parseInt(input.val(), 10);
 
@@ -80,7 +86,7 @@
                     return true;
                 },
                 max: function(input) {
-                    if (input.filter("[type=number],[type=range],[data-kendo-type=number]").filter("[max]").length && input.val() !== "") {
+                    if (input.filter(NUMBERINPUTSELECTOR).filter("[max]").length && input.val() !== "") {
                         var max = parseInt(input.attr("max"), 10) || 0,
                             val = parseInt(input.val(), 10);
 
@@ -89,7 +95,7 @@
                     return true;
                 },
                 step: function(input) {
-                    if (input.filter("[type=number],[type=range],[data-kendo-type=number]").filter("[step]").length && input.val() !== "") {
+                    if (input.filter(NUMBERINPUTSELECTOR).filter("[step]").length && input.val() !== "") {
                         var min = parseInt(input.attr("min"), 10) || 0,
                             step = parseInt(input.attr("step"), 10) || 0,
                             val = parseInt(input.val(), 10);
@@ -120,16 +126,16 @@
         _attachEvents: function() {
             var that = this;
 
-            if (that.element.is("form")) {
+            if (that.element.is(FORM)) {
                 that.element.submit(proxy(that._submit, that));
             }
 
             if (!that.element.is(INPUTSELECTOR)) {
-                that.element.delegate(INPUTSELECTOR, "blur", function() {
+                that.element.delegate(INPUTSELECTOR, BLUR, function() {
                     that._validateInput($(this));
                 });
             } else {
-                that.element.bind("blur", function() {
+                that.element.bind(BLUR, function() {
                     that._validateInput(that.element);
                 });
             }
@@ -164,15 +170,15 @@
                 result = that._checkValidity(input),
                 valid = result.valid,
                 className = "." + INVALIDMSG,
-                fieldName = input.attr("name"),
-                lbl = that.element.find(className + "[data-for=" + fieldName + "]").add(input.next(className)).hide(),
+                fieldName = input.attr(NAME),
+                lbl = that.element.find(className + "[" + DATAFOR +"=" + fieldName + "]").add(input.next(className)).hide(),
                 messageText;
 
             if (!valid) {
                 messageText = that._extractMessage(input, result.key);
                 that._errors[fieldName] = messageText;
 
-                var messageLabel = $(template({ message: messageText })).addClass(INVALIDMSG).attr("data-for", fieldName || "");
+                var messageLabel = $(template({ message: messageText })).addClass(INVALIDMSG).attr(DATAFOR, fieldName || "");
                 if (!lbl.replaceWith(messageLabel).length) {
                     messageLabel.insertAfter(input)
                 }
@@ -187,7 +193,7 @@
         _extractMessage: function(input, ruleKey) {
             var that = this,
                 customMessage = that.options.messages[ruleKey],
-                fieldName = input.attr("name");
+                fieldName = input.attr(NAME);
 
             customMessage = $.isFunction(customMessage) ? customMessage(input) : customMessage;
 
