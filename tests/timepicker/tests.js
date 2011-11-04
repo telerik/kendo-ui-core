82060d95af466ab38c48414865fc36e246ff166c
George Krystev <ggkrustev@yahoo.com>
2011-11-04 18:59:29 +0200

    Render LI items. Implement value method

---
+++ tests/timepicker/tests.js
@@ -0,0 +1,10 @@
+tests.push(
+    {
+        page: "timepicker/initialization.html",
+        title: "timepicker initialization"
+    },
+    {
+        page: "timepicker/api.html",
+        title: "timepicker api"
+    }
+);
