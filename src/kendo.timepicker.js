49b30477e977d469b8b5fa868b1ecf824e25d4b3
George Krystev <ggkrustev@yahoo.com>
2011-11-08 17:42:39 +0200

    TimePicker implementation

--- src/kendo.timepicker.js
+++ src/kendo.timepicker.js
@@ -1,15 +1,23 @@
 (function($, undefined) {
     var kendo = window.kendo,
+        touch = kendo.support.touch,
+        keys = kendo.keys,
         ui = kendo.ui,
         Widget = ui.Widget,
         keys = kendo.keys,
-        DIV = "<div/>",
+        CLICK = (touch ? "touchend" : "click"),
+        DEFAULT = "k-state-default",
+        DISABLED = "disabled",
         LI = "li",
+        DIV = "<div/>",
+        FOCUSED = "k-state-focused",
         HOVER = "k-state-hover",
-        MOUSEDOWN = "mousedown",
+        HOVEREVENTS = "mouseenter mouseleave",
+        MOUSEDOWN = (touch ? "touchstart" : "mousedown"),
         MS_PER_MINUTE = 60000,
         MS_PER_DAY = 86400000,
         SELECTED = "k-state-selected",
+        STATEDISABLED = "k-state-disabled",
         proxy = $.proxy,
         TODAY = new Date(),
         MIDNIGHT = new Date(0, 0, 0, 0, 0, 0);
@@ -21,10 +29,8 @@
 
         that.options = options;
 
-        options.format = options.format || kendo.culture().calendar.patterns["t"];
-
         that.ul = $('<ul class="k-list k-reset"/>')
-                    .css({ overflow: "auto" })
+                    .css({ overflow: "auto", width: options.anchor.width() - 6, height: "200px" })
                     .bind(MOUSEDOWN, options.clearBlurTimeout)
                     .delegate(LI, "click", proxy(that.click, that))
                     .delegate(LI, "mouseenter", function() { $(this).addClass(HOVER); })
@@ -35,40 +41,37 @@
         that.popup = new ui.Popup(list, options);
 
         that.template = kendo.template('<li class="k-item" unselectable="on">#=data#</li>', { useWithBlock: false });
-
-        that._value = options.value || TODAY;
     }
 
     TimeView.prototype = {
         click: function(e) {
-            this.select($(e.currentTarget));
+            var that = this,
+                li = $(e.currentTarget);
+
+            that.select(li);
+            that._change(li.text());
+            that.close();
         },
 
         _change: function (value) {
-            var change = this.options.change;
-            if (change) {
-                change(value);
-            }
+            this.options.change(value);
         },
 
         current: function(candidate) {
             var that = this;
 
             if (candidate !== undefined) {
-                candidate = $(candidate);
-
                 if (that._current) {
                     that._current.removeClass(SELECTED);
                 }
 
                 if (candidate) {
+                    candidate = $(candidate);
                     candidate.addClass(SELECTED);
                     that.scroll(candidate[0]);
                 }
 
                 that._current = candidate;
-
-                //call _change here
             } else {
                 return that._current;
             }
@@ -85,6 +88,7 @@
                 that.refresh();
             }
 
+            that.scroll(that._current);
             that.popup.open();
         },
 
@@ -99,6 +103,7 @@
             if (key === keys.UP || down) {
                 if (e.altKey) {
                     that.toggle(down);
+                    return;
                 } else if (down) {
                     that.select(current ? current[0].nextSibling : ul.firstChild);
                     e.preventDefault();
@@ -106,12 +111,11 @@
                     that.select(current ? current[0].previousSibling : ul.lastChild);
                     e.preventDefault();
                 }
+
+                that._change(that._current.text());
+
                 pressed = true;
-            } else if (key === keys.ENTER || key === keys.TAB) {
-                //that._change(current ? current.text() : "");
-                that._change(current.text());
-                pressed = true;
-            } else if (key === keys.ESC) {
+            } else if (key === keys.ENTER || key === keys.TAB || key === keys.ESC) {
                 that.close();
                 pressed = true;
             }
@@ -175,6 +179,10 @@
             var that = this,
                 current = that._current;
 
+            if (!li) {
+                return;
+            }
+
             if (typeof li === "string") {
                 if (!current || current.text() !== li) {
                     li = $.grep(that.ul[0].childNodes, function(node) {
@@ -186,6 +194,17 @@
             }
 
             that.current(li);
+        },
+
+        toggle: function() {
+            var that = this;
+
+            if (that.popup.visible()) {
+                that.close();
+            } else {
+                that.open();
+            }
+
         }
     };
 
@@ -209,10 +228,34 @@
 
             Widget.fn.init.call(that, element, options);
 
+            element = that.element;
+            options = that.options;
+
+            options.format = options.format || kendo.culture().calendar.patterns["t"];
+
             that._wrapper();
+
+            that.timeView = new TimeView($.extend({}, options, {
+                anchor: that.wrapper,
+                format: options.format,
+                change: function(value) {
+                    that.element.val(value);
+                }
+            }));
+
             that._icon();
 
-            that.element.addClass("k-input");
+            that.element.addClass("k-input")
+                .bind({
+                    keydown: proxy(that._keydown, that),
+                    focus: function(e) {
+                        clearTimeout(that._bluring);
+                        that._inputWrapper.addClass(FOCUSED);
+                    },
+                    blur: proxy(that._blur, that)
+                });
+
+            that.enable(!element.is('[disabled]'));
         },
         options: {
             name: "TimePicker",
@@ -221,19 +264,96 @@
             value: null,
             interval: 30
         },
+
+        enable: function(enable) {
+            var that = this,
+                arrow = that._arrow,
+                element = that.element,
+                wrapper = that._inputWrapper;
+
+            arrow.unbind(CLICK)
+                .unbind(MOUSEDOWN);
+
+            if (enable === false) {
+                wrapper
+                    .removeClass(DEFAULT)
+                    .addClass(STATEDISABLED)
+                    .unbind(HOVEREVENTS);
+
+                element.attr(DISABLED, DISABLED);
+            } else {
+                wrapper
+                    .removeClass(STATEDISABLED)
+                    .addClass(DEFAULT)
+                    .bind(HOVEREVENTS, that._toggleHover);
+
+                element
+                    .removeAttr(DISABLED);
+
+                arrow.bind(CLICK, proxy(that._click, that))
+                     .bind(MOUSEDOWN, function() {
+                         setTimeout(function() {
+                             clearTimeout(that._bluring);
+                             that.element.focus();
+                         });
+                     });
+            }
+        },
+
+        close: function() {
+            this.timeView.close();
+        },
+
+        open: function() {
+            this.timeView.open();
+        },
+
+        _blur: function() {
+            var that = this;
+
+            that._bluring = setTimeout(function() {
+                //that._change(that.element.val());
+                if (!touch) {
+                    that.close();
+                }
+                that._inputWrapper.removeClass(FOCUSED);
+            }, 100);
+        },
+
+        _click: function() {
+            this.timeView.toggle();
+        },
+
         _icon: function() {
             var that = this,
                 element = that.element,
-                icon;
+                arrow;
 
-            icon = element.next("span.k-select");
+            arrow = element.next("span.k-select");
 
-            if (!icon[0]) {
-                icon = $('<span class="k-select"><span class="k-icon k-icon-clock">select</span></span>').insertAfter(element);
+            if (!arrow[0]) {
+                arrow = $('<span class="k-select"><span class="k-icon k-icon-clock">select</span></span>').insertAfter(element);
             }
 
-            that._icon = icon;
+            that._arrow = arrow;
         },
+
+        _keydown: function(e) {
+            var that = this,
+                key = e.keyCode,
+                timeView = that.timeView;
+
+            if (timeView.popup.visible() || e.altKey || key == keys.ENTER) {
+                timeView.move(e);
+            }
+        },
+
+        _toggleHover: function(e) {
+            if (!touch) {
+                $(e.currentTarget).toggleClass(HOVER, e.type === "mouseenter");
+            }
+        },
+
         _wrapper: function() {
             var that = this,
                 element = that.element,
@@ -253,6 +373,7 @@
             });
 
             that.wrapper = wrapper.addClass("k-widget k-timepicker k-header");
+            that._inputWrapper = $(wrapper[0].firstChild);
         }
     });
 
