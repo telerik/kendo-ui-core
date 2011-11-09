0bc5b32a60c382d0477df3a411baf9be0b06c4b2
Petyo Ivanov <underlog@gmail.com>
2011-11-09 16:42:12 +0200

    Tabstrip switches active tab
    Try another way to avoid address bar flashing in iPhone. click event is
    not reliable for restoring the HRef attribute.

--- src/kendo.mobile.tabstrip.js
+++ src/kendo.mobile.tabstrip.js
@@ -9,7 +9,6 @@
         MOUSEDOWN = support.mousedown,
         MOUSEUP = support.mouseup,
         ACTIVE_STATE_CLASS = "km-state-active",
-        CLICK = "click",
         SELECT = "select",
         BUTTON_SELECTOR = "a",
         extend = $.extend,
@@ -25,47 +24,40 @@
 
             that.bind([SELECT], options);
 
-            that.element.addClass("km-tabstrip").children("a")
-                    .wrapInner('<span class="km-text"/>')
-                    .addClass("km-button")
-                    .prepend('<span class="km-icon"/>');
+            that.element.addClass("km-tabstrip");
 
 
-            that._pressProxy = proxy(that._press, that);
             that._releaseProxy = proxy(that._release, that);
             that._clickProxy = proxy(that._click, that);
 
             that.element.find(BUTTON_SELECTOR)
-                            .bind(MOUSEDOWN, that._pressProxy)
+                            .wrapInner('<span class="km-text"/>')
+                            .addClass("km-button")
+                            .prepend('<span class="km-icon"/>')
                             .bind(MOUSEUP, that._releaseProxy)
-                            .bind(CLICK, that._clickProxy);
-        },
-
-        // hack to prevent flickering of address bar in iOS
-        _press: function (e) {
-            var target = $(e.currentTarget);
-            target.data("href", target.attr("href"));
-            target.attr("href", "");
+                            .bind("click", that._clickProxy);
         },
 
         _release: function (e) {
             var that = this,
                 target = $(e.currentTarget),
-                href = target.data("href");
+                oldItem = that.element.children("." + ACTIVE_STATE_CLASS),
+                href = target.attr("href");
 
             that.trigger("select", {item: target});
 
             target.addClass(ACTIVE_STATE_CLASS);
+            oldItem.removeClass(ACTIVE_STATE_CLASS);
 
             if (kendo.application && href) {
+                target.attr("href", "#!");
+                setTimeout(function() { target.attr("href", href) }, 0);
                 kendo.application.navigate(href);
+                e.preventDefault();
             }
         },
 
         _click: function (e) {
-            var target = $(e.currentTarget);
-            target.attr("href", target.data("href"));
-
             if (kendo.application) {
                 e.preventDefault();
             }
