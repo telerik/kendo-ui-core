877b2450f0dc9b0fa69cd34b529a3a70c4cddb51
Petyo Ivanov <underlog@gmail.com>
2011-11-09 15:05:42 +0200

    Support for tabstrip select event

--- src/kendo.mobile.tabstrip.js
+++ src/kendo.mobile.tabstrip.js
@@ -10,6 +10,8 @@
         MOUSEUP = support.mouseup,
         ACTIVE_STATE_CLASS = "km-state-active",
         CLICK = "click",
+        SELECT = "select",
+        BUTTON_SELECTOR = "a",
         extend = $.extend,
         proxy = $.proxy;
 
@@ -20,13 +22,53 @@
             MobileWidget.fn.init.call(that, element, options);
 
             options = that.options;
+
+            that.bind([SELECT], options);
+
             that.element.addClass("km-tabstrip").children("a")
                     .wrapInner('<span class="km-text"/>')
                     .addClass("km-button")
-                    .prepend('<span class="km-icon"/>').click(function() {
-                        $(this).toggleClass("km-state-active")
-                        return false;
-                    });
+                    .prepend('<span class="km-icon"/>');
+
+
+            that._pressProxy = proxy(that._press, that);
+            that._releaseProxy = proxy(that._release, that);
+            that._clickProxy = proxy(that._click, that);
+
+            that.element.find(BUTTON_SELECTOR)
+                            .bind(MOUSEDOWN, that._pressProxy)
+                            .bind(MOUSEUP, that._releaseProxy)
+                            .bind(CLICK, that._clickProxy);
+        },
+
+        // hack to prevent flickering of address bar in iOS
+        _press: function (e) {
+            var target = $(e.currentTarget);
+            target.data("href", target.attr("href"));
+            target.attr("href", "");
+        },
+
+        _release: function (e) {
+            var that = this,
+                target = $(e.currentTarget),
+                href = target.data("href");
+
+            that.trigger("select", {item: target});
+
+            target.addClass(ACTIVE_STATE_CLASS);
+
+            if (kendo.application && href) {
+                kendo.application.navigate(href);
+            }
+        },
+
+        _click: function (e) {
+            var target = $(e.currentTarget);
+            target.attr("href", target.data("href"));
+
+            if (kendo.application) {
+                e.preventDefault();
+            }
         },
 
         options: {
