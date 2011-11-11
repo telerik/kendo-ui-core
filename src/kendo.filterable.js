e882ee5ba47e843adaffcbfde549585589baed99
Atanas Korchev <akorchev@gmail.com>
2011-11-11 17:03:18 +0200

    Add support for DataSource

--- src/kendo.filterable.js
+++ src/kendo.filterable.js
@@ -3,60 +3,78 @@
         ui = kendo.ui,
         Widget = ui.Widget;
 
-    function menu(options) {
-        var template = '<div class="k-filter-menu k-group">\
-                <div class="k-filter-help-text">#=messages.info#</div>\
-                <select>\
-                     <option>Is Equal To</option>\
-                     <option>Is Not Equal To</option>\
-                </select>\
-                <input class="k-input k-autocomplete" type="text"/>';
-
-        if (options.extra) {
-            template +=
-            '<select>\
-               <option>And</option>\
-               <option>Or</option>\
-            </select>\
-            <select>\
-                 <option>Is Equal To</option>\
-                 <option>Is Not Equal To</option>\
-            </select>\
-            <input class="k-input k-autocomplete" type="text"/>';
-        }
-
-        template += '<button class="k-button">#=messages.filter#</button><button class="k-button">#=messages.cancel#</button>\
-                </div>';
-
-        return $(kendo.template(template)(options));
-    }
+    var template =
+        '<div class="k-filter-menu k-group">'+
+            '<div class="k-filter-help-text">#=messages.info#</div>'+
+                '<select>'+
+                    '#for(var op in operators){#'+
+                        '<option value="#=op#">#=operators[op]#</option>'+
+                    '#}#'+
+                '</select>'+
+                '<input class="k-input k-autocomplete" type="text"/>'+
+                '#if(extra){#'+
+                    '<select>'+
+                        '<option>And</option>'+
+                        '<option>Or</option>'+
+                    '</select>'+
+                    '<select>'+
+                        '#for(var op in operators){#'+
+                            '<option value="#=op#">#=operators[op]#</option>'+
+                        '#}#'+
+                    '</select>'+
+                    '<input class="k-input k-autocomplete" type="text"/>'+
+                '#}#'+
+            '<button class="k-button">#=messages.filter#</button><button class="k-button">#=messages.cancel#</button>'+
+        '</div>';
 
     var Filterable = Widget.extend({
         init: function(element, options) {
-            var that = this, link;
+            var that = this, link, type;
 
             Widget.fn.init.call(that, element, options);
 
-            link = that.element.addClass("k-filterable").find("k-grid-filter");
+            element = that.element;
+            options = that.options;
+
+            link = element.addClass("k-filterable").find("k-grid-filter");
 
             if (!link[0]) {
-                link = that.element.prepend('<a class="k-grid-filter" href="#"><span class="k-icon k-filter"/></a>').find(".k-grid-filter");
+                link = element.prepend('<a class="k-grid-filter" href="#"><span class="k-icon k-filter"/></a>').find(".k-grid-filter");
             }
 
-            that.menu = menu(that.options);
+            that.dataSource = options.dataSource.bind("change", $.proxy(that.refresh, that));
+
+            console.log(that.dataSource.reader.model.fields);
+
+            that.menu = $(kendo.template(template)({
+                messages: options.messages,
+                extra: options.extra,
+                operators: options.operators[options.type]
+            }));
 
             that.popup = new ui.Popup(that.menu,{
                 anchor: link,
                 toggleTarget: link
             });
 
-            that.menu.find("select")
+            that.menu
+                .find("select")
                 .kendoDropDownList();
+        },
+
+        refresh: function() {
         },
 
         options: {
             name: "Filterable",
             extra: true,
+            type: "string",
+            operators: {
+                string: {
+                    eq: "Is Equal To",
+                    neq: "Is Not Equal To"
+                }
+            },
             messages: {
                 info: "Show rows with value that:",
                 filter: "Filter",
@@ -65,5 +83,5 @@
         }
     });
 
-    kendo.ui.plugin(Filterable);
+    ui.plugin(Filterable);
 })(jQuery)
