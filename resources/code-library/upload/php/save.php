afe20d51fec16675a5e11e12e44918bd46a8aceb
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-11-18 15:06:50 +0200

    Add error handling to save handler

--- resources/code-library/upload/php/save.php
+++ resources/code-library/upload/php/save.php
@@ -6,10 +6,20 @@
     if (isset($files['name']))
     {
         foreach ($files["name"] as $i => $name) {
-            $targetPath = $uploadRoot . basename($files["name"][$i]);
-            $uploadedFile = $files["tmp_name"][$i];
-            if (is_uploaded_file($uploadedFile)) {
-                move_uploaded_file($uploadedFile, $targetPath);
+            $error = $files['error'][$i];
+
+            if ($error == UPLOAD_ERR_OK) {
+                $targetPath = $uploadRoot . basename($files["name"][$i]);
+                $uploadedFile = $files["tmp_name"][$i];
+
+                if (is_uploaded_file($uploadedFile)) {
+                    if (!move_uploaded_file($uploadedFile, $targetPath)) {
+                        echo "Error moving uploaded file";
+                    }
+                }
+            } else {
+                // See http://php.net/manual/en/features.file-upload.errors.php
+                echo "Error code " . $error;
             }
         }
     }
