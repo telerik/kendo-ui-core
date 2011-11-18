18c3bb166e43905e3e6f9b8fcda66cc9d79c186a
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-11-18 14:46:34 +0200

    Add Upload PHP code library

---
+++ resources/code-library/upload/php/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Asynchronous upload</title>
+        <link href="../../../../styles/kendo.common.css" rel="stylesheet"/>
+        <link href="../../../../styles/kendo.blueopal.css" rel="stylesheet"/>
+        <script src="../../../../src/jquery.js"></script>
+        <script src="../../../../src/kendo.core.js"></script>
+        <script src="../../../../src/kendo.upload.js"></script>
+    </head>
+
+    <body>
+        <div style="width:60%">
+            <input name="files" id="files" type="file" />
+        </div>
+        <script>
+            $(document).ready(function() {
+                $("#files").kendoUpload({
+                    async: {
+                        saveUrl: "save.php",
+                        saveField: "files[]",
+                        removeUrl: "remove.php",
+                        removeField: "fileNames[]",
+                        autoUpload: true
+                    }
+                });
+            });
+        </script>
+    </body>
+</html>
+
---
+++ resources/code-library/upload/php/remove.php
@@ -0,0 +1,8 @@
+<?php
+    $fileParam = "files";
+    $uploadRoot = "uploads/";
+
+    echo($_POST["fileNames"]);
+    // Return an empty string to signify success
+    echo "";
+?>
