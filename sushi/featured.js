13dcca8be22d7405f027cf0d15e244c97fa73144
Petyo Ivanov <underlog@gmail.com>
2011-11-18 19:02:45 +0200

    First version of kendo sushi

---
+++ sushi/featured.js
@@ -0,0 +1,11 @@
+[
+    {"id": 1, "name": "California Maki", "price": 9.99, "image": "1.jpg"},
+    {"id": 2, "name": "Schnitzel Maki", "price": 5.99, "image": "2.jpg"},
+    {"id": 3, "name": "Philadelphia Maki", "price": 8.99, "image": "3.jpg"},
+    {"id": 4, "name": "Chicken Maki", "price": 20.99, "image": "4.jpg"},
+    {"id": 5, "name": "California Maki", "price": 9.99, "image": "1.jpg"},
+    {"id": 6, "name": "Schnitzel Maki", "price": 5.99, "image": "2.jpg"},
+    {"id": 7, "name": "Philadelphia Maki", "price": 8.99, "image": "3.jpg"},
+    {"id": 8, "name": "Chicken Maki", "price": 20.99, "image": "4.jpg"}
+]
+
