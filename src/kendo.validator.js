d036b28456f2a3a119716f888a3b79878e3e0d17
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2011-11-25 16:03:33 +0200

    Change validator to use kendo.attr

--- src/kendo.validator.js
+++ src/kendo.validator.js
@@ -6,11 +6,10 @@
         emailRegExp = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
         urlRegExp = /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
         INPUTSELECTOR = ":input:not(:button,[type=submit],[type=reset])",
-        NUMBERINPUTSELECTOR = "[type=number],[type=range],[data-kendo-type=number]",
+        NUMBERINPUTSELECTOR = "[type=number],[type=range]",
         BLUR = "blur",
         NAME = "name",
         FORM = "form",
-        DATAFOR = "data-kendo-for",
         NOVALIDATE = "novalidate",
         proxy = $.proxy,
         patternMatcher = function(value, pattern) {
@@ -137,7 +136,7 @@
                     return true;
                 },
                 min: function(input) {
-                    if (input.filter(NUMBERINPUTSELECTOR).filter("[min]").length && input.val() !== "") {
+                    if (input.filter(NUMBERINPUTSELECTOR + ",[" + kendo.attr("type") + "=number]").filter("[min]").length && input.val() !== "") {
                         var min = parseInt(input.attr("min"), 10) || 0,
                             val = parseInt(input.val(), 10);
 
@@ -146,7 +145,7 @@
                     return true;
                 },
                 max: function(input) {
-                    if (input.filter(NUMBERINPUTSELECTOR).filter("[max]").length && input.val() !== "") {
+                    if (input.filter(NUMBERINPUTSELECTOR + ",[" + kendo.attr("type") + "=number]").filter("[max]").length && input.val() !== "") {
                         var max = parseInt(input.attr("max"), 10) || 0,
                             val = parseInt(input.val(), 10);
 
@@ -155,7 +154,7 @@
                     return true;
                 },
                 step: function(input) {
-                    if (input.filter(NUMBERINPUTSELECTOR).filter("[step]").length && input.val() !== "") {
+                    if (input.filter(NUMBERINPUTSELECTOR + ",[" + kendo.attr("type") + "=number]").filter("[step]").length && input.val() !== "") {
                         var min = parseInt(input.attr("min"), 10) || 0,
                             step = parseInt(input.attr("step"), 10) || 0,
                             val = parseInt(input.val(), 10);
@@ -165,13 +164,13 @@
                     return true;
                 },
                 email: function(input) {
-                    return matcher(input, "[type=email],[data-kendo-type=email]", emailRegExp);
+                    return matcher(input, "[type=email],[" + kendo.attr("type") + "=email]", emailRegExp);
                 },
                 url: function(input) {
-                    return matcher(input, "[type=url],[data-kendo-type=url]", urlRegExp);
+                    return matcher(input, "[type=url],[" + kendo.attr("type") + "=url]", urlRegExp);
                 },
                 date: function(input) {
-                    if (input.filter("[type^=date],[data-kendo-type=date]").length && input.val() !== "") {
+                    if (input.filter("[type^=date],[" + kendo.attr("type") + "=date]").length && input.val() !== "") {
                         return kendo.parseDate(input.val()) !== null;
                     }
                     return true;
@@ -241,6 +240,7 @@
                 valid = result.valid,
                 className = "." + INVALIDMSG,
                 fieldName = input.attr(NAME),
+                DATAFOR = kendo.attr("for"),
                 lbl = that.element.find(className + "[" + DATAFOR +"=" + fieldName + "]").add(input.next(className)).hide(),
                 messageText;
 
@@ -267,7 +267,7 @@
 
             customMessage = $.isFunction(customMessage) ? customMessage(input) : customMessage;
 
-            return kendo.format(input.data("kendo-" + ruleKey + "-msg") || input.attr("validationMessage") || input.attr("title") || customMessage || "", fieldName, input.attr(ruleKey));
+            return kendo.format(input.attr(kendo.attr(ruleKey + "-msg")) || input.attr("validationMessage") || input.attr("title") || customMessage || "", fieldName, input.attr(ruleKey));
         },
 
         _checkValidity: function(input) {
