ed09fa793e198b75b1625c6221e07ce0743e2d2e
Hristo Germanov <hristo.germanov@telerik.com>
2011-11-28 11:11:38 +0200

    Rearrangement of the chart dataviz demos

--- demos/examples/dataviz/js/kendo.examples.nav.js
+++ demos/examples/dataviz/js/kendo.examples.nav.js
@@ -10,27 +10,10 @@ var categories = {
                     url: "overview/index.html"
                 }
             ]
-        },
-        {
-            text: "Chart",
+        }, {
+            text: "API",
             spriteCssClass: "chartIcon",
             items: [
-                {
-                    text: "Basic usage",
-                    url: "chart/index.html"
-                },
-                {
-                    text: "Pie Chart",
-                    url: "chart/pie-chart.html"
-                },
-                {
-                    text: "Binding to local data",
-                    url: "chart/local-data.html"
-                },
-                {
-                    text: "Binding to remote data",
-                    url: "chart/remote-data.html"
-                },
                 {
                     text: "Events",
                     url: "chart/events.html"
@@ -38,6 +21,49 @@ var categories = {
                 {
                     text: "API",
                     url: "chart/api.html"
+                }
+            ]
+        }, {
+            text: "Bar Charts",
+            spriteCssClass: "chartIcon",
+            items: [
+                {
+                    text: "Basic usage",
+                    url: "chart/index.html"
+                },
+                {
+                    text: "Binding to remote data",
+                    url: "chart/remote-data.html"
+                }
+            ]
+        }, {
+            text: "Line Chart",
+            spriteCssClass: "chartIcon",
+            items: [
+                {
+                    text: "Binding to local data",
+                    url: "chart/local-data.html"
+                }
+            ]
+        }, {
+            text: "Pie Chart",
+            spriteCssClass: "chartIcon",
+            items: [
+                {
+                    text: "Pie Chart",
+                    url: "chart/pie-chart.html"
+                }
+            ]
+        }, {
+            text: "Scatter Charts",
+            spriteCssClass: "chartIcon",
+            items: [
+                {
+                    text: "Scatter chart",
+                    url: "chart/scatter.html"
+                }, {
+                    text: "Scatter line chart",
+                    url: "chart/scatter.html"
                 }
             ]
         }
