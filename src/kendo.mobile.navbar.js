9014d87b296ac0b42542c11d0d5ca7152aad7236
Petyo Ivanov <underlog@gmail.com>
2011-12-13 11:35:08 +0200

    Complete and polish navbar docs

--- src/kendo.mobile.navbar.js
+++ src/kendo.mobile.navbar.js
@@ -6,8 +6,8 @@
     * The MobileNavBar usually shows the current view title in the center, and optionally some additional left and right aligned elements (back button, settings button, etc.).
     *
     * <h3>Getting Started</h3>
-    * If you use the Kendo MobileApplication, it will automatically initialize the MobileNavBar for every element with <code>role</code> data attribute set to <code>navbar</code> inside your views/layouts.
-    * You can also initialize it using a jQuery selector.
+    * The Kendo MobileApplication will automatically initialize the MobileNavBar for every element with <code>role</code> data attribute set to <code>navbar</code> present in the views/layouts markup.
+    * Alternatively, it can be initialized using a jQuery selector.
     * @exampleTitle Initialize Kendo MobileNavBar based on role data attribute
     * @example
     * <div data-role="navbar">My View Title</div>
@@ -17,9 +17,9 @@
     * var navbar = $("#navbar").kendoMobileNavBar();
     * @section <h3>Aligning widgets inside the navbar</h3>
     *
-    * After initialization, the MobileNavBar positions elements, based on the specified <code>align</code> data attribute (either <code>left</code> or <code>right</code>). By default, elements without any align are centered.
+    * After initialization, the MobileNavBar positions elements based on the specified <code>align</code> data attribute (either <code>left</code> or <code>right</code>). By default, elements without any align are centered.
     *
-    * @exampleTitle Use the <code>align</code> data attribute to specify the elements inside the navbar
+    * @exampleTitle Use the <code>align</code> data attribute to specify the elements position inside the navbar
     * @example
     * <div data-role="navbar">
     *   <a data-role="back-button" data-align="left">Back</a>
@@ -28,7 +28,7 @@
     * </div>
     *
     * @section <h3>Automatically update navbar title based on current view's title</h3>
-    * If an element with <code>role</code> data attribute set to <code>view-title</code> is present inside the MobileNavBar, the Kendo MobileApplication instance will automatically update its contents based on the current view's title when changing views. You can specify the view title by setting the <code>title</code> data attribute of the view element. This feature is particularly useful if the MobileNavBar is inside a layout.
+    * If an element with <code>role</code> data attribute set to <code>view-title</code> is present inside the MobileNavBar, the Kendo MobileApplication instance will update its text when changing views to on the current view's title.  The view title is specified by setting the <code>title</code> data attribute of the view element. This feature is particularly useful if the MobileNavBar is inside a layout.
     * @exampleTitle Use the <code>view-title</code> data attribute to auto-update the MobileNavBar title
     * @example
     * <div data-role="layout" data-id="foo">
@@ -47,6 +47,7 @@
         MobileWidget = ui.MobileWidget;
 
     function createContainer(align, element) {
+
         var items = element.find("[" + kendo.attr("align") + "=" + align + "]");
 
         if (items[0]) {
@@ -59,9 +60,8 @@
         * @constructs
         * @extends kendo.ui.MobileWidget
         * @param {DomElement} element DOM element
-        * @param {Object} options Configuration options.
         */
-        init: function(element, options) {
+        init: function(element) {
             MobileWidget.fn.init.call(this, element, options);
             element = this.element;
 
@@ -75,6 +75,18 @@
             selector: roleSelector("navbar")
         },
 
+        /**
+        * Update the title element text. The title element is specified by setting the <code>role</code> data attribute to <code>view-title</code>
+        * @param {String} value The text of title
+        * @example
+        * &lt;div data-role="navbar" id="foo"&gt;
+        *     &lt;span data-role="view-title"&gt;&lt;/span&gt;
+        * &lt;/div&gt;
+        *
+        * &lt;script&gt;
+        *   $("#foo").data("kendoMobileNavBar").title("Foo");
+        * &lt;/script&gt;
+        */
         title: function(value) {
             this.element.find(roleSelector("view-title")).text(value);
         }
