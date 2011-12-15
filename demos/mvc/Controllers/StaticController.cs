8f8ead5e154c488ef3bf22ffbd9960c42f32db4a
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-12-15 18:15:05 +0200

    Serve static content through dedicated controller

---
+++ demos/mvc/Controllers/StaticController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Kendo.Controllers
+{
+    public class StaticController : BaseController
+    {
+        //
+        // GET: /Source/
+
+        public ActionResult Resource(string assetGroup, string assetType, string file)
+        {
+            var root = Server.MapPath("~/");
+            var path = Path.Combine(root, "..", "..", AssetRoots[assetGroup + "/" + assetType], file);
+
+            return StaticContent(path);
+        }
+    }
+}
