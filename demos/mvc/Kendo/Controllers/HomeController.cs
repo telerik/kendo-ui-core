1fe608785044cd3d13657afe5ce830bb6798c427
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-12-15 15:04:57 +0200

    Add empty MVC3 project compatible with Mono/XSP4

---
+++ demos/mvc/Kendo/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Kendo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
