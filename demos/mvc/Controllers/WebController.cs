42aa1a887b9d48280fe992383ae843a9276772a8
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-12-15 15:41:46 +0200

    Add Web suite route

---
+++ demos/mvc/Controllers/WebController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Kendo.Controllers
+{
+    public class WebController : Controller
+    {
+        //
+        // GET: /Web/
+
+        public ActionResult Index(string section, string example)
+        {
+            ViewBag.section = section;
+            ViewBag.example = example;
+            return View();
+        }
+
+    }
+}
