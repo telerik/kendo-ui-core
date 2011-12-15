4f5985cb62ffba95aa20de9a6eadf3a6fd6520e6
George Krystev <ggkrustev@yahoo.com>
2011-12-15 18:44:52 +0200

    Remove swithAnimation object

--- src/kendo.mobile.switch.js
+++ src/kendo.mobile.switch.js
@@ -15,13 +15,7 @@
         bindSelectors = ".km-checkbox",
         TRANSFORMSTYLE = support.transitions.css + "transform",
         extend = $.extend,
-        proxy = $.proxy,
-        switchAnimation = {
-            manimator: ".km-switch-background",
-            animator: handleSelector,
-            effects: "slideTo",
-            duration: 150
-        };
+        proxy = $.proxy;
 
     function preventDefault(e) {
         e.preventDefault();
@@ -35,52 +29,40 @@
     var MobileSwitch = MobileWidget.extend({
         init: function(element, options) {
             var that = this;
-            element = $(element);
-
-            if (element.is("input[type=checkbox]")) {
-                element = element.wrap("<label />").parent();
-            }
-
-            options = extend(options, {handle: handleSelector});
 
             MobileWidget.fn.init.call(that, element, options);
 
-            element = that.element;
-            options = that.options;
-
             that._wrap();
+
+            element = that.element.bind(MOUSEDOWN, proxy(that._start, that));
+            options = that.options;
 
             //refactor
             that.width = element.outerWidth();
-            that.handle = element.find(options.handle);
             that.halfWidth = that.handle.outerWidth() / 2;
-            that.snapPart = (that.width - that.halfWidth * 2);
             that.constrain = that.width - that.handle.outerWidth(true) + that.halfWidth;
-            that.animator = element.find(switchAnimation.animator);
-            that.manimator = element.find(switchAnimation.manimator);
-            that._moveProxy = $.proxy(that._move, that);
-            that._stopProxy = $.proxy(that._stop, that);
+
+            //proxies
+            that._moveProxy = proxy(that._move, that);
+            that._stopProxy = proxy(that._stop, that);
             that._toggleProxy = proxy(that._toggle, that);
             that._triggerProxy = proxy(that._trigger, that);
 
-            element.bind(MOUSEDOWN, $.proxy(that._start, that));
-
             that.bind([
-                SLIDE, //these events are not triggered
-                TOGGLE
+                CHANGE
             ], options);
 
+            //check the input.is("[disabled]")
             that.enable(options.enable);
         },
 
         options: {
             name: "MobileSwitch",
             enable: true,
-            selector: kendo.roleSelector("switch"),
-            manimator: ".km-switch-background",
-            animator: handleSelector
+            selector: kendo.roleSelector("switch")
         },
 
+        //refactor
         enable: function(enable) {
             enable = typeof enable === "boolean" ? enable : true;
             var that = this;
@@ -101,6 +83,7 @@
             }
         },
 
+        //refactor
         toggle: function(toggle) {
             var that = this,
                 input = that.input,
@@ -122,7 +105,7 @@
                 position = location - that.halfWidth;
 
             that.animator.css(TRANSFORMSTYLE, "translatex(" + position + "px)"); // TODO: remove halfWidth
-            that.manimator.css("margin-left", that.origin + position);
+            that.mAnimator.css("margin-left", that.origin + position);
         },
 
         _start: function(e) {
@@ -132,11 +115,12 @@
 
             that._initial = that._getAxisLocation(e);
 
-            that.origin = that.manimator.data("origin");
+            //why we need origin?
+            that.origin = that.mAnimator.data("origin");
 
             if (!that.origin && that.origin !== 0) { //check for undefined
-               that.origin = parseInt(that.manimator.css("margin-left"), 10);
-               that.manimator.data("origin", that.origin);
+               that.origin = parseInt(that.mAnimator.css("margin-left"), 10);
+               that.mAnimator.data("origin", that.origin);
             }
 
             $(document) //cache it
@@ -151,6 +135,7 @@
                 location = that._getAxisLocation(e),
                 value;
 
+            //consider better way!
             if (Math.abs(that._initial - location) <= 2) {
                 value = !that.input[0].checked;
             } else {
@@ -168,6 +153,7 @@
             this.handle.toggleClass("km-state-active", e.type == MOUSEDOWN);
         },
 
+        //what is the diff between _toggle() and toggle()
         _toggle: function() {
             var that = this;
 
@@ -176,18 +162,20 @@
             }
         },
 
+        //refactor and rename ?
+        //
         _snap: function (value) { //snap expects true or false (checked and etc);
             var that = this,
                 handle = that.handle,
                 checked = (value == 1), distance;
 
             handle
-                .removeClass("km-switch-on")
+                .removeClass("km-switch-on") //combine those ? or toggle
                 .removeClass("km-switch-off")
                 .addClass("km-switch-" + (checked ? "on" : "off"));
 
             if (!handle.data("animating")) {
-                distance = value * (that.element.outerWidth() - that.handle.outerWidth(true));
+                distance = value * (that.element.outerWidth() - that.handle.outerWidth(true)); //this maybe is the snap part... cache it!
 
                 that.mAnimator
                     .kendoStop(true, true)
@@ -196,29 +184,40 @@
                 that.animator
                     .kendoStop(true, true)
                     .kendoAnimate(extend({
+                        effects: "slideTo",
+                        duration: 150,
+                        offset: distance + "px,0",
                         complete: function () {
                             that.input[0].checked = checked;
-                            that.trigger(TOGGLE, { checked: checked });
+                            that.trigger(TOGGLE, { checked: checked }); //should remove it ??!??!
                         }
-                    }, switchAnimation, {
-                        offset: distance + "px,0"
                     }));
             }
         },
 
+        //refactor _wrap. split this method
         _wrap: function() {
             var that = this,
-                input = that.element.children("input[type=checkbox]"),
-                handle = that.element.children(handleSelector);
+                element = that.element,
+                input,
+                handle;
 
-            if (that.element.is("label")) {
-                that.element.addClass("km-switch");
+            if (element.is("input[type=checkbox]")) {
+                that.element = element = element.wrap("<label />").parent();
+            }
+
+            input = element.children("input[type=checkbox]");
+            handle = element.children(handleSelector);
+
+            if (element.is("label")) {
+                element.addClass("km-switch");
             }
 
             if (input.length) {
+                //use kendo.attr("role") ?? maybe I should use attr not data
                 input.data("kendo-role", "switch");
             } else {
-                input = $("<input type='checkbox' " + kendo.attr("role") +  "='switch' />").appendTo(that.element);
+                input = $("<input type='checkbox' " + kendo.attr("role") +  "='switch' />").appendTo(element);
             }
 
 
@@ -230,8 +229,8 @@
 
             handle.parent().before("<span class='km-switch-wrapper'><span class='km-switch-background'></span></span>");
 
-            that.animator = that.element.find(switchAnimation.animator);
-            that.mAnimator = that.element.find(switchAnimation.manimator);
+            that.animator = handle;
+            that.mAnimator = that.element.find(".km-switch-background");
 
             var checked = input[0].checked;
             handle.addClass("km-switch-" + (checked ? "on" : "off"));
@@ -245,7 +244,6 @@
             that.input = input;
             that.handle = handle;
         }
-
     });
 
     ui.plugin(MobileSwitch);
