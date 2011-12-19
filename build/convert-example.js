ddced07eff5980caa60fe5e73dd63dc56e8b852f
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-12-19 16:42:12 +0200

    Add script for converting demos

---
+++ build/convert-example.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+// Imports ====================================================================
+var fs = require("fs"),
+    sys = require("sys"),
+    path = require("path"),
+    spawn = require('child_process').spawn,
+    kendoBuild = require("./kendo-build"),
+    processFiles = kendoBuild.processFilesRecursive,
+    mkdir = kendoBuild.mkdir;
+
+// Configuration ==============================================================
+var SUITES = ["web"],
+    EXAMPLES_ROOT = path.join("demos", "examples"),
+    MVC_ROOT = path.join("demos", "mvc", "Views");
+
+// Implementation ==============================================================
+SUITES.forEach(function(suite) {
+    var suiteRoot = path.join(EXAMPLES_ROOT, suite);
+
+    processFiles(
+        suiteRoot,
+        /.html$/,
+        function (fileName) {
+            var outputFile = fileName
+                .replace(EXAMPLES_ROOT, MVC_ROOT)
+                .replace(".html", ".cshtml");
+
+            try {
+                mkdir(path.dirname(outputFile));
+                kendoBuild.copyFileSync(fileName, outputFile);
+
+                var content = kendoBuild.readText(outputFile);
+                content = content.replace(/(.|\n)+<body>((.|\n)+)<\/body>(.|\n)+/m, "$2");
+                content = content.replace("../content", "../../content/" + suite);
+
+                kendoBuild.writeText(outputFile, content);
+            } catch (e) {
+                console.log("Trouble converting ", fileName);
+            }
+        }
+    );
+});
