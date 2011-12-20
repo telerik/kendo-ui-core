943dee5fcd1a264b3fc7746ab1ad8a31cbe03d59
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-12-20 13:48:05 +0200

    Add source controller

---
+++ demos/mvc/Controllers/SourceController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
+using IOFile = System.IO.File;
+
+namespace Kendo.Controllers
+{
+    public class SourceController : BaseController
+    {
+        //
+        // GET: /Web/
+
+        public ActionResult Index(string section, string example)
+        {
+            return Content(RenderView(section + "/" + example, "SourceLayout"), "text/plain");
+        }
+
+        public string RenderView(string viewName, string template)
+        {
+            using (var writer = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindView(ControllerContext, viewName, template);
+                var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, writer);
+                viewResult.View.Render(viewContext, writer);
+
+                return writer.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
--- demos/mvc/Controllers/WebController.cs
+++ demos/mvc/Controllers/WebController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 using System.IO;
 using System.Linq;
@@ -33,3 +34,42 @@ namespace Kendo.Controllers
         }
     }
 }
+=======
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
+using IOFile = System.IO.File;
+
+namespace Kendo.Controllers
+{
+    public class WebController : BaseController
+    {
+        //
+        // GET: /Web/
+
+        public ActionResult Index(string section, string example)
+        {
+            ViewBag.Section = section;
+            ViewBag.Example = example;
+            ViewBag.Description = LoadDescription(section);
+
+            return View(section + "/" + example);
+        }
+
+        protected string LoadDescription(string section) {
+            var path = Server.MapPath(string.Format(
+                "~/content/docs/kendo.ui.{0}.description.html", section)
+            );
+
+            if (IOFile.Exists(path)) {
+                return IOFile.ReadAllText(path);
+            }
+
+            return "";
+        }
+    }
+}
+>>>>>>> Add source controller
