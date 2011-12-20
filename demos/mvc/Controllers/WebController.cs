61c1797ea539cd81f2a37afad7e0389e8c52eba8
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-12-20 11:24:57 +0200

    Add simple description

--- demos/mvc/Controllers/WebController.cs
+++ demos/mvc/Controllers/WebController.cs
@@ -1,20 +1,35 @@
-﻿using System;
+using System;
+using System.IO;
 using System.Linq;
 using System.Web.Mvc;
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
+using IOFile = System.IO.File;
 
 namespace Kendo.Controllers
 {
-    public class WebController : Controller
+    public class WebController : BaseController
     {
         //
         // GET: /Web/
 
         public ActionResult Index(string section, string example)
         {
-            ViewBag.section = section;
-            ViewBag.example = example;
-            return View();
+            ViewBag.Description = LoadDescription(section);
+
+            return View(section + "/" + example);
         }
 
+        protected string LoadDescription(string section) {
+            var path = Server.MapPath(string.Format(
+                "~/content/docs/kendo.ui.{0}.description.html", section)
+            );
+
+            if (IOFile.Exists(path)) {
+                return IOFile.ReadAllText(path);
+            }
+
+            return "";
+        }
     }
 }
