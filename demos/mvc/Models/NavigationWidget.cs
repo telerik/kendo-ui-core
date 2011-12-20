b86a399898726e3bb1aaf71d875f3b0ea0cfbf3c
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-12-20 19:02:03 +0200

    Generate navigation in _Layout

---
+++ demos/mvc/Models/NavigationWidget.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Kendo.Models
+{
+    public class NavigationWidget
+    {
+        public string Text { get; set; }
+        public string SpriteCssClass { get; set; }
+        public bool Expanded { get; set; }
+        public NavigationExample[] Items { get; set; }
+    }
+}
