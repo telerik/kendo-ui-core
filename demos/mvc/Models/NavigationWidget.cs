10778ada0b37e17cc8abb45a6968ea9db18ef3c9
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-12-21 10:58:29 +0200

    Select active example on load

--- demos/mvc/Models/NavigationWidget.cs
+++ demos/mvc/Models/NavigationWidget.cs
@@ -5,6 +5,7 @@ namespace Kendo.Models
 {
     public class NavigationWidget
     {
+        public string Name { get; set; }
         public string Text { get; set; }
         public string SpriteCssClass { get; set; }
         public bool Expanded { get; set; }
