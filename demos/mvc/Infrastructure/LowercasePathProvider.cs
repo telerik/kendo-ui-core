854fd458e12e77c7a442cab876f69e4909764cf4
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2011-12-22 11:40:27 +0200

    Add path provider to access views in lowercase

---
+++ demos/mvc/Infrastructure/LowercasePathProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Web.Hosting;
+
+namespace Kendo.Infrastructure
+{
+    public class LowercasePathProvider : VirtualPathProvider
+    {
+        public override bool DirectoryExists(string virtualDir)
+        {
+            return base.DirectoryExists(virtualDir.ToLowerInvariant());
+        }
+
+        public override bool FileExists(string virtualPath)
+        {
+            return base.FileExists(virtualPath.ToLowerInvariant());
+        }
+
+        public override VirtualDirectory GetDirectory(string virtualDir)
+        {
+            return base.GetDirectory(virtualDir.ToLowerInvariant());
+        }
+
+        public override VirtualFile GetFile(string virtualPath)
+        {
+            return base.GetFile(virtualPath.ToLowerInvariant());
+        }
+    }
+}
