80614c2ddadf5087ca8aa52aeac06e256b9b6ef9
Bundyo <bundyo@gmail.com>
2011-12-22 10:24:06 +0200

    Update Menu, PanelBar, TabStrip documentation - init from fake dataSource (JSON object).(cherry picked from commit 6421455)

--- src/kendo.panelbar.js
+++ src/kendo.panelbar.js
@@ -88,6 +88,23 @@
      *     ]
      * });
      *
+     * @exampleTitle Initialize Kendo PanelBar using JSON data object
+     * @example
+     * $("#panelbar").kendoPanelBar({
+     *     dataSource: [
+     *         {
+     *             text: "Item 1",
+     *             items: [
+     *                 { text: "Sub Item 1" },
+     *                 { text: "Sub Item 2" }
+     *             ]
+     *         },
+     *         {
+     *             text: "Item 2"
+     *         }
+     *     ]
+     * });
+     * 
      * @section
      * <h3>Loading Content with AJAX</h3>
      * <p>
