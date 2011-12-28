7359292ba6426f1dd675d562b403f7fd17c2051e
Petyo Ivanov <underlog@gmail.com>
2011-12-28 12:31:23 +0200

    Catchup for all the mistakes in the rebase from mvc-demos to master

--- demos/mvc/Controllers/SuiteController.cs
+++ demos/mvc/Controllers/SuiteController.cs
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-using System;
-using System.IO;
-using System.Linq;
-using System.Web.Mvc;
-using HtmlAgilityPack;
-using Fizzler.Systems.HtmlAgilityPack;
-using IOFile = System.IO.File;
-
-namespace Kendo.Controllers
-{
-    public class WebController : BaseController
-    {
-        //
-        // GET: /Web/
-
-        public ActionResult Index(string section, string example)
-        {
-            ViewBag.Description = LoadDescription(section);
-
-            return View(section + "/" + example);
-        }
-
-        protected string LoadDescription(string section) {
-            var path = Server.MapPath(string.Format(
-                "~/content/docs/kendo.ui.{0}.description.html", section)
-            );
-
-            if (IOFile.Exists(path)) {
-                return IOFile.ReadAllText(path);
-            }
-
-            return "";
-        }
-    }
-}
-=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -156,4 +119,3 @@ namespace Kendo.Controllers
         }
     }
 }
->>>>>>> Add source controller
