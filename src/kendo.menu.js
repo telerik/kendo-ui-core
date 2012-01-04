9722b350d34c96dcb568de157e4dc4b34f3377eb
Bundyo <bundyo@gmail.com>
2012-01-04 13:57:13 +0200

    Update menu to close its sub menus on click if no navigation. Fix the tests, add one.(cherry picked from commit 7ccb4f9)

--- src/kendo.menu.js
+++ src/kendo.menu.js
@@ -84,7 +84,7 @@
         LINK = "k-link",
         LAST = "k-last",
         CLOSE = "close",
-        CLICK = "click",
+        CLICK = touch ? "touchend" : "click",
         TIMER = "timer",
         FIRST = "k-first",
         IMAGE = "k-image",
@@ -97,6 +97,8 @@
         SLIDEINRIGHT = "slideIn:right",
         DEFAULTSTATE = "k-state-default",
         DISABLEDSTATE = "k-state-disabled",
+        groupSelector = ".k-group",
+        allItemsSelector = ".k-item",
         disabledSelector = ".k-item.k-state-disabled",
         itemSelector = ".k-item:not(.k-state-disabled)",
         linkSelector = ".k-item:not(.k-state-disabled) > .k-link",
@@ -276,6 +278,7 @@
          * @option {Animation} [animation.close] The animation that will be used when closing sub menus.
          * @option {String} [orientation] <"horizontal"> Root menu orientation.
          * @option {Boolean} [openOnClick] <false> Specifies that the root sub menus will be opened on item click.
+         * @option {Boolean} [closeOnClick] <true> Specifies that sub menus should close after item selection (provided they won't navigate).
          * @option {Number} [hoverDelay] <100> Specifies the delay in ms before the menu is opened/closed - used to avoid accidental closure on leaving.
          */
         init: function(element, options) {
@@ -306,13 +309,17 @@
 
             that.nextItemZIndex = 100;
 
-            element.delegate(disabledSelector, CLICK, false);
-
-            element.delegate(itemSelector, MOUSEENTER, proxy(that._mouseenter, that))
-                   .delegate(itemSelector, MOUSELEAVE, proxy(that._mouseleave, that))
+            element.delegate(disabledSelector, CLICK, false)
                    .delegate(itemSelector, CLICK, proxy(that._click , that));
 
-            element.delegate(linkSelector, MOUSEENTER + " " + MOUSELEAVE, that._toggleHover);
+            if (!touch) {
+                element.delegate(itemSelector, MOUSEENTER, proxy(that._mouseenter, that))
+                       .delegate(itemSelector, MOUSELEAVE, proxy(that._mouseleave, that))
+                       .delegate(linkSelector, MOUSEENTER + " " + MOUSELEAVE, that._toggleHover);
+            } else {
+                options.openOnClick = true;
+                element.delegate(linkSelector, "touchstart touchend", that._toggleHover);
+            }
 
             $(document).click(proxy( that._documentClick, that ));
             that.clicked = false;
@@ -359,6 +366,7 @@
             },
             orientation: "horizontal",
             openOnClick: false,
+            closeOnClick: true,
             hoverDelay: 100
         },
 
@@ -546,12 +554,12 @@
             element = $(element);
 
             var that = this,
-                parent = element.parentsUntil(that.element, ".k-item"),
+                parent = element.parentsUntil(that.element, allItemsSelector),
                 group = element.parent("ul");
 
             element.remove();
 
-            if (group && !group.children(".k-item").length) {
+            if (group && !group.children(allItemsSelector).length) {
                 var container = group.parent(".k-animation-container");
                 container.length ? container.remove() : group.remove();
             }
@@ -645,10 +653,10 @@
         },
 
         _toggleHover: function(e) {
-            var target = $(e.currentTarget);
+            var target = $(kendo.eventTarget(e)).closest(allItemsSelector);
 
             if (!target.parents("li." + DISABLEDSTATE).length) {
-                target.toggleClass("k-state-hover", e.type == MOUSEENTER);
+                target.toggleClass("k-state-hover", e.type == MOUSEENTER || e.type == "touchstart");
             }
         },
 
@@ -672,7 +680,7 @@
         _mouseenter: function (e) {
             var that = this,
                 element = $(e.currentTarget),
-                hasChildren = (element.children(".k-animation-container").length || element.children(".k-group").length);
+                hasChildren = (element.children(".k-animation-container").length || element.children(groupSelector).length);
 
             if (!that.options.openOnClick || that.clicked) {
                 if (!contains(e.currentTarget, e.relatedTarget) && hasChildren) {
@@ -694,7 +702,7 @@
         _mouseleave: function (e) {
             var that = this,
                 element = $(e.currentTarget),
-                hasChildren = (element.children(".k-animation-container").length || element.children(".k-group").length);
+                hasChildren = (element.children(".k-animation-container").length || element.children(groupSelector).length);
 
             if (!that.options.openOnClick && !contains(e.currentTarget, e.relatedTarget) && hasChildren) {
                 if (that.trigger(CLOSE, { item: element[0] }) === false) {
@@ -704,9 +712,11 @@
         },
 
         _click: function (e) {
-            var that = this, openHandle;
-
-            var element = $(e.currentTarget);
+            var that = this, openHandle,
+                target = $(kendo.eventTarget(e)),
+                link = target.closest("." + LINK),
+                href = link.attr("href"),
+                element = target.closest(allItemsSelector);
 
             if (element.hasClass(DISABLEDSTATE)) {
                 e.preventDefault();
@@ -718,7 +728,10 @@
 
             e.handled = true;
 
-            if (!element.parent().hasClass(MENU) || (!that.options.openOnClick && !touch)) {
+            if (that.options.closeOnClick && !(href && href.length > 0) && !element.children(groupSelector + ",.k-animation-container").length)
+                that.close(link.parentsUntil(that.element, allItemsSelector));
+
+            if ((!element.parent().hasClass(MENU) || !that.options.openOnClick) && !touch) {
                 return;
             }
 
