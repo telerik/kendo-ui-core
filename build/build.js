e9ed3fcbe3f7b038f4da92b0524a3799b585c1c5
Petyo Ivanov <underlog@gmail.com>
2012-01-10 16:45:50 +0200

    Remove deprecated build node executables

--- build/build.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-console.log("Running build/build.js is obsolete");
-console.log("Please execute `jake bundles` instead. If you don't have jake installed, please run `sudo npm install -g jake`");
-console.log("If you are in Arch linux, add the following lines to your .zshrc: ");
-console.log("---------------------------------------------------");
-console.log("export NODE_PATH=/usr/lib/node_modules:$NODE_PATH");
-console.log("---------------------------------------------------");
