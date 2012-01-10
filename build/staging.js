d29b39274d876cdf6755057e26c7bbd43dc8183b
Petyo Ivanov <underlog@gmail.com>
2012-01-10 16:29:48 +0200

    Remove obsolete scripts

--- build/staging.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-var path = require("path"),
-    kendoExamples = require("./examples"),
-    outputPath = process.argv[2] || path.join("deploy", "staging");
-
-kendoExamples.buildStaging(outputPath);
