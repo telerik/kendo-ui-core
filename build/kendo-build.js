6710fcd6a466eb36cd09e73d2d7e98a00c2ab8d0
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-01-12 11:55:40 +0200

    Add CDN bundle and use for staging too

--- build/kendo-build.js
+++ build/kendo-build.js
@@ -269,13 +269,15 @@ function deployStyles(stylesRoot, outputRoot, header, compress) {
     copyDirSyncRecursive(stylesRoot, outputRoot, true, filesRegex);
     themes.buildThemes(stylesRoot, outputRoot);
     processFilesRecursive(outputRoot, stylesRegex, function(fileName) {
-        var css = stripBOM(readText(fileName)),
-            output = compress ? cssmin(css) : css;
+        if (fileName.indexOf(".min.") === -1) {
+            var css = stripBOM(readText(fileName)),
+                output = compress ? cssmin(css) : css;
 
-        writeText(fileName, header + output);
+            writeText(fileName, header + output);
 
-        if (compress) {
-            fs.renameSync(fileName, fileName.replace(".css", ".min.css"));
+            if (compress) {
+                fs.renameSync(fileName, fileName.replace(".css", ".min.css"));
+            }
         }
     });
 }
