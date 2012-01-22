7baf9c7be218f81bf8b746f2d29d2f2ca857cecb
Petyo Ivanov <underlog@gmail.com>
2012-01-22 13:48:02 +0200

    Introcude "elastic" draggable option

--- src/kendo.mobile.scrollview.js
+++ src/kendo.mobile.scrollview.js
@@ -187,9 +187,9 @@
             });
 
             that.draggable = new mobile.Draggable({
-                ignoreY: true,
                 minY: 0,
                 swipe: that.swipe,
+                elastic: true,
                 move: that.move
             });
 
@@ -222,7 +222,7 @@
 
             that.minSnap = - (pages - 1) * width;
             that.maxSnap = 0;
-            that.draggable.options.minX = width - scrollWidth;
+            that.draggable.minX = width - scrollWidth;
 
             for (var idx = 0; idx < pages; idx ++) {
                 pageHTML += "<li/>";
