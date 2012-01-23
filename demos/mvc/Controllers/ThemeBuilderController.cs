c7d8b7599e66b9cfc42b3b9e393d43897b04d7a8
Alex Gyoshev <alex@gyoshev.net>
2012-01-23 10:27:03 +0200

    Fix ThemeBuilder page in MVC demos

--- demos/mvc/Controllers/ThemeBuilderController.cs
+++ demos/mvc/Controllers/ThemeBuilderController.cs
@@ -15,6 +15,15 @@ namespace Kendo.Controllers
             ViewBag.DeployRoot = "http://themebuilder.kendoui.com";
             ViewBag.Title = "ThemeBuilder";
 
+#if DEBUG
+            ViewBag.Debug = true;
+#else
+            ViewBag.Debug = false;
+#endif
+
+            ViewBag.scripts = Kendo.Models.ScriptGroups.All;
+            ViewBag.styles = Kendo.Models.StyleGroups.All;
+
             return View();
         }
 
