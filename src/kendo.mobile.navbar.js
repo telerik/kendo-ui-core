e4d90f36ebe2326e10724dd74a917fc34492d7f0
Petyo Ivanov <underlog@gmail.com>
2012-01-23 15:23:25 +0200

    Proof-read navbar docs

--- src/kendo.mobile.navbar.js
+++ src/kendo.mobile.navbar.js
@@ -2,8 +2,8 @@
     /**
     * @name kendo.mobile.ui.NavBar.Description
     *
-    * @section mobile NavBar widget is iused inside a mobile view or layout header element to display an application navigation bar.
-    * The mobile NavBar can show the current view title in the center, and optionally some additional left and right aligned elements (back button, settings button, etc.).
+    * @section <p>The Kendo mobile NavBar widget is used inside a mobile View or Layout Header element to display an application navigation bar.
+    * The mobile NavBar can show the current view title in the center, and optionally some additional left and right aligned widgets (back button, settings button, etc.).</p>
     *
     * <h3>Getting Started</h3>
     * The Kendo mobile Application will automatically initialize the mobile NavBar for every element with <code>role</code> data attribute set to <code>navbar</code> present in the views/layouts markup.
@@ -15,11 +15,12 @@
     * @exampleTitle Initialize Kendo mobile NavBar using a jQuery selector
     * @example
     * var navbar = $("#navbar").kendoMobileNavBar();
-    * @section <h3>Aligning widgets inside the navbar</h3>
+    * @section <h3>Aligning Widgets Inside the NavBar</h3>
     *
-    * After initialization, the mobile NavBar positions elements based on the specified <code>align</code> data attribute (either <code>left</code> or <code>right</code>). By default, elements without any align are centered.
+    * <p>After initialization, the mobile NavBar positions its child elements based on the specified <code>align</code> data attribute (either <code>left</code> or <code>right</code>).
+    * By default, elements without any align are centered.</p>
     *
-    * @exampleTitle Use the <code>align</code> data attribute to specify the elements position inside the navbar
+    * @exampleTitle Use the <code>align</code> data attribute to specify the elements position inside the NavBar
     * @example
     * <div data-role="navbar">
     *   <a data-role="back-button" data-align="left">Back</a>
@@ -27,8 +28,14 @@
     *   <a data-role="button" data-align="right">About</a>
     * </div>
     *
-    * @section <h3>Automatically update navbar title based on current view's title</h3>
-    * If an element with <code>role</code> data attribute set to <code>view-title</code> is present inside the mobile NavBar, the Kendo mobile Application instance will update its text when changing views to on the current view's title.  The view title is specified by setting the <code>title</code> data attribute of the view element. This feature is particularly useful if the mobile NavBar is inside a layout.
+    * @section <h3>Automatically Update NavBar Title Based on Current View's Title</h3>
+    *
+    * <p>If an element with <code>role</code> data attribute set to <code>view-title</code> is present inside the mobile NavBar,
+    * the Kendo mobile Application instance will update its text to the current View's title when changing views.
+    * The View title is specified by setting the <code>title</code> data attribute of the View element. </p>
+    *
+    * <p>This feature is particularly useful if the mobile NavBar is inside a layout.</p>
+    *
     * @exampleTitle Use the <code>view-title</code> data attribute to auto-update the mobile NavBar title
     * @example
     * <div data-role="layout" data-id="foo">
@@ -42,15 +49,15 @@
     * <div data-role="view" data-layout="foo" data-title="bar"> ... </div>
     * <div data-role="view" data-layout="foo" data-title="baz"> ... </div>
     *
-    * @section <h3>Customizing mobile NavBar appearance</h3>
-    * Kendo Mobile NavBar color can be customized by simply setting its background-color (either inline or by using a CSS selector with specificity of 20+.
-    * @exampleTitle Initialize a green Kendo mobile NavBar
+    * @section <h3>Customizing Mobile NavBar Appearance</h3>
+    * <p>The mobile NavBar background color can be customized by setting its background-color CSS property either inline or using a CSS selector with specificity of 20+.
+    * Different platforms can be styled separately with their respective root classes. </p>
+    *
+    * @exampleTitle Green Kendo mobile NavBar
     * @example
     * <div data-role="navbar" style="background-color: green">My View Title</div>
     *
-    * @section
-    * You can target platforms separately with their respective root classes.
-    * @exampleTitle Initialize a green Kendo mobile NavBar in iOS and a red one in Android
+    * @exampleTitle Green Kendo mobile NavBar in iOS and a red one in Android
     * @example
     * <style>
     *     .km-ios .checkout { background-color: green; }
@@ -91,16 +98,16 @@
         },
 
         /**
-        * Update the title element text. The title element is specified by setting the <code>role</code> data attribute to <code>view-title</code>
+        * Update the title element text. The title element is specified by setting the <code>role</code> data attribute to <code>view-title</code>.
         * @param {String} value The text of title
         * @example
-        * &lt;div data-role="navbar" id="foo"&gt;
-        *     &lt;span data-role="view-title"&gt;&lt;/span&gt;
-        * &lt;/div&gt;
+        * <div data-role="navbar" id="foo">
+        *     <span data-role="view-title"></span>
+        * </div>
         *
-        * &lt;script&gt;
+        * <script>
         *   $("#foo").data("kendoMobileNavBar").title("Foo");
-        * &lt;/script&gt;
+        * </script>
         */
         title: function(value) {
             this.element.find(roleSelector("view-title")).text(value);
