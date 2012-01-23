df373a5bbc040348cc44766085ee6de248c8a2fc
Petyo Ivanov <underlog@gmail.com>
2012-01-23 11:43:18 +0200

    Proof-read button documentation

--- src/kendo.mobile.button.js
+++ src/kendo.mobile.button.js
@@ -17,10 +17,10 @@
     * @section The Button widget navigates between mobile Application views when pressed.
     *
     * <h3>Getting Started</h3>
-    * The Kendo mobile Application will automatically initialize a mobile Button for every element with <code>role</code> data attribute set to <code>button</code> present in the views/layouts markup.
-    * Alternatively, it can be initialized using a jQuery selector. The button element may be either <code>A</code> or <code>BUTTON</code> element.
+    * <p>The Kendo mobile Application will automatically initialize a mobile Button for every element with <code>role</code> data attribute set to <code>button</code> present in the views/layouts' markup.
+    * Alternatively, it can be initialized using a jQuery selector. The button element may be either <code>A</code> or <code>BUTTON</code>. </p>
     *
-    * @exampleTitle Initialize Kendo mobile Button based on role data attribute.
+    * @exampleTitle Initialize Kendo mobile Button based on role data attribute
     * @example
     * <a href="#foo" data-role="button">Foo</a>
     *
@@ -30,26 +30,30 @@
     *
     * @section
     *
-    * <h3>Customizing mobile Button appearance</h3>
-    * Every Kendo Mobile Button color can be customized by simply setting its background-color (either inline or by using a CSS selector with specificity of 20+.
-    * @exampleTitle Initialize a green Kendo mobile Button
+    * <h3>Customizing Mobile Button Appearance</h3>
+    * <p>The Kendo mobile Button color can be customized by setting its <code>background-color</code> CSS property inline or by using a CSS selector with specificity of 20+.
+    * You can target platforms separately with their respective root CSS classes.</p>
+    *
+    * @exampleTitle Green Button
     * @example
     * <a href="#foo" data-role="button" style="background-color: green">Foo</a>
     *
     * @section
-    * You can target platforms separately with their respective root classes.
-    * @exampleTitle Initialize a green Kendo mobile Button in iOS and a red one in Android
+    *
+    * @exampleTitle Green Kendo mobile Button in iOS and a red one in Android
     * @example
     * <style>
     *     .km-ios .checkout { background-color: green; }
     *     .km-android .checkout { background-color: red; }
     * </style>
+    *
     * <a href="#foo" data-role="button" class="checkout">Foo</a>
     *
     * @section
     * <h3>Button icons</h3>
-    * A button icon can be set in two ways - either by adding an <code>img</code> element inside the <code>a</code> element, or by setting an <code>icon</code> data attribute to the <code>a</code> element.
-    * Kendo mobile comes out of the box with several ready to use icons:
+    * <p>A Button icon can be set in two ways - either by adding an <code>img</code> element inside the Button element,
+    * or by setting an <code>icon</code> data attribute to the Button element.</p>
+    * <p>KendoUI Mobile comes out of the box with several ready to use icons:</p>
     *
     * <ul id="icon-list">
     *   <li title=".km-about"><span class="km-icon km-about"></span>about</li>
@@ -87,12 +91,16 @@
     *   <li title=".km-trash"><span class="km-icon km-trash"></span>trash</li>
     * </ul>
     *
-    * Additional icons may be added by defining the respective CSS class. If the <code>icon</code> data attribute is set to <code>custom</code>, the tab will receive <code>km-custom</code> CSS class.
-    * @exampleTitle Define custom button icon.
+    * <p>Additional icons may be added by defining the respective CSS class.
+    * If the <code>icon</code> data attribute is set to <code>custom</code>, the tab will receive <code>km-custom</code> CSS class.</p>
+    *
+    * @exampleTitle Define custom button icon
     * @example
+    * <style>
     * .km-custom {
-    *   background-image: ...
+    *   background-image: url("foo.jpg");
     * }
+    * </style>
     *
     * <div data-role="button">
     *   <a href="#index" data-icon="custom">Home</a>
@@ -188,7 +196,7 @@
     * @name kendo.mobile.ui.BackButton.Description
     * @section The BackButton widget navigates to the previous mobile View when pressed.
     *
-    * @exampleTitle Initialize Kendo mobile BackButton based on role data attribute.
+    * @exampleTitle Initialize Kendo mobile BackButton based on role data attribute
     * @example
     * <a data-role="back-button">Foo</a>
     *
@@ -216,9 +224,9 @@
 
     /**
     * @name kendo.mobile.ui.DetailButton.Description
-    * @section The DetailButton widget navigates to a mobile view when pressed.
+    * @section The DetailButton widget navigates to a mobile View when pressed.
     *
-    * @exampleTitle Initialize Kendo mobile DetailButton based on role data attribute.
+    * @exampleTitle Initialize Kendo mobile DetailButton based on role data attribute
     * @example
     * <a data-role="detail-button">Foo</a>
     *
