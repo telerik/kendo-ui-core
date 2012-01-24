c7efd8293c7d01cb355a3b3bf1ca18ecf199985c
Petyo Ivanov <underlog@gmail.com>
2012-01-24 09:03:37 +0200

    Add sushi to MVC demos

---
+++ demos/mvc/Controllers/IntegrationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Kendo.Controllers
+{
+    public class IntegrationController : BaseController
+    {
+        //
+        // GET: /m
+        public ActionResult Sushi()
+        {
+#if DEBUG
+            ViewBag.Debug = true;
+#else
+            ViewBag.Debug = false;
+#endif
+            return View();
+        }
+    }
+}
