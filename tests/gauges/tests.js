6b666fbc28e85a6996aef762f71beb3779191c24
Alex Gyoshev <alex@gyoshev.net>
2012-02-17 18:00:05 +0200

    Add GaugePlotArea tests to suite

--- tests/gauges/tests.js
+++ tests/gauges/tests.js
@@ -2,5 +2,9 @@ tests.push(
     {
         page: "gauges/radialscale.html",
         title: "Radial Scale"
+    },
+    {
+        page: "gauges/gaugeplotarea.html",
+        title: "PlotArea"
     }
 );
