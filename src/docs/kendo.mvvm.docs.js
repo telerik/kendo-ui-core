43fccde73af8ac9c2dcac1d18e23edd0b4fbf772
Atanas Korchev <akorchev@gmail.com>
2012-02-28 10:27:43 +0200

    Added description to the MVVM examples

--- src/docs/kendo.mvvm.docs.js
+++ src/docs/kendo.mvvm.docs.js
@@ -7,43 +7,43 @@
 
     /** @name kendo.ui.MVVM.Description
     * @section
-    *  <p>This is the MVVM </p>
+    *  <p>
+    *       Model View ViewModel (MVVM) is a design pattern which helps developers separate the Model from the View. The View-Model part of MVVM is responsible for
+    *       exposing the data objects from the Model in such a way that those objects are easily consumed in the View.
+    *  </p>
+    *  <p>
+    *       Kendo MVVM is an implementation of the MVVM pattern which is seamlessly integrated with the rest of the Kendo framework (widgets and DataSource).
+    *  </p>
+    *  <h3>Getting Started</h3>
+    *  @exampleTitle Binding HTML View to a View-Model
+    *  @example
+    *  <!-- View -->
+    *  <div id="view">
+    *    <!-- The value of the INPUT element is bound to the "firstName" field of the View-Model.
+    *    When the value changes so will the "firstName" field and vice versa.  -->
+    *    <label>First Name:<input data-bind="value: firstName" /></label>
+    *    <!-- The value of the INPUT element is bound to the "lastName" field of the View-Model.
+    *    When the value changes so will the "lastName" field and vice versa.   -->
+    *    <label>Last Name:<input data-bind="value: lastName" /></label>
+    *    <!-- The click event of the BUTTON element is bound to the "displayGreeting" method of the View-Model.
+    *    When the user clicks the button the "displayGreeting" method will be invoked.  -->
+    *    <button data-bind="click: displayGreeting">Display Greeting</button>
+    *  </div>
+    *  <script>
+    *    // View-Model
+    *    var viewModel = {
+    *       firstName: "John",
+    *       lastName: "Doe",
+    *       displayGreeting: function() {
+    *           // Get the current values of "firstName" and "lastName"
+    *           var firstName = this.get("firstName");
+    *           var lastName = this.get("lastName");
+    *
+    *           alert("Hello, " + firstName + " " + lastName + "!!!");
+    *       }
+    *    };
+    *    // Bind the View to the View-Model
+    *    kendo.bind($("#view"), viewModel);
+    *  </script>
     */
-
-    var Binding = /** @lends kendo.ui.MVVM.prototype */ {
-        /**
-        * @constructs
-        * @extends kendo.Observable
-        * @param {DomElement} source Explanation
-        * @param {String} path Explanation
-        */
-
-        init: function() {
-            //describe events using this patter.
-            //
-            /**
-             * Fires when the chart has received data from the data source
-             * and is about to render it.
-             * @name kendo.ui.Chart#dataBound
-             * @event
-             * @param {Event} e
-             * @example
-             * function onDataBound(e) {
-             *     // Series data is now available
-             * }
-             */
-        },
-
-        //Describe methods using this patter.
-        /**
-         * Reloads the data and repaints the chart.
-         * @example
-         * var chart = $("#chart").data("kendoChart");
-         *
-         * // refreshes the chart
-         * chart.refresh();
-         */
-        refresh: function() { },
-    }
-
 })();
