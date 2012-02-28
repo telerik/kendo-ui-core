2943238988b605b49ccfc30607b0312ceaa0aa56
Atanas Korchev <akorchev@gmail.com>
2012-02-28 12:06:46 +0200

    MVVM documentation - kendo.bind and kendo.unbind

--- src/docs/kendo.mvvm.docs.js
+++ src/docs/kendo.mvvm.docs.js
@@ -8,7 +8,7 @@
     /** @name kendo.ui.MVVM.Description
     * @section
     *  <p>
-    *       Model View ViewModel (MVVM) is a design pattern which helps developers separate the Model from the View. The View-Model part of MVVM is responsible for
+    *       Model View ViewModel (<a href="http://en.wikipedia.org/wiki/Model_View_ViewModel">MVVM</a>) is a design pattern which helps developers separate the Model from the View. The View-Model part of MVVM is responsible for
     *       exposing the data objects from the Model in such a way that those objects are easily consumed in the View.
     *  </p>
     *  <p>
@@ -46,4 +46,39 @@
     *    kendo.bind($("#view"), viewModel);
     *  </script>
     */
+
+    var kendo = { /** @lends kendo.ui.MVVM.prototype */
+        /**
+         * Binds a tree of HTML elements to a View-Model
+         * @param {String|Selector|Node} element
+         * The root element(s) from which the binding starts. Can be a valid jQuery string selector, a DOM element or a jQuery object.
+         * All child elements are traversed.
+         * @param {Object} viewModel
+         * The View-Model which the elements are bound to.
+         * @param {Object} namespace
+         * Optional namespace too look in when instantiating Kendo widgets. The possible values are kendo.ui and kendo.mobile.ui. The default value is kendo.ui
+         * @exampleTitle Binding all children of the BODY to a View-Model
+         * @example
+         * var viewModel = {
+         *      firstName: "John",
+         *      lastName: "Bar"
+         * };
+         *
+         * kendo.bind($("body"), viewModel);
+         */
+        bind: function(element, viewModel, namespace) {
+        },
+
+        /**
+         * Unbinds a tree of HTML elements from a View-Model.
+         * @param {String|Selector|Node} element
+         * The root element(s) from which the unbinding starts. Can be a valid jQuery string selector, a DOM element or a jQuery object.
+         * All child elements are traversed.
+         * @exampleTitle Unbinding all children of the BODY to a View-Model
+         * @example
+         * kendo.unbind($("body"));
+         */
+        unbind: function(element) {
+        }
+    }
 })();
