f29459f4f3efcfa1c9a2a054d8a04f3c2c918021
Hristo Germanov <hristo.germanov@telerik.com>
2012-03-22 14:48:51 +0200

    Update gauge animation.

--- src/kendo.dataviz.gauge.js
+++ src/kendo.dataviz.gauge.js
@@ -32,7 +32,7 @@
         uniqueId = dataviz.uniqueId;
 
     // Constants ==============================================================
-    var ANGULAR_SPEED = 250,
+    var ANGULAR_SPEED = 150,
         ARROW = "arrow",
         ARROW_POINTER = "arrowPointer",
         BAR_INDICATOR = "barIndicator",
