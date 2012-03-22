de1d73be7cb4d9af968bd6a9ff576473eb3b7f29
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2012-03-22 12:25:17 +0200

    Change local virtualization to generate data on chunks

--- demos/mvc/content/shared/js/people.js
+++ demos/mvc/content/shared/js/people.js
@@ -28,3 +28,39 @@ function createRandomData(count) {
     }
     return data;
 }
+
+function generatePeople(itemCount, callback) {
+    var data = [],
+        delay = 25,
+        interval = 500,
+        starttime;
+
+    var now = new Date();
+    setTimeout(function() {
+        starttime = +new Date();
+        do {
+            var firstName = firstNames[Math.floor(Math.random() * firstNames.length)],
+                lastName = lastNames[Math.floor(Math.random() * lastNames.length)],
+                city = cities[Math.floor(Math.random() * cities.length)],
+                title = titles[Math.floor(Math.random() * titles.length)],
+                birthDate = birthDates[Math.floor(Math.random() * birthDates.length)],
+                age = now.getFullYear() - birthDate.getFullYear();
+
+            data.push({
+                Id: data.length + 1,
+                FirstName: firstName,
+                LastName: lastName,
+                City: city,
+                Title: title,
+                BirthDate: birthDate,
+                Age: age
+            });
+        } while(data.length < itemCount && +new Date() - starttime < interval);
+
+        if (data.length < itemCount) {
+            setTimeout(arguments.callee, delay);
+        } else {
+            callback(data);
+        }
+    }, delay);
+}
