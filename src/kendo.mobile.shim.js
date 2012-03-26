4013bb6f3da942b12fb59c4eae44966ea8e53df6
Petyo Ivanov <underlog@gmail.com>
2012-03-26 18:41:36 +0300

    Shim proof of concept

---
+++ src/kendo.mobile.shim.js
@@ -0,0 +1,55 @@
+
+(function($, undefined) {
+    var kendo = window.kendo,
+        ui = kendo.mobile.ui,
+        Popup = kendo.ui.Popup,
+        Widget = ui.Widget;
+
+    var Shim = Widget.extend(/** @lends kendo.mobile.ui.Shim.prototype */{
+        /**
+        * @constructs
+        * @extends kendo.mobile.ui.Widget
+        * @param {DomElement} element DOM element
+        */
+        init: function(element, options) {
+            var that = this;
+
+            Widget.fn.init.call(that, element, options);
+            element = that.element;
+        },
+
+        viewInit: function(view) {
+            var that = this,
+                element = that.element,
+                appElement = view.application.element,
+                shim = $('<div class="km-shim" />').appendTo(appElement);
+
+            shim.append(this.element).hide();
+
+            that.shim = shim;
+
+            that.popup = new Popup(element, {
+                anchor: shim,
+                origin: "bottom left",
+                position: "bottom left",
+                animation: { open: { effects: "slideIn:up", duration: 2000 } }
+            });
+        },
+
+        show: function() {
+            this.shim.show();
+            this.popup.open();
+        },
+
+        hide: function() {
+            this.shim.hide();
+            this.popup.close();
+        },
+
+        options: {
+            name: "Shim"
+        },
+    });
+
+    ui.plugin(Shim);
+})(jQuery);
