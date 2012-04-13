766fca0c8af6e4c2c539b9d5db50be33376779cc
George Krystev <ggkrustev@yahoo.com>
2012-04-13 18:30:53 +0300

    Add open method to the DateTimePicker

---
+++ tests/datetimepicker/tests.js
@@ -0,0 +1,10 @@
+tests.push(
+    {
+        page: "datetimepicker/initialization.html",
+        title: "datetimepicker initialization"
+    },
+    {
+        page: "datetimepicker/api.html",
+        title: "datetimepicker api"
+    }
+);
