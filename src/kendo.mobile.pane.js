f64ae09bb18dbd6519b3488248f392c63d37a9a5
Petyo Ivanov <underlog@gmail.com>
2012-04-18 19:38:03 +0300

    TDD progress on pane design

---
+++ src/kendo.mobile.pane.js
@@ -0,0 +1,51 @@
+(function($, undefined) {
+    var kendo = window.kendo,
+        ui = kendo.mobile.ui,
+        Widget = ui.Widget,
+        View = ui.View;
+
+    /**
+     * @name kendo.mobile.ui.Pane.Description
+     *
+     */
+    var Pane = View.extend(/** @lends kendo.mobile.ui.Pane.prototype */{
+        /**
+        * @constructs
+        * @extends kendo.mobile.ui.Widget
+        * @param {DomElement} element DOM element
+        */
+        init: function(element, options) {
+            var that = this;
+
+            Widget.fn.init.call(that, element, options);
+
+            element = that.element;
+
+            element.addClass("km-pane");
+
+            that.viewElements = element.children(kendo.roleSelector("view splitview")).hide();
+        },
+
+        displayInitial: function(url) {
+            var that = this,
+                viewSelector = url || that.options.initial,
+                viewElement = viewSelector ? that.viewElements.filter(viewSelector) : that.viewElements.first();
+
+            if (viewElement[0]) {
+                kendo.initWidget(element, {}, kendo.mobile.ui);
+            } else {
+
+            }
+            viewElement.show();
+        },
+
+        options: {
+            name: "Pane"
+        }
+
+        _createView: function(element) {
+        }
+    });
+
+    ui.plugin(Pane);
+})(jQuery);
