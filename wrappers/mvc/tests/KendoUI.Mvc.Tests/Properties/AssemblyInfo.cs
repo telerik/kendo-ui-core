91fd16509ef2ea5f400ec612e5d100cf88b69a9e
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-04-19 14:26:22 +0300

    Add empty extensions and tests projects

---
+++ wrappers/mvc/tests/KendoUI.Mvc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KendoUI.Mvc.Tests")]
+[assembly: AssemblyDescription("KendoUI Extensions for ASP.NET MVC - Unit Tests")]
+[assembly: AssemblyCompany("Telerik")]
+[assembly: AssemblyProduct("KendoUI.Mvc.Tests")]
+[assembly: AssemblyCopyright("Copyright © Telerik 2012")]
+
+[assembly: Guid("9cd757eb-dd8a-4c3f-9b6d-2473826a3a52")]
