e1957db0bb144b8d4f9301fd4257d5a0bfba81d5
Petyo Ivanov <underlog@gmail.com>
2012-04-20 10:34:42 +0300

    Loader supports event blocking
    Call transition when starting, transitionDone after the transition is over.

--- src/kendo.mobile.loader.js
+++ src/kendo.mobile.loader.js
@@ -1,8 +1,8 @@
-
 (function($, undefined) {
     var kendo = window.kendo,
         ui = kendo.mobile.ui,
-        Widget = ui.Widget;
+        Widget = ui.Widget,
+        CAPTURE_EVENTS = ["touchstart", "touchend", "touchmove", "mousedown", "mousemove", "mouseup", "click"];
 
     /**
      * @name kendo.mobile.ui.Loader.Description
@@ -19,6 +19,10 @@
                 element = $('<div class="km-loader"><span class="km-loading km-spin"></span></div>');
 
             Widget.fn.init.call(that, element, options);
+
+            that.container = container;
+
+            that._attachCapture();
 
             element.append(that.options.loading).hide().appendTo(container);
         },
@@ -55,6 +59,29 @@
             var that = this;
             clearTimeout(that._loading);
             that.element.hide();
+        },
+
+        transition: function() {
+            this.transitioning = true;
+        },
+
+        transitionDone: function() {
+            this.transitioning = false;
+        },
+
+        _attachCapture: function() {
+            var that = this;
+            that.transitioning = false;
+
+            function capture(e) {
+                if (that.transitioning) {
+                    e.stopPropagation();
+                }
+            }
+
+            for (var i = 0; i < CAPTURE_EVENTS.length; i ++) {
+                that.container[0].addEventListener(CAPTURE_EVENTS[i], capture, true);
+            }
         }
     });
 
