f168c97e8cd98cc37d18f754df24e83ad3d235c9
Petyo Ivanov <underlog@gmail.com>
2012-04-21 14:18:25 +0300

    Extract view and layout tests

---
+++ tests/mobile/view/tests.js
@@ -0,0 +1,10 @@
+tests.push(
+    {
+        page: "../mobile/view/view.html",
+        title: "mobile view"
+    },
+    {
+        page: "../mobile/view/layout.html",
+        title: "mobile listview layout"
+    }
+);
