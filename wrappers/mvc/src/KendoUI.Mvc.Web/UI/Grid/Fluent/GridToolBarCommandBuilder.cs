13d2305253d4407093315b20d3a623f72d78ba99
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-04-23 17:41:35 +0300

    Remove obsolete license headers

--- wrappers/mvc/src/KendoUI.Mvc.Web/UI/Grid/Fluent/GridToolBarCommandBuilder.cs
+++ wrappers/mvc/src/KendoUI.Mvc.Web/UI/Grid/Fluent/GridToolBarCommandBuilder.cs
@@ -1,7 +1,3 @@
-// This source is subject to the GNU General Public License, version 2
-// See http://www.gnu.org/licenses/gpl-2.0.html. 
-// All other rights reserved.
-
 namespace KendoUI.Mvc.UI.Fluent
 {
     public class GridToolBarCommandBuilder<T> : GridToolBarCommandBuilderBase<T, GridToolBarCommandBase<T>, GridToolBarCommandBuilder<T>> where T : class
--- wrappers/mvc/src/KendoUI.Mvc.Web/UI/Grid/Fluent/GridToolBarSubmitChangesCommandBuilder.cs
+++ wrappers/mvc/src/KendoUI.Mvc.Web/UI/Grid/Fluent/GridToolBarSubmitChangesCommandBuilder.cs
@@ -1,7 +1,3 @@
-// This source is subject to the GNU General Public License, version 2
-// See http://www.gnu.org/licenses/gpl-2.0.html. 
-// All other rights reserved.
-
 namespace KendoUI.Mvc.UI.Fluent
 {
     public class GridToolBarSubmitChangesCommandBuilder<T> : GridToolBarCommandBuilderBase<T, GridToolBarSubmitChangesCommand<T>, GridToolBarSubmitChangesCommandBuilder<T>> where T : class
