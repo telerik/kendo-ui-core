efd723b5d56bed4ec29c76c5773f458f6cd68829
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-04-23 14:51:33 +0300

    Remove MVC wrappers attempt #1

--- wrappers/mvc/src/UI/IClientSerializable.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KendoUI.Mvc.UI
-{
-    public interface IClientSerializable
-    {
-        void SerializeTo(string key, IClientSideObjectWriter writer);
-    }
-}
--- wrappers/mvc/src/UI/IInputComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KendoUI.Mvc.UI
-{
-    using System;
-
-    public interface IInputComponent<T> : IViewComponent where T : struct
-    {
-        Nullable<T> Value { get; set; }
-    }
-}
