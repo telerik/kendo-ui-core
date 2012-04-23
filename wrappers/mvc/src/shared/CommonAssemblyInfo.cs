fddaa9ef286b18cb3da856c4106d4079c66555e0
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-04-23 15:15:58 +0300

    Rename project to KendoUI.Mvc.Web. Add KendoUI.Mvc solution. Remove TFS bindings.

---
+++ wrappers/mvc/src/shared/CommonAssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿// (c) Copyright Telerik Corp. 
+// This source is subject to the Microsoft Public License. 
+// See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL. 
+// All other rights reserved.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Telerik")]
+[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+#if MVC1
+[assembly: AssemblyVersion("2012.2.423.135")]
+[assembly: AssemblyFileVersion("2012.2.423.135")]
+#endif
+#if MVC2
+[assembly: AssemblyVersion("2012.2.423.235")]
+[assembly: AssemblyFileVersion("2012.2.423.235")]
+#endif
+#if MVC3
+[assembly: AssemblyVersion("2012.2.423.340")]
+[assembly: AssemblyFileVersion("2012.2.423.340")]
+#endif
+[assembly: AllowPartiallyTrustedCallers]
