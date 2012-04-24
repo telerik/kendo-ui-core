8e706977f643fcda0b1ab19d7f66e04aa84b6aa6
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-04-24 14:59:15 +0300

    Standardize assembly attributes accross projects

--- wrappers/mvc/src/shared/CommonAssemblyInfo.cs
+++ wrappers/mvc/src/shared/CommonAssemblyInfo.cs
@@ -1,14 +1,15 @@
+using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
 
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Telerik")]
-[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyCopyright("Copyright © Telerik 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 #if MVC1
 [assembly: AssemblyVersion("2012.2.423.135")]
 [assembly: AssemblyFileVersion("2012.2.423.135")]
@@ -20,5 +21,6 @@ using System.Security;
 #if MVC3
 [assembly: AssemblyVersion("2012.2.423.340")]
 [assembly: AssemblyFileVersion("2012.2.423.340")]
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
 #endif
 [assembly: AllowPartiallyTrustedCallers]
