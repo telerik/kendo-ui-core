d73ee11fa47e71678c3e3455bb6cb7bcc74b8434
Petyo Ivanov <underlog@gmail.com>
2012-04-26 18:25:42 +0300

    Fix for listview e.button parameter
    Fixes #146.

--- src/kendo.core.js
+++ src/kendo.core.js
@@ -2237,7 +2237,7 @@ function pad(number) {
         },
 
         roleSelector: function(role) {
-            return "[" + kendo.attr("role") + "=" + role + "]";
+            return role.replace(/(\S+)/g, "[" + kendo.attr("role") + "=$1],").slice(0, -1);
         },
 
         /** @ignore */
@@ -2571,5 +2571,16 @@ function pad(number) {
      */
     kendo.preventDefault = function(e) {
         e.preventDefault();
+    };
+
+    /**
+     * Retrieves the widget for a given element (if any)
+     */
+    kendo.widgetInstance = function(element, suite) {
+        var widget = suite.roles[element.data(kendo.ns + "role")];
+
+        if (widget) {
+            return element.data("kendo" + widget.fn.options.prefix + widget.fn.options.name);
+        }
     };
 })(jQuery);
