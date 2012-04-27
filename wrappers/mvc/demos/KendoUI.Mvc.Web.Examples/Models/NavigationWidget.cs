9382d070b7eaba0068e3296624c99419cda0087a
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-04-27 17:36:45 +0300

    Replace onlineOnly flag with offline nested settings.

--- wrappers/mvc/demos/KendoUI.Mvc.Web.Examples/Models/NavigationWidget.cs
+++ wrappers/mvc/demos/KendoUI.Mvc.Web.Examples/Models/NavigationWidget.cs
@@ -5,13 +5,18 @@ namespace KendoUI.Mvc.Web.Examples.Models
 {
     public class NavigationWidget
     {
+        public NavigationWidget()
+        {
+            Offline = new OfflineFilter();
+        }
+
         public string Name { get; set; }
         public string Text { get; set; }
         public string Documentation { get; set; }
         public string ThumbnailUrl { get; set; }
         public string SpriteCssClass { get; set; }
         public bool Expanded { get; set; }
-        public bool OnlineOnly { get; set; }
+        public OfflineFilter Offline { get; set; }
         public NavigationExample[] Items { get; set; }
     }
 }
