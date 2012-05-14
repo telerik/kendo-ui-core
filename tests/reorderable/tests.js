40761f971406deeb0051dbf6533cd39e006fb232
Nikolay Rusev <nikolay.rusev@telerik.com>
2012-05-14 14:08:30 +0300

    Rename reordable to reorderable

---
+++ tests/reorderable/tests.js
@@ -0,0 +1,10 @@
+tests.push(
+    {
+        page: "reorderable/reorderable.html",
+        title: "reorderable initialization"
+    },
+    {
+        page: "reorderable/lint.html",
+        title: "reorderable lint"
+    }
+);
