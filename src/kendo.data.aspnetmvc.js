aae6a0a208309656103152c5884ac1ae920cea6e
Atanas Korchev <akorchev@gmail.com>
2012-05-16 19:12:22 +0300

    Initial aspnetmvc-server data source implementation

---
+++ src/kendo.data.aspnetmvc.js
@@ -0,0 +1,33 @@
+(function ($, undefined) {
+   var kendo = window.kendo;
+   
+   function parameterMap(options) {
+       var result = {};
+
+       if (options.sort) {
+            result[this.options.prefix + "orderBy"] = $.map(options.sort, function(sort) {
+               return sort.field + "-" + sort.dir; 
+            }).join("~");
+       }
+
+       return result; 
+   }
+
+   kendo.data.transports["aspnetmvc-server"] = kendo.data.RemoteTransport.extend({
+        init: function(options) {
+            kendo.data.RemoteTransport.fn.init.call(this, $.extend(options, { parameterMap: $.proxy(parameterMap, this) } ));
+            this.options.prefix = "Grid-";
+        },
+        read: function(options) {
+            var url;
+            options = this.setup(options, "read");
+            url = options.url;
+
+            url = url + "?" + $.map(options.data, function(value, key) {
+                return key + "=" + value;
+            }).join("&");
+
+            location.href = url;
+        }
+   });
+})(jQuery);
--- wrappers/mvc/src/Kendo.Mvc/UI/DataSource/Transport.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/DataSource/Transport.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Web.Mvc;
 
 namespace Kendo.Mvc.UI
 {
