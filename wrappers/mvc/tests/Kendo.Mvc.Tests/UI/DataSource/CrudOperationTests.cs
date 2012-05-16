99c093d50ec8844f6be5576d6ff779a79b1b7f85
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2012-05-16 11:02:46 +0300

    Add builder for crudoperation

---
+++ wrappers/mvc/tests/Kendo.Mvc.Tests/UI/DataSource/CrudOperationTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Kendo.Mvc.Tests
+{
+    using Kendo.Mvc.UI;
+    using Xunit;
+    using Moq;
+    using System.Web.Mvc;
+
+    public class CrudOperationTests
+    {
+        [Fact]
+        public void ToJson_serialize_url()
+        {
+            const string expected = "bar/foo?baz=baz";         
+            var operation = new CrudOperation();
+
+            operation.Url = expected;
+
+            var result = operation.ToJson();
+
+            result["url"].ShouldEqual(expected);
+        }
+
+        [Fact]
+        public void ToJson_does_not_serialize_url_if_not_set()
+        {            
+            var operation = new CrudOperation();        
+
+            var result = operation.ToJson();
+
+            result.ContainsKey("url").ShouldBeFalse();
+        }
+    }
+}
---
+++ wrappers/mvc/tests/Kendo.Mvc.Tests/UI/DataSource/DataSourceBuilderTests.cs
@@ -0,0 +1,68 @@
+﻿namespace Kendo.Mvc.Tests
+{
+    using Xunit;
+    using Kendo.Mvc.UI;
+    using Kendo.Mvc.UI.Fluent;
+    using Moq;
+
+    public class DataSourceBuilderTests
+    {
+        private DataSource dataSource;
+        private DataSourceBuilder builder;
+
+        public DataSourceBuilderTests()
+        {
+            dataSource = new DataSource();
+            builder = new DataSourceBuilder(dataSource, TestHelper.CreateViewContext(), new Mock<IUrlGenerator>().Object);
+        }
+
+        [Fact]
+        public void PageSize_is_set_to_DataSource()
+        {
+            builder.PageSize(42);
+            dataSource.PageSize.ShouldEqual(42);
+        }
+
+        [Fact]
+        public void ServerPaging_is_set_to_DataSource()
+        {
+            builder.ServerPaging();
+            dataSource.ServerPaging.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ServerPaging_is_set_to_DataSource_if_set()
+        {
+            builder.ServerPaging(true);
+            dataSource.ServerPaging.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ServerSorting_is_set_to_DataSource()
+        {
+            builder.ServerSorting();
+            dataSource.ServerSorting.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ServerSorting_is_set_to_DataSource_if_set()
+        {
+            builder.ServerSorting(true);
+            dataSource.ServerSorting.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ServerFiltering_is_set_to_DataSource()
+        {
+            builder.ServerFiltering();
+            dataSource.ServerFiltering.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ServerFiltering_is_set_to_DataSource_if_set()
+        {
+            builder.ServerFiltering(true);
+            dataSource.ServerFiltering.ShouldBeTrue();
+        }
+    }
+}
