8eff8c51f41a773c49f22f2fe852fa48f7dba2cd
George Krystev <ggkrustev@yahoo.com>
2012-05-22 17:01:59 +0300

    Implement ExpandableAnimation which provides builder for expand/collapse animation

---
+++ wrappers/mvc/src/Kendo.Mvc/UI/Effects/ExpandableAnimation.cs
@@ -0,0 +1,50 @@
+﻿namespace Kendo.Mvc.UI
+{
+    using Extensions;
+    using System.Linq;
+
+    public class ExpandableAnimation
+    {
+        public ExpandableAnimation()
+        {
+            Enabled = true;
+            Expand = new Effects("expand");
+            Collapse = new Effects("collapse");
+        }
+
+        public bool Enabled 
+        { 
+            get; 
+            set; 
+        }
+
+        public Effects Expand 
+        { 
+            get; 
+            set; 
+        }
+
+        public Effects Collapse
+        {
+            get;
+            set;
+        }
+
+        public void SerializeTo(IClientSideObjectWriter writer)
+        {
+            if (Enabled == false)
+            {
+                writer.Append("animation", Enabled);
+            }
+            else
+            {
+                var result = string.Join(",", new string[] { Expand.Serialize(), Collapse.Serialize() }.Where(item => item.HasValue()));
+
+                if (result.HasValue())
+                {
+                    writer.Append("animation: {{{0}}}".FormatWith(result));
+                }
+            }
+        }
+    }
+}
---
+++ wrappers/mvc/src/Kendo.Mvc/UI/Effects/Fluent/ExpandableAnimationBuilder.cs
@@ -0,0 +1,37 @@
+﻿namespace Kendo.Mvc.UI.Fluent
+{
+    using System;
+
+    public class ExpandableAnimationBuilder
+    {
+        public ExpandableAnimationBuilder(ExpandableAnimation animation)
+        {
+            Animation = animation;
+        }
+
+        protected ExpandableAnimation Animation
+        { 
+            get; 
+            private set; 
+        }
+
+        public void Enable(bool enable)
+        {
+            Animation.Enabled = enable;
+        }
+
+        public ExpandableAnimationBuilder Expand(Action<EffectsBuilder> effectsAction)
+        {
+            effectsAction(new EffectsBuilder(Animation.Expand));
+
+            return this;
+        }
+
+        public ExpandableAnimationBuilder Collapse(Action<EffectsBuilder> effectsAction)
+        {
+            effectsAction(new EffectsBuilder(Animation.Collapse));
+
+            return this;
+        }
+    }
+}
