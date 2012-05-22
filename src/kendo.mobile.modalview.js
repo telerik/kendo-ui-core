a2cb14f1dbf701c212805948f17151def7b2aff4
Petyo Ivanov <underlog@gmail.com>
2012-05-22 17:43:03 +0300

    Docs for the mobile modalview

--- src/kendo.mobile.modalview.js
+++ src/kendo.mobile.modalview.js
@@ -9,6 +9,35 @@
     /**
      * @name kendo.mobile.ui.ModalView.Description
      * @section
+     * <p>The Kendo modalView is used to present self-contained functionality in the context of the current task.  </p>
+     *
+     * <h3>Getting Started</h3>
+     * <p>The Kendo mobile Application will automatically initialize a mobile ModalView widget for every <code>div</code> element with <code>role</code> data attribute set to <code>modalview</code> present in the views/layouts markup.
+     * Alternatively, it can be initialized using jQuery plugin syntax. The modalview element may contain optional header and/or footer. A mobile scroller is automatically initialized around the contents of the element.</p>
+     *
+     * @exampleTitle ModalView with header and footer
+     * @example
+     * <div data-role="view">
+     *  <a href="#foo" data-rel="modalview">Foo</a>
+     *  <div data-role="modalview" id="foo">
+     *    <div data-role="header">
+     *        <div data-role="navbar">
+     *            <a data-align="right" data-role="button">Close</a>
+     *        </div>
+     *    </div>
+     *
+     *    <ul data-role="listview">
+     *      <li>Foo</li>
+     *    </ul>
+     *
+     *    <div data-role="footer">
+     *       <div data-role="navbar">
+     *           <a data-align="right" data-role="button">Details</a>
+     *       </div>
+     *    </div>
+     *  </div>
+     * </div>
+     *
      */
     var ModalView = Widget.extend(/** @lends kendo.mobile.ui.ModalView.prototype */{
         /**
