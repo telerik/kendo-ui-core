bed238b3e1e1c13f814585c8cd152fe1e0eb36dc
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-05-24 17:23:44 +0300

    Add custom WebForm and Razor view engines with support for suite specific views

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Helpers/KendoWebFormViewEngine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples
+{
+    public class KendoWebFormViewEngine : WebFormViewEngine
+    {
+        public KendoWebFormViewEngine()
+        {
+             AreaViewLocationFormats = new[]
+             {
+                "~/Areas/{2}/Views/%suite/{1}/{0}.aspx",
+                "~/Areas/{2}/Views/%suite/Shared/{0}.aspx"
+            };
+
+             AreaMasterLocationFormats = new[] {
+                "~/Areas/{2}/Views/%suite/{1}/{0}.master",
+                "~/Areas/{2}/Views/%suite/Shared/{0}.master",
+            };
+
+             AreaPartialViewLocationFormats = new[] {
+                "~/Areas/{2}/Views/%suite/{1}/{0}.ascx",
+                "~/Areas/{2}/Views/%suite/Shared/{0}.ascx",
+            };
+        }
+
+        protected override IView CreatePartialView(ControllerContext controllerContext, string partialPath)
+        {
+            return base.CreatePartialView(controllerContext, ResolveSuite(controllerContext, partialPath));
+        }
+        
+        protected override IView CreateView(ControllerContext controllerContext, string viewPath, string masterPath)
+        {
+            return base.CreateView(controllerContext, ResolveSuite(controllerContext, viewPath), ResolveSuite(controllerContext, masterPath));
+        }
+
+        protected override bool FileExists(ControllerContext controllerContext, string virtualPath)
+        {
+            return base.FileExists(controllerContext, ResolveSuite(controllerContext, virtualPath));
+        }
+
+        private string ResolveSuite(ControllerContext controllerContext, string path)
+        {
+            var suite = controllerContext.RouteData.Values["suite"];
+            return (suite != null) ? path.Replace("%suite", suite.ToString()) : path;
+        }
+    }
+}
