f63759b9209dd8afd03ab2d6fed7290c9b712c7b
Hristo Germanov <hristo.germanov@telerik.com>
2012-05-24 14:42:53 +0300

    Update radial/linear gauge folders.

--- wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Radial_Gauge/IndexController.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Radial_Gauge/IndexController.cs
@@ -2,7 +2,7 @@
 
 namespace Kendo.Mvc.Examples.Controllers
 {
-    public partial class LinearGaugeController : Controller
+    public partial class Radial_GaugeController : Controller
     {
         public ActionResult Index()
         {
