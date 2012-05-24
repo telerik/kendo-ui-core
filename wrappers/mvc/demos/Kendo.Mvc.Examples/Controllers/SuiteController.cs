0ee00d94f7a08f1936e9e1e3fc6e9a866000078b
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-05-24 14:51:56 +0300

    Got you flowers. And suite index title.

--- wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/SuiteController.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/SuiteController.cs
@@ -9,9 +9,17 @@ namespace Kendo.Mvc.Examples.Controllers
 {
     public class SuiteController : Controller
     {
+        private readonly Dictionary<string, string> SuiteTitles =
+            new Dictionary<string, string>()
+            {
+                { "web", "Web" },
+                { "dataviz", "DataViz" }
+            };
+
         public ActionResult Index(string suite)
         {
             ViewBag.Navigation = NavigationProvider.SuiteWidgets(suite);
+            ViewBag.SuiteTitle = SuiteTitles[suite];
             
             return View();
         }
