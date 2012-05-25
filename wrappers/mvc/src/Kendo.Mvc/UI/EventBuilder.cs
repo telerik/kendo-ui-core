906a81780faa7aab738f00d0e7cfbd6f49325e5f
Atanas Korchev <akorchev@gmail.com>
2012-05-25 15:23:40 +0300

    Extract event builder base class

---
+++ wrappers/mvc/src/Kendo.Mvc/UI/EventBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kendo.Mvc.UI.Fluent
+{
+    public class EventBuilder : IHideObjectMembers
+    {
+        protected IDictionary<string, object> Events { get; private set; }
+
+        public EventBuilder(IDictionary<string, object> events)
+        {
+            Events = events;
+        }
+
+        protected void Handler(string name, string handlerName)
+        {
+            Events[name] = new ClientEvent { HandlerName = handlerName };
+        }
+
+        protected void Block(string name, Func<object, object> handler)
+        {
+            Events[name] = new ClientEvent { InlineCodeBlock = handler };
+        }
+    }
+}
--- wrappers/mvc/src/Kendo.Mvc/UI/Grid/Fluent/GridEventBuilder.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/Grid/Fluent/GridEventBuilder.cs
@@ -1,139 +1,136 @@
+using System;
+using System.Collections.Generic;
+
 namespace Kendo.Mvc.UI.Fluent
 {
-    using System;
-    using System.Collections.Generic;
-
-    public class GridEventBuilder : IHideObjectMembers
+    public class GridEventBuilder : EventBuilder
     {
-        private readonly IDictionary<string, object> events;
-
-        public GridEventBuilder(IDictionary<string, object> events)
+        public GridEventBuilder(IDictionary<string, object> events) : base(events)
         {
-            this.events = events;
         }
 
         public GridEventBuilder Change(Func<object, object> handler)
         {
-            events["change"] = new ClientEvent { InlineCodeBlock = handler };
+            Block("change", handler);
 
             return this;
         }
 
         public GridEventBuilder Change(string handlerName)
         {
-            events["change"] = new ClientEvent { HandlerName = handlerName };
+            Handler("change", handlerName);
 
             return this;
         }
 
         public GridEventBuilder Edit(Func<object, object> handler)
         {
-            events["edit"] = new ClientEvent { InlineCodeBlock = handler };
+            Block("edit", handler);
 
             return this;
         }
 
         public GridEventBuilder Edit(string handlerName)
         {
-            events["edit"] = new ClientEvent { HandlerName = handlerName };
+            Handler("edit", handlerName);
 
             return this;
         }
 
         public GridEventBuilder Save(Func<object, object> handler)
         {
-            events["save"] = new ClientEvent { InlineCodeBlock = handler };
+            Block("save", handler);
 
             return this;
         }
 
         public GridEventBuilder Save(string handlerName)
         {
-            events["save"] = new ClientEvent { HandlerName = handlerName };
+            Handler("save", handlerName);
 
             return this;
         }
 
         public GridEventBuilder SaveChanges(Func<object, object> handler)
         {
-            events["saveChanges"] = new ClientEvent { InlineCodeBlock = handler };
+            Block("saveChanges", handler);
 
             return this;
         }
 
         public GridEventBuilder SaveChanges(string handlerName)
         {
-            events["saveChanges"] = new ClientEvent { HandlerName = handlerName };
+            Handler("saveChanges", handlerName);
 
             return this;
         }
 
         public GridEventBuilder DetailExpand(Func<object, object> handler)
         {
-            events["detailExpand"] = new ClientEvent { InlineCodeBlock = handler };
+            Block("detailExpand", handler);
 
             return this;
         }
 
         public GridEventBuilder DetailExpand(string handlerName)
         {
-            events["detailExpand"] = new ClientEvent { HandlerName = handlerName };
+            Handler("detailExpand", handlerName);
 
             return this;
         }
 
         public GridEventBuilder DetailInit(Func<object, object> handler)
         {
-            events["detailInit"] = new ClientEvent { InlineCodeBlock = handler };
+            Block("detailInit", handler);
 
             return this;
         }
 
         public GridEventBuilder DetailInit(string handlerName)
         {
-            events["detailInit"] = new ClientEvent { HandlerName = handlerName };
+            Handler("detailInit", handlerName);
 
             return this;
         }
 
         public GridEventBuilder DetailCollapse(Func<object, object> handler)
         {
-            events["detailCollapse"] = new ClientEvent { InlineCodeBlock = handler };
+            Block("detailCollapse", handler);
 
             return this;
         }
 
         public GridEventBuilder DetailCollapse(string handlerName)
         {
-            events["detailCollapse"] = new ClientEvent { HandlerName = handlerName };
+            Handler("detailCollapse", handlerName);
 
             return this;
         }
 
         public GridEventBuilder Remove(Func<object, object> handler)
         {
-            events["remove"] = new ClientEvent { InlineCodeBlock = handler };
+            Block("remove", handler);
 
             return this;
         }
 
         public GridEventBuilder Remove(string handlerName)
         {
-            events["remove"] = new ClientEvent { HandlerName = handlerName };
+            Handler("remove", handlerName);
 
             return this;
         }
 
         public GridEventBuilder DataBound(Func<object, object> handler)
         {
-            events["dataBound"] = new ClientEvent { InlineCodeBlock = handler };
+            Block("dataBound", handler);
 
             return this;
         }
 
         public GridEventBuilder DataBound(string handlerName)
         {
-            events["dataBound"] = new ClientEvent { HandlerName = handlerName };
+            Handler("dataBound", handlerName);
 
             return this;
         }
