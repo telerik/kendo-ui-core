60331886556859e7fb1f606372bc4b9d8fc8c697
Atanas Korchev <akorchev@gmail.com>
2012-05-28 13:35:38 +0300

    Implement events for the data source

---
+++ wrappers/mvc/src/Kendo.Mvc/UI/DataSource/Fluent/DataSourceEventBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kendo.Mvc.UI.Fluent
+{
+    public class DataSourceEventBuilder : EventBuilder
+    {
+        public DataSourceEventBuilder(IDictionary<string, object> events) : base(events)
+        {
+        }
+
+        public DataSourceEventBuilder Change(string handler)
+        {
+            Handler("change", handler);
+
+            return this;
+        }
+
+        public DataSourceEventBuilder Change(Func<object, object> handler)
+        {
+            Handler("change", handler);
+
+            return this;
+        }
+
+        public DataSourceEventBuilder RequestStart(string handler)
+        {
+            Handler("requestStart", handler);
+
+            return this;
+        }
+
+        public DataSourceEventBuilder RequestStart(Func<object, object> handler)
+        {
+            Handler("requestStart", handler);
+
+            return this;
+        }
+
+        public DataSourceEventBuilder Error(string handler)
+        {
+            Handler("error", handler);
+
+            return this;
+        }
+
+        public DataSourceEventBuilder Error(Func<object, object> handler)
+        {
+            Handler("error", handler);
+
+            return this;
+        }
+    }
+}
