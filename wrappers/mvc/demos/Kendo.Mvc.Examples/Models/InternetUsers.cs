6dfd99de8e1204076e88f966e64292b6271bd9b1
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-05-28 17:56:10 +0300

    Add area chart remote binding demo

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Models/InternetUsers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kendo.Mvc.Examples.Models
+{
+    public class InternetUsers
+    {
+        public InternetUsers(int year, string country, double value)
+        {
+            Year = year;
+            Value = value;
+            Country = country;
+        }
+
+        public int Year { get; set; }
+        public double Value { get; set; }
+        public string Country { get; set; }
+    }
+}
