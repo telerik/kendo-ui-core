5d4ce8fb026f525815b24c837e7e8036c2d1142e
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-05-29 14:50:40 +0300

    Add pie chart demos.

--- wrappers/mvc/demos/Kendo.Mvc.Examples/Models/SpainElectricityStats.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Models/SpainElectricityStats.cs
@@ -1,23 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace Kendo.Mvc.Examples.Models
 {
     public static class SpainElectricityStats
     {
-        public static IEnumerable<ElectricityProduction> GetStats() {
+        public static IEnumerable<ElectricityProduction> GetProductionByYear() {
             return new ElectricityProduction[] {
-                new ElectricityProduction(2000, 18, 31807, 4727, 62206),
-                new ElectricityProduction(2001, 24, 43864, 6759, 63708),
-                new ElectricityProduction(2002, 30, 26270, 9342, 63016),
-                new ElectricityProduction(2003, 41, 43897, 12075, 61875),
-                new ElectricityProduction(2004, 56, 34439, 15700, 63606),
-                new ElectricityProduction(2005, 41, 23025, 21176, 57539),
-                new ElectricityProduction(2006, 119, 29831, 23297, 60126),
-                new ElectricityProduction(2007, 508, 30522, 27568, 55103),
-                new ElectricityProduction(2008, 2578, 26112, 32203, 58973)
+                new ElectricityProduction("2000", 18, 31807, 4727, 62206),
+                new ElectricityProduction("2001", 24, 43864, 6759, 63708),
+                new ElectricityProduction("2002", 30, 26270, 9342, 63016),
+                new ElectricityProduction("2003", 41, 43897, 12075, 61875),
+                new ElectricityProduction("2004", 56, 34439, 15700, 63606),
+                new ElectricityProduction("2005", 41, 23025, 21176, 57539),
+                new ElectricityProduction("2006", 119, 29831, 23297, 60126),
+                new ElectricityProduction("2007", 508, 30522, 27568, 55103),
+                new ElectricityProduction("2008", 2578, 26112, 32203, 58973)
+            };
+        }
+
+        public static IEnumerable<ElectricitySource> GetSourceBreakdown()
+        {
+            return new ElectricitySource[] {
+                new ElectricitySource("Hydro", 22) { Explode = true },
+                new ElectricitySource("Solar", 2),
+                new ElectricitySource("Nuclear", 49),
+                new ElectricitySource("Wind", 27)
             };
         }
     }
