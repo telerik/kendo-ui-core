a901fd3b37cb7e92a8681487177778839f2d924d
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-05-29 09:52:26 +0300

    Organize dataviz controller and view paths

--- wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/DataViz/Area_Charts/Local_DataController.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/DataViz/Area_Charts/Local_DataController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Kendo.Mvc.Examples.Models;
 
@@ -10,16 +7,6 @@ namespace Kendo.Mvc.Examples.Controllers
 {
     public partial class Area_ChartsController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult Remote_Data()
-        {
-            return View();
-        }
-
         public ActionResult Local_Data()
         {
             return View(InternetUsersRepository.GetData());
