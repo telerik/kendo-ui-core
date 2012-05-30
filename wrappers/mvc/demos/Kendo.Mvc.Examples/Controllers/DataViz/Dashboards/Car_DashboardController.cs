9e27a3fda05c32d2e4ff7988104eb0e3e2ed24f7
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-05-30 15:47:05 +0300

    Add car dashboard to MVC demos

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/DataViz/Dashboards/Car_DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class DashboardsController : Controller
+    {
+        public ActionResult Car_Dashboard()
+        {
+            return View();
+        }
+    }
+}
