9bfbde9deb7836b3a1db83fc7211d2dbcb05f3ad
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2012-06-15 10:37:33 +0300

    Add demo for column settings

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Areas/aspx/Views/web/grid/columnsettings.aspx
@@ -0,0 +1,40 @@
+<%@ Page Title="" Language="C#" MasterPageFile="~/Areas/aspx/Views/Shared/Web.Master" %>
+
+<asp:Content ContentPlaceHolderID="MainContent" runat="server">
+<%: Html.Kendo().Grid<Kendo.Mvc.Examples.Models.ProductViewModel>()
+        .Name("Grid")
+        .Columns(columns =>
+        {
+            columns.LoadSettings((IEnumerable<GridColumnSettings>)ViewData["Columns"]);
+        })
+        .Editable(editable => editable.Mode(GridEditMode.InLine))
+        .Pageable()
+        .Sortable()
+        .Scrollable()
+        .DataSource(dataSource => dataSource
+            .Ajax()
+            .Events(events => events.Error("error_handler"))
+            .Model(model => model.Id(p => p.ProductID))
+            .Read(read => read.Action("ColumnSettings_Read", "Grid"))
+            .Update(update => update.Action("ColumnSettings_Update", "Grid"))
+            .Destroy(update => update.Action("ColumnSettings_Destroy", "Grid"))
+        )
+%>
+
+<script type="text/javascript">
+    function error_handler(e) {    
+        if (e.errors) {
+            var message = "Errors:\n";
+            $.each(e.errors, function (key, value) {
+                if ('errors' in value) {
+                    $.each(value.errors, function() {
+                        message += this + "\n";
+                    });
+                }
+            });        
+            alert(message);
+        }
+    }
+</script>
+
+</asp:Content>
---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Web/Grid/ColumnSettingsController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Kendo.Mvc.Examples.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class GridController
+    {
+        public ActionResult ColumnSettings()
+        {
+            var columns = new[] 
+            {
+                new GridColumnSettings
+                {
+                    Member = "ProductName",
+                    Width = "200px"
+                },
+                new GridColumnSettings
+                {
+                    Member = "UnitPrice",
+                    Width = "130px",
+                    Format = "{0:c}",
+                },
+                new GridColumnSettings
+                {
+                    Member = "UnitsInStock",
+                    Width = "130px"
+                },
+                new GridColumnSettings
+                {
+                    Member = "LastSupply",
+                    Width = "130px",
+                    Format = "{0:d}"
+                },
+                new GridColumnSettings
+                {
+                    Member = "Discontinued"
+                },
+                new GridCommandColumnSettings
+                {
+                    Commands = 
+                    {
+                        new GridEditActionCommand(),
+                        new GridDestroyActionCommand()
+                    },
+                    Width = "200px",
+                    Title = "Commands"
+                }
+            };
+            ViewData["Columns"] = columns;
+
+            return View();
+        }
+
+        public ActionResult ColumnSettings_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(SessionProductRepository.All().ToDataSourceResult(request));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult ColumnSettings_Update([DataSourceRequest] DataSourceRequest request, ProductViewModel product)
+        {
+            if (product != null && ModelState.IsValid)
+            {                
+                var target = SessionProductRepository.One(p => p.ProductID == product.ProductID);
+                if (target != null)
+                {
+                    target.ProductName = product.ProductName;
+                    target.UnitPrice = product.UnitPrice;
+                    target.UnitsInStock = product.UnitsInStock;
+                    target.LastSupply = product.LastSupply;
+                    target.Discontinued = product.Discontinued;
+                    SessionProductRepository.Update(target);
+                }             
+            }            
+
+            return Json(ModelState.ToDataSourceResult());
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult ColumnSettings_Destroy([DataSourceRequest] DataSourceRequest request, ProductViewModel product)
+        {            
+            if (product != null)
+            {                
+                SessionProductRepository.Delete(product);                
+            }
+
+            return Json(ModelState.ToDataSourceResult());
+        }
+    }
+}
