ba1465821b769e7bd9b77bd5da86b54bf2b7c941
Alex Gyoshev <alex@gyoshev.net>
2012-06-20 18:15:10 +0300

    Add Remote DataSource demo and basic builder

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Web/TreeView/Remote_DataController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using System.Linq;
+using Kendo.Mvc.Examples.Models;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class TreeViewController : Controller
+    {
+        public ActionResult Remote_Data()
+        {
+            return View();
+        }
+
+        public JsonResult Employees()
+        {
+            var dataContext = new NorthwindDataContext();
+
+            var employees = from e in dataContext.Employees
+                            where e.ReportsTo == null
+                            select e;
+
+            return new JsonResult { Data = employees };
+        }
+    }
+}
