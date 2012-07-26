9496ecb2d7ef4119111c4a90f674e253f70f413f
George Krystev <ggkrustev@yahoo.com>
2012-07-26 14:45:20 +0300

    Add Direction demo for the menu

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Web/Menu/DirectionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Kendo.Mvc.UI;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class MenuController : Controller
+    {
+        public ActionResult Direction(int? direction)
+        {
+            direction = direction ?? 0;
+
+            return View((MenuDirection)direction);
+        }
+    }
+}
