dbcae1234a494b40c332c7406339a47bb275a579
Atanas Korchev <akorchev@gmail.com>
2012-08-13 19:06:18 +0300

    Add maven dependency to jsp-api.jar. Add empty autocomplete tag.

---
+++ wrappers/java/kendo-taglib/src/main/java/com/kendoui/taglib/AutoCompleteTag.java
@@ -0,0 +1,65 @@
+package com.kendoui.taglib;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.BodyContent;
+import javax.servlet.jsp.tagext.BodyTag;
+import javax.servlet.jsp.tagext.Tag;
+
+public class AutoCompleteTag implements BodyTag {
+
+	@Override
+	public int doAfterBody() throws JspException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int doEndTag() throws JspException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int doStartTag() throws JspException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Tag getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void release() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPageContext(PageContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setParent(Tag arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doInitBody() throws JspException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBodyContent(BodyContent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
