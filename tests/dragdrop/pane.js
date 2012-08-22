bf2e2eb21790cc8b50e44cb846ce7b6d69ce2bff
Petyo Ivanov <underlog@gmail.com>
2012-08-22 12:15:53 +0300

    Refactor pane tests a bit

--- tests/dragdrop/pane.js
+++ tests/dragdrop/pane.js
@@ -31,51 +31,66 @@ module("pane", {
     }
 });
 
+function press(x, y, id) {
+    triggerTouchEvent("mousedown", { pageX: x, pageY: y, identifier: id || 1});
+}
+
+function move(x, y, id) {
+    triggerTouchEvent("mousemove", { pageX: x, pageY: y, identifier: id || 1});
+}
+
+function release(id) {
+    triggerTouchEvent("mouseup", { pageX: 1, pageY: 1, identifier: id || 1});
+}
+
 test("determines zoom point", 2, function() {
-    triggerTouchEvent("mousedown", { pageX: 10, pageY: 10, identifier: 1 });
-    triggerTouchEvent("mousedown", { pageX: 13, pageY: 14, identifier: 2 });
+    press(10, 10);
+    press(13, 14, 2);
     equal(pane.touchPair.center.x, 11.5);
     equal(pane.touchPair.center.y, 12);
 })
 
 test("zooms content", function() {
-    triggerTouchEvent("mousedown", { pageX: 10, pageY: 10, identifier: 1 });
-    triggerTouchEvent("mousedown", { pageX: 13, pageY: 14, identifier: 2 });
-    triggerTouchEvent("mousemove", { pageX: 15, pageY: 22, identifier: 2 });
+    press(10, 10);
+    press(13, 14, 2);
+    move(15, 22, 2);
     equal(movable.scale, 2.6);
 })
 
 
 test("zooms to a given point", function() {
-    triggerTouchEvent("mousedown", { pageX: 10, pageY: 10, identifier: 1 });
-    triggerTouchEvent("mousedown", { pageX: 13, pageY: 14, identifier: 2 });
-    triggerTouchEvent("mousemove", { pageX: 9,  pageY: 6,  identifier: 1 });
-    triggerTouchEvent("mousemove", { pageX: 14, pageY: 18, identifier: 2 });
+    press(10, 10);
+    press(13, 14, 2);
+    move(9, 6);
+    move(14, 18, 2);
+
     equal(movable.x, -18.4, 0.1);
     equal(movable.y, -19.2, 0.1);
 })
 
 test("offsets zoom point", function() {
-    triggerTouchEvent("mousedown", { pageX: 10, pageY: 10, identifier: 1 });
-    triggerTouchEvent("mousedown", { pageX: 13, pageY: 14, identifier: 2 });
-    triggerTouchEvent("mousemove", { pageX: 5, pageY: 5, identifier: 1 });
-    triggerTouchEvent("mousemove", { pageX: 8, pageY: 9, identifier: 2 });
+    press(10, 10);
+    press(13, 14, 2);
+    move(5, 5);
+    move(8, 9, 2);
+
     equal(movable.scale, 1);
     equal(movable.x, -5, 0.1);
     equal(movable.y, -5, 0.1);
 })
 
 test("zooms to a given point after being offset", function() {
-    triggerTouchEvent("mousedown", { pageX: 30, pageY: 10, identifier: 1 });
-    triggerTouchEvent("mousemove", { pageX: 10, pageY: 10, identifier: 1 });
-    triggerTouchEvent("mouseup", { pageX: 10, pageY: 10, identifier: 1 });
+    press(30, 10);
+    move(10, 10);
+    release();
 
     equal(movable.x, -20);
 
-    triggerTouchEvent("mousedown", { pageX: 10, pageY: 10, identifier: 1 });
-    triggerTouchEvent("mousedown", { pageX: 13, pageY: 14, identifier: 2 });
-    triggerTouchEvent("mousemove", { pageX: 9,  pageY: 6,  identifier: 1 });
-    triggerTouchEvent("mousemove", { pageX: 14, pageY: 18, identifier: 2 });
+    press(10, 10);
+    press(13, 14, 2);
+    move(9, 6);
+    move(14, 18, 2);
+
     equal(movable.x, -70.4, 0.1);
     equal(movable.y, -19.2, 0.1);
 })
