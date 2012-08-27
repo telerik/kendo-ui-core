327c710aa0e07c2fba21dd1a47f9cb2e1a837711
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-08-27 16:13:09 +0300

    Use exec-sync instead of execSync npm module. Works on Windows.

--- build/themes.js
+++ build/themes.js
@@ -1,5 +1,5 @@
 var less = require("./less-js/lib/less"),
-    execSync = require("execSync"),
+    execSync = require("exec-sync"),
     kendoBuild = require("./kendo-build"),
     fs = require("fs"),
     path = require("path"),
@@ -17,8 +17,11 @@ function buildThemes(themesFolder, outputFolder) {
         if (cacheEntry) {
             kendoBuild.writeText(output, cacheEntry);
         } else {
-            var code = execSync.code("./build/less-js/bin/lessc " + file + " " + output);
-            console.log("Built theme ", theme, " (", code, ")");
+            var error = execSync("node \"build/less-js/bin/lessc\" " + file + " " + output, true).stderr;
+            if (error) {
+                throw new Error(error);
+            }
+            console.log("Built theme ", theme);
             cache[file] = kendoBuild.readText(output);
         }
     });
