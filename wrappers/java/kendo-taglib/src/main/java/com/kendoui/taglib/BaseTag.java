103ca1c603db933f08aee9d6516ba3de8552e2fd
Atanas Korchev <akorchev@gmail.com>
2012-08-30 17:45:53 +0300

    BaseTag implements Serializable

--- wrappers/java/kendo-taglib/src/main/java/com/kendoui/taglib/BaseTag.java
+++ wrappers/java/kendo-taglib/src/main/java/com/kendoui/taglib/BaseTag.java
@@ -2,13 +2,28 @@ package com.kendoui.taglib;
 
 import java.io.PrintWriter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 
+import com.kendoui.taglib.json.Serializable;
+
 @SuppressWarnings("serial")
-public abstract class BaseTag extends BodyTagSupport {
+public abstract class BaseTag extends BodyTagSupport implements Serializable {
     private String name;
-    
+    private Map<String,Object> json;
+
+    public BaseTag() {
+        json = new HashMap<String,Object>();
+    }
+
+    @Override
+    public Map<String,Object> properties() {
+        return json;
+    }
+
     public String getName() {
         return name;
     }
@@ -22,9 +37,9 @@ public abstract class BaseTag extends BodyTagSupport {
         PrintWriter out = new PrintWriter(pageContext.getOut());
 
         out.format("<input id=\"{0}\"", getName());
-        
+
         return EVAL_PAGE;
     }
-    
+
     public abstract String getTagName();
 }
