d32f9a49131b7f7929b0b83efe280f7ed22b8abf
Alex Gyoshev <alex@gyoshev.net>
2012-08-30 12:01:45 +0300

    Adjust link dialog code to kendo coding convention.

--- src/editor/link.js
+++ src/editor/link.js
@@ -17,7 +17,7 @@ var kendo = window.kendo,
 
 var LinkFormatFinder = Class.extend({
     findSuitable: function (sourceNode) {
-        return dom.parentOfType(sourceNode, ['a']);
+        return dom.parentOfType(sourceNode, ["a"]);
     }
 });
 
@@ -27,21 +27,25 @@ var LinkFormatter = Class.extend({
     },
 
     apply: function (range, attributes) {
-        var nodes = textNodes(range);
+        var nodes = textNodes(range),
+            markers, doc,
+            formatter, a;
+
         if (attributes.innerHTML) {
-            var markers = RangeUtils.getMarkers(range);
-            var document = RangeUtils.documentFromRange(range);
+            markers = RangeUtils.getMarkers(range);
+            doc = RangeUtils.documentFromRange(range);
+
             range.deleteContents();
-            var a = dom.create(document, 'a', attributes);
+            a = dom.create(doc, "a", attributes);
             range.insertNode(a);
 
             if (markers.length > 1) {
                 dom.insertAfter(markers[markers.length - 1], a);
                 dom.insertAfter(markers[1], a);
-                dom[nodes.length > 0 ? 'insertBefore' : 'insertAfter'](markers[0], a);
+                dom[nodes.length > 0 ? "insertBefore" : "insertAfter"](markers[0], a);
             }
         } else {
-            var formatter = new InlineFormatter([{ tags: ['a']}], attributes);
+            formatter = new InlineFormatter([{ tags: ["a"]}], attributes);
             formatter.finder = this.finder;
             formatter.apply(nodes);
         }
@@ -52,7 +56,7 @@ var UnlinkCommand = Command.extend({
     init: function(options) {
         options.formatter = /** @ignore */ {
             toggle : function(range) {
-                new InlineFormatter([{ tags: ['a']}]).remove(textNodes(range));
+                new InlineFormatter([{ tags: ["a"]}]).remove(textNodes(range));
             }
         };
         this.options = options;
@@ -71,37 +75,33 @@ var LinkCommand = Command.extend({
     },
 
     exec: function () {
-        var range = this.getRange();
-
-        var collapsed = range.collapsed;
-
-        range = this.lockRange(true);
-
-        var nodes = textNodes(range);
-
-        var initialText = null;
-
-        var that = this;
+        var that = this,
+            range = that.getRange(),
+            collapsed = range.collapsed,
+            range = that.lockRange(true),
+            nodes = textNodes(range),
+            initialText = null;
 
         function apply(e) {
-            var href = $('#k-editor-link-url', dialog.element).val();
+            var href = $("#k-editor-link-url", dialog.element).val(),
+                title, text, target;
 
-            if (href && href != 'http://') {
+            if (href && href != "http://") {
                 that.attributes = { href: href };
 
-                var title = $('#k-editor-link-title', dialog.element).val();
+                title = $("#k-editor-link-title", dialog.element).val();
                 if (title) {
                     that.attributes.title = title;
                 }
 
-                var text = $('#k-editor-link-text', dialog.element).val();
+                text = $("#k-editor-link-text", dialog.element).val();
                 if (text !== initialText) {
                     that.attributes.innerHTML = text || href;
                 }
 
-                var target = $('#k-editor-link-target', dialog.element).is(':checked');
+                target = $("#k-editor-link-target", dialog.element).is(":checked");
                 if (target) {
-                    that.attributes.target = '_blank';
+                    that.attributes.target = "_blank";
                 }
 
                 that.formatter.apply(range, that.attributes);
@@ -128,48 +128,49 @@ var LinkCommand = Command.extend({
         var shouldShowText = nodes.length <= 1 || (nodes.length == 2 && collapsed);
 
         var windowContent =
-            '<div class="k-editor-dialog">' +
-                '<ol>' +
-                    '<li class="k-form-text-row"><label for="k-editor-link-url">Web address</label><input type="text" class="k-input" id="k-editor-link-url"/></li>' +
-                    (shouldShowText ? '<li class="k-form-text-row"><label for="k-editor-link-text">Text</label><input type="text" class="k-input" id="k-editor-link-text"/></li>' : '') +
-                    '<li class="k-form-text-row"><label for="k-editor-link-title">Tooltip</label><input type="text" class="k-input" id="k-editor-link-title"/></li>' +
-                    '<li class="k-form-checkbox-row"><input type="checkbox" id="k-editor-link-target"/><label for="k-editor-link-target">Open link in new window</label></li>' +
-                '</ol>' +
-                '<div class="k-button-wrapper">' +
-                    '<button class="k-dialog-insert k-button">Insert</button>' +
-                    '&nbsp;or&nbsp;' +
-                    '<a href="#" class="k-dialog-close k-link">Close</a>' +
-                '</div>' +
-            '</div>';
+            "<div class='k-editor-dialog'>" +
+                "<ol>" +
+                    "<li class='k-form-text-row'><label for='k-editor-link-url'>Web address</label><input type='text' class='k-input' id='k-editor-link-url'></li>" +
+                    (shouldShowText ? "<li class='k-form-text-row'><label for='k-editor-link-text'>Text</label><input type='text' class='k-input' id='k-editor-link-text'></li>" : "") +
+                    "<li class='k-form-text-row'><label for='k-editor-link-title'>Tooltip</label><input type='text' class='k-input' id='k-editor-link-title'></li>" +
+                    "<li class='k-form-checkbox-row'><input type='checkbox' id='k-editor-link-target'><label for='k-editor-link-target'>Open link in new window</label></li>" +
+                "</ol>" +
+                "<div class='k-button-wrapper'>" +
+                    "<button class='k-dialog-insert k-button'>Insert</button>" +
+                    "&nbsp;or&nbsp;" +
+                    "<a href='#' class='k-dialog-close k-link'>Close</a>" +
+                "</div>" +
+            "</div>";
 
         var dialog = $(windowContent).appendTo(document.body).kendoWindow($.extend({}, this.editor.options.dialogOptions, {
             title: "Insert link",
             close: close
         }))
             .hide()
-            .find('.k-dialog-insert').click(apply).end()
-            .find('.k-dialog-close').click(close).end()
-            .find('.k-form-text-row input').keydown(function (e) {
-                if (e.keyCode == 13) {
+            .find(".k-dialog-insert").click(apply).end()
+            .find(".k-dialog-close").click(close).end()
+            .find(".k-form-text-row input").keydown(function (e) {
+                var keys = kendo.keys;
+                if (e.keyCode == keys.ENTER) {
                     apply(e);
-                } else if (e.keyCode == 27) {
+                } else if (e.keyCode == keys.ESC) {
                     close(e);
                 }
             }).end()
             // IE < 8 returns absolute url if getAttribute is not used
-            .find('#k-editor-link-url').val(a ? a.getAttribute('href', 2) : 'http://').end()
-            .find('#k-editor-link-text').val(nodes.length > 0 ? (nodes.length == 1 ? nodes[0].nodeValue : nodes[0].nodeValue + nodes[1].nodeValue) : '').end()
-            .find('#k-editor-link-title').val(a ? a.title : '').end()
-            .find('#k-editor-link-target').attr('checked', a ? a.target == '_blank' : false).end()
+            .find("#k-editor-link-url").val(a ? a.getAttribute("href", 2) : "http://").end()
+            .find("#k-editor-link-text").val(nodes.length > 0 ? (nodes.length == 1 ? nodes[0].nodeValue : nodes[0].nodeValue + nodes[1].nodeValue) : "").end()
+            .find("#k-editor-link-title").val(a ? a.title : "").end()
+            .find("#k-editor-link-target").attr("checked", a ? a.target == "_blank" : false).end()
             .show()
-            .data('kendoWindow')
+            .data("kendoWindow")
             .center();
 
         if (shouldShowText && nodes.length > 0) {
-            initialText = $('#k-editor-link-text', dialog.element).val();
+            initialText = $("#k-editor-link-text", dialog.element).val();
         }
 
-        $('#k-editor-link-url', dialog.element).focus().select();
+        $("#k-editor-link-url", dialog.element).focus().select();
     },
 
     redo: function () {
@@ -185,19 +186,19 @@ var LinkCommand = Command.extend({
 var UnlinkTool = Tool.extend({
     init: function(options) {
         this.options = options;
-        this.finder = new InlineFormatFinder([{tags:['a']}]);
+        this.finder = new InlineFormatFinder([{tags:["a"]}]);
 
         Tool.fn.init.call(this, $.extend(options, {command:UnlinkCommand}));
     },
 
-    initialize: function($ui) {
-        $ui.attr('unselectable', 'on')
-           .addClass('k-state-disabled');
+    initialize: function(ui) {
+        ui.attr("unselectable", "on")
+          .addClass("k-state-disabled");
     },
 
-    update: function ($ui, nodes) {
-        $ui.toggleClass('k-state-disabled', !this.finder.isFormatted(nodes))
-            .removeClass('k-state-hover');
+    update: function (ui, nodes) {
+        ui.toggleClass("k-state-disabled", !this.finder.isFormatted(nodes))
+          .removeClass("k-state-hover");
     }
 });
 
@@ -209,7 +210,7 @@ extend(kendo.ui.editor, {
     UnlinkTool: UnlinkTool
 });
 
-registerTool("createLink", new Tool({ key: 'K', ctrl: true, command: LinkCommand, template: new ToolTemplate({template: EditorUtils.buttonTemplate, title: "Create Link"})}));
-registerTool("unlink", new UnlinkTool({ key: 'K', ctrl: true, shift: true, template: new ToolTemplate({template: EditorUtils.buttonTemplate, title: "Remove Link"})}));
+registerTool("createLink", new Tool({ key: "K", ctrl: true, command: LinkCommand, template: new ToolTemplate({template: EditorUtils.buttonTemplate, title: "Create Link"})}));
+registerTool("unlink", new UnlinkTool({ key: "K", ctrl: true, shift: true, template: new ToolTemplate({template: EditorUtils.buttonTemplate, title: "Remove Link"})}));
 
 })(jQuery);
