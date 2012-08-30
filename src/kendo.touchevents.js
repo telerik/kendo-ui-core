e99b5a7c9b27c233b4f73ae654e86e051993e42c
Petyo Ivanov <underlog@gmail.com>
2012-08-30 15:10:15 +0300

    Touchstart event implementation

--- src/kendo.touchevents.js
+++ src/kendo.touchevents.js
@@ -1,6 +1,11 @@
 (function ($, undefined) {
     var kendo = window.kendo,
+        support = kendo.support,
+        Class = kendo.Class,
         Widget = kendo.ui.Widget,
+
+        extend = $.extend,
+
         // DOM events
         DOM_PRESS = "mousedown",
         DOM_MOVE = "mousemove",
@@ -9,11 +14,78 @@
         // Widget Events
         TOUCH_START = "touchstart";
 
-    if (kendo.support.touch) {
+    if (support.touch) {
         DOM_PRESS = "touchstart";
         DOM_MOVE = "touchmove";
         DOM_UP = "touchend touchcancel";
     }
+
+    function getTouches(e) {
+        var touches = [],
+            originalEvent = e.originalEvent,
+            idx = 0, length,
+            changedTouches,
+            touch;
+
+        if (support.touch) {
+            changedTouches = originalEvent.changedTouches;
+            for (length = changedTouches.length; idx < length; idx ++) {
+                touch = changedTouches[idx];
+                touches.push({
+                    location: touch,
+                    event: e,
+                    target: touch.target,
+                    id: touch.identifier
+                });
+            }
+        }
+        else if (support.pointers) {
+            touches.push({
+                location: originalEvent,
+                event: e,
+                target: e.target,
+                id: originalEvent.pointerId
+
+            });
+        } else {
+            touches.push({
+                id: 1, // hardcoded ID for mouse event;
+                event: e,
+                target: e.target,
+                location: e
+            });
+        }
+
+        return touches;
+    }
+
+    var TouchAxis = Class.extend({
+        init: function(axis, location) {
+            var that = this,
+                offset = location["page" + axis];
+
+            that.axis = axis;
+            that.startLocation = that.location = offset;
+            that.client = location["client" + axis];
+            that.screen = location["screen" + axis];
+        }
+    });
+
+    var Touch = Class.extend({
+        init: function(touchEvents, eventInfo) {
+            var that = this;
+
+            extend(this, {
+                x: new TouchAxis("X", eventInfo.location),
+                y: new TouchAxis("Y", eventInfo.location),
+            });
+
+            touchEvents.trigger(TOUCH_START, {
+                event: eventInfo.event,
+                touch: this
+            });
+        }
+    })
 
     var TouchEvents = Widget.extend({
         init: function(element, options) {
@@ -23,9 +95,7 @@
 
             element = $(element);
 
-            element.on(DOM_PRESS, function(e) {
-                that.trigger(TOUCH_START, {event: e});
-            });
+            element.on(DOM_PRESS, function(e) { that._press(e) });
         },
 
         events: [
@@ -34,6 +104,18 @@
 
         options: {
             name: "TouchEvents"
+        },
+
+        _press: function(e) {
+            var that = this,
+                touches = getTouches(e),
+                idx = 0,
+                length = touches.length,
+                touchEvent;
+
+            for (; idx < length; idx ++) {
+                new Touch(that, touches[idx]);
+            }
         }
     });
 
--- tests/touchevents/touchevents.js
+++ tests/touchevents/touchevents.js
@@ -8,20 +8,44 @@ module("Touch Events - touchstart", {
     }
 });
 
+function triggerTouchEvent(type, eventData) {
+    element.trigger($.Event(type, { originalEvent: { changedTouches: [eventData] } }));
+}
 
 test("Triggers touchstart when pressed", 1, function() {
     touchEvents.bind("touchstart", function() {
         ok(true);
     });
 
-    element.trigger($.Event("touchstart"));
+    triggerTouchEvent("touchstart", {});
 })
 
-test("passes jQuery event in handler", 1, function() {
-    var event = $.Event("touchstart", { pageX: 10, pageY: 10});
+test("passes jQuery event", 1, function() {
     touchEvents.bind("touchstart", function(e) {
-        equal(e.event, event);
+        ok(e.event instanceof $.Event);
     });
 
-    element.trigger(event);
+    triggerTouchEvent("touchstart", {});
+});
+
+test("passes event coordinates", 6, function() {
+    touchEvents.bind("touchstart", function(e) {
+        var x = e.touch.x, y = e.touch.y;
+        equal(x.location, 10);
+        equal(x.client, 100);
+        equal(x.screen, 80);
+
+        equal(y.location, 10);
+        equal(y.client, 100);
+        equal(y.screen, 80);
+    });
+
+    triggerTouchEvent("touchstart", {
+        pageX: 10,
+        pageY: 10,
+        clientX: 100,
+        clientY: 100,
+        screenX: 80,
+        screenY: 80,
+    });
 })
