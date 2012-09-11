67fc1decac71754af499c9dfa53b934c3627f97e
Nikolay Rusev <nikolay.rusev@telerik.com>
2012-09-11 15:10:09 +0300

    Fix how drop target is acquired for IE8. Closes #651

--- src/kendo.userevents.js
+++ src/kendo.userevents.js
@@ -228,7 +228,7 @@
 
             that._finished = true;
 
-            activeTouches.splice(activeTouches.indexOf(that), 1);
+            activeTouches.splice($.inArray(that, activeTouches), 1);
         },
 
         skip: function() {
