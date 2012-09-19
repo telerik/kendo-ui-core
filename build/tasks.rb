77b0156a2e93f41ce3412ae1d9d4106bb225d2c5
Petyo Ivanov <underlog@gmail.com>
2012-09-19 09:32:25 +0300

    Add readme to bundles

--- build/tasks.rb
+++ build/tasks.rb
@@ -1,7 +1,8 @@
 require 'rbconfig'
 
-LEGAL = File.join('resources', 'legal', BETA ? 'beta' : 'official')
-THIRD_PARTY_LEGAL = File.join('resources', 'legal', 'third-party')
+LEGAL_DIR = File.join('resources', 'legal', BETA ? 'beta' : 'official')
+README_DIR = 'resources'
+THIRD_PARTY_LEGAL_DIR = File.join('resources', 'legal', 'third-party')
 
 class MergeTask < Rake::FileTask
     def execute(args=nil)
@@ -114,12 +115,14 @@ end
 
 def bundle(options)
     name = options[:name]
-    path = "dist/bundles/#{name}"
-    prerequisites = []
     eula = options[:eula]
+    readme = options[:readme]
+    path = "dist/bundles/#{name}"
     license = "#{path}.license"
 
-    file_license license => File.join(LEGAL, "#{options[:license]}.txt")
+    prerequisites = [:js, :less]
+
+    file_license license => File.join(LEGAL_DIR, "#{options[:license]}.txt")
 
     options[:contents].each do |target, contents|
 
@@ -136,24 +139,28 @@ def bundle(options)
     if eula
         license_agreements_path = File.join(path, "license-agreements")
         third_party_path = File.join(license_agreements_path, "third-party")
-        source_path = File.join(LEGAL, eula + "-eula")
+        source_path = File.join(LEGAL_DIR, eula + "-eula")
 
         tree :to => license_agreements_path,
              :from =>  File.join(source_path, "*"),
              :root => source_path
 
         tree :to => third_party_path,
-             :from =>  File.join(THIRD_PARTY_LEGAL, "*"),
-             :root => THIRD_PARTY_LEGAL
+             :from =>  File.join(THIRD_PARTY_LEGAL_DIR, "*"),
+             :root => THIRD_PARTY_LEGAL_DIR
 
         prerequisites.push(license_agreements_path)
         prerequisites.push(third_party_path)
     end
 
+    if readme
+        readme_path = File.join(path, "README")
+        file_copy :to => readme_path, :from => File.join(README_DIR, "#{readme}.txt")
+        prerequisites.push(readme_path)
+    end
 
-    zip "#{path}.zip" => [:js, :less] + prerequisites
+    zip "#{path}.zip" =>  prerequisites
 
-    task name => "#{path}.zip"
     desc description(name)
     task "bundles:#{name}" => "#{path}.zip"
 end
