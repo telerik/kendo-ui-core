b046ec226ab5c8e5fbc307deae0034a5491af35f
Atanas Korchev <akorchev@gmail.com>
2012-09-25 15:49:17 +0300

    Remove zip task output

--- build/archive.rb
+++ build/archive.rb
@@ -10,13 +10,10 @@ class ZipTask < Rake::FileTask
         rm_rf name
 
         Zip::ZipFile.open(name, Zip::ZipFile::CREATE) do |file|
-            $stderr.puts "Archive "
             Dir[File.join(dir, '**', '*')].each do |src|
                 entry = src.sub(File.join(dir, File::SEPARATOR), "")
-                $stderr.puts "\t#{entry}"
                 file.add entry, src
             end
-            $stderr.puts "to #{name}"
         end
     end
 end
