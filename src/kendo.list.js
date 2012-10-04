4a3cec00eb4431b3bca9e28cf875b153b4a0108b
George Krystev <ggkrustev@yahoo.com>
2012-10-04 12:08:56 +0300

    Widget does not persist width of the popup element. Fixes #718

--- src/kendo.list.js
+++ src/kendo.list.js
@@ -221,19 +221,21 @@
                 wrapper = this.wrapper,
                 computedStyle, computedWidth;
 
-            computedStyle = window.getComputedStyle ? window.getComputedStyle(wrapper[0], null) : 0;
-            computedWidth = computedStyle ? parseFloat(computedStyle.width) : wrapper.outerWidth();
+            if (!width) {
+                computedStyle = window.getComputedStyle ? window.getComputedStyle(wrapper[0], null) : 0;
+                computedWidth = computedStyle ? parseFloat(computedStyle.width) : wrapper.outerWidth();
 
-            if (computedStyle && (browser.mozilla || browser.msie)) { // getComputedStyle returns different box in FF and IE.
-                computedWidth += parseFloat(computedStyle.paddingLeft) + parseFloat(computedStyle.paddingRight) + parseFloat(computedStyle.borderLeftWidth) + parseFloat(computedStyle.borderRightWidth);
+                if (computedStyle && (browser.mozilla || browser.msie)) { // getComputedStyle returns different box in FF and IE.
+                    computedWidth += parseFloat(computedStyle.paddingLeft) + parseFloat(computedStyle.paddingRight) + parseFloat(computedStyle.borderLeftWidth) + parseFloat(computedStyle.borderRightWidth);
+                }
+
+                width = computedWidth - (list.outerWidth() - list.width());
+
+                list.css({
+                    fontFamily: wrapper.css("font-family"),
+                    width: width
+                });
             }
-
-            width = computedWidth - (list.outerWidth() - list.width());
-
-            list.css({
-                fontFamily: wrapper.css("font-family"),
-                width: width
-            });
 
             return true;
         },
