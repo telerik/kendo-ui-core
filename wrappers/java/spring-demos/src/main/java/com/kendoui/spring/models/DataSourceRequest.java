3f9da036e8df8f88216f27a298f1e0f26be9179f
Atanas Korchev <akorchev@gmail.com>
2012-10-16 15:15:47 +0300

    Move the datasource request processing in the DataSourceRequest class. Case-insensitive string comparisons.

--- wrappers/java/spring-demos/src/main/java/com/kendoui/spring/models/DataSourceRequest.java
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/models/DataSourceRequest.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Junction;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.util.AutoPopulatingList;
 
 public class DataSourceRequest {
@@ -84,4 +90,115 @@ public class DataSourceRequest {
         this.filter = filter;
     }
 
+    private static void restrict(Junction junction, Map<String, Object> filter) {
+        String operator = filter.get("operator").toString();
+        String field = filter.get("field").toString();
+        Object value = filter.get("value");
+        
+        try {
+            value = Double.parseDouble(value.toString());
+        }catch(NumberFormatException nfe) {
+        }
+        
+        switch(operator) {
+            case "eq":
+                if (value instanceof String) {
+                    junction.add(Restrictions.ilike(field, value.toString(), MatchMode.EXACT));
+                } else {
+                    junction.add(Restrictions.eq(field, value));
+                }
+                break;
+            case "neq":
+                if (value instanceof String) {
+                    junction.add(Restrictions.not(Restrictions.ilike(field, value.toString(), MatchMode.EXACT)));
+                } else {
+                    junction.add(Restrictions.ne(field, value));
+                }
+                break;
+            case "gt":
+                junction.add(Restrictions.gt(field, value));
+                break;
+            case "gte":
+                junction.add(Restrictions.ge(field, value));
+                break;
+            case "lt":
+                junction.add(Restrictions.lt(field, value));
+                break;
+            case "lte":
+                junction.add(Restrictions.le(field, value));
+                break;
+            case "startswith":
+                junction.add(Restrictions.ilike(field, value.toString(), MatchMode.START));
+                break;
+            case "endswith":
+                junction.add(Restrictions.ilike(field, value.toString(), MatchMode.END));
+                break;
+            case "contains":
+                junction.add(Restrictions.ilike(field, value.toString(), MatchMode.ANYWHERE));
+                break;                
+            case "doesnotcontain":
+                junction.add(Restrictions.not(Restrictions.ilike(field, value.toString(), MatchMode.ANYWHERE)));
+                break;
+        }
+
+    }
+    
+    private static void filter(Criteria criteria, Map<String, Object> filter) {
+        List<Map<String, Object>> filters = (List<Map<String, Object>>)filter.get("filters");
+        
+        if (!filters.isEmpty()) {
+            Junction junction = Restrictions.conjunction();
+            
+            if (filter.get("logic").toString().equals("or")) {
+                junction = Restrictions.disjunction();
+            }
+            
+            for(Map<String, Object> entry : filters) {
+                if (entry.containsKey("logic")) {
+                    filter(criteria, entry);
+                } else {
+                    restrict(junction, entry);
+                }
+            }
+            
+            criteria.add(junction);
+        }
+    }
+    
+    private static void sort(Criteria criteria, List<Map<String, String>> sort) {
+        if (sort != null && !sort.isEmpty()) {
+            for (Map<String, String> entry : sort) {
+                String field = entry.get("field");
+                String dir = entry.get("dir");
+                
+                if (dir.equals("asc")) {
+                    criteria.addOrder(Order.asc(field));    
+                } else if (dir.equals("desc")) {
+                    criteria.addOrder(Order.desc(field));
+                }
+            }
+        }
+    }
+
+    public DataSourceResult toDataSourceResult(Criteria criteria) {
+        filter(criteria, getFilter());
+        
+        long total = (long)criteria.setProjection(Projections.rowCount()).uniqueResult();
+        
+        
+        criteria.setProjection(null);
+        criteria.setResultTransformer(Criteria.ROOT_ENTITY);
+        
+        sort(criteria, getSort());
+        
+        criteria.setMaxResults(getTake());
+        criteria.setFirstResult(getSkip());
+        
+        DataSourceResult result = new DataSourceResult();
+        
+        result.setTotal(total);
+        result.setData(criteria.list());
+        
+        return result;
+    }
 }
--- wrappers/java/spring-demos/src/main/java/com/kendoui/spring/models/ProductDaoImpl.java
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/models/ProductDaoImpl.java
@@ -1,15 +1,9 @@
 package com.kendoui.spring.models;
 
 import java.util.List;
-import java.util.Map;
 
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Junction;
-import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,104 +20,11 @@ public class ProductDaoImpl implements ProductDao {
         return sessionFactory.getCurrentSession().createQuery("from Product").list();
     }
     
-    private static void restrict(Junction junction, Map<String, Object> filter) {
-        String operator = filter.get("operator").toString();
-        String field = filter.get("field").toString();
-        Object value = filter.get("value");
-        
-        try {
-            value = Double.parseDouble(value.toString());
-        }catch(NumberFormatException nfe) {
-        }
-        
-        switch(operator) {
-            case "eq":
-                junction.add(Restrictions.eq(field, value));
-                break;
-            case "neq":
-                junction.add(Restrictions.ne(field, value));
-                break;
-            case "gt":
-                junction.add(Restrictions.gt(field, value));
-                break;
-            case "gte":
-                junction.add(Restrictions.ge(field, value));
-                break;
-            case "lt":
-                junction.add(Restrictions.lt(field, value));
-                break;
-            case "lte":
-                junction.add(Restrictions.le(field, value));
-                break;
-            case "startswith":
-                junction.add(Restrictions.ilike(field, value.toString(), MatchMode.START));
-                break;
-            case "endswith":
-                junction.add(Restrictions.ilike(field, value.toString(), MatchMode.END));
-                break;
-            case "contains":
-                junction.add(Restrictions.ilike(field, value.toString(), MatchMode.ANYWHERE));
-                break;                
-            case "doesnotcontain":
-                junction.add(Restrictions.not(Restrictions.ilike(field, value.toString(), MatchMode.ANYWHERE)));
-                break;
-        }
-
-    }
     
-    private static void filter(Criteria criteria, Map<String, Object> filter) {
-        List<Map<String, Object>> filters = (List<Map<String, Object>>)filter.get("filters");
-        
-        if (!filters.isEmpty()) {
-            Junction junction = Restrictions.conjunction();
-            
-            if (filter.get("logic").toString().equals("or")) {
-                junction = Restrictions.disjunction();
-            }
-            
-            for(Map<String, Object> entry : filters) {
-                if (entry.containsKey("logic")) {
-                    filter(criteria, entry);
-                } else {
-                    restrict(junction, entry);
-                }
-            }
-            
-            criteria.add(junction);
-        }
-    }
-
     @Override
     public DataSourceResult getList(DataSourceRequest request) {
-        DataSourceResult result = new DataSourceResult();
-        
         Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Product.class);
         
-        filter(criteria, request.getFilter());
-        
-        result.setTotal((long)criteria.setProjection(Projections.rowCount()).uniqueResult());
-        
-        criteria.setProjection(null);
-        criteria.setResultTransformer(Criteria.ROOT_ENTITY);
-        
-        if (request.getSort() != null && !request.getSort().isEmpty()) {
-            for (Map<String, String> sort : request.getSort()) {
-                String field = sort.get("field");
-                String dir = sort.get("dir");
-                
-                if (dir.equals("asc")) {
-                    criteria.addOrder(Order.asc(field));    
-                } else if (dir.equals("desc")) {
-                    criteria.addOrder(Order.desc(field));
-                }
-            }
-        }
-        
-        criteria.setMaxResults(request.getTake());
-        criteria.setFirstResult(request.getSkip());
-        
-        result.setData(criteria.list());
-        
-        return result;
+        return request.toDataSourceResult(criteria);
     }
 }
