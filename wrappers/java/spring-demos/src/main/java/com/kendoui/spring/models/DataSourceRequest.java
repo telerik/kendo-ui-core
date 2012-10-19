53cbea4929298014fbdec4f5c3ec60c9ba0acf94
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2012-10-19 11:22:24 +0300

    Implement in-memory grouping

--- wrappers/java/spring-demos/src/main/java/com/kendoui/spring/models/DataSourceRequest.java
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/models/DataSourceRequest.java
@@ -2,18 +2,21 @@ package com.kendoui.spring.models;
 
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.criterion.Junction;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.transform.ResultTransformer;
 import org.springframework.util.AutoPopulatingList;
 
 public class DataSourceRequest {
@@ -198,44 +201,73 @@ public class DataSourceRequest {
                 }
             }
         }
+    }   
+    
+    private static List<?> groupBy(List<?> items, List<Map<String, String>> group, Class<?> clazz)  throws IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    	ArrayList<Map<String, Object>> result = new ArrayList<Map<String,  Object>>();    	
+    	                        
+        if (!items.isEmpty() && group != null && !group.isEmpty()) {
+        	String field = group.get(0).get("field");
+        	
+            Method accessor = new PropertyDescriptor(field, clazz).getReadMethod();          		
+            
+            Object groupValue = accessor.invoke(items.get(0));
+                        
+            Map<String, Object> groupItem = new HashMap<String, Object>();
+            result.add(groupItem);
+            
+            groupItem.put("value", groupValue);
+            groupItem.put("field", field);
+            groupItem.put("hasSubgroups", group.size() > 1);
+            groupItem.put("aggregates", new HashMap<String, Object>());
+                        
+            List<Object> groupItems = new ArrayList<Object>();           
+            
+            groupItem.put("items", groupItems);
+            
+            for (Object item : items) {            	
+            	Object currentValue = accessor.invoke(item);
+            	
+				if (!groupValue.equals(currentValue)) {
+					groupValue = currentValue;
+					groupItem = new HashMap<String, Object>();
+					result.add(groupItem);
+					
+					groupItem.put("value", groupValue);
+		            groupItem.put("field", field);
+		            groupItem.put("hasSubgroups", group.size() > 1);
+		            groupItem.put("aggregates", new HashMap<String, Object>());
+		                        
+		            groupItems = new ArrayList<Object>();
+		            groupItem.put("items", groupItems);
+				}
+				groupItems.add(item);
+			}        
+            
+            if (group.size() > 1) {
+            	for (Map<String,Object> g : result) {           		
+            		g.put("items", groupBy((List<?>)g.get("items"), group.subList(1, group.size()), clazz));
+				}
+            }
+        }
+        
+    	return result;
     }
     
-    private static void group(final Criteria criteria, final List<Map<String, String>> group, final Session session, final Class<?> clazz) {
+    private static List<?>  group(final Criteria criteria, final List<Map<String, String>> group, final Session session, final Class<?> clazz) {
+    	List<?> result = new ArrayList<Object>();
+    	
         if (group != null && !group.isEmpty()) {
-            Map<String, String> entry = group.get(0);
-            
-            final String field = entry.get("field");
-            String dir = entry.get("dir");
-            
-            criteria.setProjection(Projections.groupProperty(field));
-            criteria.setResultTransformer(new ResultTransformer() {
-                
-                @Override
-                public Object transformTuple(Object[] value, String[] arg1) {
-                    Criteria criteria = session.createCriteria(clazz);
-                    Map<String, Object> result = new HashMap<String, Object>();
-                    
-                    result.put("value", value[0]);
-                    result.put("field", field);
-                    result.put("hasSubgroups", group.size() > 1);
-                    result.put("aggregates", new HashMap<String, Object>());
-                    
-                    criteria.add(Restrictions.eq(field, value[0]));
-                    
-                    group(criteria, group.subList(1, group.size()), session, clazz);
-                    
-                    result.put("items", criteria.list());
-                    
-                    return result;
-                }
-                
-                @Override
-                public List transformList(List arg0) {
-                    // TODO Auto-generated method stub
-                    return arg0;
-                }
-            });
+            try {
+				result = groupBy(criteria.list(), group, clazz);
+			} catch (IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException | HibernateException
+					| IntrospectionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
+        return result;
     }
 
     private static long total(Criteria criteria) {
@@ -259,17 +291,32 @@ public class DataSourceRequest {
         
         long total = total(criteria);
         
-        group(criteria, getGroup(), session, clazz);
+        List<Map<String, String>> sort = new ArrayList<Map<String, String>>();
         
-        sort(criteria, getSort());
+        List<Map<String, String>> groups = getGroup();
+        List<Map<String, String>> sorts = getSort();
+        
+        if (groups != null) {
+        	sort.addAll(groups);
+        }        
+        
+        if (sorts != null) {
+        	sort.addAll(sorts);
+        }       
+        
+        sort(criteria, sort);
     
-        page(criteria, getTake(), getSkip());
+        page(criteria, getTake(), getSkip());        
         
         DataSourceResult result = new DataSourceResult();
         
         result.setTotal(total);
-        result.setData(criteria.list());
         
+        if (groups != null && !groups.isEmpty()) {        	
+			result.setData(group(criteria, groups, session, clazz));									
+        } else {
+        	result.setData(criteria.list());	
+        }
         return result;
     }
 
