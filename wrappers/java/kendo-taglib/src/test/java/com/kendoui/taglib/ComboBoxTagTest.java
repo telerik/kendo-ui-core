ba92405c2cbeda02851569bdcb8109e857e6e18f
George Krystev <ggkrustev@yahoo.com>
2012-10-31 16:14:57 +0200

    Add tests for the createElement override in the input widgets

---
+++ wrappers/java/kendo-taglib/src/test/java/com/kendoui/taglib/ComboBoxTagTest.java
@@ -0,0 +1,24 @@
+package com.kendoui.taglib;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.spy;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ComboBoxTagTest {
+    private ComboBoxTag tag;
+    
+    @Before
+    public void setUp() throws IOException {
+        tag = spy(new ComboBoxTag());
+
+        tag.setName("foo");
+    }
+    
+    @Test
+    public void createElementCreatedInputElement() throws IOException {
+        assertEquals(tag.html().outerHtml(), "<input id=\"foo\" />");         
+    }
+}
