25700d4f99df7f0b74385369ef390e9676f28f7e
Alex Gyoshev <alex@gyoshev.net>
2012-11-02 11:32:33 +0200

    Add checkboxes example

---
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/controllers/treeview/CheckboxesController.java
@@ -0,0 +1,16 @@
+package com.kendoui.spring.controllers.treeview;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("treeview-checkboxes-controller")
+@RequestMapping(value="/web/treeview/")
+public class CheckboxesController {
+    
+    @RequestMapping(value = "/checkboxes", method = RequestMethod.GET)
+    public String index() {
+        return "web/treeview/checkboxes";
+    }
+}
+
