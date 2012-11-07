db4500c691ffba77b018fc0153e76d9364df6e97
Dimo Dimov <dimodi@gmail.com>
2012-11-07 14:32:55 +0200

    Use global uglifyjs for Window rake js task. Otherwise built path does not work.

--- build/tasks.rb
+++ build/tasks.rb
@@ -2,7 +2,7 @@ require 'rbconfig'
 
 README_DIR = 'resources'
 THIRD_PARTY_LEGAL_DIR = File.join('resources', 'legal', 'third-party')
-UGLIFYJS = File.join(Rake.application.original_dir, "node_modules", "uglify-js2", "bin", "uglifyjs2");
+UGLIFYJS = RUBY_PLATFORM =~ /w32/ ? "uglifyjs2" : File.join(Rake.application.original_dir, "node_modules", "uglify-js2", "bin", "uglifyjs2");
 LESSC = File.join(Rake.application.original_dir, "build", "less-js", "bin", "lessc");
 CSSMIN = File.join(Rake.application.original_dir, "node_modules", "cssmin", "bin", "cssmin");
 
