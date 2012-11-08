7976cec050cf9ebf18cb989a5bfff5906dadd1d9
Nikolay Rusev <nikolay.rusev@telerik.com>
2012-11-08 15:36:51 +0200

    Fix less build on windows

--- build/tasks.rb
+++ build/tasks.rb
@@ -4,7 +4,7 @@ README_DIR = 'resources'
 THIRD_PARTY_LEGAL_DIR = File.join('resources', 'legal', 'third-party')
 UGLIFYJS = RUBY_PLATFORM =~ /w32/ ? "uglifyjs2" : File.join(Rake.application.original_dir, "node_modules", "uglify-js2", "bin", "uglifyjs2");
 LESSC = File.join(Rake.application.original_dir, "build", "less-js", "bin", "lessc");
-CSSMIN = File.join(Rake.application.original_dir, "node_modules", "cssmin", "bin", "cssmin");
+CSSMIN = RUBY_PLATFORM =~ /w32/ ? "cssmin" : File.join(Rake.application.original_dir, "node_modules", "cssmin", "bin", "cssmin");
 
 class MergeTask < Rake::FileTask
     def execute(args=nil)
