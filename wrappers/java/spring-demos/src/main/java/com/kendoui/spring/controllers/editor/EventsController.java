f449e26d5e100bfd66835ca1c2166d0afa27c9e4
Atanas Korchev <akorchev@gmail.com>
2012-11-08 15:46:20 +0200

    Add editor events demo.

---
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/controllers/editor/EventsController.java
@@ -0,0 +1,15 @@
+package com.kendoui.spring.controllers.editor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("editor-events-controller")
+@RequestMapping(value="/web/editor/")
+public class EventsController {
+    
+    @RequestMapping(value = "/events", method = RequestMethod.GET)
+    public String index() {       
+        return "web/editor/events";
+    }    
+}
