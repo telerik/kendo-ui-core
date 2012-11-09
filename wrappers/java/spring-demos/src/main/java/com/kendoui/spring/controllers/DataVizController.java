9ce31fbabf03d4875403f0b6b82bf3dcb533cb7e
Atanas Korchev <akorchev@gmail.com>
2012-11-09 11:35:19 +0200

    Hide links for dataviz examples which do not exist.

--- wrappers/java/spring-demos/src/main/java/com/kendoui/spring/controllers/DataVizController.java
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/controllers/DataVizController.java
@@ -2,8 +2,12 @@ package com.kendoui.spring.controllers;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.codehaus.jackson.JsonFactory;
@@ -16,6 +20,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kendoui.spring.navigation.Example;
 import com.kendoui.spring.navigation.Widget;
 
 @Controller
@@ -26,14 +31,31 @@ public class DataVizController {
     
     @RequestMapping(value="/")
     public String index(Model model) throws JsonParseException, JsonMappingException, IOException {
+        ServletContext context =request.getSession().getServletContext();
         
-        String path = request.getSession().getServletContext().getRealPath("/resources/dataviz.nav.json");
+        String path = context.getRealPath("/resources/dataviz.nav.json");
         
         ObjectMapper mapper = new ObjectMapper(new JsonFactory());
         
         HashMap<String, Widget[]> navigation = mapper.readValue(new File(path), new TypeReference<HashMap<String,Widget[]>>() {}); 
         
         navigation.remove("Sample Dashboards");
+        
+        for (Map.Entry<String, Widget[]> entry : navigation.entrySet()) {
+            for (Widget widget : entry.getValue()) {
+                List<Example> examples = new ArrayList<Example>();
+                
+                for (Example example : widget.getItems()) {
+                    path = context.getRealPath("WEB-INF/views/dataviz/" +example.getUrl().replace(".html", ".jsp"));
+                    
+                    if (new File(path).exists()) {
+                        examples.add(example);
+                    }
+                }
+                
+                widget.setItems(examples.toArray(new Example[examples.size()]));
+            }
+        }
         
         model.addAttribute("navigation", navigation);
         
