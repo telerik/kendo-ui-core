67cf58d99f1eb35e76a8270aaac3ccd13c86a46b
Mihai Bazon <mihai@bazon.net>
2012-11-12 14:26:32 +0200

    make minified files loadable via RequireJS/AMD

---
+++ build/compile.js
@@ -0,0 +1,117 @@
+#! /usr/bin/env node
+
+var fs = require("fs");
+var sys = require("util");
+var u2 = require("uglify-js2");
+var path = require("path");
+var OPT = require("optimist");
+var ARGV = OPT
+    .describe("amd", "Wrap for RequireJS")
+    .describe("deps", "List dependencies")
+    .boolean("amd")
+    .boolean("deps")
+    .wrap(80)
+    .argv;
+
+var get_wrapper = (function(wrapper){
+    var code = '((typeof define == "function" && define.amd) ? define : function(a, b){ return b() })($DEPS, $CONT)';
+    return function() {
+        if (wrapper) return wrapper;
+        wrapper = u2.parse(code);
+        wrapper.wrap = function(deps, cont) {
+            return wrapper.transform(new u2.TreeTransformer(
+                null,           // need no 'before'
+                function after(node){
+                    if (node instanceof u2.AST_SymbolRef) switch (node.name) {
+                      case "$DEPS":
+                        return new u2.AST_Array({
+                            elements: deps.map(function(x){
+                                return new u2.AST_String({ value: x });
+                            })
+                        });
+                      case "$CONT":
+                        cont = cont.clone();
+                        cont.argnames = [];
+                        return new u2.AST_Function(cont);
+                        break;
+                    }
+                }
+            ));
+        };
+        return wrapper;
+    };
+})();
+
+var files = ARGV._.slice();
+
+var deps_file = path.join(path.dirname(fs.realpathSync(__filename)),
+                          "../download-builder/config/kendo-config.VERSION_NUMBER.json");
+deps_file = fs.readFileSync(deps_file, "utf8");
+deps_file = JSON.parse(deps_file);
+
+files.forEach(function(file){
+    var code = fs.readFileSync(file, "utf8");
+    var deps = fetch_dependencies(file);
+    if (ARGV.deps) {
+        sys.puts(file + ": " + deps.join(", "));
+        return;
+    }
+    deps = deps.map(function(dep){
+        return "./" + dep.replace(/\.js$/, "");
+    });
+    var ast = u2.parse(code, { filename: file });
+    ast = get_wrapper().wrap(deps, ast);
+    var compressor = u2.Compressor({ warnings: false });
+    ast.figure_out_scope();
+    ast = ast.transform(compressor);
+    ast.figure_out_scope();
+    ast.compute_char_frequency();
+    ast.mangle_names();
+    var output = ast.print_to_string();
+    sys.print(output);
+});
+
+function assert(cond, msg) {
+    if (!cond) throw new Error(msg || "Failed assertion");
+};
+
+function fetch_dependencies(file) {
+    var deps = {};
+    var done = {};
+
+    function add_component(c) {
+        if (done[c.id]) return;
+        done[c.id] = true;
+        if (c.source) deps[c.source] = true;
+    }
+
+    var comp = find_component_by_source(file);
+    comp.forEach(function(c){
+        add_component(c);
+        if (c.depends) c.depends.forEach(function(id){
+            add_component(find_component(id));
+        });
+        if (c.features) c.features.forEach(function(c){
+            if (c.depends) c.depends.forEach(function(id){
+                add_component(find_component(id));
+            });
+        });
+    });
+
+    return Object.keys(deps).slice(1); // because the first is `file` itself.
+};
+
+function find_component(id) {
+    return deps_file.components.filter(function(c){ return c.id == id })[0];
+};
+
+function find_component_by_source(source) {
+    source = path.basename(source);
+    return deps_file.components.filter(function(c){
+        var src = c.source;
+        if (src) {
+            src = src.toLowerCase().replace(/\.min/g, "");
+            return src == source;
+        }
+    });
+};
--- build/tasks.rb
+++ build/tasks.rb
@@ -5,6 +5,7 @@ THIRD_PARTY_LEGAL_DIR = File.join('resources', 'legal', 'third-party')
 UGLIFYJS = File.join(Rake.application.original_dir, "node_modules", "uglify-js2", "bin", "uglifyjs2")
 LESSC = File.join(Rake.application.original_dir, "build", "less-js", "bin", "lessc")
 CSSMIN = File.join(Rake.application.original_dir, "node_modules", "cssmin", "bin", "cssmin")
+COMPILEJS = File.join(Rake.application.original_dir, "build", "compile.js");
 
 class MergeTask < Rake::FileTask
     def execute(args=nil)
