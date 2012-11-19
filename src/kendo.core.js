83f57491a8a78dfdc759395494ab87788d722feb
Mihai Bazon <mihai@bazon.net>
2012-11-19 12:12:05 +0200

    make jshint happy

--- src/kendo.core.js
+++ src/kendo.core.js
@@ -2760,44 +2760,54 @@ function pad(number, digits, end) {
 })(jQuery);
 
 // this function, along with calls to it, will not exist in compressed builds
-var KENDO_COMPONENT = (function(cache, $, base_url){
+var KENDO_COMPONENT = (function(cache, $, base_url, evil){
+    /*global console:true */
     function url(file){
         return base_url + file;
-    };
+    }
     function load_file(file) {
         var addr = url(file);
         // no point to load it if there's a <script> that already asked for it.
         var scripts = $("script").filter(function(){
-            return this.src.indexOf(addr) == 0;
+            return this.src.indexOf(addr) === 0;
         });
-        if (scripts.length == 0) {
+        if (scripts.length === 0) {
             var req = $.ajax({ url: addr, async: false });
             if (req.status == 200) {
-                (1, eval)(req.responseText);
+                evil(req.responseText);
             } else {
                 console.error("Failed to load dependency: " + addr);
             }
         }
-    };
+    }
     return function(comp) {
         if (!cache[comp.id] || !comp.id) {
-            if (comp.id) cache[comp.id] = comp;
+            if (comp.id) {
+                cache[comp.id] = comp;
+            }
             var deps = comp.depends ? comp.depends.slice(0) : [];
-            if (comp.features) comp.features.forEach(function(f){
-                if (f.depends) deps = deps.concat(f.depends);
-            });
+            if (comp.features) {
+                comp.features.forEach(function(f){
+                    if (f.depends) {
+                        deps = deps.concat(f.depends);
+                    }
+                });
+            }
             deps.forEach(function(id){
-                if (!cache[id])
+                if (!cache[id]) {
                     load_file("kendo." + id + ".js");
+                }
             });
-            if (comp.files) comp.files.forEach(load_file);
+            if (comp.files) {
+                comp.files.forEach(load_file);
+            }
         }
     };
 })({}, jQuery,
    // the following figures out the base URL of the running script.
    (function(x){
        return x[x.length - 1].src.replace(/\/+[^\/]*$/, "/");
-   })(document.getElementsByTagName("script")));
+   })(document.getElementsByTagName("script")), eval);
 
 KENDO_COMPONENT({
     id: "core",
