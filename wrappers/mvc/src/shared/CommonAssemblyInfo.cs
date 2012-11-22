512df2917dd49af755e3436cb865bab0301dfc2e
stefanrahnev <stefan.rahnev@telerik.com>
2012-11-22 16:46:47 +0200

    Updated once again the copyright message string

--- wrappers/mvc/src/shared/CommonAssemblyInfo.cs
+++ wrappers/mvc/src/shared/CommonAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Security;
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Telerik")]
-[assembly: AssemblyCopyright("Copyright © 2012 Telerik AD")]
+[assembly: AssemblyCopyright("Copyright © 2012-2013 Telerik AD")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
