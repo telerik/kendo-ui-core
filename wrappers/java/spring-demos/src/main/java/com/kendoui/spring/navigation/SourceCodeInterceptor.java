170b9a45cee0d31f5227ae063099d91ea7fe7cd0
Atanas Korchev <akorchev@gmail.com>
2012-11-22 14:14:45 +0200

    Change the implementation as per Spring's PM suggestion.

--- wrappers/java/spring-demos/src/main/java/com/kendoui/spring/navigation/SourceCodeInterceptor.java
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/navigation/SourceCodeInterceptor.java
@@ -1,27 +1,29 @@
 package com.kendoui.spring.navigation;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.InputStreamReader;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.context.support.ServletContextResource;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class SourceCodeInterceptor extends HandlerInterceptorAdapter  {
+public class SourceCodeInterceptor extends HandlerInterceptorAdapter implements ServletContextAware  {
+    private ServletContext context;
+    
     @Override
     public void postHandle(HttpServletRequest request, 
             HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
         
         if (modelAndView != null) {
-           
-            String filename = modelAndView.getViewName() + ".jsp";
+            ServletContextResource resource = new ServletContextResource(context, "/WEB-INF/views/" + modelAndView.getViewName() + ".jsp");
             
-            String realPath = request.getSession().getServletContext().getRealPath("/WEB-INF/views/" + filename);
-            
-            BufferedReader input = new BufferedReader(new FileReader(realPath));
+            BufferedReader input = new BufferedReader(new InputStreamReader(resource.getInputStream()));
             
             StringBuilder content = new StringBuilder();
             String line = input.readLine();
@@ -35,5 +37,10 @@ public class SourceCodeInterceptor extends HandlerInterceptorAdapter  {
             
             modelAndView.addObject("view", content.toString());
         }
+    }
+
+    @Override
+    public void setServletContext(ServletContext context) {
+        this.context = context;
     }
 }
