008a73d0bc9e8769d4ef885189805bfc73940e4e
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2012-11-27 15:09:13 +0200

    Display no records text when server binding is used. Fixes #870

--- wrappers/mvc/src/Kendo.Mvc/UI/Grid/Html/GridPagerStatusBuilder.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/Grid/Html/GridPagerStatusBuilder.cs
@@ -9,10 +9,16 @@ namespace Kendo.Mvc.UI.Html
         {
             var firstItemInPage = pager.Total > 0 ? (pager.Page - 1) * pager.PageSize + 1 : 0;
             var lastItemInPage = Math.Min(pager.PageSize * pager.Page, pager.Total);
+            var message = pager.Messages.Empty;
+
+            if (pager.Total > 0)
+            {
+                message = pager.Messages.Display.FormatWith(firstItemInPage, lastItemInPage, pager.Total);
+            }
 
             return new HtmlElement("span")
                         .AddClass("k-pager-info", "k-label")
-                        .Text(pager.Messages.Display.FormatWith(firstItemInPage, lastItemInPage, pager.Total));
+                        .Text(message);
         }
     }
 }
