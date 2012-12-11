cc76f9ad7038fa8cb16d7c8d9dbd750729ed91d5
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-12-11 15:03:28 +0200

    Fast-forward docs and update StockChart Java wrapper

---
+++ wrappers/java/kendo-taglib/src/main/java/com/kendoui/taglib/stockchart/SeriesItemHighlightTag.java
@@ -0,0 +1,80 @@
+
+package com.kendoui.taglib.stockchart;
+
+
+import com.kendoui.taglib.BaseTag;
+
+
+
+
+
+
+import javax.servlet.jsp.JspException;
+
+@SuppressWarnings("serial")
+public class SeriesItemHighlightTag extends  BaseTag  /* interfaces *//* interfaces */ {
+    
+    @Override
+    public int doEndTag() throws JspException {
+//>> doEndTag
+
+
+        SeriesItemTag parent = (SeriesItemTag)findParentWithClass(SeriesItemTag.class);
+
+
+        parent.setHighlight(this);
+
+//<< doEndTag
+
+        return super.doEndTag();
+    }
+
+    @Override
+    public void initialize() {
+//>> initialize
+//<< initialize
+
+        super.initialize();
+    }
+
+    @Override
+    public void destroy() {
+//>> destroy
+//<< destroy
+
+        super.destroy();
+    }
+
+//>> Attributes
+
+    public static String tagName() {
+        return "stockChart-seriesItem-highlight";
+    }
+
+    public void setBorder(SeriesItemHighlightBorderTag value) {
+        setProperty("border", value);
+    }
+
+    public void setLine(SeriesItemHighlightLineTag value) {
+        setProperty("line", value);
+    }
+
+    public float getOpacity() {
+        return (float)getProperty("opacity");
+    }
+
+    public void setOpacity(float value) {
+        setProperty("opacity", value);
+    }
+
+    public String getColor() {
+        return (String)getProperty("color");
+    }
+
+    public void setColor(String value) {
+        setProperty("color", value);
+    }
+
+//<< Attributes
+
+}
