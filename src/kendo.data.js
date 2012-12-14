6c82dde4f48854652a1f65b65e7ae8831c0a4960
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2012-12-14 15:40:12 +0200

    Fix sorting on negative numbers and zeros. Fixes #928

--- src/kendo.data.js
+++ src/kendo.data.js
@@ -646,11 +646,11 @@ kendo_module({
                     return 0;
                 }
 
-                if ((a && !b) || b == null) {
+                if ((a && !b && a > 0) || b == null) {
                     return 1;
                 }
 
-                if (b && !a) {
+                if (b && !a && b > 0) {
                     return -1;
                 }
 
@@ -668,11 +668,11 @@ kendo_module({
                     return 0;
                 }
 
-                if ((a && !b) || b == null) {
+                if ((a && !b && a > 0) || b == null) {
                     return -1;
                 }
 
-                if ((b && !a) || a == null) {
+                if ((b && !a && b > 0) || a == null) {
                     return 1;
                 }
 
