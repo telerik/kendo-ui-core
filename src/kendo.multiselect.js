03588a6c868ea46bc854ddde2c3ebedf4d66880a
George Krystev <ggkrustev@yahoo.com>
2012-12-15 18:41:16 +0200

    Implement simple selection of items

--- src/kendo.multiselect.js
+++ src/kendo.multiselect.js
@@ -1,15 +1,16 @@
 kendo_module({
-    id: "combobox",
-    name: "ComboBox",
+    id: "multiselect",
+    name: "MultiSelect",
     category: "web",
-    description: "The ComboBox widget allows the selection from pre-defined values or entering a new value.",
+    description: "The MultiSelect widget allows the selection from pre-defined values.",
     depends: [ "list" ]
 });
 
 (function($, undefined) {
     var kendo = window.kendo,
         ui = kendo.ui,
-        Widget = ui.Widget;
+        Widget = ui.Widget,
+        CHANGE = "change";
 
     var MultiSelect = Widget.extend({
 
@@ -19,14 +20,28 @@ kendo_module({
             Widget.fn.init.call(that, element, options);
 
             that._wrapper();
-            that._list();
+            that._container();
             that._input();
+            that.input.on("click", function() {
+                that.popup.open();
+            });
+
+            that._templates();
+            that._dataSource();
+
+            that._list();
+            that._popup();
 
             that.element.hide();
+
+            if (that.options.autoBind) {
+                that.dataSource.fetch();
+            }
         },
 
         options: {
-            name: "MultiSelect"
+            name: "MultiSelect",
+            autoBind: true
         },
 
         events: [
@@ -37,14 +52,124 @@ kendo_module({
 
         },
 
+        refresh: function() {
+            var that = this,
+                data = that.dataSource.view();
+
+            that.ul[0].innerHTML = kendo.render(that.itemTemplate, data);
+        },
+
+        _popup: function() {
+            var that = this,
+                list = that.list,
+                options = that.options,
+                wrapper = that.wrapper;
+
+            that.popup = new ui.Popup(list, $.extend({}, options.popup, {
+                anchor: wrapper,
+                isRtl: kendo.support.isRtl(wrapper)
+            }));
+        },
+
+        _templates: function() {
+            var that = this,
+                options = that.options,
+                itemTemplate = options.itemTemplate,
+                tagTemplate = options.tagTemplate,
+                hasDataSource = options.dataSource;
+
+            if (that.element.is("select") && that.element[0].length) {
+                if (!hasDataSource) {
+                    options.dataTextField = options.dataTextField || "text";
+                    options.dataValueField = options.dataValueField || "value";
+                }
+            }
+
+            if (!itemTemplate) {
+                that.itemTemplate = kendo.template('<li tabindex="-1" role="option" unselectable="on" class="k-item">${data' + (options.dataTextField ? "." : "") + options.dataTextField + "}</li>", { useWithBlock: false });
+            } else {
+                itemTemplate = kendo.template(itemTemplate);
+                that.itemTemplate = function(data) {
+                    return '<li tabindex="-1" role="option" unselectable="on" class="k-item">' + itemTemplate(data) + "</li>";
+                };
+            }
+
+            //TODO: use options.tagTemplate if defined
+            that.tagTemplate = kendo.template('<li><span class="">#:data' + (options.dataTextField ? "." : "") + options.dataTextField + "#</span><span>delete</span></li>", { useWithBlock: false });
+        },
+
+        _unbindDataSource: function() {
+            var that = this;
+
+            that.dataSource.unbind(CHANGE, that._refreshHandler);
+                           //.unbind(PROGRESS, that._progressHandler)
+                           //.unbind(REQUESTEND, that._requestEndHandler);
+        },
+
+        _dataSource: function() {
+            var that = this,
+                element = that.element,
+                options = that.options,
+                dataSource = options.dataSource || {},
+                idx;
+
+            dataSource = $.isArray(dataSource) ? {data: dataSource} : dataSource;
+
+            /*if (element.is(SELECT)) {
+                idx = element[0].selectedIndex;
+                if (idx > -1) {
+                    options.index = idx;
+                }
+
+                dataSource.select = element;
+                dataSource.fields = [{ field: options.dataTextField },
+                                     { field: options.dataValueField }];
+            }*/
+
+            dataSource.select = element;
+            dataSource.fields = [{ field: options.dataTextField },
+                                 { field: options.dataValueField }];
+
+            if (that.dataSource && that._refreshHandler) {
+                that._unbindDataSource();
+            } else {
+                that._refreshHandler = $.proxy(that.refresh, that);
+                //that._progressHandler = proxy(that._showBusy, that);
+                //that._requestEndHandler = proxy(that._requestEnd, that);
+            }
+
+            that.dataSource = kendo.data.DataSource.create(dataSource)
+                                   .bind(CHANGE, that._refreshHandler);
+                                   //.bind(PROGRESS, that._progressHandler)
+                                   //.bind(REQUESTEND, that._requestEndHandler);
+        },
+
         _input: function() {
             this.input = $('<input class="k-input" style="width: 100%" />')
                             .appendTo(this._innerWraper);
         },
 
+        _container: function() {
+            this.container = $('<ul unselectable="on" class="k-list k-reset"/>')
+                                .appendTo(this._innerWraper);
+        },
+
+        _click: function(e) {
+            var li = $(e.currentTarget),
+                data = this.dataSource.view(),
+                dataItem = data[li.index()];
+
+            this.container.append(this.tagTemplate(dataItem));
+
+            li.hide();
+        },
+
         _list: function() {
-            this.list = $('<ul unselectable="on" class="k-list k-reset"/>')
-                            .appendTo(this._innerWraper);
+            this.ul = $('<ul unselectable="on" class="k-list k-reset"/>')
+                        .on("click", "li.k-item", $.proxy(this._click, this));
+
+            this.list = $("<div class='k-list-container'/>")
+                        .append(this.ul);
         },
 
         _wrapper: function() {
--- tests/multiselect/tests.js
+++ tests/multiselect/tests.js
@@ -2,5 +2,9 @@ tests.push(
     {
         page: "multiselect/initialization.html",
         title: "multiselect initialization"
+    },
+    {
+        page: "multiselect/interaction.html",
+        title: "multiselect interaction"
     }
 );
