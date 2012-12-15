2fa0082e7e45bca35d046c2c6c6de05b1ac4b070
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2012-12-15 10:56:15 +0200

    Revert to one INSERT per row. Add clustered index on Date.

--- playground/intraday/dbcreate.rb
+++ playground/intraday/dbcreate.rb
@@ -8,29 +8,30 @@ YEARS_TO_ADD = Time.now.year - 2009
 DATE_FORMAT = '%d-%m-%Y %H:%M:%S'
 
 puts %Q{
-CREATE TABLE Intraday (
-    [Symbol] VARCHAR(10) NOT NULL,
-    [Date] DATETIME NOT NULL,
-    [Open] DECIMAL(9,3) NOT NULL,
-    [High] DECIMAL(9,3) NOT NULL,
-    [Low] DECIMAL(9,3) NOT NULL,
-    [Close] DECIMAL(9,3) NOT NULL,
-    [Volume] BIGINT NOT NULL
-)
+    CREATE TABLE Intraday (
+        [Symbol] VARCHAR(10) NOT NULL,
+        [Date] DATETIME NOT NULL,
+        [Open] DECIMAL(9,3) NOT NULL,
+        [High] DECIMAL(9,3) NOT NULL,
+        [Low] DECIMAL(9,3) NOT NULL,
+        [Close] DECIMAL(9,3) NOT NULL,
+        [Volume] BIGINT NOT NULL
+    )
+    GO
 
-INSERT INTO Intraday
-    ([Symbol], [Date], [Open], [High], [Low], [Close], [Volume])
-VALUES
+    CREATE CLUSTERED INDEX IX_Intraday_Date
+        ON Intraday ([Date])
+    GO
 }
 
-lastline = nil
 CSV.foreach('data.csv', :headers => true) do |row|
-    puts lastline + ',' unless lastline.nil?
-
     date = DateTime.strptime("#{row['Date']} #{row['Time']}", '%m/%d/%Y %H:%M')
     date = date << -YEARS_TO_ADD * 12
 
-    lastline = "('ACME', CONVERT(DATETIME, '#{date.strftime(DATE_FORMAT)}', 103), #{row['Open']}, #{row['High']}, #{row['Low']}, #{row['Close']}, #{row['Volume']})"
+    puts %Q{
+    INSERT INTO Intraday
+        ([Symbol], [Date], [Open], [High], [Low], [Close], [Volume])
+    VALUES
+        ('ACME', CONVERT(DATETIME, '#{date.strftime(DATE_FORMAT)}', 103), #{row['Open']}, #{row['High']}, #{row['Low']}, #{row['Close']}, #{row['Volume']})
+    }
 end
-
-puts lastline
