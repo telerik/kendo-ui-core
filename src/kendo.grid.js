61348e6bae69474afae6d2a36a6ba39a77324a4f
Nikolay Rusev <nikolay.rusev@telerik.com>
2012-12-21 17:07:47 +0200

    Fix incell editing on ipad #950.

--- src/kendo.grid.js
+++ src/kendo.grid.js
@@ -1489,9 +1489,9 @@
                 }
 
                 if (editable && editable.create !== false) {
-                    container.on("touchend" + NS + " " + CLICK + NS, ".k-grid-add", function(e) { e.preventDefault(); that.addRow(); })
-                        .on("touchend" + NS + " " + CLICK + NS, ".k-grid-cancel-changes", function(e) { e.preventDefault(); that.cancelChanges(); })
-                        .on("touchend" + NS + " " + CLICK + NS, ".k-grid-save-changes", function(e) { e.preventDefault(); that.saveChanges(); });
+                    container.on(CLICK + NS, ".k-grid-add", function(e) { e.preventDefault(); that.addRow(); })
+                        .on(CLICK + NS, ".k-grid-cancel-changes", function(e) { e.preventDefault(); that.cancelChanges(); })
+                        .on(CLICK + NS, ".k-grid-save-changes", function(e) { e.preventDefault(); that.saveChanges(); });
                 }
             }
         },
