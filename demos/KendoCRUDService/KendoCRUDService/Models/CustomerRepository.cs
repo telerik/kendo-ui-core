c64fe5fe06a64bfe8cd270d4a7ebcc903b53bc03
Petur Subev <petur.subev@gmail.com>
2013-01-02 16:47:52 +0200

    Add Customers service to KendoCRUDService

---
+++ demos/KendoCRUDService/KendoCRUDService/Models/CustomerRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoCRUDService.Models
+{
+    public static class CustomerRepository
+    {
+        public static IList<CustomerModel> All()
+        {
+            IList<CustomerModel> result = HttpContext.Current.Session["Customers"] as IList<CustomerModel>;
+
+            if (result == null)
+            {
+                HttpContext.Current.Session["Customers"] = result = new NorthwindDataContext().Customers.Select(c => new CustomerModel
+                {
+                    CustomerID = c.CustomerID,
+                    ContactName = c.ContactName,
+                    CompanyName = c.CompanyName,
+                }).ToList();
+            }
+
+            return result;
+        }
+    }
+}
