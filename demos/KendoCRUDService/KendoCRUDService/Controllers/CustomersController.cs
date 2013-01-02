5828f83c1f4106db03e6df3439fdffd8093ec746
Petur Subev <petur.subev@gmail.com>
2013-01-02 16:47:52 +0200

    Add Customers service to KendoCRUDService

---
+++ demos/KendoCRUDService/KendoCRUDService/Controllers/CustomersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using KendoCRUDService.Models;
+using KendoCRUDService.Common;
+
+namespace KendoCRUDService.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.Jsonp(CustomerRepository.All());
+        }
+    }
+}
---
+++ demos/KendoCRUDService/KendoCRUDService/Models/CustomerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoCRUDService.Models
+{
+    public class CustomerModel
+    {
+        public string CustomerID { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
