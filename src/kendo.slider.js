5a804cbe5ee426c0735430b7ba824b7e8b95fab0
Hristo Germanov <hristo.germanov@telerik.com>
2013-01-02 13:44:30 +0200

    Slider loses drag events in IE8. Closes #970

--- src/kendo.slider.js
+++ src/kendo.slider.js
@@ -936,7 +936,7 @@ kendo_module({
         that.type = type;
 
         that.draggable = new Draggable(dragHandle, {
-            threshold: 0,
+            distance: 0,
             dragstart: proxy(that._dragstart, that),
             drag: proxy(that.drag, that),
             dragend: proxy(that.dragend, that),
