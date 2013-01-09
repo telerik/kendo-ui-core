363dc9250ca53a07e30b08f949a2a61d49368238
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2013-01-09 11:42:08 +0200

    Add mvc version of Filter menu customization demo. #954

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Web/Grid/Filter_Menu_CustomizationController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Kendo.Mvc.Examples.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System.Linq;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class GridController : Controller
+    {
+        public ActionResult Filter_Menu_Customization()
+        {            
+            return View();
+        }
+
+        public ActionResult FilterMenuCustomization_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(GetEmployees().ToDataSourceResult(request));
+        }
+
+        public ActionResult FilterMenuCustomization_Cities()
+        {
+            var db = new NorthwindDataContext();
+            return Json(db.Employees.Select(e => e.City).Distinct(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FilterMenuCustomization_Titles()
+        {
+            var db = new NorthwindDataContext();
+            return Json(db.Employees.Select(e => e.Title).Distinct(), JsonRequestBehavior.AllowGet);
+        } 
+    }
+}
