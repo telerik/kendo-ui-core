0364463859d13ef740a995bf131db79ce8d5880e
Alex Gyoshev <alex@gyoshev.net>
2013-01-09 14:47:08 +0200

    Add tests for demos navigation JSON

--- tests/demos/tests.js
+++ tests/demos/tests.js
@@ -4,6 +4,10 @@ tests.push(
         title: "demos application"
     },
     {
+        page: "demos/navigation.html",
+        title: "demos navigation"
+    },
+    {
         page: "demos/lint.html",
         title: "demos lint"
     }
