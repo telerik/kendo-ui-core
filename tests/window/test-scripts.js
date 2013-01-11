4fc93160ab4c7abb774585a11ddf1213fb644811
Alex Gyoshev <alex@gyoshev.net>
2013-01-11 15:35:08 +0200

    Move window test scripts to common include

---
+++ tests/window/test-scripts.js
@@ -0,0 +1,16 @@
+document.write('\
+    <script src="../../src/jquery.js"></script>\
+    <script src="../../src/kendo.core.js"></script>\
+    <script src="../../src/kendo.data.js"></script>\
+    <script src="../../src/kendo.binder.js"></script>\
+    <script src="../../src/kendo.fx.js"></script>\
+    <script src="../../src/kendo.userevents.js"></script>\
+    <script src="../../src/kendo.draganddrop.js"></script>\
+    <script src="../../src/kendo.window.js"></script>\
+    <script src="../qunit/qunit/qunit.js"></script>\
+    <script src="../qunit-runner.js"></script>\
+    <link rel="stylesheet" href="../qunit/qunit/qunit.css" />\
+    <link rel="stylesheet/less" href="../../styles/web/kendo.common.less">\
+    <link rel="stylesheet/less" href="../../styles/web/kendo.default.less">\
+    <script src="../../demos/mvc/content/shared/js/less.js"></script>\
+');
