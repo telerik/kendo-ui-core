062b7ab1944e629f4ec78633c6a60967ba044167
Hristo Germanov <hristo.germanov@telerik.com>
2013-01-14 22:47:50 +0200

    Add stock chart events in the java wrapper. Update documentation.

---
+++ wrappers/java/kendo-taglib/src/main/java/com/kendoui/taglib/stockchart/DragFunctionTag.java
@@ -0,0 +1,48 @@
+
+package com.kendoui.taglib.stockchart;
+
+import com.kendoui.taglib.FunctionTag;
+
+import com.kendoui.taglib.StockChartTag;
+
+
+import javax.servlet.jsp.JspException;
+
+@SuppressWarnings("serial")
+public class DragFunctionTag extends FunctionTag /* interfaces */ /* interfaces */ {
+    
+    @Override
+    public int doEndTag() throws JspException {
+//>> doEndTag
+
+
+        StockChartTag parent = (StockChartTag)findParentWithClass(StockChartTag.class);
+
+
+        parent.setDrag(this);
+
+//<< doEndTag
+
+        return super.doEndTag();
+    }
+
+    @Override
+    public void initialize() {
+//>> initialize
+//<< initialize
+
+        super.initialize();
+    }
+
+    @Override
+    public void destroy() {
+//>> destroy
+//<< destroy
+
+        super.destroy();
+    }
+
+//>> Attributes
+//<< Attributes
+
+}
