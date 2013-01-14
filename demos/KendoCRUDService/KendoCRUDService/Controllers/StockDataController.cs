b825ecb300331f549b354c6b98b922c487d822bc
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2013-01-14 15:27:27 +0200

    Add StockData service to Kendo Service application

---
+++ demos/KendoCRUDService/KendoCRUDService/Controllers/StockDataController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using KendoCRUDService.Common;
+using KendoCRUDService.Models;
+using KendoCRUDService.Models.EF;
+
+namespace KendoCRUDService.Controllers
+{
+    public class StockDataController : Controller
+    {
+        private readonly DateTime MIN_DATE = new DateTime(1990, 1, 1);
+        private readonly BaseUnit DEFAULT_UNIT = BaseUnit.Months;
+
+        public ActionResult Index()
+        {
+            var dateFrom = MIN_DATE;
+            var dateTo = DateTime.Now;
+            IEnumerable<StockDataPoint> result;
+
+            using (var db = new StockDataEntities())
+            {
+                BaseUnit baseUnit;
+                if (!Enum.TryParse(this.HttpContext.Request.QueryString.Get("baseUnit"), true, out baseUnit))
+                {
+                    baseUnit = DEFAULT_UNIT;
+                }
+
+                var filter = this.DeserializeObject<DataFilter>("filter");
+                if (filter != null && filter.Filters != null)
+                {
+                    dateFrom = DateTime.Parse(filter.Filters[0].Value.ToString());
+                    dateTo = DateTime.Parse(filter.Filters[1].Value.ToString());
+                }
+
+                var groupedData =
+                    from s in db.Intraday
+                    where s.Date >= dateFrom && s.Date <= dateTo
+                    group s by new
+                    {
+                        Year = s.Date.Year,
+                        Month = baseUnit >= BaseUnit.Months ? s.Date.Month : 1,
+                        Day = baseUnit >= BaseUnit.Weeks ? s.Date.Day : 1,
+                        Hour = baseUnit >= BaseUnit.Hours ? s.Date.Hour : 0,
+                        Minute = baseUnit >= BaseUnit.Minutes ? s.Date.Minute : 0
+                    } into g
+                    select g;
+
+                var points =
+                    from g in groupedData.ToList()
+                    select new StockDataPoint
+                    {
+                        Date = new DateTime(g.Key.Year, g.Key.Month, g.Key.Day, g.Key.Hour, g.Key.Minute, 0),
+                        Open = g.Max(s => s.Open),
+                        High = g.Max(s => s.High),
+                        Low = g.Min(s => s.Low),
+                        Close = g.Max(s => s.Close),
+                        Volume = g.Sum(s => s.Volume)
+                    };
+
+                result = points.ToList();
+            }
+
+
+            return this.Jsonp(result);
+        }
+    }
+}
