c4a0b6609337de28d10bc1837492ccc4ecbe6d8a
Dimo Dimov <dimodi@gmail.com>
2013-01-14 14:51:19 +0200

    remove alert

--- src/kendo.window.js
+++ src/kendo.window.js
@@ -605,7 +605,6 @@
             if (!$(activeElement).is(winElement) &&
                 !$(target).is(TITLEBAR_BUTTONS + "," + TITLEBAR_BUTTONS + " .k-icon,:input") &&
                 (!winElement.find(activeElement).length || !winElement.find(target).length)) {
-                    alert("focus");
                 winElement.focus();
 
                 var scrollTop = $(window).scrollTop(),
