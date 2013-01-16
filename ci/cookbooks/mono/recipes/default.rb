6d6e3203188018dcf4078ae4c5068f2e580c81f5
Petyo Ivanov <underlog@gmail.com>
2013-01-16 11:03:26 +0200

    Include moz certificates in mono setup

--- ci/cookbooks/mono/recipes/default.rb
+++ ci/cookbooks/mono/recipes/default.rb
@@ -2,6 +2,11 @@ package "mono-devel"
 package "mono-xsp4"
 package "mono-xbuild"
 
+bash "setup moz certificates" do
+    code "mozroots --import --sync --machine"
+end
 remote_directory '/usr/lib/mono/xbuild/Microsoft/VisualStudio/v10.0' do
     source 'v10.0'
 end
+
+
