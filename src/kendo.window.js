1d31abf8b1abe17be78003765695526979b17c8c
Dimo Dimov <dimodi@gmail.com>
2013-01-16 14:01:04 +0200

    fix LINT validation - Window

--- src/kendo.window.js
+++ src/kendo.window.js
@@ -594,7 +594,7 @@ kendo_module({
             try {
                 // IE throws error when Window contains iframe
                 activeElement = document.activeElement;
-            } catch (e) {}
+            } catch (err) {}
 
             $(KWINDOW).each(function(i, element) {
                 var windowObject = $(element),
