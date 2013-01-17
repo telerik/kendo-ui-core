4480758e66da15d700f8ec79d9c9fe7cff0dbc1d
Atanas Korchev <akorchev@gmail.com>
2013-01-17 15:49:50 +0200

    Create demos startup page.

--- wrappers/php/index.php
+++ wrappers/php/index.php
@@ -1,54 +1,22 @@
 <!doctype html>
 <html>
     <head>
-        <link href="http://cdn.kendostatic.com/2012.3.1315/styles/kendo.common.min.css" rel="stylesheet" />
-        <link href="http://cdn.kendostatic.com/2012.3.1315/styles/kendo.default.min.css" rel="stylesheet" />
-        <link href="http://cdn.kendostatic.com/2012.3.1315/styles/kendo.dataviz.min.css" rel="stylesheet" />
-        <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-        <script src="http://cdn.kendostatic.com/2012.3.1315/js/kendo.all.min.js"></script>
+        <title>Kendo UI for PHP - Offline Examples</title>
+        <link href="content/shared/styles/suite.css" rel="stylesheet">
     </head>
     <body>
-<?php
-
-require_once 'lib/kendo/Autoload.php';
-
-class Window extends kendo\ui\Widget {
-    public function tagName() {
-        return 'div';
-    }
-
-    public function name() {
-        return 'Window';
-    }
-
-    public function createElement() {
-        $element = parent::createElement();
-
-        $element->html($this->content);
-
-        return $element;
-    }
-
-    private $content;
-
-    public function setContent($content) {
-        $this->content = $content;
-    }
-}
-
-$window = new Window('window');
-
-ob_start();
-
-?>
-
-<strong>Window</strong> content goes here
-
-<?php
-
-$window->setContent(ob_get_clean());
-
-echo $window->render();
-?>
+        <div id="page">
+            <h2>Kendo UI for PHP</h2>
+             <ul id="suites">
+                 <li class="box">
+                    <a href="web/index.php" class="imgPlate"><img src="content/shared/styles/demos-web.png" /></a>
+                    <h2><a href="web/index.php">Web examples</a></h2>
+                </li>
+                 <li class="box">
+                    <a href="dataviz/index.php" class="imgPlate"><img src="content/shared/styles/demos-dataviz.png" /></a>
+                    <h2><a href="dataviz/index.php">DataViz examples</a></h2>
+                </li>
+            </ul>
+        </div>
     </body>
 </html>
