8fdd8b17d00010855a94aaf682889b1ca93602f5
George Krystev <ggkrustev@yahoo.com>
2013-01-18 13:51:11 +0200

    Implement combobox filtering in cascading example

--- wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Web/ComboBox/CascadingComboBoxController.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Web/ComboBox/CascadingComboBoxController.cs
@@ -18,7 +18,7 @@
             return Json(northwind.Categories.Select(c => new { CategoryId = c.CategoryID, CategoryName = c.CategoryName }), JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetCascadeProducts(string categories)
+        public JsonResult GetCascadeProducts(string categories, string productFilter)
         {
             var northwind = new NorthwindDataContext();
             var products = northwind.Products.AsQueryable();
@@ -28,10 +28,15 @@
                 products = products.Where(p => p.CategoryID.ToString() == categories);
             }
 
-            return Json(products.Select(p => new { ProductID = p.ProductID, ProductName = p.ProductName}), JsonRequestBehavior.AllowGet);
+            if (!string.IsNullOrEmpty(productFilter))
+            {
+                products = products.Where(p => p.ProductName.Contains(productFilter));
+            }
+
+            return Json(products.Select(p => new { ProductID = p.ProductID, ProductName = p.ProductName }), JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetCascadeOrders(string products)
+        public JsonResult GetCascadeOrders(string products, string orderFilter)
         {
             var northwind = new NorthwindDataContext();
             var orders = northwind.Order_Details.AsQueryable();
@@ -39,6 +44,11 @@
             if (!string.IsNullOrEmpty(products))
             {
                 orders = orders.Where(o => o.ProductID.ToString() == products);
+            }
+
+            if (!string.IsNullOrEmpty(orderFilter))
+            {
+                orders = orders.Where(o => o.Order.ShipCity.Contains(orderFilter));
             }
 
             return Json(orders.Select(o => new { OrderID = o.OrderID, ShipCity = o.Order.ShipCity }), JsonRequestBehavior.AllowGet);
