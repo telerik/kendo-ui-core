d16633b165b55ea947ffb6fbd2d9052fc94d7749
Atanas Korchev <akorchev@gmail.com>
2013-01-22 11:08:52 +0200

    Set the selected index to -1 if the value is not found. Closes #1077.

--- src/kendo.binder.js
+++ src/kendo.binder.js
@@ -631,6 +631,7 @@
                     value = this.bindings[VALUE].get(),
                     values = value,
                     field = this.options.valueField || this.options.textField,
+                    found = false,
                     optionValue;
 
                 if (!(values instanceof ObservableArray)) {
@@ -652,8 +653,13 @@
 
                         if (optionValue == value) {
                             options[optionIndex].selected = true;
+                            found = true;
                         }
                     }
+                }
+
+                if (!found) {
+                    element.selectedIndex = -1;
                 }
             },
             destroy: function() {
