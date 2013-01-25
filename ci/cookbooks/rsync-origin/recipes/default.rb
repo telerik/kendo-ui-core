9e9b7738aad01bf8ef7bfed735676821ca5ea1fd
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2013-01-25 18:02:41 +0200

    Remove xinetd and use SSH

--- ci/cookbooks/rsync-origin/recipes/default.rb
+++ ci/cookbooks/rsync-origin/recipes/default.rb
@@ -1,20 +1,5 @@
-package "xinetd"
 package "rsync"
-
-cookbook_file "/etc/xinetd.d/rsync" do
-    source "rsync"
-end
 
 cookbook_file "/etc/rsyncd.conf" do
     source "rsyncd.conf"
-end
-
-bash "enable_rsync" do
-    code %Q{
-        chkconfig xinetd on
-    }
-end
-
-service "xinetd" do
-    action :restart
 end
