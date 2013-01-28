c68acc5de1e8d7cbaf49ab9229f3984a974ce03a
Petyo Ivanov <underlog@gmail.com>
2013-01-28 14:12:07 +0200

    Refactor chef bootstrap scripting
    Make it cross-platform (Ubuntu/CentOS)

--- ci/chef-setup-ubuntu.sh
+++
@@ -1,6 +0,0 @@
-apt-get install -y python-software-properties
-apt-add-repository -y ppa:brightbox/ruby-ng
-apt-get update -y
-
-apt-get install -y ruby1.9.3 build-essential
-gem install chef --no-ri --no-rdoc
