c31cda36206777ab5a55072d12f1b32241e4d365
Petyo Ivanov <underlog@gmail.com>
2013-01-28 16:46:27 +0200

    cdn deploy deploys to kendorigin
    And pushes to kendo-cdn git repo

--- build/cdn.rb
+++ build/cdn.rb
@@ -1,6 +1,20 @@
 namespace :cdn do
     desc('Deploy assets to CloudFront')
+
     task :deploy => 'bundles:cdn.commercial' do
-        msbuild 'build/cdn.proj', "/p:Version=#{VERSION} /p:BundleRoot=../dist/bundles/cdn.commercial"
+        sh <<-SH
+        if [ ! -d kendo-cdn ]
+        then
+            git clone git@github.com:telerik/kendo-cdn.git
+        fi
+        cd kendo-cdn;
+        git pull;
+        mkdir -p #{VERSION};
+        rsync -av ../dist/bundles/cdn.commercial/ #{VERSION}/;
+        git add #{VERSION};
+        git commit --message "Automatic CDN upload of #{VERSION}";
+        git push;
+        rsync -avz --exclude '.git' --delete-excluded ./ kendoorigin:/usr/share/nginx/html/;
+        SH
     end
 end
