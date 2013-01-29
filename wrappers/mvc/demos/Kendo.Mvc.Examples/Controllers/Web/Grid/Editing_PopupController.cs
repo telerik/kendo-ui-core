f4d2888da48ba164916bca09f8bda389bb77d629
Daniel Karadachki <karada@abv.bg>
2013-01-29 22:35:27 +0200

    Unify Grid Popup editing demos

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Web/Grid/Editing_PopupController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Kendo.Mvc.Examples.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class GridController : Controller
+    {
+        public ActionResult Editing_Popup()
+        {
+            return View();
+        }
+
+        public ActionResult EditingPopup_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(SessionProductRepository.All().ToDataSourceResult(request));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult EditingPopup_Create([DataSourceRequest] DataSourceRequest request, ProductViewModel product)
+        {
+            if (product != null && ModelState.IsValid)
+            {
+                SessionProductRepository.Insert(product);
+            }
+
+            return Json(new[] { product }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult EditingPopup_Update([DataSourceRequest] DataSourceRequest request, ProductViewModel product)
+        {
+            if (product != null && ModelState.IsValid)
+            {
+                var target = SessionProductRepository.One(p => p.ProductID == product.ProductID);
+                if (target != null)
+                {
+                    target.ProductName = product.ProductName;
+                    target.UnitPrice = product.UnitPrice;
+                    target.UnitsInStock = product.UnitsInStock;
+                    target.LastSupply = product.LastSupply;
+                    target.Discontinued = product.Discontinued;
+                    SessionProductRepository.Update(target);
+                }
+            }
+
+            return Json(ModelState.ToDataSourceResult());
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult EditingPopup_Destroy([DataSourceRequest] DataSourceRequest request, ProductViewModel product)
+        {
+            if (product != null)
+            {
+                SessionProductRepository.Delete(product);
+            }
+
+            return Json(ModelState.ToDataSourceResult());
+        }
+
+    }
+}
