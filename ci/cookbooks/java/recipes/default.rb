a71433928d18c9c2a2acdc8617ac715df5902576
Alex Gyoshev <alex@gyoshev.net>
2013-02-01 12:12:02 +0200

    Add proper nginx proxy to tomcat. Add tomcat recipe to run list. Export JAVA_HOME and CATALINA_HOME.

--- ci/cookbooks/java/recipes/default.rb
+++ ci/cookbooks/java/recipes/default.rb
@@ -1,2 +1,8 @@
 package "openjdk-7-jdk"
 package "maven"
+
+link "/usr/lib/jvm/default-java" do
+  to "/usr/lib/jvm/java-7-openjdk-amd64/"
+end
+
+ENV['JAVA_HOME'] = "/usr/lib/jvm/default-java"
--- ci/cookbooks/tomcat/recipes/default.rb
+++ ci/cookbooks/tomcat/recipes/default.rb
@@ -1,15 +1,29 @@
 package "tomcat7"
 package "tomcat7-admin"
 
+ENV['CATALINA_HOME'] = "/var/lib/tomcat7"
+
+link "/usr/share/tomcat7conf" do
+  to "/var/lib/tomcat7/conf"
+end
+
+link "/usr/share/tomcat7conf/catalina.policy" do
+  to "/etc/tomcat7/policy.d/03catalina.policy"
+end
+
+link "/usr/share/tomcat7log" do
+  to "/var/log/tomcat7"
+end
+
+service "tomcat7" do
+    action :enable
+end
+
 cookbook_file "/etc/tomcat7/server.xml" do
     source "server.xml"
     owner "root"
     group "root"
     mode "0644"
-    notifies :restart, resources(:service => "tomcat")
-end
-
-service "tomcat" do
-    action :restart
+    notifies :restart, resources(:service => "tomcat7")
 end
 
