67bfcdb6514308fd3bcbc12d0eba00a745f3fca7
Alex Gyoshev <alex@gyoshev.net>
2013-02-01 18:24:35 +0200

    WIP on treeview / remote data example

---
+++ wrappers/php/lib/Kendo/Data/HierarchicalDataSourceSchemaModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Kendo\Data;
+
+class HierarchicalDataSourceSchemaModel extends \Kendo\Data\DataSourceSchemaModel {
+
+    /**
+    * Sets the hasChildren property
+    * @param boolean|string $value
+    * @return \Kendo\Data\HierarchicalDataSourceSchemaModel
+    */
+    public function hasChildren($value) {
+        return $this->setProperty('hasChildren', $value);
+    }
+
+//>> Properties
+//<< Properties
+}
+
+?>
+
---
+++ wrappers/php/web/treeview/remote-data.php
@@ -0,0 +1,93 @@
+<?php
+require_once '../../lib/DataSourceResult.php';
+require_once '../../lib/Kendo/Autoload.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    header('Content-Type: application/json');
+
+    $request = file_get_contents('php://input');
+
+    $employeeId = 2;
+
+    $db = new PDO('sqlite:../../northwind.db');
+
+    $sql = 'SELECT m.EmployeeID, m.FirstName, m.LastName, m.EmployeeID, '
+        . '(SELECT COUNT(*) FROM Employees x WHERE x.ReportsTo=m.EmployeeID) as HasEmployees '
+        . 'FROM Employees m '
+        . 'WHERE m.ReportsTo is ?';
+        //. 'WHERE m.ReportsTo is null';
+
+    $statement = $db->prepare($sql);
+
+    $statement->execute(array($employeeId));
+
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    // iterate over data to set computed keys
+    $employees = array();
+
+    foreach ($data as $employee) {
+        $employee["FullName"] = $employee["FirstName"] . " " . $employee["LastName"];
+        $employee["HasEmployees"] = $employee["HasEmployees"] != 0;
+        $employees[] = $employee;
+    }
+
+    echo json_encode($employees);
+
+    exit;
+}
+
+require_once '../../include/header.php';
+
+$transport = new \Kendo\Data\DataSourceTransport();
+
+$read = new \Kendo\Data\DataSourceTransportRead();
+
+$read->url('remote-data.php')
+     ->contentType('application/json')
+     ->type('POST');
+
+// Connecting to live service on demos.kendoui.com
+// $read->url('http://demos.kendoui.com/service/Employees')
+//     ->dataType('jsonp');
+
+$transport->read($read);
+
+$model = new \Kendo\Data\HierarchicalDataSourceSchemaModel();
+
+$model->id("EmployeeID")
+      ->hasChildren("HasEmployees");
+
+$schema = new \Kendo\Data\HierarchicalDataSourceSchema();
+$schema->model($model);
+
+$dataSource = new \Kendo\Data\HierarchicalDataSource();
+
+$dataSource->transport($transport)
+           ->schema($schema);
+
+$treeview = new \Kendo\UI\TreeView('treeview');
+$treeview->attr('class', 'demo-section');
+
+$treeview->dataSource($dataSource);
+$treeview->dataTextField("FullName");
+
+echo $treeview->render();
+?>
+
+<style scoped>
+    #example {
+        text-align: center;
+    }
+
+    .demo-section {
+        display: inline-block;
+        vertical-align: top;
+        width: 320px;
+        height: 300px;
+        text-align: left;
+        margin: 0 2em;
+    }
+</style>
+
+<?php require_once '../../include/footer.php'; ?>
