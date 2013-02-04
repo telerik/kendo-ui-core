e5d5f417ad0274d04919129bfc1044a9d8500e07
Alex Gyoshev <alex@gyoshev.net>
2013-02-04 16:19:50 +0200

    API example

---
+++ wrappers/php/web/colorpicker/api.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once '../../include/header.php';
+require_once '../../lib/Kendo/Autoload.php';
+
+?>
+
+<div class="demo-section" style="float: left">
+    <label for="colorpicker">ColorPicker:</label>
+<?php
+    $colorPicker = new \Kendo\UI\ColorPicker('colorpicker');
+
+    $colorPicker
+        ->value('#00f')
+        ->toolIcon('k-foreColor');
+
+    echo $colorPicker->render();
+?>
+</div>
+
+<div class="configuration k-widget k-header" style="width: 220px">
+    <span class="configHead">API Functions</span>
+    <ul class="options">
+        <li><button id="get" class="k-button">Get value</button></li>
+        <li>
+            <input id="value" value="#ff0000" style="float: none; width: 6em" />
+            <button id="set" class="k-button">Set value</button>
+        </li>
+        <li>
+            <button id="enable" class="k-button">Enable</button> or
+            <button id="disable" class="k-button">Disable</button>
+        </li>
+        <li>
+            <button id="open" class="k-button">Open</button> or
+            <button id="close" class="k-button">Close</button> the color picker
+        </li>
+    </ul>
+</div>
+
+<script>
+    $(document).ready(function() {
+        var colorpicker = $("#colorpicker").data("kendoColorPicker");
+
+        $("#enable").click(function(){
+            colorpicker.enable();
+        });
+
+        $("#disable").click(function(){
+            colorpicker.enable(false);
+        });
+
+        $("#get").click(function(){
+            alert( colorpicker.value() );
+        });
+
+        $("#set").click(function(){
+            var color = $("#value").val();
+            try {
+                color = kendo.parseColor(color);
+                colorpicker.value(color);
+            } catch(ex) {
+                alert('Cannot parse color: "' + color + '"');
+            }
+        });
+
+        $("#open").click(function(){
+            colorpicker.open();
+        });
+
+        $("#close").click(function(){
+            colorpicker.close();
+        });
+    });
+</script>
+
+<style scoped>
+    .demo-section {
+        padding: 30px;
+    }
+
+    .demo-section label {
+        padding-right: 5px;
+    }
+
+</style>
+
+<?php require_once '../../include/footer.php'; ?>
+
+
