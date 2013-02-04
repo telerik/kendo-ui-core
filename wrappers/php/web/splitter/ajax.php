4135715abc2a87e72c69e8f625acdf6b4a9cfa1c
Alex Gyoshev <alex@gyoshev.net>
2013-02-04 14:06:12 +0200

    Events and AJAX demos

---
+++ wrappers/php/web/splitter/ajax.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../../include/header.php';
+require_once '../../lib/Kendo/Autoload.php';
+
+?>
+
+<?php
+    $splitter = new \Kendo\UI\Splitter('splitter');
+
+    $leftPane = new \Kendo\UI\SplitterPane();
+    $leftPane->contentUrl('../../content/web/splitter/ajax/ajaxContent1.html');
+    $splitter->addPane($leftPane);
+
+    $rightPane = new \Kendo\UI\SplitterPane();
+    $rightPane->contentUrl('../../content/web/splitter/ajax/ajaxContent2.html');
+    $splitter->addPane($rightPane);
+
+    echo $splitter->render();
+?>
+
+<?php require_once '../../include/footer.php'; ?>
