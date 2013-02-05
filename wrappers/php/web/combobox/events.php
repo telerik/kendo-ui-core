f64929f2b356fbd5fa4077d353b6ec587afae4a5
Atanas Korchev <akorchev@gmail.com>
2013-02-05 17:01:31 +0200

    Add events combobox PHP example.

---
+++ wrappers/php/web/combobox/events.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once '../../lib/DataSourceResult.php';
+require_once '../../lib/Kendo/Autoload.php';
+
+require_once '../../include/header.php';
+
+$comboBox = new \Kendo\UI\ComboBox('input');
+
+$comboBox->dataTextField('text')
+         ->dataValueField('value')
+         ->dataSource(array(
+            array('text' => 'Item 1', 'value' => '1'),
+            array('text' => 'Item 2', 'value' => '2'),
+            array('text' => 'Item 3', 'value' => '3')
+         ))
+         ->select('onSelect')
+         ->change('onChange')
+         ->close('onClose')
+         ->open('onOpen');
+
+echo $comboBox->render();
+?>
+
+<script>
+    function onOpen() {
+        if ("kendoConsole" in window) {
+            kendoConsole.log("event: open");
+        }
+    }
+
+    function onClose() {
+        if ("kendoConsole" in window) {
+            kendoConsole.log("event: close");
+        }
+    }
+
+    function onChange() {
+        if ("kendoConsole" in window) {
+            kendoConsole.log("event: change");
+        }
+    }
+
+    function onSelect(e) {
+        if ("kendoConsole" in window) {
+            var dataItem = this.dataItem(e.item.index());
+            kendoConsole.log("event: select (" + dataItem.text + " : " + dataItem.value + ")" );
+        }
+    };
+</script>
+<div class="console"></div>
+<?php require_once '../../include/footer.php'; ?>
