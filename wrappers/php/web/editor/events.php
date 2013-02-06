ecf81e36e0d59dc088dfb2dff9ecb7c17381939e
Alex Gyoshev <alex@gyoshev.net>
2013-02-06 15:51:08 +0200

    Events demo

---
+++ wrappers/php/web/editor/events.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once '../../include/header.php';
+require_once '../../lib/Kendo/Autoload.php';
+
+?>
+
+<div class="configuration k-widget k-header">
+    <span class="configHead">Events log</span>
+    <div class="console"></div>
+</div>
+
+<?php
+    $editor = new \Kendo\UI\Editor('editor');
+
+    $editor
+        ->attr('style', 'width:490px;height:400px')
+        ->startContent();
+?>
+    &lt;p&gt;&lt;img src=&quot;http://www.kendoui.com/Image/kendo-logo.png&quot; alt=&quot;Editor for ASP.NET MVC logo&quot; style=&quot;display:block;margin-left:auto;margin-right:auto;&quot; /&gt;&lt;/p&gt;
+    &lt;p&gt;
+        Kendo UI Editor allows your users to edit HTML in a familiar, user-friendly way.&lt;br /&gt;
+        In this version, the Editor provides the core HTML editing engine, which includes basic text formatting, hyperlinks, lists,
+        and image handling. The widget &lt;strong&gt;outputs identical HTML&lt;/strong&gt; across all major browsers, follows
+        accessibility standards and provides API for content manipulation.
+    &lt;/p&gt;
+    &lt;p&gt;Features include:&lt;/p&gt;
+    &lt;ul&gt;
+        &lt;li&gt;Text formatting &amp; alignment&lt;/li&gt;
+        &lt;li&gt;Bulleted and numbered lists&lt;/li&gt;
+        &lt;li&gt;Hyperlink and image dialogs&lt;/li&gt;
+        &lt;li&gt;Cross-browser support&lt;/li&gt;
+        &lt;li&gt;Identical HTML output across browsers&lt;/li&gt;
+        &lt;li&gt;Gracefully degrades to a &lt;code&gt;textarea&lt;/code&gt; when JavaScript is turned off&lt;/li&gt;
+    &lt;/ul&gt;
+    &lt;p&gt;
+        Read &lt;a href=&quot;http://docs.kendoui.com&quot;&gt;more details&lt;/a&gt; or send us your
+        &lt;a href=&quot;http://www.kendoui.com/forums.aspx&quot;&gt;feedback&lt;/a&gt;!
+    &lt;/p&gt;
+
+<?php
+    $editor->endContent();
+
+    // attach event handlers
+    $editor
+        ->change("onChange")
+        ->select("onSelect")
+        ->execute("onExecute")
+        ->paste("onPaste");
+
+    echo $editor->render();
+?>
+
+<script>
+    function onChange(e) {
+        kendoConsole.log("value change");
+    }
+    function onSelect(e) {
+        kendoConsole.log("selection change");
+    }
+    function onExecute(e) {
+        kendoConsole.log("command :: " + e.name);
+    }
+    function onPaste(e) {
+        kendoConsole.log("paste");
+    }
+</script>
+
+<style scoped>
+    .configuration {
+        height: 376px;
+        width: 200px;
+        margin-bottom: -21px;
+    }
+    .configuration .console {
+        background-color: transparent;
+        border: 0;
+        height: 342px;
+        overflow: auto;
+    }
+</style>
+
+<?php require_once '../../include/footer.php'; ?>
+
