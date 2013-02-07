8d8efc2e334e305b06a5b65665fec0c52c4ae3dd
Alex Gyoshev <alex@gyoshev.net>
2013-02-07 18:58:10 +0200

    Navigation and RTL examples

---
+++ wrappers/php/web/menu/rtl.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once '../../include/header.php';
+require_once '../../lib/Kendo/Autoload.php';
+
+?>
+
+<div class="k-rtl">
+<?php
+    $menu = new \Kendo\UI\Menu('menu');
+
+    $menu->attr('style', 'margin-bottom: 200px');
+
+    $first = new \Kendo\UI\MenuItem("First Item");
+    $first->addItem(
+        new \Kendo\UI\MenuItem("Sub Item 1"),
+        new \Kendo\UI\MenuItem("Sub Item 2"),
+        new \Kendo\UI\MenuItem("Sub Item 3"),
+        new \Kendo\UI\MenuItem("Sub Item 4")
+    );
+    $menu->addItem($first);
+
+    $second = new \Kendo\UI\MenuItem("Second Item");
+    $second->addItem(
+        new \Kendo\UI\MenuItem("Sub Item 1"),
+        new \Kendo\UI\MenuItem("Sub Item 2"),
+        new \Kendo\UI\MenuItem("Sub Item 3"),
+        new \Kendo\UI\MenuItem("Sub Item 4")
+    );
+    $menu->addItem($second);
+
+    $third = new \Kendo\UI\MenuItem("Third Item");
+    $third->addItem(
+        new \Kendo\UI\MenuItem("Sub Item 1"),
+        new \Kendo\UI\MenuItem("Sub Item 2"),
+        new \Kendo\UI\MenuItem("Sub Item 3"),
+        new \Kendo\UI\MenuItem("Sub Item 4")
+    );
+    $menu->addItem($third);
+
+    $fourth = new \Kendo\UI\MenuItem("Fourth Item");
+    $fourth->addItem(
+        new \Kendo\UI\MenuItem("Sub Item 1"),
+        new \Kendo\UI\MenuItem("Sub Item 2"),
+        new \Kendo\UI\MenuItem("Sub Item 3"),
+        new \Kendo\UI\MenuItem("Sub Item 4")
+    );
+    $menu->addItem($fourth);
+
+    $fifth = new \Kendo\UI\MenuItem("Fifth Item");
+    $fifth->addItem(
+        new \Kendo\UI\MenuItem("Sub Item 1"),
+        new \Kendo\UI\MenuItem("Sub Item 2"),
+        new \Kendo\UI\MenuItem("Sub Item 3"),
+        new \Kendo\UI\MenuItem("Sub Item 4")
+    );
+    $menu->addItem($fifth);
+
+    echo $menu->render();
+?>
+</div>
+
+<?php require_once '../../include/footer.php'; ?>
+
