10986b3a576f3673fe8dc282e3b7d69ef2a750dd
George Krystev <ggkrustev@yahoo.com>
2013-02-07 15:17:18 +0200

    Add demos for the MultiSelect

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Web/MultiSelect/TemplateController.cs
@@ -0,0 +1,12 @@
+﻿namespace Kendo.Mvc.Examples.Controllers
+{
+    using System.Web.Mvc;
+
+    public partial class MultiSelectController : Controller
+    {
+        public ActionResult Template()
+        {
+            return View();
+        }
+    }
+}
