fda9128124ce5cd9b0ee8d3e6758db9e5983c45c
George Krystev <ggkrustev@yahoo.com>
2013-02-08 16:49:52 +0200

    Add iso8601 format without time zone to the predefined list

--- src/kendo.core.js
+++ src/kendo.core.js
@@ -1320,6 +1320,8 @@ function pad(number, digits, end) {
             formats[++idx] = "yyyy-MM-ddTHH:mm:sszzz";
             formats[++idx] = "yyyy-MM-ddTHH:mmzzz";
             formats[++idx] = "yyyy-MM-ddTHH:mmzz";
+            formats[++idx] = "yyyy-MM-ddTHH:mm:ss";
+            formats[++idx] = "yyyy-MM-ddTHH:mm";
             formats[++idx] = "yyyy-MM-dd";
 
             idx = 0;
