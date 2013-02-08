7664f49cd6552365d8b285b8969718ee3ac32c26
Petyo Ivanov <underlog@gmail.com>
2013-02-08 11:59:36 +0200

    Scaffold of a new file + tests

---
+++ src/kendo.spa.js
@@ -0,0 +1,11 @@
+kendo_module({
+    id: "spa",
+    name: "Single Page Application",
+    category: "framework",
+    depends: [ "core" ],
+    hidden: false
+});
+
+(function ($, undefined) {
+
+})(window.kendo.jQuery);
---
+++ tests/spa/tests.js
@@ -0,0 +1,10 @@
+tests.push(
+    {
+        page: "spa/spa.html",
+        title: "Single Page Application"
+    },
+    {
+        page: "spa/lint.html",
+        title: "Single Page Application Lint"
+    }
+);
