e12f08aa03f29fb197711359d4f1424d96f0c2b5
Nikolay Rusev <nikolay.rusev@telerik.com>
2013-02-08 12:06:39 +0200

    Ungrouping recreates grid footer. Fixes #1166

--- src/kendo.grid.js
+++ src/kendo.grid.js
@@ -2730,7 +2730,7 @@
                 that.detailTemplate = that._detailTmpl(options.detailTemplate || "");
             }
 
-            if ((!isEmptyObject(aggregates) && !footer.length) ||
+            if ((that._group && !isEmptyObject(aggregates)) || (!isEmptyObject(aggregates) && !footer.length) ||
                 grep(that.columns, function(column) { return column.footerTemplate; }).length) {
 
                 that.footerTemplate = that._footerTmpl(aggregates, "footerTemplate", "k-footer-template");
