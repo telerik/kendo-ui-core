c4874c865e529f0b4edb555cdcfb9a70c363abef
Petyo Ivanov <underlog@gmail.com>
2013-02-12 10:36:37 +0200

    ScrollView resizes pages correctly
    Fixes  #1175

--- src/kendo.mobile.scrollview.js
+++ src/kendo.mobile.scrollview.js
@@ -146,8 +146,9 @@ kendo_module({
                 // if no pages present, try to retain the current position
                 if (!pageElements[0]) {
                     that.page = Math.floor((-that.movable.x) / width);
-                    that.scrollTo(that.page);
                 }
+
+                that.scrollTo(that.page, true);
 
                 pages = that.pages = ceil(dimension.getTotal() / width);
 
@@ -167,13 +168,13 @@ kendo_module({
            this.dimensions.refresh();
         },
 
-        scrollTo: function(page) {
+        scrollTo: function(page, instant) {
             this.page = page;
-            this._moveTo(- page * this.dimension.getSize(), Transition.easeOutExpo);
+            this._moveTo(- page * this.dimension.getSize(), Transition.easeOutExpo, instant);
         },
 
-        _moveTo: function(location, ease) {
-            this.transition.moveTo({ location: location, duration: this.options.duration, ease: ease });
+        _moveTo: function(location, ease, instant) {
+            this.transition.moveTo({ location: location, duration: (instant ? 1 : this.options.duration), ease: ease });
         },
 
         _dragEnd: function(e) {
