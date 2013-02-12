cc3c4c179ca1100d90d7dce4ad5358de70a39260
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2013-02-12 13:55:35 +0200

    Merge branch 'date-value-axis'

--- src/kendo.dataviz.chart.js
+++ src/kendo.dataviz.chart.js
@@ -1793,7 +1793,7 @@
                 max = options.max || seriesMax,
                 baseUnit = options.baseUnit || axis.timeUnits(max - min),
                 baseUnitTime = TIME_PER_UNIT[baseUnit],
-                autoMin = floorDate(toTime(min) - 1, baseUnit),
+                autoMin = floorDate(toTime(min) - 1, baseUnit) || toDate(max),
                 autoMax = ceilDate(toTime(max) + 1, baseUnit),
                 userMajorUnit = options.majorUnit ? options.majorUnit : undefined,
                 majorUnit = userMajorUnit || dataviz.ceil(
@@ -6512,14 +6512,17 @@
                 seriesIx,
                 series = plotArea.series,
                 currentSeries,
+                seriesAxisName,
                 firstPointValue,
-                dateData;
+                typeSamples = [axisOptions.min, axisOptions.max],
+                inferredDate;
 
             for (seriesIx = 0; seriesIx < series.length; seriesIx++) {
                 currentSeries = series[seriesIx];
-                if (currentSeries[vertical ? "yAxis" : "xAxis"] == axisOptions.name) {
+                seriesAxisName = currentSeries[vertical ? "yAxis" : "xAxis"];
+                if ((seriesAxisName == axisOptions.name) || (axisIndex == 0 && !seriesAxisName)) {
                     firstPointValue = bindPoint(currentSeries, 0).value;
-                    dateData = firstPointValue[vertical ? "y" : "x"] instanceof Date;
+                    typeSamples.push(firstPointValue[vertical ? "y" : "x"]);
 
                     break;
                 }
@@ -6530,7 +6533,14 @@
                 range.max = math.max(range.max, defaultRange.max);
             }
 
-            if (equalsIgnoreCase(axisOptions.type, DATE) || (!axisOptions.type && dateData)) {
+            for (i = 0; i < typeSamples.length; i++) {
+                if (typeSamples[i] instanceof Date) {
+                    inferredDate = true;
+                    break;
+                }
+            }
+
+            if (equalsIgnoreCase(axisOptions.type, DATE) || (!axisOptions.type && inferredDate)) {
                 axis = new DateValueAxis(range.min, range.max, axisOptions);
             } else {
                 axis = new NumericAxis(range.min, range.max, axisOptions);
