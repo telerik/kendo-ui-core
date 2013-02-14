fa7dc7f2ccf397d454d16b86f0d3b827ccf783fb
George Krystev <ggkrustev@yahoo.com>
2013-02-14 16:30:01 +0200

    Widget does not navigate to current focused date on open. Fixes #1195

--- src/kendo.datepicker.js
+++ src/kendo.datepicker.js
@@ -122,7 +122,7 @@ kendo_module({
                 calendar.min(options.min);
                 calendar.max(options.max);
 
-                calendar.navigate(that._value, options.start);
+                calendar.navigate(that._value || that._current, options.start);
                 that.value(that._value);
             }
         },
