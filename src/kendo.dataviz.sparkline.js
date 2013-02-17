eca8024c57ceafee0ec4bc788ce377ca2a11a151
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2013-02-18 01:05:56 +0200

    Alias bar for column in sparklines

--- src/kendo.dataviz.sparkline.js
+++ src/kendo.dataviz.sparkline.js
@@ -116,6 +116,23 @@ kendo_module({
             pointWidth: 5
         },
 
+        _getModel: function() {
+            var chart = this,
+                series = chart.options.series,
+                i,
+                currentSeries;
+
+            for (i = 0; i < series.length; i++) {
+                currentSeries = series[i];
+
+                if (currentSeries.type === "bar") {
+                    currentSeries.type = "column";
+                }
+            }
+
+            return Chart.fn._getModel.call(chart);
+        },
+
         _modelOptions: function() {
             var chart = this,
                 chartOptions = chart.options,
