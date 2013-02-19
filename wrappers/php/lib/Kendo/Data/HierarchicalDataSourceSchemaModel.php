80940d9eb6272771b8eb8be83d3bfcd692d5022c
Atanas Korchev <akorchev@gmail.com>
2013-02-19 11:38:37 +0200

    Add children method. Closes #1173.

--- wrappers/php/lib/Kendo/Data/HierarchicalDataSourceSchemaModel.php
+++ wrappers/php/lib/Kendo/Data/HierarchicalDataSourceSchemaModel.php
@@ -13,6 +13,15 @@ class HierarchicalDataSourceSchemaModel extends \Kendo\Data\DataSourceSchemaMode
         return $this->setProperty('hasChildren', $value);
     }
 
+    /**
+    * Sets the hasChildren property
+    * @param mixed|string $value
+    * @return \Kendo\Data\HierarchicalDataSourceSchemaModel
+    */
+    public function children($value) {
+        return $this->setProperty('children', $value);
+    }
+
 //>> Properties
 //<< Properties
 }
