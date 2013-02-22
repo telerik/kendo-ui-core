139a74e67006b0d547d9ef9175d53fea17bcb0a6
Hristo Germanov <hristo.germanov@telerik.com>
2013-02-22 11:22:42 +0200

    Update stock chart wrappers. Update docs.

--- wrappers/java/kendo-taglib/src/main/java/com/kendoui/taglib/stockchart/SeriesItemTag.java
+++ wrappers/java/kendo-taglib/src/main/java/com/kendoui/taglib/stockchart/SeriesItemTag.java
@@ -121,6 +121,14 @@ public class SeriesItemTag extends  BaseTag  /* interfaces *//* interfaces */ {
         setProperty("colorField", value);
     }
 
+    public String getCurrentField() {
+        return (String)getProperty("currentField");
+    }
+
+    public void setCurrentField(String value) {
+        setProperty("currentField", value);
+    }
+
     public String getDashType() {
         return (String)getProperty("dashType");
     }
@@ -255,6 +263,14 @@ public class SeriesItemTag extends  BaseTag  /* interfaces *//* interfaces */ {
 
     public void setStack(Object value) {
         setProperty("stack", value);
+    }
+
+    public String getTargetField() {
+        return (String)getProperty("targetField");
+    }
+
+    public void setTargetField(String value) {
+        setProperty("targetField", value);
     }
 
     public String getType() {
