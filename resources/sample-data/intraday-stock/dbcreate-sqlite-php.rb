cac29aa348ef752246e308197706c8737a93d1e6
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2013-02-22 14:25:24 +0200

    Convert Intraday.Date to true date in sample.db

--- resources/sample-data/intraday-stock/dbcreate-sqlite-php.rb
+++ resources/sample-data/intraday-stock/dbcreate-sqlite-php.rb
@@ -5,11 +5,11 @@ require 'csv'
 $/ = "\r\n"
 
 YEARS_TO_ADD = Time.now.year - 2009
-DATE_FORMAT = '%d-%m-%Y %H:%M:%S'
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 puts %Q{
     CREATE TABLE Intraday (
-        [ID] INT IDENTITY(1,1) NOT NULL,
+        [ID] INTEGER PRIMARY KEY AUTOINCREMENT,
         [Symbol] VARCHAR(10) NOT NULL,
         [Date] DATETIME NOT NULL,
         [Open] DECIMAL(9,3) NOT NULL,
@@ -17,10 +17,9 @@ puts %Q{
         [Low] DECIMAL(9,3) NOT NULL,
         [Close] DECIMAL(9,3) NOT NULL,
         [Volume] BIGINT NOT NULL
-    )
-    GO
+    );
 
-    BEGIN TRANSACTION
+    BEGIN TRANSACTION;
 }
 
 CSV.foreach('data.csv', :headers => true) do |row|
@@ -31,14 +30,13 @@ CSV.foreach('data.csv', :headers => true) do |row|
     INSERT INTO Intraday
         ([Symbol], [Date], [Open], [High], [Low], [Close], [Volume])
     VALUES
-        ('ACME', CONVERT(DATETIME, '#{date.strftime(DATE_FORMAT)}', 103), #{row['Open']}, #{row['High']}, #{row['Low']}, #{row['Close']}, #{row['Volume']})
+        ('ACME', '#{date.strftime(DATE_FORMAT)}', #{row['Open']}, #{row['High']}, #{row['Low']}, #{row['Close']}, #{row['Volume']});
     }
 end
 
 puts %Q{
-    COMMIT TRANSACTION
+    COMMIT TRANSACTION;
 
     CREATE INDEX IX_Intraday_Date
-        ON Intraday ([Date])
-    GO
+        ON Intraday ([Date]);
 }
