6d25718fd0737b3c9a97a2efed16f27e7d401d4f
Bundyo <bundyo@gmail.com>
2013-02-26 10:58:43 +0200

    Fix AeroViewr IE8 support. Fix input styling. Hide Avatar if missing.

--- demos/mvc/content/spa/aeroviewr/js/aeroviewr.js
+++ demos/mvc/content/spa/aeroviewr/js/aeroviewr.js
@@ -30,7 +30,7 @@ var model,
         26: "Transporation",
         27: "Urban Exploration"
     },
-    imagePlaceHolder = { image_url: "../content/spa/aeroviewr/images/loading.gif", calculatedWidth: 400 },
+    imagePlaceHolder = { image_url: "../content/spa/aeroviewr/images/loading.gif", calculatedWidth: 400, noAvatar: true },
     popularQuery = { url: '/photos', settings: { feature: 'popular', page: 1, image_size: 1, rpp: 50 } };
 
 var photos = new kendo.data.DataSource({
@@ -61,6 +61,7 @@ model = kendo.observable({
 
             photo.calculatedWidth = (500 / photo.height) * photo.width;
             photo.categoryName = photo.category ? categories[photo.category] : "";
+            photo.noAvatar = photo.user.userpic_url == "/graphics/userpic.png";
             model.set("currentPhoto", photo);
         });
     },
