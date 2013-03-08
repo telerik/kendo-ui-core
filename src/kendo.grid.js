e5e5ed54ef6c3e75823070e16bb117355be5ca81
Dimo Dimov <dimodi@gmail.com>
2013-03-08 09:46:40 +0200

    Fix empty space appearing after column hide and ungroup in IE8

--- src/kendo.grid.js
+++ src/kendo.grid.js
@@ -431,6 +431,7 @@
     function normalizeCols(table, visibleColumns, hasDetails, groups) {
         var colgroup = table.find(">colgroup"),
             width,
+            browser = kendo.support.browser,
             cols = map(visibleColumns, function(column) {
                     width = column.width;
                     if (width && parseInt(width, 10) !== 0) {
@@ -451,6 +452,12 @@
         colgroup = $("<colgroup/>").append($(new Array(groups + 1).join('<col class="k-group-col">') + cols.join("")));
 
         table.prepend(colgroup);
+        
+        // fill gap after column hiding
+        if (browser.msie && browser.version == 8) {
+            table.css("display", "inline-table");
+            window.setTimeout(function(){table.css("display", "");}, 1);
+        }
     }
 
     function convertToObject(array) {
