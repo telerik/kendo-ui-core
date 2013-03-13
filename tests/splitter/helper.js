ff42525ac5ba4e3a2b918d735ea4f03bd4cb1f59
Alex Gyoshev <alex@gyoshev.net>
2013-03-13 13:38:20 +0200

    Refactor splitter tests

--- tests/splitter/helper.js
+++ tests/splitter/helper.js
@@ -5,3 +5,16 @@ function getSplitter(id) {
 function getSplitterHtml(paneCount) {
     return "<div style='width: 207px;height:100px'>" +  new Array((paneCount + 1) || 3).join("<div/>") + "</div>";
 }
+
+var splitter, splitterObject;
+
+function createSplitter(options, paneCount) {
+    splitter = $(getSplitterHtml(paneCount || 2))
+            .appendTo(document.body)
+            .kendoSplitter(options);
+
+    splitterObject = splitter.data("kendoSplitter");
+
+    return splitter;
+}
+
