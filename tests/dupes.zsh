b210b071c052c0269c426db38b63fd2ee8dae633
Alex Gyoshev <alex@gyoshev.net>
2013-03-20 16:08:23 +0200

    Update dupes zsh script to show which tests are duplicate

--- tests/dupes.zsh
+++ tests/dupes.zsh
@@ -1,14 +1,21 @@
 #!/bin/zsh
 
+# remove artifacts from incomplete runs
 rm **/*tmp
+
+# scan for duplicates
 for test in **/*.*; do
-    grep 'est(' $test > $test.tmp
+    grep -E '^\s*(asyncTest|test)\(' $test > $test.tmp
     sort -u $test.tmp > $test.u.tmp
     sort $test.tmp > $test.s.tmp
 
-    diff $test.s.tmp $test.u.tmp -q 2>/dev/null
+    diff $test.s.tmp $test.u.tmp -q 1>/dev/null 2>/dev/null
 
-    if [ $? -ne 1 ]; then
-        echo "good tests found in $test"
+    if [ $? -ne 0 ]; then
+        echo "$test contains duplicate tests"
+        comm -3 $test.s.tmp $test.u.tmp
     fi
 done
+
+# remove artifacts after run
+rm **/*tmp
