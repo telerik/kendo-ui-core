d88a21a63da200f7200d3304ea91e9ef37047a52
Atanas Korchev <akorchev@gmail.com>
2013-03-22 16:03:06 +0200

    Nuget build.

--- build/version.rb
+++ build/version.rb
@@ -11,7 +11,8 @@ month = now.month + (now.year - VERSION_YEAR) * 12
 VERSION_Q = version_data['release'].to_i
 VERSION_SERVICE_PACK = version_data['servicePack']
 
-VERSION = "#{VERSION_YEAR}.#{VERSION_Q}.#{month * 100 + now.day}"
+VERSION = ENV['VERSION'] || "#{VERSION_YEAR}.#{VERSION_Q}.#{month * 100 + now.day}"
+
 CURRENT_COMMIT = `git rev-parse HEAD`.strip
 BETA = !!ENV['BETA']
 
