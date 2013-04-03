1757f7af5854e38febcc0ed22c528c36d63ab413
Petyo Ivanov <underlog@gmail.com>
2013-04-03 11:42:58 +0300

    Include multiselect and rtl in winjs build

--- build/js.rb
+++ build/js.rb
@@ -203,6 +203,7 @@ WIN_JS = FileList[
     "src/kendo.pager.js",
     "src/kendo.popup.js",
     "src/kendo.list.js",
+    "src/kendo.multiselect.js",
     "src/kendo.calendar.js",
     "src/kendo.datepicker.js",
     "src/kendo.autocomplete.js",
