0b730f32045d00e8f571d733638ea798be836006
Alex Gyoshev <alex@gyoshev.net>
2013-04-03 16:12:16 +0300

    Add url item property for menu wrappers

--- wrappers/php/lib/Kendo/UI/MenuItem.php
+++ wrappers/php/lib/Kendo/UI/MenuItem.php
@@ -46,6 +46,15 @@ class MenuItem extends \Kendo\UI\NavigationalItem {
     }
 
     /**
+    * Specifies the URL that the item links to
+    * @param string $value
+    * @return \Kendo\UI\MenuItem
+    */
+    public function url($value) {
+        return $this->setProperty('url', $value);
+    }
+
+    /**
     * Specifies the URL of the image displayed by the item
     * @param string $value
     * @return \Kendo\UI\MenuItem
