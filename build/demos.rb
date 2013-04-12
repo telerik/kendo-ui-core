fae8bf81fabd014cedf0f89cbfa9874f3ad62cfe
Bundyo <bundyo@gmail.com>
2013-04-12 15:33:31 +0300

    Fix the contentPath in WebSushi offline examples.

--- build/demos.rb
+++ build/demos.rb
@@ -11,7 +11,7 @@ OFFLINE_DEMO_TEMPLATE_OPTIONS = {
     "/web/spa/Sushi.html" => {
         skip_back_button: true,
         additional_code: <<-SCRIPT,
-            window.contentPath = "../../content";
+            window.contentPath = "../../content/spa/websushi";
         SCRIPT
         additional_scripts: %W(
             ../../content/spa/websushi/js/sushi.js
