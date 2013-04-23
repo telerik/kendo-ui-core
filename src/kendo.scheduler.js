09dfb79807945a4a5b0d1f2df5fe080c6954f776
George Krystev <ggkrustev@yahoo.com>
2013-04-23 18:18:32 +0300

    Parse common RRULE properties

--- src/kendo.scheduler.js
+++ src/kendo.scheduler.js
@@ -32,19 +32,140 @@ kendo_module({
         }
     });
 
+    var FREQUENCY = {
+        "SECONDLY": 0,
+        "MINUTELY": 1,
+        "HOURLY": 2,
+        "DAILY": 3,
+        "WEEKLY": 4,
+        "MONTHLY": 5,
+        "YEARLY": 6
+    }
+
+    var WEEK_DAYS = {
+        "SU": 0,
+        "MO": 1,
+        "TU": 2,
+        "WE": 3,
+        "TH": 4,
+        "FR": 5,
+        "SA": 6
+    }
+
+    var DATE_FORMATS = [
+        "yyyy-MM-ddTHH:mm:ss.fffzzz",
+        "yyyy-MM-ddTHH:mm:sszzz",
+        "yyyy-MM-ddTHH:mm:ss",
+        "yyyy-MM-ddTHH:mm",
+        "yyyy-MM-ddTHH",
+        "yyyy-MM-dd",
+        "yyyyMMddTHHmmssfffzzz",
+        "yyyyMMddTHHmmsszzz",
+        "yyyyMMddTHHmmss",
+        "yyyyMMddTHHmm",
+        "yyyyMMddTHH",
+        "yyyyMMdd"
+    ];
+
+    function parseArray(list, range) {
+        var idx = 0,
+            length = list.length,
+            value;
+
+        for (; idx < length; idx++) {
+            value = parseInt(list[idx], 10);
+            if (isNaN(value) || value < range.start || value > range.end) {
+                return null;
+            }
+
+            list[idx] = value;
+        }
+
+        return list;
+    }
+
+    function parseWeekDayList(list) {
+        var idx = 0, length = list.length,
+            value, valueLength;
+
+        for (; idx < length; idx++) {
+            value = list[idx];
+            valueLength = value.length;
+
+            list[idx] = {
+                offset: parseInt(value.substring(0, valueLength - 2), 10) || 1,
+                day: value.substring(valueLength - 2, valueLength).toUpperCase()
+            };
+
+            if (WEEK_DAYS[list[idx].day] === undefined) {
+                return null;
+            }
+        }
+        return list;
+    }
+
     var rrule_parser = function(rrule) {
         var result = {},
             property,
-            pair;
+            splits, value,
+            idx = 0, length;
 
         rrule = rrule.split(";");
+        length = rrule.length;
 
-        for (var idx = 0, length = rrule.length; idx < length; idx++) {
+        for (; idx < length; idx++) {
             property = rrule[idx];
-            pair = property.split("=");
+            splits = property.split("=");
+            value = $.trim(splits[1]).split(",");
 
-            if (pair[0].toUpperCase() === "FREQ") {
-                result.freq = pair[1];
+            switch (splits[0].toUpperCase()) {
+                case "FREQ" : {
+                    result.freq = value[0].toUpperCase();
+                    break;
+                }
+                case "UNTIL" : {
+                    result.until = kendo.parseDate(value[0], DATE_FORMATS);
+                    break;
+                }
+                case "COUNT" : {
+                    result.count = parseInt(value[0], 10);
+                    break;
+                }
+                case "INTERVAL" : {
+                    result.interval = parseInt(value[0], 10);
+                    break;
+                }
+                case "BYSECOND" : {
+                    result.seconds = parseArray(value, { start: 0, end: 60 });
+                    break;
+                }
+                case "BYMINUTE" : {
+                    result.minutes = parseArray(value, { start: 0, end: 59 });
+                    break;
+                }
+                case "BYHOUR" : {
+                    result.hours = parseArray(value, { start: 0, end: 23 });
+                    break;
+                }
+                case "BYMONTHDAY" : {
+                    result.monthDays = parseArray(value, { start: 1, end: 31 });
+                    break;
+                }
+                case "BYYEARDAY" : {
+                    result.yearDays = parseArray(value, { start: 1, end: 366 });
+                    break;
+                }
+                case "BYMONTH" : {
+                    result.months = parseArray(value, { start: 1, end: 12 });
+                    break;
+                }
+                case "BYDAY" : {
+                    result.weekDays = parseWeekDayList(value);
+                    break;
+                }
+                //parse bysetpos
+                //parse wkst
+                //by week number
             }
         }
 
