de049d75352603c331b1e2ac1756b97b141567da
Petyo Ivanov <underlog@gmail.com>
2013-04-26 13:56:45 +0300

    Fix DOM leaks in widgets
    Conflicts:
    	playground/ddleak.html

--- src/kendo.core.js
+++ src/kendo.core.js
@@ -2330,6 +2330,7 @@ function pad(number, digits, end) {
             var that = this;
 
             that.element.removeData("kendo" + that.options.prefix + that.options.name);
+            that.element.removeData("handler");
 
             that.unbind();
         }
