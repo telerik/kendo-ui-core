22f29e26920fb906d13eda12ebc548527b6a8fbb
Atanas Korchev <akorchev@gmail.com>
2013-05-09 16:23:36 +0300

    Remove DropDownEventBuilderBase.

--- wrappers/mvc/src/Kendo.Mvc/UI/AutoComplete/Fluent/AutoCompleteEventBuilder.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/AutoComplete/Fluent/AutoCompleteEventBuilder.cs
@@ -3,7 +3,10 @@ namespace Kendo.Mvc.UI.Fluent
     using System;
     using System.Collections.Generic;
 
-    public class AutoCompleteEventBuilder : DropDownListEventBuilderBase<AutoCompleteEventBuilder>
+    /// <summary>
+    /// The fluent API for subscribing to Kendo UI AutoComplete events.
+    /// </summary>
+    public class AutoCompleteEventBuilder : EventBuilder
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="AutoCompleteEventBuilder"/> class.
@@ -12,6 +15,226 @@ namespace Kendo.Mvc.UI.Fluent
         public AutoCompleteEventBuilder(IDictionary<string, object> events)
             : base(events)
         {
+        }
+
+        /// <summary>
+        /// Defines the inline handler of the Select client-side event
+        /// </summary>
+        /// <param name="handler">The handler code wrapped in a text tag (Razor syntax).</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  @(Html.Kendo().AutoComplete()
+        ///            .Name("AutoComplete")
+        ///            .Events(events => events.Select(
+        ///                 @&lt;text&gt;
+        ///                 function(e) {
+        ///                     //event handling code
+        ///                 }
+        ///                 &lt;/text&gt;
+        ///            ))
+        /// )
+        /// </code>
+        /// </example>
+        public AutoCompleteEventBuilder Select(Func<object, object> handler)
+        {
+            Handler("select", handler);
+
+            return this;
+        }
+
+        /// <summary>
+        ///  Defines the name of the JavaScript function that will handle the the Select client-side event.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the event.</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  @(Html.Kendo().AutoComplete()
+        ///             .Name("AutoComplete")
+        ///             .Events(events => events.Select("select"))
+        /// )
+        /// </code>
+        /// </example>
+        public AutoCompleteEventBuilder Select(string handler)
+        {
+            Handler("select", handler);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Defines the inline handler of the Change client-side event
+        /// </summary>
+        /// <param name="handler">The handler code wrapped in a text tag (Razor syntax).</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  @(Html.Kendo().AutoComplete()
+        ///            .Name("AutoComplete")
+        ///            .Events(events => events.Change(
+        ///                 @&lt;text&gt;
+        ///                 function(e) {
+        ///                     //event handling code
+        ///                 }
+        ///                 &lt;/text&gt;
+        ///            ))
+        /// )
+        /// </code>
+        /// </example>
+        public AutoCompleteEventBuilder Change(Func<object, object> handler)
+        {
+            Handler("change", handler);
+
+            return this;
+        }
+
+        /// <summary>
+        ///  Defines the name of the JavaScript function that will handle the the Change client-side event.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the event.</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  @(Html.Kendo().AutoComplete()
+        ///             .Name("AutoComplete")
+        ///             .Events(events => events.Change("change"))
+        /// )
+        /// </code>
+        /// </example>
+        public AutoCompleteEventBuilder Change(string handler)
+        {
+            Handler("change", handler);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Defines the inline handler of the DataBound client-side event
+        /// </summary>
+        /// <param name="handler">The handler code wrapped in a text tag (Razor syntax).</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  @(Html.Kendo().AutoComplete()
+        ///            .Name("AutoComplete")
+        ///            .Events(events => events.DataBound(
+        ///                 @&lt;text&gt;
+        ///                 function(e) {
+        ///                     //event handling code
+        ///                 }
+        ///                 &lt;/text&gt;
+        ///            ))
+        /// )
+        /// </code>
+        /// </example>
+        public AutoCompleteEventBuilder DataBound(Func<object, object> handler)
+        {
+            Handler("dataBound", handler);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Defines the name of the JavaScript function that will handle the the DataBound client-side event.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the event.</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  &lt;%= Html.Kendo().AutoComplete()
+        ///             .Name("AutoComplete")
+        ///             .Events(events => events.DataBound("dataBound"))
+        /// %&gt;
+        /// </code>
+        /// </example>
+        public AutoCompleteEventBuilder DataBound(string handler)
+        {
+            Handler("dataBound", handler);
+
+            return this;
+        }
+
+        /// <summary>
+        ///  Defines the name of the JavaScript function that will handle the the Open client-side event.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the event.</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  @(Html.Kendo().AutoComplete()
+        ///             .Name("AutoComplete")
+        ///             .Events(events => events.Open("open"))
+        /// )
+        /// </code>
+        /// </example>
+        public AutoCompleteEventBuilder Open(string handler)
+        {
+            Handler("open", handler);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Defines the inline handler of the Open client-side event
+        /// </summary>
+        /// <param name="handler">The handler code wrapped in a text tag (Razor syntax).</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  @(Html.Kendo().AutoComplete()
+        ///            .Name("AutoComplete")
+        ///            .Events(events => events.Open(
+        ///                 @&lt;text&gt;
+        ///                 function(e) {
+        ///                     //event handling code
+        ///                 }
+        ///                 &lt;/text&gt;
+        ///            ))
+        /// )
+        /// </code>
+        /// </example>
+        public AutoCompleteEventBuilder Open(Func<object, object> handler)
+        {
+            Handler("open", handler);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Defines the inline handler of the Close client-side event
+        /// </summary>
+        /// <param name="handler">The handler code wrapped in a text tag (Razor syntax).</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  @(Html.Kendo().AutoComplete()
+        ///            .Name("AutoComplete")
+        ///            .Events(events => events.Close(
+        ///                 @&lt;text&gt;
+        ///                 function(e) {
+        ///                     //event handling code
+        ///                 }
+        ///                 &lt;/text&gt;
+        ///            ))
+        /// )
+        /// </code>
+        /// </example>
+        public AutoCompleteEventBuilder Close(Func<object, object> handler)
+        {
+            Handler("close", handler);
+
+            return this;
+        }
+
+        /// <summary>
+        ///  Defines the name of the JavaScript function that will handle the the Close client-side event.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the event.</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  @(Html.Kendo().AutoComplete()
+        ///             .Name("AutoComplete")
+        ///             .Events(events => events.Close("close"))
+        /// )
+        /// </code>
+        /// </example>
+        public AutoCompleteEventBuilder Close(string handler)
+        {
+            Handler("close", handler);
+
+            return this;
         }
     }
 }
