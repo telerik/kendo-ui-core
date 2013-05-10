telerik/kendo-ui-core

commit 7a66fdf3f4153b45d22a299f68fddcf53d0bc7ff

Generate download builder sources in-place

build/compile.js
@@ -13,7 +13,7 @@ var ARGV = OPT
     .describe("overwrite", "Only for kendo-config, if specified the file will be overwritten")
     .describe("beautify", "Output indented code (helps debugging)")
     .describe("nomangle", "Don't mangle names (helps debugging)")
-    .boolean("amd")
+    .boolean("amd").default("amd", true)
     .boolean("deps")
     .boolean("bundle")
     .boolean("kendo-config")
@@ -29,6 +29,37 @@ var deps_file_name = path.join(KENDO_SRCDIR, "download-builder/config/kendo-conf
 var template = JSON.parse(fs.readFileSync("download-builder/config/categories.json", "utf8"));
 
 var files = ARGV._.slice();
+
+var get_wrapper = (function(wrapper){
+    var code = '((typeof define == "function" && define.amd) ? define : function(deps, body){ return body() })($DEPS, $CONT)';
+    return function() {
+        if (wrapper) return wrapper;
+        wrapper = u2.parse(code);
+        wrapper.wrap = function(id, deps, cont) {
+            return wrapper.transform(new u2.TreeTransformer(
+                null,           // need no 'before'
+                function after(node){
+                    if (node instanceof u2.AST_SymbolRef) switch (node.name) {
+                      case "$ID":
+                        return new u2.AST_String({ value: id });
+                      case "$DEPS":
+                        return new u2.AST_Array({
+                            elements: deps.map(function(x){
+                                return new u2.AST_String({ value: x });
+                            })
+                        });
+                      case "$CONT":
+                        cont = cont.clone();
+                        cont.argnames = [];
+                        return new u2.AST_Function(cont);
+                        break;
+                    }
+                }
+            ));
+        };
+        return wrapper;
+    };
+})();
 
 if (ARGV.bundle) {
     var files = ARGV._.slice();
@@ -90,37 +121,6 @@ if (ARGV["kendo-config"]) {
     process.exit(0);
 }
 
-var get_wrapper = (function(wrapper){
-    var code = '((typeof define == "function" && define.amd) ? define : function(deps, body){ return body() })($DEPS, $CONT)';
-    return function() {
-        if (wrapper) return wrapper;
-        wrapper = u2.parse(code);
-        wrapper.wrap = function(id, deps, cont) {
-            return wrapper.transform(new u2.TreeTransformer(
-                null,           // need no 'before'
-                function after(node){
-                    if (node instanceof u2.AST_SymbolRef) switch (node.name) {
-                      case "$ID":
-                        return new u2.AST_String({ value: id });
-                      case "$DEPS":
-                        return new u2.AST_Array({
-                            elements: deps.map(function(x){
-                                return new u2.AST_String({ value: x });
-                            })
-                        });
-                      case "$CONT":
-                        cont = cont.clone();
-                        cont.argnames = [];
-                        return new u2.AST_Function(cont);
-                        break;
-                    }
-                }
-            ));
-        };
-        return wrapper;
-    };
-})();
-
 function compile_one_file(file) {
     var code = fs.readFileSync(file, "utf8");
     var ast = u2.parse(code, { filename: file });
@@ -158,9 +158,11 @@ function squeeze(ast) {
 }
 
 files.forEach(function (file){
-    var output = file.replace(/\.js$/i, ".min.js");
-    if (output == file)
-        throw new Error("Won't overwrite " + file);
+    output = file;
+    if (!file.match(/\.min\.js$/)) {
+        output = file.replace(/\.js$/i, ".min.js");
+    }
+
     var ast = compile_one_file(file);
     ast = squeeze(ast);
     if (!ARGV["nomangle"]) {
@@ -168,13 +170,13 @@ files.forEach(function (file){
         ast.compute_char_frequency();
         ast.mangle_names();
     }
+
     var codegen_options = {};
     if (ARGV["beautify"]) {
         codegen_options.beautify = true;
     }
-    code = ast.print_to_string(codegen_options);
-    fs.writeFileSync(output, code);
-    if (ARGV.amd) {
+
+    if (!ARGV["amd"]) {
         // save the non-RequireJS version for the download builder
         ast = ast.transform(new u2.TreeTransformer(function(node){
             if (node === ast) {
@@ -199,9 +201,10 @@ files.forEach(function (file){
 
             return node;
         }));
-        code = ast.print_to_string(codegen_options);
-        fs.writeFileSync(output + ".no-amd", code);
     }
+
+    code = ast.print_to_string(codegen_options);
+    fs.writeFileSync(output, code);
 });
 
 function extract_widget_info(ast) {

.gitignore
@@ -31,8 +31,6 @@ src/kendo.web.js
 src/kendo.editor.js
 src/kendo.aspnetmvc.js
 src/kendo.icenium.js
-src/*.no-amd
-src/**/*.no-amd
 styles/**/*.css
 !styles/web/kendo.rtl.css
 !styles/dataviz/kendo.dataviz.css

build/download_builder.rb
@@ -8,6 +8,16 @@ BUILDER_CONFIG_NAME = File.join('config', 'kendo-config.json')
 
 BUILDER_INDEX_TEMPLATE = ERB.new(File.read(File.join('download-builder', 'index.html.erb')))
 
+#Build minified version with no AMD headers for use by the download builder
+rule /^dist\/download-builder.+\.min\.js$/ =>
+    lambda { |t| t.sub(/^dist\/download-builder.+\/js/, 'src').sub('.min.js', '.js') } do |t|
+        FileUtils.mkdir_p File.dirname(t.name)
+        File.open t.name, 'w' do |f|
+            f.write File.read(t.source);
+        end
+        compilejs(t.name, "--no-amd")
+    end
+
 namespace :download_builder do
 
     def download_builder_prerequisites(path, service_url)
@@ -17,11 +27,8 @@ namespace :download_builder do
             :root => BUILDER_SOURCE_PATH
 
         assets_path = File.join(dist_path, 'service', 'App_Data', VERSION)
-
         js_assets_path = File.join(assets_path, 'js')
-        tree :to => js_assets_path,
-            :from => CDN_MIN_JS,
-            :root => 'src'
+        task js_assets_path => MIN_JS.sub('src', js_assets_path)
 
         styles_assets_path = File.join(assets_path, 'styles')
         tree :to => styles_assets_path,

build/js.rb
@@ -24,7 +24,7 @@ JS_BUILDFILE = 'build/js.rb'
 
 #Build src/*.min.js files by running uglifyjs over src/*.js
 rule '.min.js' => lambda { |t| t.sub('min.js', 'js') } do |t|
-    compilejs(t.source, t.name)
+    compilejs(t.source)
 end
 
 #Rebuild kendo-config.json based on data present in the source code

build/tasks.rb
@@ -80,8 +80,8 @@ def uglifyjs(from, to)
     sh cmd, :verbose => VERBOSE
 end
 
-def compilejs(from, to)
-    cmd = "node #{COMPILEJS} #{from} --amd"
+def compilejs(from, options=nil)
+    cmd = "node #{COMPILEJS} #{from} #{options}"
     sh cmd, :verbose => VERBOSE
 end
 

commit 134abf1403e73a049db03d870f7a1d68aba43840

Add the RequireJS boilerplate to bundles, depend on "./jquery.min".
Also fix bundles (don't add the RJS wrapper to individual files; was a
regression after 7a66fdf3f4153b45d22a299f68fddcf53d0bc7ff).

build/compile.js
@@ -69,7 +69,7 @@ if (ARGV.bundle) {
     var orig_data = [];
     files.forEach(function(file){
         orig_data.push(fs.readFileSync(file, "utf8"));
-        var ast = compile_one_file(file, ast);
+        var ast = compile_one_file(file, true);
         toplevel.body = toplevel.body.concat(ast.body);
     });
     fs.writeFileSync(destination, orig_data.join("\n"));
@@ -82,6 +82,16 @@ if (ARGV.bundle) {
     var codegen_options = {};
     if (ARGV["beautify"]) {
         codegen_options.beautify = true;
+    }
+    if (ARGV.amd) {
+        // return the global kendo object as being "exported"
+        toplevel.body.push(new u2.AST_Return({
+            value: new u2.AST_Dot({
+                expression: new u2.AST_SymbolRef({ name: "window" }),
+                property: "kendo"
+            })
+        }));
+        toplevel = get_wrapper().wrap("kendo", [ "./jquery.min" ], toplevel);
     }
     fs.writeFileSync(destination_min, toplevel.print_to_string(codegen_options));
     process.exit(0);
@@ -121,11 +131,11 @@ if (ARGV["kendo-config"]) {
     process.exit(0);
 }
 
-function compile_one_file(file) {
+function compile_one_file(file, bundle) {
     var code = fs.readFileSync(file, "utf8");
     var ast = u2.parse(code, { filename: file });
     ast = extract_deps(ast, file);
-    if (!ast.is_bundle) {
+    if (!ast.is_bundle && !bundle) {
         var deps = ast.deps;
         var id = "kendo." + ast.component.id;
         if (ARGV.deps) {