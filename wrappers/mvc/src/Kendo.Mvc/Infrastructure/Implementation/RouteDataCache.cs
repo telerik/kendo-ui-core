7621a16821d7e146e88e97aa529e8438f3c86958
George Krystev <ggkrustev@yahoo.com>
2013-05-10 19:42:31 +0300

    Proxy most of the current HttpRequest methods in order to avoid NotImplemented exceptions

--- wrappers/mvc/src/Kendo.Mvc/Infrastructure/Implementation/RouteDataCache.cs
+++ wrappers/mvc/src/Kendo.Mvc/Infrastructure/Implementation/RouteDataCache.cs
@@ -50,21 +50,93 @@
         {
             this.appCurrentUrl = appCurrentUrl;
         }
-
+       
         public override string AppRelativeCurrentExecutionFilePath
         {
             get { return MakeCurrentUrlRelative(appCurrentUrl); }
-        }
-
-        public override string PathInfo
-        {
-            get { return ""; }
         }
 
         public override string CurrentExecutionFilePath
         {
             get { return appCurrentUrl; }
         }
+
+        #region  proxy current HttpRequest
+        public override string[] AcceptTypes
+        {
+            get { return HttpContext.Current.Request.AcceptTypes; }
+        }
+
+        public override string ApplicationPath
+        {
+            get { return HttpContext.Current.Request.ApplicationPath; }
+        }
+
+        public override string AnonymousID
+        {
+            get { return HttpContext.Current.Request.AnonymousID; }
+        }
+
+        public override bool IsAuthenticated
+        {
+            get { return HttpContext.Current.Request.IsAuthenticated; }
+        }
+
+        public override bool IsLocal
+        {
+            get { return HttpContext.Current.Request.IsLocal; }
+        }
+
+        public override bool IsSecureConnection
+        {
+            get { return HttpContext.Current.Request.IsSecureConnection; }
+        }
+
+        public override System.Security.Principal.WindowsIdentity LogonUserIdentity
+        {
+            get { return HttpContext.Current.Request.LogonUserIdentity; }
+        }
+
+        public override string Path
+        {
+            get { return HttpContext.Current.Request.Path; }
+        }
+
+        public override string PathInfo
+        {
+            get { return HttpContext.Current.Request.PathInfo; }
+        }
+
+        public override string PhysicalApplicationPath
+        {
+            get { return HttpContext.Current.Request.PhysicalApplicationPath; }
+        }
+
+        public override string PhysicalPath
+        {
+            get { return HttpContext.Current.Request.PhysicalPath; }
+        }
+
+        public override string RawUrl
+        {
+            get { return HttpContext.Current.Request.RawUrl; }
+        }
+
+        public override string UserHostAddress
+        {
+            get { return HttpContext.Current.Request.UserHostAddress; }
+        }
+
+        public override string UserHostName
+        {
+            get { return HttpContext.Current.Request.UserHostName; }
+        }
+
+        public override System.Collections.Specialized.NameValueCollection QueryString
+        {
+            get { return new System.Collections.Specialized.NameValueCollection(); }
+        }
+        #endregion
 
         private string MakeCurrentUrlRelative(string appCurrentUrl)
         {
