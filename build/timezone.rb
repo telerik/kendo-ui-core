555644c7f7892cb10a7281070114450aa49aab7b
Atanas Korchev <akorchev@gmail.com>
2013-05-24 09:58:58 +0300

    Add a submodule to timezone-js.

--- build/timezone.rb
+++ build/timezone.rb
@@ -1,6 +1,6 @@
 desc 'Build time zone database'
 task :timezone do
     sh 'curl ftp://ftp.iana.org/tz/tzdata-latest.tar.gz -o tzdata-latest.tar.gz --ftp-pasv'
-    sh 'tar -xvzf tzdata-latest.tar.gz -C src/iana'
+    sh 'tar -xvzf tzdata-latest.tar.gz -C build/tz'
     sh 'rm tzdata-latest.tar.gz'
 end
