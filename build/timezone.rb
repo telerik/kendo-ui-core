0c5d9caf3413883a9744420f0de863f1908099c9
Atanas Korchev <akorchev@gmail.com>
2013-05-24 10:09:12 +0300

    Add timezones.json to git. Ignore the rest of build/tz.

--- build/timezone.rb
+++ build/timezone.rb
@@ -3,4 +3,5 @@ task :timezone do
     sh 'curl ftp://ftp.iana.org/tz/tzdata-latest.tar.gz -o tzdata-latest.tar.gz --ftp-pasv'
     sh 'tar -xvzf tzdata-latest.tar.gz -C build/tz'
     sh 'rm tzdata-latest.tar.gz'
+    sh 'node build/timezone-js/src/node-preparse.js build/tz > build/tz/timezones.json'
 end
