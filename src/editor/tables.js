c7da80f9db2d319bba0757e7890989a012ea57c3
Alex Gyoshev <alex@gyoshev.net>
2013-06-09 13:40:52 +0300

    Add visual clues for insert table popup. Use debug scripts in playground editor demo.

--- src/editor/tables.js
+++ src/editor/tables.js
@@ -2,9 +2,11 @@
 
 var kendo = window.kendo,
     extend = $.extend,
+    proxy = $.proxy,
     Editor = kendo.ui.editor,
     EditorUtils = Editor.EditorUtils,
     Command = Editor.Command,
+    NS = ".kendoEditor",
     Tool = Editor.Tool,
     ToolTemplate = Editor.ToolTemplate,
     BlockFormatFinder = Editor.BlockFormatFinder,
@@ -55,13 +57,15 @@ var InsertTableTool = Tool.extend({
         this.options = options;
         this.finder = new BlockFormatFinder([{tags:["table"]}]);
 
+        this.cols = 8;
+        this.rows = 6;
         Tool.fn.init.call(this, $.extend(options, { command: TableCommand }));
     },
 
     _template: function() {
         return "<div class='k-ct-popup'>" +
                 "<div class='k-status'>Cancel</div>" +
-                new Array(8 * 6 + 1).join("<div class='k-ct-cell' />") +
+                new Array(this.cols * this.rows + 1).join("<div class='k-ct-cell' />") +
             "</div>";
     },
 
@@ -70,12 +74,58 @@ var InsertTableTool = Tool.extend({
 
         var popup = $(this._template()).appendTo("body").kendoPopup({
             anchor: ui,
-            copyAnchorStyles: false
+            copyAnchorStyles: false,
+            activate: proxy(this._attachEvents, this),
+            close: proxy(this._detachEvents, this)
         }).data("kendoPopup");
 
-        ui.click($.proxy(popup.toggle, popup));
+        ui.click(proxy(popup.toggle, popup));
 
         this._popup = popup;
+    },
+
+    _attachEvents: function() {
+        var element = this._popup.element,
+            status = element.find(".k-status"),
+            cells = element.find(".k-ct-cell"),
+            firstCell = cells.eq(0),
+            lastCell = cells.eq(cells.length - 1),
+            start = firstCell.offset(),
+            end = lastCell.offset(),
+            cols = this.cols,
+            rows = this.rows,
+            cellWidth, cellHeight;
+
+        end.left += lastCell[0].offsetWidth;
+        end.top += lastCell[0].offsetHeight;
+
+        cellWidth = (end.left - start.left) / cols;
+        cellHeight = (end.top - start.top) / rows;
+
+        element
+            .on("mousemove" + NS, function(e) {
+                var col = Math.floor((e.clientX - start.left) / cellWidth) + 1;
+                var row = Math.floor((e.clientY - start.top) / cellHeight) + 1;
+
+                if (row <= 0 || col <= 0 || row > rows || col > cols) {
+                    status.text("Cancel");
+                    cells.removeClass("k-state-active");
+                } else {
+                    status.text(kendo.format("Create a {0} x {1} table", row, col));
+
+                    cells.each(function(i) {
+                        $(this).toggleClass("k-state-active", i % cols < col && i / cols < row);
+                    });
+                }
+            })
+            .on("mouseleave" + NS, function(e) {
+                cells.removeClass("k-state-active");
+                status.text("Cancel");
+            });
+    },
+
+    _detachEvents: function() {
+        this._popup.element.off(NS);
     },
 
     update: function (ui, nodes) {
