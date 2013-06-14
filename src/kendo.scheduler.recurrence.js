35c6f7887f0fd7ddf78a9b6159d76f3f142013d1
George Krystev <ggkrustev@yahoo.com>
2013-06-14 13:52:40 +0300

    Fix js dependance

--- src/kendo.scheduler.recurrence.js
+++ src/kendo.scheduler.recurrence.js
@@ -2,7 +2,7 @@ kendo_module({
     id: "scheduler.recurrence",
     name: "Recurrence",
     category: "web",
-    depends: [ "scheduler" ],
+    depends: [ "dropdownlist, datepicker, numerictextbox" ],
     hidden: true
 });
 
