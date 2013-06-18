c5893998865337cc653287b1f212f886842c7143
Nikolay Rusev <nikolay.rusev@telerik.com>
2013-06-18 17:20:44 +0300

    Add mobile Drawer demos

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Mobile/Drawer/Two_Way_DrawersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class DrawerController : Controller
+    {       
+        public ActionResult Two_Way_Drawers()
+        {
+            return View();
+        }       
+    }
+}
