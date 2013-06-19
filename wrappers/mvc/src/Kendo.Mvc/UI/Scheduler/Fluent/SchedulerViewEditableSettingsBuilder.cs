d17f07316e4e9bc60c03760cf27e464920623067
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2013-06-19 19:04:56 +0300

    Add Scheduler View EditableSettings class and builder.

---
+++ wrappers/mvc/src/Kendo.Mvc/UI/Scheduler/Fluent/SchedulerViewEditableSettingsBuilder.cs
@@ -0,0 +1,72 @@
+﻿namespace Kendo.Mvc.UI.Fluent
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Defines the fluent interface for configuring the <see cref="SchedulerViewEditableSettings"/>.
+    /// </summary>
+    public class SchedulerViewEditableSettingsBuilder
+    {
+        private readonly SchedulerViewEditableSettings container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SchedulerViewEditableSettingsBuilder"/> class.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        public SchedulerViewEditableSettingsBuilder(SchedulerViewEditableSettings container)
+        {
+            this.container = container;
+        }
+
+        /// <summary>
+        /// Sets the create option.
+        /// </summary>
+        /// <param name="create">The create.</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  //TODO: CODE EXAMPLE
+        /// </code>
+        /// </example>
+        public SchedulerViewEditableSettingsBuilder Create(bool create)
+        {
+            container.Create = create;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the destroy option.
+        /// </summary>
+        /// <param name="destroy">The destroy.</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  //TODO: CODE EXAMPLE
+        /// </code>
+        /// </example>
+        public SchedulerViewEditableSettingsBuilder Destroy(bool destroy)
+        {
+            container.Destroy = destroy;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the update option.
+        /// </summary>
+        /// <param name="update">The update.</param>
+        /// <example>
+        /// <code lang="CS">
+        ///  //TODO: CODE EXAMPLE
+        /// </code>
+        /// </example>
+        public SchedulerViewEditableSettingsBuilder Update(bool update)
+        {
+            container.Update = update;
+
+            return this;
+        }
+    }
+}
---
+++ wrappers/mvc/src/Kendo.Mvc/UI/Scheduler/Settings/SchedulerViewEditableSettings.cs
@@ -0,0 +1,53 @@
+﻿namespace Kendo.Mvc.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class SchedulerViewEditableSettings : JsonObject
+    {
+        public SchedulerViewEditableSettings()
+        {
+            this.Create = true;
+            this.Destroy = true;
+            this.Update = true;
+        }
+
+        public bool Create 
+        { 
+            get;
+            set;
+        }
+
+        public bool Destroy
+        {
+            get;
+            set;
+        }
+
+        public bool Update
+        {
+            get;
+            set;
+        }
+
+        protected override void Serialize(IDictionary<string, object> json)
+        {
+            if (Create != true)
+            {
+                json["create"] = Create;
+            }
+
+            if (Destroy != true)
+            {
+                json["destroy"] = Destroy;
+            }
+
+            if (Update != true)
+            {
+                json["update"] = Update;
+            }
+        }
+    }
+}
