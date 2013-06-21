5b9258f191a9608a79c6a899aff6b7ea71e30dab
Nikolay Rusev <nikolay.rusev@telerik.com>
2013-06-21 15:33:15 +0300

    Add mobile ListView ASP.NET MVC wrapper hierarchical binding demo

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Mobile/ListView/Hierarchical_DatabindingController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.Examples.Models;
+using Kendo.Mvc.UI;
+
+namespace Kendo.Mvc.Examples.Controllers.Mobile
+{
+    public partial class ListViewController : Controller
+    {       
+        public ActionResult Hierarchical_Databinding()
+        {
+            return View();
+        }
+
+        public JsonResult Employees(int? EmployeeId)
+        {
+            var dataContext = new NorthwindDataContext();
+
+            var employees = from e in dataContext.Employees
+                            where (EmployeeId.HasValue ? e.ReportsTo == EmployeeId : e.ReportsTo == null)
+                            select new
+                            {
+                                EmployeeId = e.EmployeeID,
+                                FullName = e.FirstName + " " + e.LastName,
+                                HasEmployees = e.Employees.Any()
+                            };
+
+            return Json(employees, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
