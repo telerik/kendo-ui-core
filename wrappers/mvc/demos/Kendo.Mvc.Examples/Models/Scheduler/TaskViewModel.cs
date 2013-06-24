68bf36fb98406cb36788a2e785d097b74e35c1dd
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2013-06-24 20:54:53 +0300

    Update Scheduler entities and add TaskViewModel.

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Models/Scheduler/TaskViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kendo.Mvc.UI;
+
+namespace Kendo.Mvc.Examples.Models.Scheduler
+{
+    public class TaskViewModel : ISchedulerEvent
+    {
+        public int TaskID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        private DateTime start;
+        public DateTime Start
+        {
+            get
+            {
+                return start;
+            }
+            set
+            {
+                start = value.ToUniversalTime();
+            }
+        }
+
+        private DateTime end;
+        public DateTime End
+        {
+            get
+            {
+                return end;
+            }
+            set
+            {
+                end = value.ToUniversalTime();
+            }
+        }
+
+        public string RecurrenceRule { get; set; }
+        public int? RecurrenceID { get; set; }
+        public string RecurrenceException { get; set; }
+        public bool IsAllDay { get; set; }
+        public int? OwnerID { get; set; }
+    }
+}
