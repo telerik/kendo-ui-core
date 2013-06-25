4280811246cedca0fe0f89295b882d23f2135d52
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2013-06-25 13:36:14 +0300

    Add wind rose demo and update it to use categoryField

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Models/WindDataItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Kendo.Mvc.Examples.Models
+{
+    public class WindDataItem
+    {
+        public int Dir { get; set; }
+        public string DirText { get; set; }
+        public string Category { get; set; }
+        public double Frequency { get; set; }
+
+        public WindDataItem(int dir, string dirText, string category, double frequency)
+        {
+            Dir = dir;
+            DirText = dirText;
+            Category = category;
+            Frequency = frequency;
+        }
+    }
+}
