2e295095c09f64cf0e49c950b868d1a681cec98b
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2013-07-15 17:39:33 +0300

    Remove Scheduler aspnetmvc script.

--- build/js.rb
+++ build/js.rb
@@ -61,8 +61,7 @@ file_merge "src/kendo.aspnetmvc.js" => [
     "src/aspnetmvc/kendo.combobox.aspnetmvc.js",
     "src/aspnetmvc/kendo.multiselect.aspnetmvc.js",
     "src/aspnetmvc/kendo.imagebrowser.aspnetmvc.js",
-    "src/aspnetmvc/kendo.validator.aspnetmvc.js",
-    "src/aspnetmvc/kendo.scheduler.aspnetmvc.js"
+    "src/aspnetmvc/kendo.validator.aspnetmvc.js"
 ]
 file "src/kendo.aspnetmvc.js" => JS_BUILDFILE
 
