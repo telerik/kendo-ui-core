telerik/kendo-ui-core

commit d706b9856d3bbbf79d83b85691a4a43000ecbba9

Fix fade/zoom effects behavior

src/kendo.fx.js
@@ -991,11 +991,9 @@ kendo_module({
         }
     });
 
-    function createToggleEffect(name, property, endValue) {
+    function createToggleEffect(name, property, defaultStart, defaultEnd) {
         createEffect(name, {
             directions: IN_OUT,
-
-            restore: [ property ],
 
             startValue: function(value) {
                 this._startValue = value;
@@ -1008,36 +1006,46 @@ kendo_module({
             },
 
             shouldHide: function() {
-               return (this._direction === "out") ? !this._reverse : this._reverse;
-            },
-
-            _end: function() {
-                return this._endValue || endValue;
-            },
-
-            _start: function() {
-                return this._startValue || 1;
+               return this._shouldHide;
             },
 
             prepare: function(start, end) {
                 var that = this,
-                    opacity = that.element.data(property),
-                    out = that.shouldHide(),
-                    value = isNaN(opacity) || opacity === "" ? that._start() : opacity;
+                    startValue,
+                    endValue,
+                    out = this._direction === "out",
+                    startDataValue = that.element.data(property),
+                    startDataValueIsSet = !(isNaN(startDataValue) || startDataValue == defaultStart);
 
-                start[property] = end[property] = that._end();
-
-                if (out) {
-                    start[property] = value;
+                if (startDataValueIsSet) {
+                    startValue = startDataValue;
+                } else if (typeof this._startValue !== "undefined") {
+                    startValue = this._startValue;
                 } else {
-                    end[property] = value;
+                    startValue = out ? defaultStart : defaultEnd;
                 }
+
+                if (typeof this._endValue !== "undefined") {
+                    endValue = this._endValue;
+                } else {
+                    endValue = out ? defaultEnd : defaultStart;
+                }
+
+                if (this._reverse) {
+                    start[property] = endValue;
+                    end[property] = startValue;
+                } else {
+                    start[property] = startValue;
+                    end[property] = endValue;
+                }
+
+                that._shouldHide = end[property] === defaultEnd;
             }
         });
     }
 
-    createToggleEffect("fade", "opacity", 0);
-    createToggleEffect("zoom", "scale", 0.01);
+    createToggleEffect("fade", "opacity", 1, 0);
+    createToggleEffect("zoom", "scale", 1, 0.01);
 
     createEffect("slideMargin", {
         prepare: function(start, end) {

tests/fx/new.html
@@ -4,6 +4,7 @@
         <title>FX animation tests</title>
         <script src="../jquery-loader.js"></script>
         <script src="../qunit/qunit/qunit.js"></script>
+        <script src="../qunit/addons/close-enough/qunit-close-enough.js"></script>
         <script src="../kendo-test-helpers.js"></script>
         <script src="../../src/kendo.core.js"></script>
         <script src="../../src/kendo.fx.js"></script>

tests/fx/new.js
@@ -37,9 +37,13 @@ test("Creating effects registers API constructor", 2, function() {
 
 module("FX integration tests")
 
-function verifyEffect(effectName, before, after, reverse) {
+function verifyEffect(effectName, before, after, withEffect) {
+    withEffect = withEffect || $.noop;
+
     var effect = kendo.fx($("<div style='width:200px; height: 200px' />").appendTo(document.body))[effectName]();
     effect.duration(0);
+
+    withEffect(effect);
 
     var setup = effect.setup;
     effect.setup = function() {
@@ -80,12 +84,28 @@ asyncTest("fade in fades the element", 2, function() {
     );
 });
 
+asyncTest("fade in accepts custom start/end values", 2, function() {
+    verifyEffect("fadeIn",
+        function(element) { QUnit.close(element.css("opacity"), 0.3, 0.1) },
+        function(element) { start(); QUnit.close(element.css("opacity"), 0.8, 0.1); },
+        function(effect) { effect.startValue(0.3).endValue(0.8); }
+    );
+});
+
+asyncTest("fade out can fade the element to a given value", 3, function() {
+    verifyEffect("fadeOut",
+        function(element) { QUnit.close(element.css("opacity"), 1, 0.1); },
+        function(element) { start(); QUnit.close(element.css("opacity"), 0.8, 0.1); equal(element.css("display"), "block"); },
+        function(effect) { effect.endValue(0.8); }
+    );
+});
+
 asyncTest("fade out fades the element and hides it", 3, function() {
     verifyEffect("fadeOut",
         function(element) { equal(element.css("opacity"), "1") },
         function(element) {
             start();
-            equal(element.css("opacity"), "1");
+            equal(element.css("opacity"), "0");
             equal(element.css("display"), "none");
         }
     );

commit 52e4cec6ce12a88aa1f5c8ff7154663df2210b94

Fix regression of kendoAnimate caused by d706b98. Closes #2052.

src/kendo.fx.js
@@ -1016,17 +1016,22 @@ kendo_module({
                     out = this._direction === "out",
                     startDataValue = that.element.data(property),
                     startDataValueIsSet = !(isNaN(startDataValue) || startDataValue == defaultStart);
+                var cssValue = this.element.css(property);
 
                 if (startDataValueIsSet) {
                     startValue = startDataValue;
                 } else if (typeof this._startValue !== "undefined") {
                     startValue = this._startValue;
+                } else if (out && cssValue) {
+                    startValue = cssValue;
                 } else {
                     startValue = out ? defaultStart : defaultEnd;
                 }
 
                 if (typeof this._endValue !== "undefined") {
                     endValue = this._endValue;
+                } else if (!out && cssValue) {
+                    endValue = cssValue;
                 } else {
                     endValue = out ? defaultEnd : defaultStart;
                 }

tests/fx/animation.html
@@ -4,6 +4,7 @@
         <title>FX animation tests</title>
         <script src="../jquery-loader.js"></script>
         <script src="../qunit/qunit/qunit.js"></script>
+        <script src="../qunit/addons/close-enough/qunit-close-enough.js"></script>
         <script src="../kendo-test-helpers.js"></script>
         <script src="../../src/kendo.core.js"></script>
         <script src="../../src/kendo.fx.js"></script>
@@ -225,6 +226,30 @@
                 });
 
                 ok(!called);
+            });
+
+            asyncTest("fade:out animates from CSS start value", function() {
+                span.css("opacity", .1)
+                    .kendoAnimate({
+                        effects: "fade:out",
+                        duration: 100,
+                        init: function() {
+                            QUnit.close(span.css("opacity"), .1, .05)
+                            start();
+                        }
+                    })
+            });
+
+            asyncTest("fade:in animates to CSS end value", function() {
+                span.css("opacity", .5)
+                    .kendoAnimate({
+                        effects: "fade:in",
+                        duration: 100,
+                        complete: function() {
+                            equal(span.css("opacity"), .5)
+                            start();
+                        }
+                    })
             });
 
             if (!kendo.support.browser.mozilla) {