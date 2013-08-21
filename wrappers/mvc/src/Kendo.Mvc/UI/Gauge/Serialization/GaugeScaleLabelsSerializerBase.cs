33f5e695177f1ebb66b4d47ee3cf8a0bc7dfd3c0
Hristo Germanov <hristo.germanov@telerik.com>
2013-08-21 16:56:12 +0300

    Add radial scale labels position.

--- wrappers/mvc/src/Kendo.Mvc/UI/Gauge/Serialization/GaugeScaleLabelsSerializerBase.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/Gauge/Serialization/GaugeScaleLabelsSerializerBase.cs
@@ -1,24 +1,10 @@
 namespace Kendo.Mvc.UI
 {
-    using System.Collections.Generic;
-    using Kendo.Mvc.Infrastructure;
-    using Kendo.Mvc.Extensions;
-
     internal class GaugeScaleLabelsSerializerBase : ChartLabelsBase
     {
-        private readonly GaugeScaleLabelsBase labels;
-
         public GaugeScaleLabelsSerializerBase(GaugeScaleLabelsBase labels)
             : base (labels)
         {
-            this.labels = labels;
-        }
-
-        public override IDictionary<string, object> Serialize()
-        {
-            var result = base.Serialize();
-
-            return result;
         }
     }
 }
