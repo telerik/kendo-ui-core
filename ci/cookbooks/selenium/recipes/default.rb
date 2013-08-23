572a89ad8f27dd107d17f11a111309f16cb9ddc6
Alex Gyoshev <alex@gyoshev.net>
2013-08-23 13:53:10 +0300

    Update selenium server to 2.35

--- ci/cookbooks/selenium/recipes/default.rb
+++ ci/cookbooks/selenium/recipes/default.rb
@@ -3,7 +3,7 @@ directory "/usr/share/selenium-server/" do
 end
 
 remote_file "/usr/share/selenium-server/selenium-server-standalone.jar" do
-    source "http://selenium.googlecode.com/files/selenium-server-standalone-2.26.0.jar"
+    source "http://selenium.googlecode.com/files/selenium-server-standalone-2.35.0.jar"
     mode "0644"
-    action :create_if_missing
+    action :create
 end
