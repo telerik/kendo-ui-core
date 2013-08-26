4b664cf7656abf1b0149963b3f8b74c4018bf964
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2013-08-26 14:33:22 +0300

    Add map playground

---
+++ src/kendo.dataviz.map.js
@@ -0,0 +1,13 @@
+kendo_module({
+    id: "dataviz.map",
+    name: "Map",
+    category: "dataviz",
+    description: "",
+    depends: [ "data", "userevents", "dataviz.core", "dataviz.svg", "dataviz.themes" ]
+});
+
+(function ($, undefined) {
+    // Imports ================================================================
+
+    // Exports ================================================================
+})(window.kendo.jQuery);
