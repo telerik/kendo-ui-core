5ba01e95946a919e8bfeb63a9aedce123ae60c7d
Alex Gyoshev <alex@gyoshev.net>
2013-09-02 13:46:13 +0300

    Fix location of php-fpm socket to prevent service from failing due to missing dir

--- ci/cookbooks/php/recipes/default.rb
+++ ci/cookbooks/php/recipes/default.rb
@@ -19,12 +19,6 @@ cookbook_file "/etc/php5/fpm/pool.d/www.conf" do
     source "www.conf"
 end
 
-directory "/var/run/php-fpm/" do
-    owner "jenkins"
-    group "nogroup"
-    recursive true
-end
-
 service "php5-fpm" do
     action :restart
 end
