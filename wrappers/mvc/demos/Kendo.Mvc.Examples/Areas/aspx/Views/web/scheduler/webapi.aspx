5be3f0b225a262d5b6440cf36cf2c71e78fa1a37
Atanas Korchev <akorchev@gmail.com>
2013-09-19 16:56:31 +0300

    Scheduler Web API demo.

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Areas/aspx/Views/web/scheduler/webapi.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Areas/aspx/Views/Shared/Web.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ContentPlaceHolderID="MainContent" runat="server">
+
+<%: Html.Kendo().Scheduler<Kendo.Mvc.Examples.Models.Scheduler.TaskViewModel>()
+    .Name("scheduler")
+    .Date(new DateTime(2013, 6, 13))
+    .StartTime(new DateTime(2013, 6, 13, 7, 00, 00))
+    .Height(600)
+    .Views(views =>
+    {
+        views.DayView();
+        views.WeekView(weekView => weekView.Selected(true));
+        views.MonthView();
+        views.AgendaView();
+    })
+    .Timezone("Etc/UTC")
+    .DataSource(d => d
+        .Model(m => m.Id(f => f.TaskID))
+        .Events(events => events.Error("error_handler"))
+        .Read(read => read.Url(Url.Content("~/api/task")).Type(HttpVerbs.Get))
+        .Create(create => create.Url(Url.Content("~/api/task")).Type(HttpVerbs.Post))
+        .Update(update => update.Url(Url.Content("~/api/task")).Type(HttpVerbs.Put))
+        .Destroy(destroy => destroy.Url(Url.Content("~/api/task")).Type(HttpVerbs.Delete))
+    )
+%>
+<script>
+    function error_handler(e) {
+        var errors = $.parseJSON(e.xhr.responseText);
+
+        if (errors) {
+            alert("Errors:\n" + errors.join("\n"));
+        }
+    }
+
+    $(function() {
+        var scheduler = $("#scheduler").data("kendoScheduler");
+
+        // Web API needs the ID of the entity to be part of the URL e.g. PUT /api/Product/80
+        scheduler.dataSource.transport.options.update.url = function(data) {
+            return "<%= Url.Content("~/api/task/") %>" + data.TaskID;
+        }
+
+        // Web API needs the ID of the entity to be part of the URL e.g. DELETE /api/Product/80
+        scheduler.dataSource.transport.options.destroy.url = function(data) {
+            return "<%= Url.Content("~/api/task/") %>" + data.TaskID;
+        }
+    });
+</script>
+</asp:Content>
---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Web/Scheduler/WebApiController.cs
@@ -0,0 +1,106 @@
+﻿namespace Kendo.Mvc.Examples.Controllers
+{
+    using System.Web.Mvc;
+    using Kendo.Mvc.UI;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.Examples.Models.Scheduler;
+    using System.Net.Http;
+    using Kendo.Mvc.Examples.Models;
+    using System.Linq;
+    using System.Net;
+    using System;
+    using System.Data.Entity.Infrastructure;
+
+    public partial class SchedulerController
+    {
+        public ActionResult WebApi()
+        {
+            return View();
+        }
+    }
+
+    public class TaskController : System.Web.Http.ApiController
+    {
+        SchedulerTaskService service;
+
+        public TaskController()
+        {
+            service = new SchedulerTaskService(new SampleEntities());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            service.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        // GET api/task
+        public DataSourceResult Get([System.Web.Http.ModelBinding.ModelBinder(typeof(WebApiDataSourceRequestModelBinder))]DataSourceRequest request)
+        {
+            return service.GetAll().ToDataSourceResult(request);
+        }
+
+        // POST api/task
+        public HttpResponseMessage Post(TaskViewModel task)
+        {
+            if (ModelState.IsValid)
+            {
+                service.Insert(task, null);
+
+                var response = Request.CreateResponse(HttpStatusCode.Created, new DataSourceResult { Data = new[] { task }, Total = 1 });
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = task.TaskID }));
+                return response;
+            }
+            else
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors).Select(error => error.ErrorMessage);
+
+                return Request.CreateResponse(HttpStatusCode.BadRequest, errors);
+            }
+        }
+
+        // PUT api/task/5
+        public HttpResponseMessage Put(int id, TaskViewModel task)
+        {
+            if (ModelState.IsValid && id == task.TaskID)
+            {
+                try
+                {
+                    service.Update(task, null);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors).Select(error => error.ErrorMessage);
+                return Request.CreateResponse(HttpStatusCode.BadRequest, errors);
+            }
+        }
+
+        // DELETE api/task/5
+        public HttpResponseMessage Delete(int id)
+        {
+            TaskViewModel task = new TaskViewModel
+            {
+                TaskID = id
+            };
+
+            try
+            {
+                service.Delete(task, null);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, task);
+        }
+    }
+}
