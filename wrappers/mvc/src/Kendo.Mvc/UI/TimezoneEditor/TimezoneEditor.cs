feeded0fbca56c855a5253a8d11939d9aac377db
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2013-10-21 12:19:25 +0300

    Add Timezone editor wrapper and update project file.

---
+++ wrappers/mvc/src/Kendo.Mvc/UI/TimezoneEditor/TimezoneEditor.cs
@@ -0,0 +1,57 @@
+﻿namespace Kendo.Mvc.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Web.Mvc;
+    using Kendo.Mvc.Infrastructure;
+    using Kendo.Mvc.Extensions;
+    using System.Text.RegularExpressions;
+
+    public class TimezoneEditor : WidgetBase
+    {
+        public TimezoneEditor(ViewContext viewContext,
+                    IJavaScriptInitializer initializer,
+                    ViewDataDictionary viewData)
+            : base(viewContext, initializer, viewData)
+        {
+
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        public override void WriteInitializationScript(TextWriter writer)
+        {
+            var options = this.SeriailzeBaseOptions();
+
+            writer.Write(Initializer.Initialize(Selector, "TimezoneEditor", options));
+
+            base.WriteInitializationScript(writer);
+        }
+
+        protected virtual IDictionary<string, object> SeriailzeBaseOptions()
+        {
+            var options = new Dictionary<string, object>(Events);
+
+            if (!string.IsNullOrEmpty(Value))
+            {
+                options["value"] = Value;
+            }
+
+            return options;
+        }
+
+        protected override void WriteHtml(System.Web.UI.HtmlTextWriter writer)
+        {
+            TimezoneEditorHtmlBuilder builder = new TimezoneEditorHtmlBuilder(this);
+
+            builder.Build().WriteTo(writer);
+
+            base.WriteHtml(writer);
+        }
+    }
+}
