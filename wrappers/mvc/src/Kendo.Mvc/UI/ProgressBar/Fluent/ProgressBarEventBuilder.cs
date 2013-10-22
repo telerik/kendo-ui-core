f3033d13307ea5bd3620a81b25c2d62d428582e3
Raisolution <d.madjarov@gmail.com>
2013-10-22 14:03:50 +0300

    Removed start event from MVC Wrappers

--- wrappers/mvc/src/Kendo.Mvc/UI/ProgressBar/Fluent/ProgressBarEventBuilder.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/ProgressBar/Fluent/ProgressBarEventBuilder.cs
@@ -15,51 +15,6 @@
         }
 
         /// <summary>
-        /// Defines the inline handler of the Start client-side event
-        /// </summary>
-        /// <param name="inlineCodeBlock">The handler code wrapped in a text tag (Razor syntax).</param>
-        /// <example>
-        /// <code lang="CS">
-        /// &lt;% Html.Kendo().ProgressBar()
-        ///            .Name(&quot;progressBar&quot;)
-        ///            .Events(events => events.Start(
-        ///                 @&lt;text&gt;
-        ///                 function(e) {
-        ///                     //event handling code
-        ///                 }
-        ///                 &lt;/text&gt;
-        ///            ))
-        ///            .Render();
-        /// %&gt;
-        /// </code>
-        /// </example>
-        public ProgressBarEventBuilder Start(Func<object, object> inlineCodeBlock)
-        {
-            Handler("start", inlineCodeBlock);
-
-            return this;
-        }
-
-        /// <summary>
-        /// Defines the name of the JavaScript function that will handle the the Start client-side event.
-        /// </summary>
-        /// <param name="onStartHandlerName">The name of the JavaScript function that will handle the event.</param>
-        /// <example>
-        /// <code lang="CS">
-        /// &lt;%= Html.Kendo().ProgressBar()
-        ///             .Name(&quot;progressBar&quot;)
-        ///             .Events(events => events.Start("onStart"))
-        /// %&gt;
-        /// </code>
-        /// </example>
-        public ProgressBarEventBuilder Start(string onStartHandlerName)
-        {
-            Handler("start", onStartHandlerName);
-
-            return this;
-        }
-
-        /// <summary>
         /// Defines the inline handler of the Change client-side event
         /// </summary>
         /// <param name="inlineCodeBlock">The handler code wrapped in a text tag (Razor syntax).</param>
