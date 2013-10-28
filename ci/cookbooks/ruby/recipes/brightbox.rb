49e3e6b8529684b30c7816804ecb9dbf6d7a04ba
Petyo Ivanov <underlog@gmail.com>
2013-10-28 14:17:26 +0200

    Make it work behind the corporate firewall

--- ci/cookbooks/ruby/recipes/brightbox.rb
+++ ci/cookbooks/ruby/recipes/brightbox.rb
@@ -2,7 +2,7 @@ apt_repository "ruby-ng" do
   uri "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu"
   distribution node['lsb']['codename']
   components ["main"]
-  keyserver "keyserver.ubuntu.com"
+  keyserver "hkp://keyserver.ubuntu.com:80/"
   key "C3173AA6"
 end
 
