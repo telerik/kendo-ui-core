ec6c7a324c3499f5e05a91612c963de0dbeaa8cd
Bundyo <bundyo@gmail.com>
2013-10-29 17:46:13 +0200

    Add data-stretch to the Pane-wrapper's Pane to fix some scrolling issues. Fix some gaps in Scheduler. Add override for the Scheduler View changer in demos.

--- src/kendo.mobile.pane.js
+++ src/kendo.mobile.pane.js
@@ -264,7 +264,7 @@ kendo_module({
 
     Pane.wrap = function(element) {
         if (!element.is(roleSelector("view"))) {
-            element = element.wrap('<div data-' + kendo.ns + 'role="view"></div>').parent();
+            element = element.wrap('<div data-' + kendo.ns + 'role="view" data-stretch="true"></div>').parent();
         }
 
         var paneContainer = element.wrap('<div class="km-pane-wrapper"><div></div></div>').parent(),
