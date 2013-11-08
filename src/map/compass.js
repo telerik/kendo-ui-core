77847689ac855fe2d712356b80ddf3e4975061fc
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2013-11-08 17:59:04 +0200

    Adjust Compass size. Add panStep option

--- src/map/compass.js
+++ src/map/compass.js
@@ -12,22 +12,39 @@ kendo_module({
     var NS = ".kendoCompass";
 
     function button(dir) {
-       return kendo.format('<button class="k-button k-compass-{0}"><span class="k-icon k-i-arrow-{0}"/></button>', dir);
+       return kendo.format(
+           '<button class="k-button k-compass-{0}">' +
+               '<span class="k-icon k-i-arrow-{0}"/>' +
+           '</button>', dir);
     }
 
-    var BUTTONS = button("n") + button("e") + button("s") + button("w") + '<button class="k-button k-compass-c"><span/></button>';
+    var BUTTONS = button("n") + button("e") + button("s") + button("w") +
+        '<button class="k-button k-compass-c"><span/></button>';
 
     var Compass = Widget.extend({
         init: function(element, options) {
             Widget.fn.init.call(this, element, options);
+            this._initOptions(options);
 
             this.element.addClass("k-widget k-header k-shadow k-compass")
                         .append(BUTTONS)
                         .on("click" + NS, ".k-button", $.proxy(this, "_click"));
         },
+
+        options: {
+            name: "Compass",
+            panStep: 1
+        },
+
+        events: [
+            "center",
+            "pan"
+        ],
+
         _click: function(e) {
             var x = 0;
             var y = 0;
+            var panStep = this.options.panStep;
             var button = $(e.currentTarget);
 
             if (button.is(".k-compass-c")) {
@@ -42,16 +59,13 @@ kendo_module({
                 } else if (button.is(".k-compass-w")) {
                     x = -1;
                 }
-                this.trigger("pan", { x: x, y: y});
+
+                this.trigger("pan", {
+                    x: x * panStep,
+                    y: y * panStep
+                });
             }
-        },
-        options: {
-            name: "Compass"
-        },
-        events: [
-            "center",
-            "pan"
-        ]
+        }
     });
 
     kendo.dataviz.ui.plugin(Compass);
