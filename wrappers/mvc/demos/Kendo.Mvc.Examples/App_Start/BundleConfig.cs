3bf1b670051fe14cddaaea59272313e70b63c229
Atanas Korchev <akorchev@gmail.com>
2013-11-28 11:25:29 +0200

    Use ASP.NET Bundles in the ASP.NET MVC demos.

--- wrappers/mvc/demos/Kendo.Mvc.Examples/App_Start/BundleConfig.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/App_Start/BundleConfig.cs
@@ -9,35 +9,41 @@ namespace Kendo.Mvc.Examples
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/kendo-web").Include(
+                        "~/Scripts/kendo.web.min.js",
+                        "~/Scripts/kendo.aspnetmvc.min.js",
+                        "~/Scripts/kendo.timezones.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/kendo-dataviz").Include(
+                        "~/Scripts/kendo.all.min.js",
+                        "~/Scripts/kendo.aspnetmvc.min.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/kendo-mobile").Include(
+                        "~/Scripts/kendo.all.min.js",
+                        "~/Scripts/kendo.aspnetmvc.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/demo").Include(
+                        "~/Scripts/console.min.js",
+                        "~/Scripts/prettify.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/Content/web/css").Include(
+                        "~/Content/web/kendo.common.min.css",
+                        "~/Content/web/kendo.rtl.min.css",
+                        "~/Content/web/kendo.default.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/dataviz/css").Include(
+                        "~/Content/dataviz/kendo.dataviz.min.css",
+                        "~/Content/dataviz/kendo.dataviz.default.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/mobile/css").Include(
+                        "~/Content/mobile/kendo.mobile.all.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/shared/css").Include(
+                        "~/Content/shared/examples-offline.css"));
+
+            bundles.IgnoreList.Clear();
         }
     }
 }
