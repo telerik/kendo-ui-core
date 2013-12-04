a2166362ec148825c6a2d6d5e1c4fe299270fa9e
Hristo Germanov <hristo.germanov@telerik.com>
2013-12-04 17:26:27 +0200

    Migrate chart tests.

--- tests/chart/crosshair.js
+++ tests/chart/crosshair.js
@@ -3,81 +3,83 @@
         Box2D = dataviz.Box2D,
         TOLERANCE = 1;
 
-    var crosshair, element,
-        chartBox = Box2D(0,0,200,200),
-        plotArea, view;
+    (function() {
+        var crosshair, element,
+            chartBox = Box2D(0,0,200,200),
+            plotArea, view;
 
-    function PlotAreaStub() { }
+        function PlotAreaStub() { }
 
-    $.extend(PlotAreaStub.prototype, {
-        options: { },
-        backgroundBox: function () {
-            return chartBox;
+        $.extend(PlotAreaStub.prototype, {
+            options: { },
+            backgroundBox: function () {
+                return chartBox;
+            }
+        });
+
+        function createAxis(options) {
+            options = options || {};
+            var axis = new dataviz.NumericAxis(0, 1, options);
+            axis.reflow(chartBox);
+            axis.plotArea = new PlotAreaStub();
+            var categoryAxis = new dataviz.CategoryAxis(
+                $.extend({
+                    categories: ["Foo", "Bar"]
+                }, options)
+            );
+            categoryAxis.reflow(chartBox);
+
+            axis.pane = { axes: [categoryAxis] };
+            return axis;
         }
-    });
 
-    function createAxis(options) {
-        options = options || {};
-        var axis = new dataviz.NumericAxis(0, 1, options);
-        axis.reflow(chartBox);
-        axis.plotArea = new PlotAreaStub();
-        var categoryAxis = new dataviz.CategoryAxis(
-            $.extend({
-                categories: ["Foo", "Bar"]
-            }, options)
-        );
-        categoryAxis.reflow(chartBox);
+        function createCrosshair(options, axisOptions) {
+            view = new ViewStub();
+            crosshair = new dataviz.Crosshair(createAxis(), options);
+            crosshair.getViewElements(view);
+            element = crosshair.element;
+        }
 
-        axis.pane = { axes: [categoryAxis] };
-        return axis;
-    }
+        // ------------------------------------------------------------
+        module("Crosshair / Configuration", {
+            setup: function() {
+                createCrosshair();
+            }
+        });
 
-    function createCrosshair(options, axisOptions) {
-        view = new ViewStub();
-        crosshair = new dataviz.Crosshair(createAxis(), options);
-        crosshair.getViewElements(view);
-        element = crosshair.element;
-    }
+        test("sets width", function() {
+            createCrosshair({ width: 2 });
+            equal(element.options.strokeWidth, 2);
+        });
 
-    // ------------------------------------------------------------
-    module("Crosshair / Configuration", {
-        setup: function() {
+        test("sets opacity", function() {
+            createCrosshair({ opacity: 0.5 });
+            equal(element.options.strokeOpacity, 0.5);
+        });
+
+        test("sets color", function() {
+            createCrosshair({ color: "color" });
+            equal(element.options.stroke, "color");
+        });
+
+        test("sets dashType", function() {
+            createCrosshair({ dashType: "dashType" });
+            equal(element.options.dashType, "dashType");
+        });
+
+        // ------------------------------------------------------------
+        module("Crosshair / LinePoints", {
+            setup: function() {
+                createCrosshair();
+            }
+        });
+
+        test("returns vertical points", function() {
             createCrosshair();
-        }
-    });
+            points = crosshair.linePoints();
+            deepEqual([points[0].x, points[0].y], [0,7.5]);
+            deepEqual([points[1].x, points[1].y], [199,7.5]);
+        });
 
-    test("sets width", function() {
-        createCrosshair({ width: 2 });
-        equal(element.options.strokeWidth, 2);
-    });
-
-    test("sets opacity", function() {
-        createCrosshair({ opacity: 0.5 });
-        equal(element.options.strokeOpacity, 0.5);
-    });
-
-    test("sets color", function() {
-        createCrosshair({ color: "color" });
-        equal(element.options.stroke, "color");
-    });
-
-    test("sets dashType", function() {
-        createCrosshair({ dashType: "dashType" });
-        equal(element.options.dashType, "dashType");
-    });
-
-    // ------------------------------------------------------------
-    module("Crosshair / LinePoints", {
-        setup: function() {
-            createCrosshair();
-        }
-    });
-
-    test("returns vertical points", function() {
-        createCrosshair();
-        points = crosshair.linePoints();
-        deepEqual([points[0].x, points[0].y], [0,7.5]);
-        deepEqual([points[1].x, points[1].y], [199,7.5]);
-    });
-
+    })();
 })();
