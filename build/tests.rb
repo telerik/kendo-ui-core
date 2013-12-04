446ad68ad6e1ca0d0a7590b6cd2669dcf97f55cd
Petyo Ivanov <underlog@gmail.com>
2013-12-04 16:49:47 +0200

    log xvfb-run

--- build/tests.rb
+++ build/tests.rb
@@ -17,7 +17,7 @@ namespace :tests do
         file output => [MIN_JS, MIN_CSS, KENDO_CONFIG_FILE, tests].flatten do |t|
             sh <<-SH
               if which xvfb-run >/dev/null; then
-                xvfb-run #{cmd}
+                xvfb-run -e /dev/stdout #{cmd}
               else
                 #{cmd}
               fi
