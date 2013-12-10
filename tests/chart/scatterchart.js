5987c47305071996e1e7f2bd7fada09952e31d2e
Hristo Germanov <hristo.germanov@telerik.com>
2013-12-10 15:55:46 +0200

    Note tests for the bubble, scatter and scatter line charts.

--- tests/chart/scatterchart.js
+++ tests/chart/scatterchart.js
@@ -494,4 +494,79 @@
         });
 
     })();
+
+    (function() {
+        var note;
+
+        module("Scatter Chart / Note", {
+            setup: function() {
+                var chart = createChart({
+                    series: [{
+                        name: "Value",
+                        type: "scatter",
+                        data: [{ x: 1, y: 10, noteText: "A" }]
+                    }]
+                });
+
+                note = chart._plotArea.charts[0].points[0].note;
+            },
+            teardown: function() {
+                destroyChart();
+            }
+        });
+
+        test("should have text", function() {
+            equal(note.options.label.text, "A");
+        });
+
+        module("Scatter Chart / Note Template", {
+            teardown: function() {
+                destroyChart();
+            }
+        });
+
+        function createNote(options) {
+            var chart = createChart({
+                series: [{
+                    name: "Value",
+                    type: "scatter",
+                    data: [{ x: 1, y: 10, noteText: "A", test: "test" }],
+                    notes: $.extend({}, options),
+                    name: "name"
+                }]
+            });
+
+            note = chart._plotArea.charts[0].points[0].note;
+        }
+
+        test("dataItem", function() {
+            createNote({
+                label: {
+                    template: "#= dataItem.test #"
+                }
+            });
+
+            equal(note.options.label.text, "test");
+        });
+
+        test("value", function() {
+            createNote({
+                label: {
+                    template: "x: #= value.x # y: #= value.y #"
+                }
+            });
+
+            equal(note.options.label.text, "x: 1 y: 10");
+        });
+
+        test("series", function() {
+            createNote({
+                label: {
+                    template: "#= series.name #"
+                }
+            });
+
+            equal(note.options.label.text, "name");
+        });
+    })();
 })();
