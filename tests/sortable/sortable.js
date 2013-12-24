ff9aaa95b9c2d8437fb430d817320ec489e9509a
Alexander Valchev <a.valchevbox@gmail.com>
2013-12-24 18:08:04 +0200

    Fix old and add new tests.

--- tests/sortable/sortable.js
+++ tests/sortable/sortable.js
@@ -1,9 +1,21 @@
 (function() {
     var Sortable = kendo.ui.Sortable,
         Draggable = kendo.ui.Draggable,
-        element;
+        element,
+        filteredIndex,
+        excludedIndex;
 
-    module("kendo.ui.Sortable", {
+    function triggerDraggableEvent(type, e, element) {
+        element.data("kendoDraggable").trigger(type, e);
+    }
+
+    function moveToSort(draggableElement, x, y) {
+        //event should be simulated twice because hint is updated **after** drag event of the Draggable fires
+        move(draggableElement, x, y);
+        move(draggableElement, x, y);
+    }
+
+    module("Sortable - sorting with the mouse", {
         setup: function() {
             QUnit.fixture.append(
                 '<ul id="sortable">' +
@@ -21,14 +33,6 @@
         }
     });
 
-    function triggerDraggableEvent(type, e, element) {
-        element.data("kendoDraggable").trigger(type, e);
-    }
-
-    function trigger(type, e, element) {
-        element.trigger($.Event(type, e));
-    }
-
     test("widget creates draggable component", 1, function() {
         element.kendoSortable();
         ok(element.data("kendoDraggable") instanceof Draggable);
@@ -38,7 +42,11 @@
         var draggedElement = element.children().eq(0),
             sortable = element.kendoSortable().data("kendoSortable");
 
-        triggerDraggableEvent("dragstart", { currentTarget: draggedElement }, element);
+        triggerDraggableEvent(
+            "dragstart",
+            { currentTarget: draggedElement, target: draggedElement }, 
+            element
+        );
 
         equal(element.children().length, 5, "placeholder is attached to the sortable element");
         equal(draggedElement.text(), draggedElement.prev().text(), "placeholder is attached before draggedElement");
@@ -49,7 +57,11 @@
         var draggedElement = element.children().eq(0),
             sortable = element.kendoSortable().data("kendoSortable");
 
-        triggerDraggableEvent("dragstart", { currentTarget: draggedElement }, element);
+        triggerDraggableEvent(
+            "dragstart",
+            { currentTarget: draggedElement, target: draggedElement },
+            element
+        );
 
         ok(!draggedElement.is(":visible"), "draggedElement is hidden");
     });
@@ -59,7 +71,11 @@
             sortable = element.kendoSortable().data("kendoSortable"),
             initialChildrenCount = element.children().length;
 
-        triggerDraggableEvent("dragstart", { currentTarget: draggedElement }, element);
+        triggerDraggableEvent(
+            "dragstart",
+            { currentTarget: draggedElement, target: draggedElement },
+            element
+        );
         triggerDraggableEvent("dragcancel", {}, element);
 
         ok(draggedElement.is(":visible"), "draggableElement is shown");
@@ -73,19 +89,158 @@
             targetOffset = kendo.getOffset(targetElement),
             sortable = element.kendoSortable().data("kendoSortable");
 
-        //simulate press and move to trigger draggable's hint initialization
+        //simulate press to trigger draggable's hint initialization
         press(draggableElement, draggableOffset.left, draggableOffset.top);
-        move(draggableElement, targetOffset.left, targetOffset.top + 10);
-
-        //hint is re-located after move
-        equal(sortable.placeholder.index(), 1);
-
-        triggerDraggableEvent("drag",  {
-            x: { client: targetOffset.left },
-            y: { client: targetOffset.top + 10 }
-        }, element);
+        moveToSort(draggableElement, targetOffset.left, targetOffset.top + 10);
 
         equal(sortable.placeholder.index(), 2, "placeholder is moved under the element under cursor");
+
+        targetElement = element.children().last();
+        targetOffset = kendo.getOffset(targetElement);
+
+        moveToSort(draggableElement, targetOffset.left, targetOffset.top + 10);
+        equal(sortable.placeholder.index(), 4, "placeholder changes its position while the draggableElement moves");
     });
+
+    test("placeholder is not moved if item is dragged outside of the sortable container", 1, function() {
+        var draggableElement = element.children().eq(1),
+            draggableOffset = kendo.getOffset(draggableElement),
+            sortable = element.kendoSortable().data("kendoSortable");
+
+        //simulate press to trigger draggable's hint initialization
+        press(draggableElement, draggableOffset.left, draggableOffset.top);
+        moveToSort(draggableElement, 500, 500);
+
+        equal(sortable.placeholder.index(), 1, "placeholder position does not change");
+    });
+
+    test("item is sorted correctly on dragend", 1, function() {
+        var draggableElement = element.children().eq(3),
+            draggableOffset = kendo.getOffset(draggableElement),
+            targetElement = element.children().eq(1),
+            targetOffset = kendo.getOffset(targetElement),
+            sortable = element.kendoSortable().data("kendoSortable");
+
+        //simulate press to trigger draggable's hint initialization
+        press(draggableElement, draggableOffset.left, draggableOffset.top);
+        moveToSort(draggableElement, targetOffset.left, targetOffset.top - 10);
+        release(draggableElement, targetOffset.left, targetOffset.top - 10);
+
+        equal(draggableElement.index(), 1, "draggableElement changes its position");
+    });
+
+    test("item is placed at the last valid position even if item is released outside of the sortable container", 1, function() {
+        var draggableElement = element.children().eq(2),
+            draggableOffset = kendo.getOffset(draggableElement),
+            targetElement = element.children().eq(3),
+            targetOffset = kendo.getOffset(targetElement),
+            sortable = element.kendoSortable().data("kendoSortable");
+
+        //simulate press to trigger draggable's hint initialization
+        press(draggableElement, draggableOffset.left, draggableOffset.top);
+        moveToSort(draggableElement, targetOffset.left, targetOffset.top + 10);
+        moveToSort(draggableElement, 500, 500);
+        release(draggableElement, 500, 500);
+
+        equal(draggableElement.index(), 3, "draggableElement is placed at the last valid position");
+    });
+
+    module("Sortable - filtering and excluding items", {
+        setup: function() {
+            QUnit.fixture.append(
+                '<ul id="sortable">' +
+                    '<li class="item">foo</li>' +
+                    '<li class="filtered">bar</li>' +
+                    '<li class="item">baz</li>' +
+                    '<li class="item excluded">qux</li>' +
+                '</ul>'
+            );
+
+            element = $("#sortable");
+            filteredIndex = $("#sortable .filtered").index();
+            excludedIndex = $("#sortable .excluded").index();
+        },
+        teardown: function() {
+            kendo.destroy(QUnit.fixture);
+        }
+    });
+
+    test("user is not able to drag items that does not match the filter", 2, function() {
+        var draggableElement = element.children().eq(filteredIndex),
+            draggableOffset = kendo.getOffset(draggableElement),
+            targetElement = element.children().eq(2),
+            targetOffset = kendo.getOffset(targetElement),
+            sortable = element.kendoSortable({
+                    filter: ".item"
+                }).data("kendoSortable");
+
+        sortable.bind("start", function(e) {
+            ok(false, "start event is not fired");
+        });
+
+        press(draggableElement, draggableOffset.left, draggableOffset.top);
+        moveToSort(draggableElement, targetOffset.left, targetOffset.top + 10);
+
+        ok(!sortable.placeholder, "placeholder is not initialized");
+        equal(draggableElement.index(), filteredIndex, "draggableElement did not change its position");
+    });
+
+    test("filtered items are valid drop targets", 1, function() {
+        var draggableElement = element.children().eq(2),
+            draggableOffset = kendo.getOffset(draggableElement),
+            targetElement = element.children().eq(filteredIndex),
+            targetOffset = kendo.getOffset(targetElement),
+            sortable = element.kendoSortable({
+                    filter: ".item"
+                }).data("kendoSortable");
+
+        press(draggableElement, draggableOffset.left, draggableOffset.top);
+        moveToSort(draggableElement, targetOffset.left, targetOffset.top - 10);
+
+        notEqual(targetElement.index(), filteredIndex, "filtered item changes its position");
+    });
+
+    test("user is not able to drag excluded items", 1, function() {
+        var draggableElement = element.children().eq(excludedIndex),
+            draggableOffset = kendo.getOffset(draggableElement),
+            targetElement = element.children().eq(0),
+            targetOffset = kendo.getOffset(targetElement),
+            sortable = element.kendoSortable({
+                    excluded: ".excluded"
+                }).data("kendoSortable");
+
+        sortable.bind("start", function(e) {
+            ok(false, "start event is not fired");
+        });
+
+        press(draggableElement, draggableOffset.left, draggableOffset.top);
+        moveToSort(draggableElement, targetOffset.left, targetOffset.top + 10);
+
+        equal(draggableElement.index(), excludedIndex, "draggableElement did not change its position");
+    });
+/*
+    test("excluded items are not valid drop targets and does not move then users drags an item onto them", 2, function() {
+        var draggableElement = element.children().eq(0),
+            draggableOffset = kendo.getOffset(draggableElement),
+            targetElement = element.children().eq(excludedIndex),
+            targetOffset = kendo.getOffset(targetElement),
+            sortable = element.kendoSortable({
+                    excluded: ".excluded"
+                }).data("kendoSortable");
+
+        sortable.bind("start", function(e) {
+            ok(false, "start event is not fired");
+        });
+
+        press(draggableElement, draggableOffset.left, draggableOffset.top);
+        moveToSort(draggableElement, targetOffset.left, targetOffset.top + 10);
+
+        //+1 is added because placeholder is appended to the element which changes the index
+        equal(targetElement.index(), excludedIndex + 1, "The excluded item did not change its position");
+
+        release(draggableElement, targetElement.left, targetOffset,top + 10);
+        equal(draggableElement.index(), 0, "draggableElement did not change its position");
+    });
+*/
 
 })();
