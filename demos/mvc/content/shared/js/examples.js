c8d3ba036d5175a7b3d255eb611a45a25ab7aee9
Alex Gyoshev <alex@gyoshev.net>
2014-01-02 14:17:39 +0200

    Remove console statements from examples.js

--- demos/mvc/content/shared/js/examples.js
+++ demos/mvc/content/shared/js/examples.js
@@ -100,8 +100,6 @@
 
         fetchWidget: function (href) {
             var wrapInner = $("#mainWrapInner");
-            console.log("foo");
-            console.trace();
 
             $.get(href + "?nav=true", function (html) {
                 var parts = href.split("/"),
