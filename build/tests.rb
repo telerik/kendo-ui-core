ca29142efe64987582729dc9fe153b152718e6c5
Petyo Ivanov <underlog@gmail.com>
2014-01-10 16:51:52 +0200

    Fix test runner

--- build/tests.rb
+++ build/tests.rb
@@ -2,7 +2,7 @@ TESTS = FileList["tests/**/*"]
 GRUNT = "./node_modules/.bin/grunt"
 XVFB_RUN = "xvfb-run"
 GRUNT_CMD = system("which", XVFB_RUN, :out => "/dev/null") ? [XVFB_RUN, "-a", GRUNT] : [GRUNT]
-DEPS = [MIN_JS, MIN_CSS, KENDO_CONFIG_FILE, TESTS].flatten
+DEPS = [MIN_JS.sub("dist/js", "src").sub("min.js", "js"), MIN_CSS, KENDO_CONFIG_FILE, TESTS].flatten
 SUPPORTED_JQUERY_VERSIONS = ["1.10.2", "2.0.3"]
 
 namespace :tests do
@@ -18,20 +18,20 @@ namespace :tests do
     desc "Run tests in supported jQuery versions"
     task :jquery => DEPS do
         SUPPORTED_JQUERY_VERSIONS.each do |version|
-            sh *(GRUNT_CMD + [ "ci", "--junit-results=jquery-#{version}-test-results.xml", "--jquery=#{version}" ])
+            sh *(GRUNT_CMD + [ "ci", "--junit-results=jquery-#{version}-test-results.xml", "--single-run=true", "--jquery=#{version}" ])
         end
     end
 
     desc "Run tests in firefox"
     task :firefox => DEPS do
-        sh *(GRUNT_CMD + [ "ci", "--junit-results=firefox-test-results.xml", "--browser=Firefox" ])
+        sh *(GRUNT_CMD + [ "ci", "--junit-results=firefox-test-results.xml", "--single-run=true", "--browser=Firefox" ])
     end
 
     %w[CI Production TZ].each do |env|
         output = "#{env}-test-results.xml"
 
         file output => DEPS do |t|
-            sh *(GRUNT_CMD + [ "ci", "karma", "--junit-results=#{output}" ])
+            sh *(GRUNT_CMD + [ "jshint", "ci", "--junit-results=#{output}", "--single-run=true" ])
         end
 
         desc "Run #{env} tests"
