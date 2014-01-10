a2adefb99ef78b13fdf58859dd486586e650aeb5
Georgi Krustev <ggkrustev@yahoo.com>
2014-01-10 17:31:11 +0200

    Add MaskInput skeleton. Render a wrapper

---
+++ src/kendo.maskinput.js
@@ -0,0 +1,52 @@
+kendo_module({
+    id: "maskinput",
+    name: "MaskInput",
+    category: "web",
+    description: "The MaskInput widget allows to specify a mask type on an input field.",
+    depends: [ "core" ]
+});
+
+(function($, undefined) {
+    var kendo = window.kendo;
+    var ui = kendo.ui;
+    var Widget = ui.Widget;
+
+    var MaskInput = Widget.extend({
+        init: function(element, options) {
+            var that = this;
+
+            Widget.fn.init.call(that, element, options);
+
+            that._wrapper();
+        },
+
+        options: {
+            name: "MaskInput"
+        },
+
+        events: [],
+
+        _wrapper: function() {
+            var element = this.element;
+            var DOMelement = element[0];
+            var wrapper = element.parent("span.k-widget");
+
+            if (!wrapper[0]) {
+                wrapper = element.wrap("<span />").parent();
+            }
+
+            wrapper[0].style.cssText = DOMelement.style.cssText;
+            element.css({
+                width: "100%",
+                height: DOMelement.style.height
+            });
+
+            this.wrapper = wrapper
+                            .addClass("k-widget k-maskinput k-header")
+                            .addClass(DOMelement.className);
+        }
+    });
+
+    ui.plugin(MaskInput);
+
+})(window.kendo.jQuery);
