bda30a8b2218e881b6c0b5baa72cc51e4ac56435
Georgi Krustev <ggkrustev@yahoo.com>
2014-01-16 19:14:19 +0200

    Implement typing and deleting symbols using one method. Replace of an old symbol is done using 'insert' mode

--- tests/maskinput/navigation.js
+++ tests/maskinput/navigation.js
@@ -75,12 +75,38 @@
         equal(input.val(), "0-_");
     });
 
-    test("MaskInput places caret after static character", function() {
+    test("MaskInput inserts after a static character", function() {
         var maskinput = new MaskInput(input, {
-            mask: "0-0"
+            mask: "-0"
         });
 
         input.focus();
+        input.pressKey("0");
+
+        equal(caret(input[0])[0], 2);
+    });
+
+    /*test("MaskInput moves the rest of input value", function() {
+        var maskinput = new MaskInput(input, {
+            mask: "0-000"
+        });
+
+        input.focus();
+        input.val("0-123");
+        caret(input[0], 2);
+        input.pressKey("0");
+
+        equal(caret(input[0])[0], 3);
+        equal(input.val(), "0-012");
+    });*/
+
+    test("MaskInput prevents user input if end of mask is reached", function() {
+        var maskinput = new MaskInput(input, {
+            mask: "-0"
+        });
+
+        input.focus();
+        caret(input[0], 2);
         input.pressKey("0");
 
         equal(caret(input[0])[0], 2);
@@ -98,6 +124,48 @@
         input.pressKey(kendo.keys.BACKSPACE, "keydown");
 
         equal(input.val(), "2-_");
-        equal(caret(input[0])[0], 1);
+        equal(caret(input[0])[0], 2);
+    });
+
+    test("MaskInput prevents user input on BACKSPACE if start is reached", function() {
+        var maskinput = new MaskInput(input, {
+            mask: "0-0"
+        });
+
+        input.focus();
+
+        input.pressKey(kendo.keys.BACKSPACE, "keydown");
+
+        equal(input.val(), "_-_");
+        equal(caret(input[0])[0], 0);
+    });
+
+    test("MaskInput skips characters on BACKSPACE", function() {
+        var maskinput = new MaskInput(input, {
+            mask: "0-0"
+        });
+
+        input.focus();
+        caret(input[0], 2);
+
+        input.pressKey(kendo.keys.BACKSPACE, "keydown");
+
+        equal(input.val(), "_-_");
+        equal(caret(input[0])[0], 0);
+    });
+
+    test("MaskInput clears first possible position on BACKSPACE", function() {
+        var maskinput = new MaskInput(input, {
+            mask: "0-0"
+        });
+
+        input.focus();
+        input.val("2-2");
+        caret(input[0], 2);
+
+        input.pressKey(kendo.keys.BACKSPACE, "keydown");
+
+        equal(input.val(), "_-2");
+        equal(caret(input[0])[0], 0);
     });
 })();
