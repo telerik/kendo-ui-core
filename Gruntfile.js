e1f1cfb029a6052deba71eaa24cd392220ddab4d
Petyo Ivanov <underlog@gmail.com>
2014-01-17 15:58:43 +0200

    Include grunt debug tool

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ var PATH = require("path");
 
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-debug-task');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
