dc3d8316ea1946acdb0ccd11ca8aade85bd0547e
Georgi Krustev <ggkrustev@yahoo.com>
2014-01-20 18:47:35 +0200

    Show/hide empty mask on focus/blur. Remove input value if empty symbol has been left

--- tests/maskinput/interaction.js
+++ tests/maskinput/interaction.js
@@ -11,6 +11,48 @@
         }
     });
 
+    function caret(element, start, end) {
+        var range;
+        var isPosition = start !== undefined;
+
+        if (end === undefined) {
+            end = start;
+        }
+
+        if (element.selectionStart !== undefined) {
+            if (isPosition) {
+                element.focus();
+                element.setSelectionRange(start, end);
+            } else {
+                start = [element.selectionStart, element.selectionEnd];
+            }
+        } else if (document.selection) {
+            if ($(element).is(":visible")) {
+                element.focus();
+            }
+            range = document.selection.createRange();
+            if (isPosition) {
+                range.collapse(true);
+                range.moveStart("character", start);
+                range.moveEnd("character", end - start);
+                range.select();
+            } else {
+                var rangeElement = element.createTextRange(),
+                    rangeDuplicated = rangeElement.duplicate(),
+                    selectionStart, selectionEnd;
+
+                    rangeElement.moveToBookmark(range.getBookmark());
+                    rangeDuplicated.setEndPoint('EndToStart', rangeElement);
+                    selectionStart = rangeDuplicated.text.length;
+                    selectionEnd = selectionStart + rangeElement.text.length;
+
+                start = [selectionStart, selectionEnd];
+            }
+        }
+
+        return start;
+    }
+
     test("MaskInput shows empty mask on focus", function() {
         var maskinput = new MaskInput(input, {
             mask: "0-0"
@@ -19,6 +61,34 @@
         input.focus();
 
         equal(input.val(), "_-_");
+        equal(caret(input[0])[0], 0);
+        equal(caret(input[0])[1], 0);
+    });
+
+    test("MaskInput selects whole text if value", function() {
+        var maskinput = new MaskInput(input, {
+            mask: "0-0"
+        });
+
+        maskinput.value("1-1");
+
+        input.focus();
+
+        equal(input.val(), "1-1");
+        equal(caret(input[0])[0], 0);
+        equal(caret(input[0])[1], 3);
+    });
+
+    test("MaskInput does not remove input value on blur", function() {
+        var maskinput = new MaskInput(input, {
+            mask: "0-0"
+        });
+
+        maskinput.value("1-1");
+
+        input.focus().blur();
+
+        equal(input.val(), "1-1");
     });
 
     test("MaskInput removes empty mask on blur", function() {
@@ -27,6 +97,18 @@
         });
 
         input.focus().blur();
+
+        equal(input.val(), "");
+    });
+
+    test("MaskInput removes widget value if empty symbol is left", function() {
+        var maskinput = new MaskInput(input, {
+            mask: "0-0"
+        });
+
+        input.focus();
+        input.val("1-_");
+        input.blur();
 
         equal(input.val(), "");
     });
