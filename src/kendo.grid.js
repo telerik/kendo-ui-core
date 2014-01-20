363847bd90dce4c5506ecee148860e1a2afc6d0a
Petur Subev <petur.subev@gmail.com>
2014-01-20 14:36:33 +0200

    Fix footer-scrolling when using virtual scrolling

--- src/kendo.grid.js
+++ src/kendo.grid.js
@@ -2825,7 +2825,12 @@ var __meta__ = {
                 }
 
                 if (footerWrap) {
-                    var offset = options.scrollable ? that.wrapper.find('.k-virtual-scrollable-wrap').scrollLeft() : that.content.scrollLeft();
+                    var offset = that.content.scrollLeft();
+
+                    var hasVirtualScroll = options.scrollable !== true && scrollable.virtual && !that.virtualScrollable;
+                    if(hasVirtualScroll){
+                        offset = that.wrapper.find('.k-virtual-scrollable-wrap').scrollLeft();
+                    }
                     footerWrap.scrollLeft(offset);
                 }
             }
