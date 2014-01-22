65bd6dbd6f6f6f7544b5f20c1f62a125a0a431f7
Atanas Korchev <akorchev@gmail.com>
2014-01-22 15:09:34 +0200

    Update the zip file names according to rebranding.

--- build/version.rb
+++ build/version.rb
@@ -16,10 +16,16 @@ VERSION = ENV['VERSION'] || "#{VERSION_YEAR}.#{VERSION_Q}.#{month * 100 + now.da
 CURRENT_COMMIT = `git rev-parse HEAD`.strip
 BETA = !!ENV['BETA']
 
+def prefix_bundle_name(bundle)
+    prefix = (bundle =~ /mvc|php|jsp/) ? 'telerik.ui.for.' : 'kendoui.'
+
+    prefix + bundle
+end
+
 def versioned_bundle_name(bundle)
-    ('kendoui.' + bundle).sub(/\.[^\.]+$/, ".#{VERSION}\\0")
+    prefix_bundle_name(bundle).sub(/\.[^\.]+$/, ".#{VERSION}\\0")
 end
 
 def latest_bundle_name(bundle)
-    ('kendoui.' + bundle).sub(/\.[^\.]+$/, ".latest\\0")
+    prefix_bundle_name(bundle).sub(/\.[^\.]+$/, ".latest\\0")
 end
