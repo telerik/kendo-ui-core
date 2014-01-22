587ebced8b277ca31b1d075469b9aa692bcc6914
Georgi Krustev <ggkrustev@yahoo.com>
2014-01-22 12:36:57 +0200

    Parse date literals with timezone offset. Fixes #2737

--- src/kendo.core.js
+++ src/kendo.core.js
@@ -1091,6 +1091,7 @@ function pad(number, digits, end) {
         shortTimeZoneRegExp = /[+|\-]\d{1,2}/,
         longTimeZoneRegExp = /[+|\-]\d{1,2}:\d{2}/,
         dateRegExp = /^\/Date\((.*?)\)\/$/,
+        tzOffsetRegExp = /[+-]{1}\d+/,
         formatsSequence = ["G", "g", "d", "F", "D", "y", "m", "T", "t"],
         numberRegExp = {
             2: /^\d{1,2}/,
@@ -1441,14 +1442,24 @@ function pad(number, digits, end) {
             return value;
         }
 
-        var idx = 0,
-            date = null,
-            length, patterns;
+        var idx = 0;
+        var date = null;
+        var length, patterns;
+        var tzoffset;
 
         if (value && value.indexOf("/D") === 0) {
             date = dateRegExp.exec(value);
             if (date) {
-                return new Date(parseInt(date[1], 10));
+                date = date[1];
+
+                tzoffset = tzOffsetRegExp.exec(date);
+                date = parseInt(date, 10);
+
+                if (tzoffset) {
+                    date -= (parseInt(tzoffset[0], 10) * kendo.date.MS_PER_MINUTE);
+                }
+
+                return new Date(date);
             }
         }
 
