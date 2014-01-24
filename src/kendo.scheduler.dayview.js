4c1023f22b31f8fbe3ca755d33ebaa475c404684
Dimo Dimov <dimo.dimov@telerik.com>
2014-01-24 14:36:11 +0200

    Add missing name to WorkWeek view

--- src/kendo.scheduler.dayview.js
+++ src/kendo.scheduler.dayview.js
@@ -1528,38 +1528,39 @@ var __meta__ = {
         }
     });
 
-   extend(true, ui, {
-       MultiDayView: MultiDayView,
-       DayView: MultiDayView.extend({
-           options: {
-               title: "Day"
-           },
-           name: "day"
-       }),
-       WeekView: MultiDayView.extend({
-           options: {
-               title: "Week",
-               selectedDateFormat: "{0:D} - {1:D}"
-           },
-           name: "week",
-           calculateDateRange: function() {
-               var selectedDate = this.options.date,
-                   start = kendo.date.dayOfWeek(selectedDate, this.calendarInfo().firstDay, -1),
-                   idx, length,
-                   dates = [];
+    extend(true, ui, {
+        MultiDayView: MultiDayView,
+        DayView: MultiDayView.extend({
+            options: {
+                title: "Day"
+            },
+            name: "day"
+        }),
+        WeekView: MultiDayView.extend({
+            options: {
+                title: "Week",
+                selectedDateFormat: "{0:D} - {1:D}"
+            },
+            name: "week",
+            calculateDateRange: function() {
+                var selectedDate = this.options.date,
+                    start = kendo.date.dayOfWeek(selectedDate, this.calendarInfo().firstDay, -1),
+                    idx, length,
+                    dates = [];
 
-               for (idx = 0, length = 7; idx < length; idx++) {
-                   dates.push(start);
-                   start = kendo.date.nextDay(start);
-               }
-               this._render(dates);
-           }
-       }),
-       WorkWeekView: MultiDayView.extend({
-           options: {
-              title: "Work Week",
-              selectedDateFormat: "{0:D} - {1:D}"
-           },
+                for (idx = 0, length = 7; idx < length; idx++) {
+                    dates.push(start);
+                    start = kendo.date.nextDay(start);
+                }
+                this._render(dates);
+            }
+        }),
+        WorkWeekView: MultiDayView.extend({
+            options: {
+                title: "Work Week",
+                selectedDateFormat: "{0:D} - {1:D}"
+            },
+            name: "workweeek",
             nextDate: function() {
                 return kendo.date.dayOfWeek(kendo.date.nextDay(this.endDate()), this.options.workWeekStart, 1);
             },
@@ -1578,9 +1579,9 @@ var __meta__ = {
                 }
                 this._render(dates);
             }
-       })
-
+        })
     });
+
 })(window.kendo.jQuery);
 
 return window.kendo;
