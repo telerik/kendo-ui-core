08e7fd77e0da5f8fdbf20e3f6baac4b0c80cb7c3
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2014-01-31 10:06:39 +0200

    Fix typo in the workweek view name. Fixes #2787

--- src/kendo.scheduler.dayview.js
+++ src/kendo.scheduler.dayview.js
@@ -1560,7 +1560,7 @@ var __meta__ = {
                 title: "Work Week",
                 selectedDateFormat: "{0:D} - {1:D}"
             },
-            name: "workweeek",
+            name: "workweek",
             nextDate: function() {
                 return kendo.date.dayOfWeek(kendo.date.nextDay(this.endDate()), this.options.workWeekStart, 1);
             },
