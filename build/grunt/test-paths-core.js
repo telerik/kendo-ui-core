cc28bdbbfa12ce6b8797af6ff9d8c48eae886bf7
Petyo Ivanov <underlog@gmail.com>
2014-02-03 11:39:00 +0200

    Support for full test run in private repository

--- build/grunt/test-paths-core.js
+++ build/grunt/test-paths-core.js
@@ -2,9 +2,7 @@ exports.beforeTestFiles = [
     { pattern: 'styles/**/*.*', watched: true, included: false },
     { pattern: 'tests/router/sandbox.html', watched: true, included: false },
     { pattern: 'tests/window/blank.html', watched: true, included: false },
-    { pattern: 'tests/editor/editorStyles.css', included: false },
-    { pattern: 'tests/**/*-fixture.html' },
-    { pattern: 'demos/mvc/App_Data/*json', included: false },
+    { pattern: 'tests/**/*-fixture.html' }
 ];
 
 exports.afterTestFiles = [
@@ -13,46 +11,13 @@ exports.afterTestFiles = [
     'src/cultures/kendo.culture.bg-BG.js',
     'src/cultures/kendo.culture.en-ZA.js',
     "src/cultures/kendo.culture.es-ES.js",
-
     'tests/kendo-test-helpers.js',
     'tests/**/test-helper.js',
-    'demos/mvc/content/shared/js/less.js',
-    'demos/mvc/content/mobilethemebuilder/scripts/colorengine.js',
-    'demos/mvc/content/mobilethemebuilder/scripts/gradientengine.js',
-
-    'tests/chart/util.js',
-    'tests/map/util.js',
-    'tests/map/layers/base.js',
-
-    'themebuilder/scripts/themebuilder.js',
-
-    'tests/chart/util.js',
-    'tests/upload/helper.js',
-    'tests/upload/select.js',
-    'tests/upload/selection.js',
-    'tests/upload/async.js',
-    'tests/upload/asyncnomultiple.js',
-    'tests/upload/asyncnoauto.js',
-    'tests/upload/upload.js',
-    'tests/upload/success.js',
-    'tests/upload/error.js',
-    'tests/upload/cancel.js',
-    'tests/upload/remove.js',
-
-    { pattern: 'src/kendo.editor.js', included: false }, // download builder needs this
-    { pattern: 'src/kendo.aspnetmvc.js', included: false }, // download builder needs this
-
-    'download-builder/scripts/script-resolver.js',
-    'tests/diagram/common.js',
-
-    'demos/mvc/content/shared/js/trykendo.js',
-    'tests/demos/trykendo.js'
+    'build/less-js/dist/less-1.6.0.min.js'
 ];
 
 exports.compiledStyleSheets = [
-    'dist/styles/mobile/kendo.mobile.all.css',
-    'dist/styles/web/kendo.common.css',
-    'dist/styles/dataviz/kendo.dataviz.css',
+    'dist/styles/web/kendo.common.core.css',
     'dist/styles/web/kendo.rtl.css'
 ];
 
