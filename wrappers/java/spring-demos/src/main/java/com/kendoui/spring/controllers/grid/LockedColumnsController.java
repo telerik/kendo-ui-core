ab56cbb6d0c4de64c64c8c073ef856db1245e333
Nikolay Rusev <nikolay.rusev@telerik.com>
2014-02-18 15:59:28 +0200

    Add locked columns grid example in java wrappers

---
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/controllers/grid/LockedColumnsController.java
@@ -0,0 +1,29 @@
+package com.kendoui.spring.controllers.grid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kendoui.spring.models.DataSourceRequest;
+import com.kendoui.spring.models.DataSourceResult;
+import com.kendoui.spring.models.OrderDao;
+
+@Controller("grid-locked-columns-controller")
+@RequestMapping(value="/web/grid/")
+public class LockedColumnsController {
+    @Autowired 
+    private OrderDao order;
+
+    @RequestMapping(value = "/locked-columns", method = RequestMethod.GET)
+    public String index() {
+        return "web/grid/locked-columns";
+    }
+    
+    @RequestMapping(value = "/locked-columns/read", method = RequestMethod.POST)
+    public @ResponseBody DataSourceResult read(@RequestBody DataSourceRequest request) {
+        return order.getList(request);
+    }
+}
