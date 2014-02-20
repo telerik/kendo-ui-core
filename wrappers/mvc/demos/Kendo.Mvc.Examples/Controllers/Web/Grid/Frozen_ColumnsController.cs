f7e358e10c19610ca4821f45db3c5d039cd814a9
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2014-02-20 15:07:20 +0200

    Add missing frozen column files

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Web/Grid/Frozen_ColumnsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class GridController : Controller
+    {        
+        public ActionResult Frozen_Columns()
+        {
+            return View();
+        }
+
+        public ActionResult FrozenColumns_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(GetOrders().ToDataSourceResult(request));
+        }
+    }
+}
