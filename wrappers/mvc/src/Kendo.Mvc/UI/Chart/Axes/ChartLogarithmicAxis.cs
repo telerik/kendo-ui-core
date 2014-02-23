8c709e2d25281e099059dc8a60b1af94374438aa
Daniel Karadachki <karada@abv.bg>
2014-02-24 01:59:37 +0200

    Add logarithmic axis mvc demos

---
+++ wrappers/mvc/src/Kendo.Mvc/UI/Chart/Axes/ChartLogarithmicAxis.cs
@@ -0,0 +1,21 @@
+﻿namespace Kendo.Mvc.UI
+{
+    using System.Collections.Generic;
+
+    public class ChartLogarithmicAxis<T>: ChartNumericAxis<T> where T:class
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChartLogarithmicAxis{T}" /> class.
+        /// </summary>
+        /// <param name="chart">The chart.</param>
+        public ChartLogarithmicAxis(Chart<T> chart)
+            : base(chart)
+        {
+        }
+
+        public override IChartSerializer CreateSerializer()
+        {
+            return new ChartLogarithmicAxisSerializer(this);
+        }
+    }
+}
---
+++ wrappers/mvc/src/Kendo.Mvc/UI/Chart/Serialization/ChartLogarithmicAxisSerializer.cs
@@ -0,0 +1,20 @@
+﻿namespace Kendo.Mvc.UI
+{
+    using System.Collections.Generic;
+
+    internal class ChartLogarithmicAxisSerializer: ChartNumericAxisSerializer
+    {
+        public ChartLogarithmicAxisSerializer(IChartNumericAxis axis)
+            : base(axis)
+        {
+        }
+
+        public override IDictionary<string, object> Serialize()
+        {
+            var result = base.Serialize();
+            result["type"] = "log";
+
+            return result;
+        }
+    }
+}
