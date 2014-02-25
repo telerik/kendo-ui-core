eca3d65bb42900b8ad30e740943bbbdfc677a67b
Petyo Ivanov <underlog@gmail.com>
2014-02-25 22:10:53 +0200

    The animations should not use the global queue - too choppy

--- src/kendo.fx.js
+++ src/kendo.fx.js
@@ -1486,7 +1486,7 @@ var __meta__ = {
             that.tick();
 
             if (!that.done()) {
-                kendo.queueAnimation(that._tickProxy);
+                kendo.animationFrame(that._tickProxy);
             } else {
                 that._started = false;
                 that.onEnd();
