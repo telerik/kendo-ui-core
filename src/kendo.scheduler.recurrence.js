75a7b98bc58e7a53a62e02b981e2701bc7d2ea6c
Georgi Krustev <ggkrustev@yahoo.com>
2014-02-26 08:33:23 +0200

    Add localized text for hourly freq in mobile recurring widget

--- src/kendo.scheduler.recurrence.js
+++ src/kendo.scheduler.recurrence.js
@@ -2335,6 +2335,9 @@ var __meta__ = {
                 daily: {
                     interval: ""
                 },
+                hourly: {
+                    interval: ""
+                },
                 weekly: {
                     interval: ""
                 },
