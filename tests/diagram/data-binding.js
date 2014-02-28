12c80c16d76280f70db6e164c12598aa78faa50e
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-02-28 15:55:24 +0200

    Add diagram data binding tests

---
+++ tests/diagram/data-binding.js
@@ -0,0 +1,60 @@
+(function() {
+    var diagram;
+
+    function createDiagram(options) {
+        var div = $("<div id='container' />").appendTo(QUnit.fixture);
+        div.kendoDiagram(options);
+
+        return div.data("kendoDiagram");
+    }
+
+    function destroyDiagram() {
+        kendo.destroy(QUnit.fixture);
+        QUnit.fixture.empty();
+    }
+
+    // ------------------------------------------------------------
+    module("Diagram / Data Binding", {
+          teardown: destroyDiagram
+    });
+
+    test("Binds to hierarchical data", function() {
+        diagram = createDiagram({
+            dataSource: {
+                data: [{
+                    id: "1",
+                    items: [{
+                        id: "1.1"
+                    }]
+                }],
+                schema: {
+                    model: {
+                        children: "items"
+                    }
+                }
+            }
+        });
+
+        equal(diagram.shapes.length, 2);
+    });
+
+    test("Binds to hierarchical data (custom field)", function() {
+        diagram = createDiagram({
+            dataSource: {
+                data: [{
+                    id: "1",
+                    bars: [{
+                        id: "1.1"
+                    }]
+                }],
+                schema: {
+                    model: {
+                        children: "bars"
+                    }
+                }
+            }
+        });
+
+        equal(diagram.shapes.length, 2);
+    });
+})();
