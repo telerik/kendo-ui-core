cec68be18d0bdb17d24fe899b2444f0f6b615156
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-03-03 15:21:15 +0200

    Add scripts for sample stock data

---
+++ resources/sample-data/stock/dbcreate.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/ruby
+require 'csv'
+
+# Switch to CRLF in output
+$/ = "\r\n"
+
+DATE_FORMAT = '%d-%m-%Y'
+
+puts %Q{
+    CREATE TABLE Stock (
+        [ID] INT IDENTITY(1,1) NOT NULL,
+        [Symbol] VARCHAR(10) NOT NULL,
+        [Date] DATETIME NOT NULL,
+        [Open] DECIMAL(9,3) NOT NULL,
+        [High] DECIMAL(9,3) NOT NULL,
+        [Low] DECIMAL(9,3) NOT NULL,
+        [Close] DECIMAL(9,3) NOT NULL
+    )
+    GO
+
+    BEGIN TRANSACTION
+}
+
+CSV.foreach('data.csv', :headers => true) do |row|
+    date = DateTime.strptime(row['Date'], '%Y/%m/%d')
+
+    puts %Q{
+    INSERT INTO Stock
+        ([Symbol], [Date], [Open], [High], [Low], [Close])
+    VALUES
+        ('BA', CONVERT(DATETIME, '#{date.strftime(DATE_FORMAT)}', 103), #{row['Open']}, #{row['High']}, #{row['Low']}, #{row['Close']})
+    }
+end
+
+puts %Q{
+    COMMIT TRANSACTION
+
+    CREATE INDEX IX_Stock_Date
+        ON Stock ([Date])
+    GO
+}
