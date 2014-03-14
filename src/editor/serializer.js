302b1960d8dbaf916aa94107593d9ddf945e920a
Alex Gyoshev <alex@gyoshev.net>
2014-03-14 11:43:06 +0200

    Add proper check for empty elements. Closes #2943.

--- src/editor/serializer.js
+++ src/editor/serializer.js
@@ -42,7 +42,7 @@ var Serializer = {
                     node = node.firstChild;
                 }
 
-                if (node.nodeType == 1 && node.tagName.toLowerCase() != "img") {
+                if (node.nodeType == 1 && !dom.empty[dom.name(node)]) {
                     node.innerHTML = kendo.ui.editor.emptyElementContent;
                 }
             }
