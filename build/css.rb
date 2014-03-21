686726e29bd2554e0d49290f123c179d90b589a2
Petyo Ivanov <underlog@gmail.com>
2014-03-21 18:43:54 +0200

    Core bundle definition

--- build/css.rb
+++ build/css.rb
@@ -9,7 +9,6 @@ MIN_CSS = FileList['styles/**/kendo*.less']
     .sub('styles/', 'dist/styles/')
     .uniq
 
-
 MIN_CSS_RESOURCES = FileList[MIN_CSS + FileList['styles/*/*/*'].sub('styles/', DIST_STYLES_ROOT)].exclude('**/*.less').exclude('**/*.winjs.*')
 
 WEB_MIN_CSS = FileList[MIN_CSS_RESOURCES].keep_if { |f| f =~ /styles\/web\// }
@@ -20,6 +19,24 @@ MOBILE_SRC_CSS = FileList[SRC_CSS].keep_if { |f| f =~ /styles\/(mobile|common)\/
 
 DATAVIZ_MIN_CSS = FileList[MIN_CSS_RESOURCES].keep_if { |f| f =~ /styles\/dataviz\// }
 DATAVIZ_SRC_CSS = FileList[SRC_CSS].keep_if { |f| f =~ /styles\/dataviz\// }
+
+CORE_MIN_CSS_RESOURCES = FileList[MIN_CSS_RESOURCES]
+    .exclude("dist/styles/kendo.common-bootstrap.min.css")
+    .exclude("dist/styles/kendo.common.min.css")
+    .keep_if { |f| f =~ /styles\/(web|common)\// }
+
+CORE_SRC_CSS = FileList[SRC_CSS]
+    .keep_if { |f| f =~ /styles\/(web|common)\// }
+    .exclude("dist/styles/web/kendo.common-bootstrap.css")
+    .exclude("dist/styles/web/kendo.common-bootstrap.less")
+    .exclude("dist/styles/web/kendo.common.css")
+    .exclude("dist/styles/web/kendo.common.less")
+    .exclude("dist/styles/web/common/grid.less")
+    .exclude("dist/styles/web/common/editor.less")
+    .exclude("dist/styles/web/common/scheduler.less")
+    .exclude("dist/styles/web/common/imagebrowser.less")
+    .exclude("dist/styles/web/common/treeview.less")
+    .exclude("dist/styles/web/common/upload.less")
 
 WIN_SRC_CSS = FileList["#{DIST_STYLES_ROOT}web/kendo.common.css"].include("#{DIST_STYLES_ROOT}dataviz/kendo.dataviz.css").include("#{DIST_STYLES_ROOT}web/kendo.rtl.css")
 WIN_MIN_CSS = FileList["#{DIST_STYLES_ROOT}web/kendo.winjs.min.css"]
