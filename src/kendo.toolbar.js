670ce187cb5520c14cf9eed39fe36e775e6e177a
Alexander Valchev <a.valchevbox@gmail.com>
2014-04-16 15:19:48 +0300

    Add ToolBar file. Create basic widget structure.

---
+++ src/kendo.toolbar.js
@@ -0,0 +1,48 @@
+(function(f, define){
+    define([ "./kendo.core" ], f);
+})(function(){
+
+var __meta__ = {
+    id: "toolbar",
+    name: "ToolBar",
+    category: "web",
+    description: "The ToolBar widget displays one or more command buttons divided into groups.",
+    depends: [ "core" ]
+};
+
+(function($, undefined) {
+    var kendo = window.kendo,
+        Widget = kendo.ui.Widget;
+
+        var ToolBar = Widget.extend({
+            init: function(element, options) {
+                var that = this;
+
+                Widget.fn.init.call(that, element, options);
+
+                options = that.options;
+                element = that.element;
+
+                element.addClass("k-toolbar");
+            },
+
+            events: [
+                //event list
+            ],
+
+            options: {
+                name: "ToolBar"
+                //option list
+            },
+
+            destroy: function() {
+                Widget.fn.destroy.call(this);
+            }
+        });
+
+    kendo.ui.plugin(ToolBar);
+})(window.kendo.jQuery);
+
+return window.kendo;
+
+}, typeof define == 'function' && define.amd ? define : function(_, f){ f(); });
