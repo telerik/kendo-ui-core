4c34ffa75d24654acf83a6c2119bde3d1a442133
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2014-04-28 15:16:24 +0300

    Pass columns, rows and measures settings to the transport read

--- src/kendo.pivotgrid.js
+++ src/kendo.pivotgrid.js
@@ -14,9 +14,42 @@ var __meta__ = {
     var kendo = window.kendo,
         ui = kendo.ui,
         Widget = ui.Widget,
+        DataSource = kendo.data.DataSource,
         extend = $.extend;
 
-    var PivotDataSource = kendo.data.DataSource.extend({});
+    var PivotDataSource = DataSource.extend({
+        init: function(options) {
+            DataSource.fn.init.call(this, options);
+
+            this._columns = options.columns || [];
+            this._rows = options.rows || [];
+            this._measures = options.measures || [];
+        },
+
+        columns: function() {
+            return this._columns;
+        },
+
+        rows: function() {
+            return this._rows;
+        },
+
+        measures: function() {
+            return this._measures;
+        },
+
+        _params: function(data) {
+            var options = DataSource.fn._params.call(this, data);
+
+            options = extend({
+                measures: this.measures(),
+                columns: this.columns(),
+                rows: this.rows()
+            }, options);
+
+            return options;
+        }
+    });
 
     PivotDataSource.create = function(options) {
         options = options && options.push ? { data: options } : options;
--- tests/pivotgrid/dataSource.js
+++ tests/pivotgrid/dataSource.js
@@ -15,4 +15,55 @@
        throws(function() { PivotDataSource.create(new kendo.data.DataSource()); } );
     });
 
+    test("setting columns during initialization", function() {
+        var dataSource = new PivotDataSource({
+            columns: ["foo", "bar"]
+        });
+
+        equal(dataSource.columns()[0], "foo");
+        equal(dataSource.columns()[1], "bar");
+    });
+
+    test("setting measures during initialization", function() {
+        var dataSource = new PivotDataSource({
+            measures: ["foo", "bar"]
+        });
+
+        equal(dataSource.measures()[0], "foo");
+        equal(dataSource.measures()[1], "bar");
+    });
+
+    test("setting rows during initialization", function() {
+        var dataSource = new PivotDataSource({
+            rows: ["foo", "bar"]
+        });
+
+        equal(dataSource.rows()[0], "foo");
+        equal(dataSource.rows()[1], "bar");
+    });
+
+    test("columns and rows are pass to the transport read", function() {
+        var dataSource = new PivotDataSource({
+            columns: ["foo"],
+            rows: ["bar"],
+            transport: {
+                read: function(options) {
+                    equal(options.data.columns[0], "foo");
+                    equal(options.data.rows[0], "bar"); }
+            }
+        });
+        dataSource.read();
+    });
+
+    test("measures are pass to the transport read", function() {
+        var dataSource = new PivotDataSource({
+            measures: ["foo"],
+            transport: {
+                read: function(options) {
+                    equal(options.data.measures[0], "foo"); }
+            }
+        });
+        dataSource.read();
+    });
+
 })();
