0d4459f1147f3ddfb9bfdd9d8e2ae2111915a3df
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2014-04-29 17:05:01 +0300

    Add XmlaDataReader base implementation

--- src/kendo.pivotgrid.js
+++ src/kendo.pivotgrid.js
@@ -7,7 +7,7 @@ var __meta__ = {
     name: "PivotGrid",
     category: "web",
     description: "The PivotGrid widget is a data summarization tool.",
-    depends: [ "dom" ]
+    depends: [ "dom", "data" ]
 };
 
 (function($, undefined) {
@@ -15,6 +15,7 @@ var __meta__ = {
         ui = kendo.ui,
         Widget = ui.Widget,
         DataSource = kendo.data.DataSource,
+        toString = {}.toString,
         extend = $.extend;
 
     var PivotDataSource = DataSource.extend({
@@ -74,11 +75,92 @@ var __meta__ = {
         }
     });
 
+    function asArray(object) {
+        if (object == null) {
+            return [];
+        }
+
+        var type = toString.call(object);
+        if (type !== "[object Array]") {
+            return [object]
+        }
+
+        return object;
+    }
+
+    function translateAxis(axis) {
+        var result = { tuples: [] };
+        var tuples = asArray(kendo.getter("Tuples.Tuple", true)(axis));
+        var captionGetter = kendo.getter("Caption['#text']");
+        var unameGetter = kendo.getter("UName['#text']");
+        var levelNameGetter = kendo.getter("LName['#text']");
+        var levelNumGetter = kendo.getter("LNum['#text']");
+        var childrenGetter = kendo.getter("CHILDREN_CARDINALITY['#text']", true);
+        var hierarchyGetter = kendo.getter("['Hierarchy']");
+        var parentNameGetter = kendo.getter("PARENT_UNIQUE_NAME['#text']", true);
+
+        for (var idx = 0; idx < tuples.length; idx++) {
+            var members = [];
+            var member = asArray(tuples[idx].Member);
+            for (var memberIdx = 0; memberIdx < member.length; memberIdx++) {
+                members.push({
+                    caption: captionGetter(member[memberIdx]),
+                    name: unameGetter(member[memberIdx]),
+                    levelName: levelNameGetter(member[memberIdx]),
+                    levelNum: levelNumGetter(member[memberIdx]),
+                    hasChildren: parseInt(childrenGetter(member[memberIdx]), 10) > 0,
+                    parentName: parentNameGetter(member[memberIdx]),
+                    hierarchy: hierarchyGetter(member[memberIdx])
+                });
+            }
+
+            result.tuples.push({ members: members });
+        }
+        return result;
+    }
+
+    var XmlaDataReader = kendo.data.XmlDataReader.extend({
+        parse: function(xml) {
+            var result = kendo.data.XmlDataReader.fn.parse(xml);
+            return kendo.getter("['soap:Envelope']['soap:Body']", true)(result);
+        },
+        errors: function(root) {
+            var fault = kendo.getter("['soap:Fault']", true)(root);
+            if (fault) {
+                return [{
+                    faultstring: kendo.getter("faultstring['#text']", true)(fault),
+                    faultcode: kendo.getter("faultcode['#text']", true)(fault),
+                }];
+            }
+            return null;
+        },
+        axes: function(root) {
+            root = kendo.getter("ExecuteResponse.return.root", true)(root);
+
+            var axes = kendo.getter("Axes.Axis", true)(root);
+            var columns = translateAxis(axes[0]);
+            var rows = {};
+
+            if (axes.length > 2) {
+                rows = translateAxis(axes[1]);
+            }
+
+            return {
+                columns: columns,
+                rows: rows
+            }
+        }
+    });
+
     extend(true, kendo.data, {
        PivotDataSource: PivotDataSource,
        XmlaTransport: XmlaTransport,
+       XmlaDataReader: XmlaDataReader,
        transports: {
            xmla: XmlaTransport
+       },
+       readers: {
+           xmla: XmlaDataReader
        }
     });
 
--- tests/pivotgrid/dataSource.js
+++ tests/pivotgrid/dataSource.js
@@ -85,4 +85,68 @@
         transport.read({success: $.noop, data: {}});
     });
 
+    module("XmlaDataReader initialziation", { });
+
+    test("parse returns the body tag content", function() {
+        var reader = new kendo.data.XmlaDataReader({ });
+        var response = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><foo>bar</foo></soap:Body></soap:Envelope>';
+
+        var body = reader.parse(response);
+        equal(body.foo["#text"], "bar");
+    });
+
+    test("errors are read from response", function() {
+        var reader = new kendo.data.XmlaDataReader({ });
+        var response = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> <soap:Body> <soap:Fault xmlns="http://schemas.xmlsoap.org/soap/envelope/"> <faultcode>code</faultcode> <faultstring>description</faultstring> <detail> <Error ErrorCode="3238985738" Description="description" HelpFile="" /> </detail> </soap:Fault> </soap:Body> </soap:Envelope>';
+
+        var body = reader.parse(response);
+        var errors = reader.errors(body);
+
+        equal(errors.length, 1);
+        equal(errors[0].faultcode, "code");
+        equal(errors[0].faultstring, "description");
+    });
+
+    test("axes are read from response", function() {
+        var reader = new kendo.data.XmlaDataReader({ });
+        var response = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><ExecuteResponse xmlns="urn:schemas-microsoft-com:xml-analysis"> <return> <root> <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"> </xs:schema> <OlapInfo></OlapInfo><Axes><Axis name="Axis0"><Tuples> <Tuple> <Member Hierarchy="[Date].[Calendar]"> <UName>[Date].[Calendar].[Year].&amp;[2005]</UName> <Caption>2005</Caption> <LName>[Date].[Calendar].[Year]</LName> <LNum>1</LNum> <DisplayInfo>2</DisplayInfo> <CHILDREN_CARDINALITY>2</CHILDREN_CARDINALITY> <PARENT_UNIQUE_NAME>[Date].[Calendar].[All]</PARENT_UNIQUE_NAME> </Member> </Tuple> </Tuples> </Axis> <Axis name="Axis1"> <Tuples> <Tuple> <Member Hierarchy="[Product].[Product Name]"> <UName>[Product].[Product Name].[All]</UName> <Caption>All</Caption> <LName>[Product].[Product Name].[(All)]</LName> <LNum>0</LNum> <DisplayInfo>504</DisplayInfo> <CHILDREN_CARDINALITY>504</CHILDREN_CARDINALITY> </Member> </Tuple> </Tuples> </Axis> <Axis name="SlicerAxis"> <Tuples> <Tuple> <Member Hierarchy="[Measures]"> <UName>[Measures].[Internet Order Lines Count]</UName> <Caption>Internet Order Lines Count</Caption> <LName>[Measures].[MeasuresLevel]</LName> <LNum>0</LNum> <DisplayInfo>0</DisplayInfo> </Member></Tuple> </Tuples> </Axis> </Axes> <CellData> </CellData> </root> </return> </ExecuteResponse> </soap:Body> </soap:Envelope>';
+
+        var body = reader.parse(response);
+        var axes = reader.axes(body);
+
+        equal(axes.columns.tuples.length, 1);
+        equal(axes.rows.tuples.length, 1);
+    });
+
+    test("empty column axes and no rows", function() {
+        var reader = new kendo.data.XmlaDataReader({ });
+        var response = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><ExecuteResponse xmlns="urn:schemas-microsoft-com:xml-analysis"> <return> <root> <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"> </xs:schema><OlapInfo></OlapInfo> <Axes> <Axis name="Axis0"> <Tuples/> </Axis> <Axis name="SlicerAxis"> <Tuples> <Tuple> <Member Hierarchy="[Measures]"> <UName>[Measures].[Internet Order Lines Count]</UName> <Caption>Internet Order Lines Count</Caption> <LName>[Measures].[MeasuresLevel]</LName> <LNum>0</LNum> <DisplayInfo>0</DisplayInfo> </Member>                 </Tuple> </Tuples> </Axis> </Axes> <CellData> </CellData> </root> </return> </ExecuteResponse> </soap:Body> </soap:Envelope>';
+
+        var body = reader.parse(response);
+        var axes = reader.axes(body);
+
+        equal(axes.columns.tuples.length, 0);
+        ok(!axes.rows.tuples);
+    });
+
+    test("axes tuples are read from response", function() {
+        var reader = new kendo.data.XmlaDataReader({ });
+        var response = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><ExecuteResponse xmlns="urn:schemas-microsoft-com:xml-analysis"> <return> <root> <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"> </xs:schema> <OlapInfo></OlapInfo><Axes><Axis name="Axis0"><Tuples> <Tuple> <Member Hierarchy="[Date].[Calendar]"> <UName>[Date].[Calendar].[Year].&amp;[2005]</UName> <Caption>2005</Caption> <LName>[Date].[Calendar].[Year]</LName> <LNum>1</LNum> <DisplayInfo>2</DisplayInfo> <CHILDREN_CARDINALITY>2</CHILDREN_CARDINALITY> <PARENT_UNIQUE_NAME>[Date].[Calendar].[All]</PARENT_UNIQUE_NAME> </Member> </Tuple> </Tuples> </Axis> <Axis name="Axis1"> <Tuples> <Tuple> <Member Hierarchy="[Product].[Product Name]"> <UName>[Product].[Product Name].[All]</UName> <Caption>All</Caption> <LName>[Product].[Product Name].[(All)]</LName> <LNum>0</LNum> <DisplayInfo>504</DisplayInfo> <CHILDREN_CARDINALITY>504</CHILDREN_CARDINALITY> </Member> </Tuple> </Tuples> </Axis> <Axis name="SlicerAxis"> <Tuples> <Tuple> <Member Hierarchy="[Measures]"> <UName>[Measures].[Internet Order Lines Count]</UName> <Caption>Internet Order Lines Count</Caption> <LName>[Measures].[MeasuresLevel]</LName> <LNum>0</LNum> <DisplayInfo>0</DisplayInfo> </Member></Tuple> </Tuples> </Axis> </Axes> <CellData> </CellData> </root> </return> </ExecuteResponse> </soap:Body> </soap:Envelope>';
+
+        var body = reader.parse(response);
+        var axes = reader.axes(body);
+
+        var columnTuples = axes.columns.tuples;
+        var rowTuples = axes.rows.tuples;
+
+        equal(columnTuples[0].members.length, 1);
+        equal(columnTuples[0].members[0].caption, "2005");
+        equal(columnTuples[0].members[0].hasChildren, true);
+        equal(columnTuples[0].members[0].parentName, "[Date].[Calendar].[All]");
+        equal(columnTuples[0].members[0].name, "[Date].[Calendar].[Year].&[2005]");
+        ok(!columnTuples[0].members[0].hierarchy);
+
+        equal(rowTuples[0].members.length, 1);
+    });
+
 })();
