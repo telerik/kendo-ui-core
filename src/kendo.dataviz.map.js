a0d25031c841f33df5b8eca7a04081f6340eba74
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-05-19 09:04:43 +0300

    Add Drawing API as a pseudo-widget dependency

--- src/kendo.dataviz.map.js
+++ src/kendo.dataviz.map.js
@@ -2,13 +2,7 @@
     define([
         "./kendo.data", "./kendo.userevents", "./kendo.tooltip", "./kendo.dataviz.core", "./kendo.mobile.scroller",
 
-        "./dataviz/util",
-        "./dataviz/geometry",
-        "./dataviz/drawing/core",
-        "./dataviz/drawing/shapes",
-        "./dataviz/drawing/parser",
-        "./dataviz/drawing/svg",
-        "./dataviz/drawing/vml",
+        "./dataviz/drawing",
         "./dataviz/map/location",
         "./dataviz/map/attribution",
         "./dataviz/map/navigator",
