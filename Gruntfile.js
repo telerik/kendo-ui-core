701fd61d48976262adb4724858d263f60b9c4859
Petyo Ivanov <underlog@gmail.com>
2014-06-04 17:56:18 +0300

    Include svg files in styles resources

--- Gruntfile.js
+++ Gruntfile.js
@@ -162,7 +162,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: "styles",
-                    src: ["**/*.less", "**/*.woff", "**/*.ttf", "**/*.png", "**/*.gif", "**/*.css" ],
+                    src: ["**/*.less", "**/*.woff", "**/*.ttf", "**/*.png", "**/*.gif", "**/*.css", "**/*.svg" ],
                     dest: '<%= kendo.options.stylesDestDir %>/',
                 }]
             }
