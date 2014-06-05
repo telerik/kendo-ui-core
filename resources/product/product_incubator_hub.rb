1785fb4c8fd4c71f9aac447593bd7bbb789e3ac2
Stefan Rahnev <stefan.rahnev@telerik.com>
2014-06-05 15:46:02 +0300

    namespace syntax fix

--- resources/product/product_incubator_hub.rb
+++ resources/product/product_incubator_hub.rb
@@ -1,7 +1,7 @@
 require 'product_generator'
 require 'code_library_creator'
 
-namespace :kendo_product
+namespace :kendo_product do
   desc "Create new product and its accompanying forum and code library, and assign its support to the appropriate Telerik team"
     #product name and suite are fetched from environmental/runtime variables: ENV["ProductName"] and ENV["SuiteAlias"]
       task :create do
