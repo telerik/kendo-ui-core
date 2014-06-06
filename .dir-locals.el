0cd785b56d2e324f4ca04fb074dd29232e4afde0
Mihai Bazon <mihai@bazon.net>
2014-06-06 13:16:43 +0300

    seems we're not consistent about DOS CRLF. :-\

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
-((nil . ((buffer-file-coding-system . utf-8-dos)))
+(
+ ;;(nil . ((buffer-file-coding-system . utf-8-dos)))
  (js-mode . ((js-indent-level . 4)))
  (css-mode . ((css-indent-offset . 4)))
  (less-css-mode . ((css-indent-offset . 4))))
