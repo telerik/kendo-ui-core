69f66edfe986b5b69f78bfa0393c2017aaa9f0b0
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2014-06-10 17:11:24 +0300

    Add PivotGrid and it's DataSource tests.

---
+++ wrappers/mvc/tests/Kendo.Mvc.Tests/UI/PivotGrid/PivotGridBuilderTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Kendo.Mvc.UI.Fluent.Tests
+{
+    using Moq;
+    using System.IO;
+    using System.Web.UI;
+    using Xunit;
+
+    public class PivotGridBuilderTests
+    {
+        private readonly PivotGrid pivotGrid;
+        private readonly PivotGridBuilder builder;
+
+        public PivotGridBuilderTests()
+        {
+            Mock<HtmlTextWriter> writer = new Mock<HtmlTextWriter>(TextWriter.Null);
+            pivotGrid = PivotGridTestHelper.CreatePivotGrid(writer.Object);
+            builder = new PivotGridBuilder(pivotGrid);
+        }
+
+        [Fact]
+        public void AutoBind_sets_the_corresponding_property()
+        {
+            builder.AutoBind(false);
+            Assert.False(pivotGrid.AutoBind);
+        }
+    }
+}
---
+++ wrappers/mvc/tests/Kendo.Mvc.Tests/UI/PivotGrid/PivotGridTestHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace Kendo.Mvc.UI.Fluent.Tests
+{
+    using Kendo.Mvc.Infrastructure;
+    using Moq;
+    using System.IO;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.UI;
+
+    public class PivotGridTestHelper
+    {
+        public static PivotGrid CreatePivotGrid(HtmlTextWriter writer)
+        {
+            Mock<HttpContextBase> httpContext = TestHelper.CreateMockedHttpContext();
+
+            if (writer != null)
+            {
+                httpContext.Setup(c => c.Request.Browser.CreateHtmlTextWriter(It.IsAny<TextWriter>())).Returns(writer);
+            }
+
+            Mock<IViewDataContainer> viewDataContainer = new Mock<IViewDataContainer>();
+            Mock<IUrlGenerator> urlGenerator = new Mock<IUrlGenerator>();
+
+            viewDataContainer.SetupGet(container => container.ViewData).Returns(new ViewDataDictionary());
+
+            ViewContext viewContext = TestHelper.CreateViewContext();
+
+            var initializer = new Mock<IJavaScriptInitializer>();
+
+            PivotGrid pivotGrid = new PivotGrid(viewContext, initializer.Object, urlGenerator.Object) { Name = "pivotGrid" };
+
+            return pivotGrid;
+        }
+    }
+}
