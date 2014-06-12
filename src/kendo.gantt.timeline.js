78770c9cf5610875398b37657eaea82ecfd385e7
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2014-06-12 17:19:39 +0300

    Fix Clicking the task delete button navigates to '#'
    Fix #3359

--- src/kendo.gantt.timeline.js
+++ src/kendo.gantt.timeline.js
@@ -2006,6 +2006,7 @@ var __meta__ = {
                 this.wrapper
                     .on(CLICK + NS, DOT + styles.taskDelete, function(e) {
                         that.trigger("removeTask", { uid: $(this).closest(DOT + styles.task).attr("data-uid") });
+                        e.preventDefault();
                     })
                     .on(KEYDOWN + NS, function(e) {
                         var selectedTask;
