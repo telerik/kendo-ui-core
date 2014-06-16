70f57a2f153b89e3fb45172b034fa8a3bc092f2c
Raisolution <d.madjarov@gmail.com>
2014-06-16 17:39:44 +0300

    Add Range Bar Chart Mvc tests

---
+++ wrappers/mvc/tests/Kendo.Mvc.Tests/UI/Chart/ChartRangeBarSeriesBuilderTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Kendo.Mvc.UI.Tests.Chart
+{
+    using Kendo.Mvc.UI;
+    using Kendo.Mvc.UI.Fluent;
+    using System;
+    using Xunit;
+
+    public class ChartRangeBarSeriesBuilderTests
+    {
+        protected IChartRangeBarSeries series;
+        protected ChartRangeBarSeriesBuilder<SalesData> builder;
+        protected readonly Func<object, object> nullFunc;
+
+        public ChartRangeBarSeriesBuilderTests()
+        {
+            var chart = ChartTestHelper.CreateChart<SalesData>();
+            series = new ChartRangeBarSeries<SalesData, decimal, string>(m => m.RepSalesLow, m => m.RepSalesHigh, null);
+            builder = new ChartRangeBarSeriesBuilder<SalesData>(series);
+            nullFunc = (o) => null;
+        }
+
+        [Fact]
+        public void FromFieldExpression_should_set_fromField()
+        {
+            series.FromField.ShouldEqual("RepSalesLow");
+        }
+
+        [Fact]
+        public void ToFieldExpression_should_set_toField()
+        {
+            series.FromField.ShouldEqual("RepSalesHigh");
+        }
+    }
+}
