5c461a0e7a0447f6d32d6458eb1517cb49f515a3
Alex Gyoshev <alex@gyoshev.net>
2014-06-16 15:00:20 +0300

    Correctly determine container position.
    Closes #80. Refactor interaction tests.

--- tests/window/interaction.js
+++ tests/window/interaction.js
@@ -13,7 +13,6 @@
 
     var ResizableWidget = Widget.extend({
         init: function(element, options) {
-            
             Widget.fn.init.call(this, element, options);
             this.element.css({ width: "100%", height: "100%" });
         },
@@ -21,6 +20,8 @@
             name: "ResizableWidget"
         }
     });
+
+    var dialog;
 
     module("interaction", {
         setup: function() {
@@ -36,7 +37,7 @@
     }
 
     test("double-clicking refresh does not maximize window", function() {
-        var dialog = createWindow({
+        dialog = createWindow({
             actions: [ "refresh" ]
         });
 
@@ -45,64 +46,94 @@
         ok(!dialog.options.isMaximized);
     });
 
-    test("resizing window out of viewport", function() {
-        var dialog = createWindow({});
-        var userEvents = dialog.resizing._draggable.userEvents;
-
-        var handle = dialog.wrapper.find(".k-resize-nw");
-        var start = handle.offset();
-
-        userEvents.press(start.left, start.top, handle[0]);
-        userEvents.move(-10, -10, handle[0]);
-        userEvents.end(-10, -10, handle[0]);
-
-        equal(parseInt(dialog.wrapper.css("top"), 10), 0);
-        equal(parseInt(dialog.wrapper.css("left"), 10), 0);
-    });
-
     test("resizing window resizes inner widgets", 1, function() {
-        var dialog = createWindow({});
+        dialog = createWindow({});
         var widgetElement = $("<div />").appendTo(dialog.element).kendoResizableWidget();
         widgetElement.data("kendoResizableWidget").bind("resize", function() { ok(true) });
 
         dialog.resize();
     });
 
-    test("resizing window updates widget options", 2, function() {
-        var initialSize = 200,
-            resizeChange = 20,
-            dialog = createWindow({width:initialSize, height: initialSize, position: {top: "0", left: "0"}}),
-            userEvents = dialog.resizing._draggable.userEvents;
+    var initialSize = 200;
+    var resizeChange = 20;
 
-        var handle = dialog.wrapper.find(".k-resize-se");
+    function resize(direction, left, top, absolute) {
+        var handle = dialog.wrapper.find(".k-resize-" + direction);
         var start = handle.offset();
-        var end = initialSize + resizeChange;
+        var userEvents = dialog.resizing._draggable.userEvents;
+
+        var end = {
+            left: absolute ? left : (start.left + left),
+            top: absolute ? top : (start.top + top)
+        };
 
         userEvents.press(start.left, start.top, handle[0]);
-        userEvents.move(end, end + parseInt(dialog.wrapper.css("padding-top"), 10), handle[0]);
-        userEvents.end(0, 0, handle[0]);
+        userEvents.move(end.left, end.top);
+        userEvents.end(end.left, end.top);
+    }
 
-        equal(dialog.options.width, end + "px");
-        equal(dialog.options.height, end + "px");
+    module("resizing", {
+        setup: function() {
+            kendo.effects.disable();
+            dialog = createWindow({
+                width: initialSize,
+                height: initialSize,
+                position: {
+                    top: "0",
+                    left: "0"
+                }
+            });
+        },
+        teardown: cleanup
     });
 
-    var ddl, dialog;
+    test("resizing window out of viewport", function() {
+        resize("nw", -10, -10, true);
+
+        equal(parseInt(dialog.wrapper.css("top"), 10), 0);
+        equal(parseInt(dialog.wrapper.css("left"), 10), 0);
+    });
+
+    test("resizing window updates widget options", function() {
+        resize("se", 100, 100);
+
+        equal(dialog.options.width, dialog.wrapper.width() + "px");
+        equal(dialog.options.height, dialog.wrapper.height() + "px");
+    });
+
+    test("resizing window horizontally does not update vertical positioning", function() {
+        var initialWidth = dialog.wrapper.width();
+
+        resize("w", 0, 100);
+
+        equal(dialog.wrapper.width(), initialWidth);
+    });
+
+    test("resizing window vertically does not update horizontal positioning", function() {
+        var initialHeight = dialog.wrapper.height();
+
+        resize("n", 100, 0);
+
+        equal(dialog.wrapper.height(), initialHeight);
+    });
 
     module("integration", {
         setup: function() {
             kendo.effects.disable();
             dialog = createWindow();
-            ddl = $("<input id='ddl' />").appendTo(dialog.element)
-                .kendoDropDownList({
-                    dataSource: [ "foo", "bar" ],
-                    animation: false
-                }).data("kendoDropDownList");
         },
         teardown: cleanup
     });
 
     test("popups are closed when closing window", function() {
+        var ddl = $("<input id='ddl' />").appendTo(dialog.element)
+            .kendoDropDownList({
+                dataSource: [ "foo", "bar" ],
+                animation: false
+            }).data("kendoDropDownList");
+
         ddl.open();
+
         dialog.wrapper.find(".k-window-titlebar").trigger("mousedown");
 
         ok($("#ddl-list").is(":hidden"));
