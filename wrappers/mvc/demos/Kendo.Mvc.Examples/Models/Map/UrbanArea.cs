cbfa64dd681e6cc3181cf6c6cac187154bd4eeb0
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-06-17 14:17:53 +0300

    Add UrbanArea entities

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Models/Map/UrbanArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kendo.Mvc.Examples.Models
+{
+    public partial class UrbanArea
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]      
+        public decimal[] Location {
+            get
+            {
+                return new decimal[] { this.Latitude, this.Longitude };
+            }
+
+            private set { }
+        }
+    }
+}
--- wrappers/mvc/demos/Kendo.Mvc.Examples/Models/Sample.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Models/Sample.cs
@@ -6,3 +6,4 @@
 //    Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+
