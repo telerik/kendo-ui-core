c2c0ceb40ef156806fc28aa5865676ac9ded2078
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-06-17 14:27:10 +0300

    Add Bubble Layer MVC demos

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Map/BubbleLayerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Examples.Models;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class MapController : Controller
+    {
+        public ActionResult Bubble_Layer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult _UrbanAreas()
+        {
+            IEnumerable<UrbanArea> result;
+
+            using (var db = new SampleEntities())
+            {
+                result = (from u in db.UrbanAreas select u).ToList();
+            }
+
+            return Json(result);
+        }
+    }
+}
