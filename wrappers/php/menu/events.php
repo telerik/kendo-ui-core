ab143ad0932257d8f3f80b6cd6d4a0a1f219cd50
Bundyo <bundyo@gmail.com>
2014-06-23 12:01:31 +0300

    Update PHP Events demo - add ContextMenu.

--- wrappers/php/menu/events.php
+++ wrappers/php/menu/events.php
@@ -62,15 +62,61 @@ require_once '../lib/Kendo/Autoload.php';
          ->deactivate('onDeactivate');
 
     echo $menu->render();
+
+?>
+
+<div id="context-target">
+    Context Menu Target
+</div>
+
+<?php
+    $contextMenu = new \Kendo\UI\ContextMenu('context-menu-events');
+
+    $first = new \Kendo\UI\ContextMenuItem("Item 1");
+    $first->addItem(
+        new \Kendo\UI\ContextMenuItem("Sub Item 1"),
+        new \Kendo\UI\ContextMenuItem("Sub Item 2"),
+        new \Kendo\UI\ContextMenuItem("Sub Item 3"),
+        new \Kendo\UI\ContextMenuItem("Sub Item 4")
+    );
+    $contextMenu->addItem($first);
+
+    $second = new \Kendo\UI\ContextMenuItem("Item 2");
+    $second->addItem(
+        new \Kendo\UI\ContextMenuItem("Sub Item 1"),
+        new \Kendo\UI\ContextMenuItem("Sub Item 2"),
+        new \Kendo\UI\ContextMenuItem("Sub Item 3"),
+        new \Kendo\UI\ContextMenuItem("Sub Item 4")
+    );
+    $contextMenu->addItem($second);
+
+    $third = new \Kendo\UI\ContextMenuItem("Item 3");
+    $third->addItem(
+        new \Kendo\UI\ContextMenuItem("Sub Item 1"),
+        new \Kendo\UI\ContextMenuItem("Sub Item 2"),
+        new \Kendo\UI\ContextMenuItem("Sub Item 3"),
+        new \Kendo\UI\ContextMenuItem("Sub Item 4")
+    );
+    $contextMenu->addItem($third);
+
+    $contextMenu->target('#context-target')
+                ->select('onSelect')
+                ->open('onOpen')
+                ->close('onClose')
+                ->activate('onActivate')
+                ->deactivate('onDeactivate');
+
+    echo $contextMenu->render();
+
 ?>
 
 <script>
     function onOpen(e) {
-        kendoConsole.log("Opened: " + $(e.item).children(".k-link").text());
+        kendoConsole.log("Opened: " + ($(e.item).children(".k-link").text() || "ContextMenu"));
     }
 
     function onClose(e) {
-        kendoConsole.log("Closed: " + $(e.item).children(".k-link").text());
+        kendoConsole.log("Closed: " + ($(e.item).children(".k-link").text() || "ContextMenu"));
     }
 
     function onSelect(e) {
@@ -78,15 +124,24 @@ require_once '../lib/Kendo/Autoload.php';
     }
 
     function onActivate(e) {
-        kendoConsole.log("Activated: " + $(e.item).children(".k-link").text());
+        kendoConsole.log("Activated: " + ($(e.item).children(".k-link").text() || "ContextMenu"));
     }
 
     function onDeactivate(e) {
-        kendoConsole.log("Deactivated: " + $(e.item).children(".k-link").text());
+        kendoConsole.log("Deactivated: " + ($(e.item).children(".k-link").text() || "ContextMenu"));
     }
 </script>
 
 <div class="console"></div>
+
+<style scoped>
+    #context-target
+    {
+        height: 50px;
+        border: 1px solid red;
+        margin-bottom: 20px;
+    }
+</style>
 
 <?php require_once '../include/footer.php'; ?>
 
