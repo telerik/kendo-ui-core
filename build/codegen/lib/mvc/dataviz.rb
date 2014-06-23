5777d14af54acbbae9259d073363442ff97a15f5
Alexander Valchev <a.valchevbox@gmail.com>
2014-06-23 12:24:38 +0300

    Modify the MVC wrappers generation to create and serialize Template() and TemplateId() for configuration properties that ends with 'template'.

--- build/codegen/lib/mvc/dataviz.rb
+++ build/codegen/lib/mvc/dataviz.rb
@@ -23,8 +23,92 @@ module CodeGen::MVC::Wrappers::DataViz
 
     end
 
+        TEMPLATE_FIELD_DECLARATION = ERB.new(%{
+        public <%= csharp_declaration_type %> <%= csharp_name %> { get; set; }
+
+        public <%= csharp_declaration_type %> <%= csharp_name %>Id { get; set; }
+        })
+
+        TEMPLATE_FIELD_BUILDERS = ERB.new(%{
+        /// <summary>
+        /// <%= description.gsub(/\r?\n/, '\n\t\t/// ').html_encode()%>
+        /// </summary>
+        /// <param name="value">The value that configures the <%= csharp_name.downcase %>.</param>
+        public <%= csharp_builder_name %> <%= csharp_name %>(<%= is_csharp_array ? 'params ' : '' %><%= csharp_type %> value)
+        {
+            container.<%= csharp_name %> = value;
+
+            return this;
+        }
+
+        /// <summary>
+        /// <%= description.gsub(/\r?\n/, '\n\t\t/// ').html_encode()%>
+        /// </summary>
+        /// <param name="value">The value that configures the <%= csharp_name.downcase %>.</param>
+        public <%= csharp_builder_name %> <%= csharp_name %>Id(<%= is_csharp_array ? 'params ' : '' %><%= csharp_type %> value)
+        {
+            container.<%= csharp_name %>Id = value;
+
+            return this;
+        }
+        })
+
     class Option < CodeGen::MVC::Wrappers::Option
         include Options
+
+        def csharp_declaration_type
+            if csharp_type == 'string' || is_csharp_array
+                csharp_type
+            else
+                csharp_type + '?'
+            end
+        end
+
+        def csharp_name
+            name.slice(0,1).capitalize + name.slice(1..-1)
+        end
+
+        def template?
+            /template$/i.match(name)
+        end
+
+        def declaration_template
+            if template?
+                TEMPLATE_FIELD_DECLARATION
+            else
+                super
+            end
+        end
+
+        def to_fluent
+            if template?
+                TEMPLATE_FIELD_BUILDERS.result(binding)
+            else
+                super
+            end
+        end
+
+        def to_client_option
+            if template?
+            ERB.new(%{
+            if (!string.IsNullOrEmpty(<%= csharp_name %>Id))
+            {
+                json["<%= name %>"] = new ClientHandlerDescriptor {
+                    HandlerName = string.Format(
+                        "jQuery('\#{0}').html()",
+                        <%= csharp_name %>Id
+                    )
+                };
+            }
+            else if (!string.IsNullOrEmpty(<%= csharp_name %>))
+            {
+                json["<%= name %>"] = <%= csharp_name %>;
+            }
+                }).result(binding)
+            else
+                super
+            end
+        end
     end
 
     class CompositeOption < CodeGen::MVC::Wrappers::CompositeOption
