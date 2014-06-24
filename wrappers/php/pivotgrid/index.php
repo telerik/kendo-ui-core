d4d90c4316e4fd5aca939ccb760a76b6b38715f5
Nikolay Rusev <nikolay.rusev@telerik.com>
2014-06-24 13:09:32 +0300

    Update PivotGrid PHP demo

--- wrappers/php/pivotgrid/index.php
+++ wrappers/php/pivotgrid/index.php
@@ -51,10 +51,27 @@ $pivotgrid = new \Kendo\UI\PivotGrid('pivotgrid');
 $pivotgrid->dataSource($dataSource)
     ->height(550);
 
+$configurator = new \Kendo\UI\PivotConfigurator('configurator');
+$configurator->dataSource($dataSource);
 ?>
 
 <?php
 echo $pivotgrid->render();
+echo $configurator->render();
 ?>
 
+<style>
+    #pivotgrid
+    {
+        display: inline-block;
+        vertical-align: top;
+        width: 60%;
+    }
+
+    #configurator
+    {
+        display: inline-block;
+        vertical-align: top;
+    }
+</style>
 <?php require_once '../include/footer.php'; ?>
