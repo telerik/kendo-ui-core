8218f4376e707cf367216d45b2c4b23585567ec5
Bundyo <bundyo@gmail.com>
2014-06-24 13:34:12 +0300

    Remove ContextMenu from the auto generated widgets.

--- build/codegen.rb
+++ build/codegen.rb
@@ -112,8 +112,7 @@ namespace :generate do
                     'docs/api/dataviz/diagram.md',
                     'docs/api/dataviz/treemap.md',
                     'docs/api/web/gantt.md',
-                    'docs/api/web/toolbar.md',
-                    'docs/api/web/contextmenu.md'
+                    'docs/api/web/toolbar.md'
                 ]
 
                 components = markdown.map { |filename| CodeGen::MarkdownParser.read(filename, CodeGen::MVC::Wrappers::DataViz::Component) }
