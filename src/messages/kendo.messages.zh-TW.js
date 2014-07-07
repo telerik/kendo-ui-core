c0e259bb11eba2a9d808dfc0669748c8589e97f0
Alexander Valchev <a.valchevbox@gmail.com>
2014-07-07 14:46:14 +0300

    Merge pull request #148 from hustcer/master
    Finish zh-CN message translation, Add zh-HK and zh-TW

--- src/messages/kendo.messages.zh-TW.js
+++ src/messages/kendo.messages.zh-TW.js
@@ -3,7 +3,7 @@
 if (kendo.ui.FlatColorPicker) {
 kendo.ui.FlatColorPicker.prototype.options.messages =
 $.extend(true, kendo.ui.FlatColorPicker.prototype.options.messages,{
-  "apply": "确定",
+  "apply": "確定",
   "cancel": "取消"
 });
 }
@@ -13,7 +13,7 @@ $.extend(true, kendo.ui.FlatColorPicker.prototype.options.messages,{
 if (kendo.ui.ColorPicker) {
 kendo.ui.ColorPicker.prototype.options.messages =
 $.extend(true, kendo.ui.ColorPicker.prototype.options.messages,{
-  "apply": "确定",
+  "apply": "確定",
   "cancel": "取消"
 });
 }
@@ -25,12 +25,12 @@ kendo.ui.ColumnMenu.prototype.options.messages =
 $.extend(true, kendo.ui.ColumnMenu.prototype.options.messages,{
   "sortAscending": "升序",
   "sortDescending": "降序",
-  "filter": "过滤",
+  "filter": "過濾",
   "columns": "列",
   "done": "完成",
-  "settings": "列设置",
-  "lock": "锁定",
-  "unlock": "解除锁定"
+  "settings": "列設置",
+  "lock": "鎖定",
+  "unlock": "解除鎖定"
 });
 }
 
@@ -39,65 +39,65 @@ $.extend(true, kendo.ui.ColumnMenu.prototype.options.messages,{
 if (kendo.ui.Editor) {
 kendo.ui.Editor.prototype.options.messages =
 $.extend(true, kendo.ui.Editor.prototype.options.messages,{
-  "bold": "粗体",
-  "italic": "斜体",
-  "underline": "下划线",
-  "strikethrough": "删除线",
-  "superscript": "上标",
-  "subscript": "下标",
+  "bold": "粗體",
+  "italic": "斜體",
+  "underline": "下劃線",
+  "strikethrough": "刪除線",
+  "superscript": "上標",
+  "subscript": "下標",
   "justifyCenter": "居中",
-  "justifyLeft": "左对齐",
-  "justifyRight": "右对齐",
-  "justifyFull": "两端对齐",
-  "insertUnorderedList": "插入无序列表",
+  "justifyLeft": "左對齊",
+  "justifyRight": "右對齊",
+  "justifyFull": "兩端對齊",
+  "insertUnorderedList": "插入無序列表",
   "insertOrderedList": "插入有序列表",
-  "indent": "增加缩进",
-  "outdent": "减少缩进",
-  "createLink": "插入链接",
-  "unlink": "移除链接",
-  "insertImage": "插入图片",
+  "indent": "增加縮進",
+  "outdent": "減少縮進",
+  "createLink": "插入鏈接",
+  "unlink": "移除鏈接",
+  "insertImage": "插入圖片",
   "insertFile": "插入文件",
   "insertHtml": "插入 HTML",
   "viewHtml": "查看 HTML",
-  "fontName": "选择字体",
-  "fontNameInherit": "（继承的字体）",
-  "fontSize": "选择字号",
-  "fontSizeInherit": "（继承的字号）",
-  "formatBlock": "格式化块",
+  "fontName": "選擇字體",
+  "fontNameInherit": "（繼承的字體）",
+  "fontSize": "選擇字號",
+  "fontSizeInherit": "（繼承的字號）",
+  "formatBlock": "格式化塊",
   "formatting": "格式化",
-  "foreColor": "颜色",
+  "foreColor": "顏色",
   "backColor": "背景色",
-  "style": "风格",
-  "emptyFolder": "文件夹为空",
-  "uploadFile": "上传",
-  "orderBy": "排序条件:",
+  "style": "風格",
+  "emptyFolder": "文件夾為空",
+  "uploadFile": "上傳",
+  "orderBy": "排序條件:",
   "orderBySize": "大小",
   "orderByName": "名字",
-  "invalidFileType": "选中的文件 \"{0}\" 非法，支持的文件类型为 {1}。",
-  "deleteFile": '您确定要删除 \"{0}\"?',
-  "overwriteFile": '当前文件夹已存在文件名为 \"{0}\" 的文件，您确定要覆盖么？',
-  "directoryNotFound": "此文件夹未找到",
-  "imageWebAddress": "图片地址",
+  "invalidFileType": "選中的文件 \"{0}\" 非法，支持的文件類型為 {1}。",
+  "deleteFile": '您確定要刪除 \"{0}\"?',
+  "overwriteFile": '當前文件夾已存在文件名為 \"{0}\" 的文件，您確定要覆蓋麽？',
+  "directoryNotFound": "此文件夾未找到",
+  "imageWebAddress": "圖片地址",
   "imageAltText": "替代文本",
-  "imageWidth": "宽度 (px)",
+  "imageWidth": "寬度 (px)",
   "imageHeight": "高度 (px)",
   "fileWebAddress": "文件地址",
-  "fileTitle": "标题",
-  "linkWebAddress": "链接地址",
-  "linkText": "链接文字",
-  "linkToolTip": "链接提示",
-  "linkOpenInNewWindow": "在新窗口中打开",
-  "dialogUpdate": "上传",
+  "fileTitle": "標題",
+  "linkWebAddress": "鏈接地址",
+  "linkText": "鏈接文字",
+  "linkToolTip": "鏈接提示",
+  "linkOpenInNewWindow": "在新窗口中打開",
+  "dialogUpdate": "上傳",
   "dialogInsert": "插入",
   "dialogButtonSeparator": "或",
   "dialogCancel": "取消",
-  "createTable": "创建表格",
-  "addColumnLeft": "左侧添加列",
-  "addColumnRight": "右侧添加列",
+  "createTable": "創建表格",
+  "addColumnLeft": "左側添加列",
+  "addColumnRight": "右側添加列",
   "addRowAbove": "上方添加行",
   "addRowBelow": "下方添加行",
-  "deleteRow": "删除行",
-  "deleteColumn": "删除列"
+  "deleteRow": "刪除行",
+  "deleteColumn": "刪除列"
 });
 }
 
@@ -106,16 +106,16 @@ $.extend(true, kendo.ui.Editor.prototype.options.messages,{
 if (kendo.ui.FileBrowser) {
 kendo.ui.FileBrowser.prototype.options.messages =
 $.extend(true, kendo.ui.FileBrowser.prototype.options.messages,{
-  "uploadFile": "上传",
-  "orderBy": "排序条件",
-  "orderByName": "名称",
+  "uploadFile": "上傳",
+  "orderBy": "排序條件",
+  "orderByName": "名稱",
   "orderBySize": "大小",
-  "directoryNotFound": "此文件夹未找到",
-  "emptyFolder": "文件夹为空",
-  "deleteFile": '您确定要删除 \"{0}\"?',
-  "invalidFileType": "选中的文件 \"{0}\" 非法，支持的文件类型为 {1}。",
-  "overwriteFile": "当前文件夹已存在文件名为 \"{0}\" 的文件，您确定要覆盖么？",
-  "dropFilesHere": "拖拽要上传的文件到此处",
+  "directoryNotFound": "此文件夾未找到",
+  "emptyFolder": "文件夾為空",
+  "deleteFile": '您確定要刪除 \"{0}\"?',
+  "invalidFileType": "選中的文件 \"{0}\" 非法，支持的文件類型為 {1}。",
+  "overwriteFile": "當前文件夾已存在文件名為 \"{0}\" 的文件，您確定要覆蓋麽？",
+  "dropFilesHere": "拖拽要上傳的文件到此處",
   "search": "搜索"
 });
 }
@@ -125,15 +125,15 @@ $.extend(true, kendo.ui.FileBrowser.prototype.options.messages,{
 if (kendo.ui.FilterMenu) {
 kendo.ui.FilterMenu.prototype.options.messages =
 $.extend(true, kendo.ui.FilterMenu.prototype.options.messages,{
-  "info": "显示符合以下条件的行",
-  "isTrue": "为真",
-  "isFalse": "为假",
-  "filter": "过滤",
+  "info": "顯示符合以下條件的行",
+  "isTrue": "為真",
+  "isFalse": "為假",
+  "filter": "過濾",
   "clear": "清除",
-  "and": "并且",
+  "and": "並且",
   "or": "或",
-  "selectValue": "-选择-",
-  "operator": "运算符",
+  "selectValue": "-選擇-",
+  "operator": "運算符",
   "value": "值",
   "cancel": "取消"
 });
@@ -145,32 +145,32 @@ if (kendo.ui.FilterMenu) {
 kendo.ui.FilterMenu.prototype.options.operators =
 $.extend(true, kendo.ui.FilterMenu.prototype.options.operators,{
   "string": {
-    "eq": "等于",
-    "neq": "不等于",
-    "startswith": "开头为",
+    "eq": "等於",
+    "neq": "不等於",
+    "startswith": "開頭為",
     "contains": "包含",
     "doesnotcontain": "不包含",
-    "endswith": "结尾为"
+    "endswith": "結尾為"
   },
   "number": {
-    "eq": "等于",
-    "neq": "不等于",
-    "gte": "大于等于",
-    "gt": "大于",
-    "lte": "小于等于",
-    "lt": "小于"
+    "eq": "等於",
+    "neq": "不等於",
+    "gte": "大於等於",
+    "gt": "大於",
+    "lte": "小於等於",
+    "lt": "小於"
   },
   "date": {
-    "eq": "等于",
-    "neq": "不等于",
-    "gte": "大于等于",
-    "gt": "大于",
-    "lte": "小于等于",
-    "lt": "小于"
+    "eq": "等於",
+    "neq": "不等於",
+    "gte": "大於等於",
+    "gt": "大於",
+    "lte": "小於等於",
+    "lt": "小於"
   },
   "enum": {
-    "eq": "等于",
-    "neq": "不等于"
+    "eq": "等於",
+    "neq": "不等於"
   }
 });
 }
@@ -187,10 +187,10 @@ $.extend(true, kendo.ui.Gantt.prototype.options.messages,{
     "month": "月"
   },
   "actions": {
-    "append": "添加任务",
-    "addChild": "添加子任务",
+    "append": "添加任務",
+    "addChild": "添加子任務",
     "insertBefore": "添加到前面",
-    "insertAfter": "添加到后面"
+    "insertAfter": "添加到後面"
   }
 });
 }
@@ -204,16 +204,16 @@ $.extend(true, kendo.ui.Grid.prototype.options.messages,{
     "cancel": "取消",
     "canceledit": "取消",
     "create": "新增",
-    "destroy": "删除",
-    "edit": "编辑",
+    "destroy": "刪除",
+    "edit": "編輯",
     "save": "保存",
-    "select": "选择",
+    "select": "選擇",
     "update": "更新"
   },
   "editable": {
     "cancelDelete": "取消",
-    "confirmation": "确定要删除吗？",
-    "confirmDelete": "删除"
+    "confirmation": "確定要刪除嗎？",
+    "confirmDelete": "刪除"
   }
 });
 }
@@ -223,7 +223,7 @@ $.extend(true, kendo.ui.Grid.prototype.options.messages,{
 if (kendo.ui.Groupable) {
 kendo.ui.Groupable.prototype.options.messages =
 $.extend(true, kendo.ui.Groupable.prototype.options.messages,{
-  "empty": "托拽列标题到此处按列组合显示"
+  "empty": "托拽列標題到此處按列組合顯示"
 });
 }
 
@@ -233,7 +233,7 @@ if (kendo.ui.NumericTextBox) {
 kendo.ui.NumericTextBox.prototype.options =
 $.extend(true, kendo.ui.NumericTextBox.prototype.options,{
   "upArrowText": "增加",
-  "downArrowText": "减少"
+  "downArrowText": "減少"
 });
 }
 
@@ -242,15 +242,15 @@ $.extend(true, kendo.ui.NumericTextBox.prototype.options,{
 if (kendo.ui.Pager) {
 kendo.ui.Pager.prototype.options.messages =
 $.extend(true, kendo.ui.Pager.prototype.options.messages,{
-  "display": "显示条目 {0} - {1} 共 {2}",
-  "empty": "没有可显示的记录。",
-  "page": "页",
+  "display": "顯示條目 {0} - {1} 共 {2}",
+  "empty": "沒有可顯示的記錄。",
+  "page": "頁",
   "of": "共 {0}",
-  "itemsPerPage": "每页",
-  "first": "首页",
-  "last": "末页",
-  "next": "下一页",
-  "previous": "上一页",
+  "itemsPerPage": "每頁",
+  "first": "首頁",
+  "last": "末頁",
+  "next": "下一頁",
+  "previous": "上一頁",
   "refresh": "刷新",
   "morePages": "更多..."
 });
@@ -261,9 +261,9 @@ $.extend(true, kendo.ui.Pager.prototype.options.messages,{
 if (kendo.ui.PivotGrid) {
 kendo.ui.PivotGrid.prototype.options.messages =
 $.extend(true, kendo.ui.PivotGrid.prototype.options.messages,{
-  "measureFields": "拖放数据字段于此",
-  "columnFields": "拖放列字段于此",
-  "rowFields": "拖放行字段于此"
+  "measureFields": "拖放數據字段於此",
+  "columnFields": "拖放列字段於此",
+  "rowFields": "拖放行字段於此"
 });
 }
 
@@ -273,52 +273,52 @@ if (kendo.ui.RecurrenceEditor) {
 kendo.ui.RecurrenceEditor.prototype.options.messages =
 $.extend(true, kendo.ui.RecurrenceEditor.prototype.options.messages,{
   "frequencies": {
-    "never": "从不",
-    "hourly": "每小时",
+    "never": "從不",
+    "hourly": "每小時",
     "daily": "每天",
     "weekly": "每周",
     "monthly": "每月",
     "yearly": "每年"
   },
   "hourly": {
-    "repeatEvery": "重复周期: ",
-    "interval": " 小时"
+    "repeatEvery": "重復周期: ",
+    "interval": " 小時"
   },
   "daily": {
-    "repeatEvery": "重复周期: ",
+    "repeatEvery": "重復周期: ",
     "interval": " 天"
   },
   "weekly": {
     "interval": " 周",
-    "repeatEvery": "重复周期: ",
-    "repeatOn": "重复于:"
+    "repeatEvery": "重復周期: ",
+    "repeatOn": "重復於:"
   },
   "monthly": {
-    "repeatEvery": "重复周期: ",
-    "repeatOn": "重复于:",
+    "repeatEvery": "重復周期: ",
+    "repeatOn": "重復於:",
     "interval": " 月",
     "day": "日期"
   },
   "yearly": {
-    "repeatEvery": "重复周期: ",
-    "repeatOn": "重复于: ",
+    "repeatEvery": "重復周期: ",
+    "repeatOn": "重復於: ",
     "interval": " 年",
     "of": " 月份: "
   },
   "end": {
-    "label": "截止时间:",
-    "mobileLabel": "截止时间",
-    "never": "从不",
-    "after": "重复 ",
-    "occurrence": " 次后",
-    "on": "止于 "
+    "label": "截止時間:",
+    "mobileLabel": "截止時間",
+    "never": "從不",
+    "after": "重復 ",
+    "occurrence": " 次後",
+    "on": "止於 "
   },
   "offsetPositions": {
     "first": "第一",
     "second": "第二",
     "third": "第三",
     "fourth": "第四",
-    "last": "最后"
+    "last": "最後"
   },
   "weekdays": {
     "day": "天",
@@ -337,9 +337,9 @@ $.extend(true, kendo.ui.Scheduler.prototype.options.messages,{
   "today": "今天",
   "save": "保存",
   "cancel": "取消",
-  "destroy": "删除",
-  "deleteWindowTitle": "删除事件",
-  "ariaSlotLabel": "选择从 {0:t} 到 {1:t}",
+  "destroy": "刪除",
+  "deleteWindowTitle": "刪除事件",
+  "ariaSlotLabel": "選擇從 {0:t} 到 {1:t}",
   "ariaEventLabel": "{0} on {1:D} at {2:t}",
   "views": {
     "day": "日",
@@ -349,28 +349,28 @@ $.extend(true, kendo.ui.Scheduler.prototype.options.messages,{
     "month": "月"
   },
   "recurrenceMessages": {
-    "deleteWindowTitle": "删除周期条目",
-    "deleteWindowOccurrence": "删除当前事件",
-    "deleteWindowSeries": "删除序列",
-    "editWindowTitle": "修改周期条目",
-    "editWindowOccurrence": "修改当前事件",
+    "deleteWindowTitle": "刪除周期條目",
+    "deleteWindowOccurrence": "刪除當前事件",
+    "deleteWindowSeries": "刪除序列",
+    "editWindowTitle": "修改周期條目",
+    "editWindowOccurrence": "修改當前事件",
     "editWindowSeries": "修改序列"
   },
   "editor": {
-    "title": "标题",
+    "title": "標題",
     "start": "起始",
-    "end": "终止",
+    "end": "終止",
     "allDayEvent": "全天事件",
     "description": "描述",
-    "repeat": "重复",
+    "repeat": "重復",
     "timezone": " ",
-    "startTimezone": "起始时区",
-    "endTimezone": "终止时区",
-    "separateTimezones": "使用独立的起始和终止时区",
-    "timezoneEditorTitle": "时区",
-    "timezoneEditorButton": "时区",
-    "timezoneTitle": "选择时区",
-    "noTimezone": "无",
+    "startTimezone": "起始時區",
+    "endTimezone": "終止時區",
+    "separateTimezones": "使用獨立的起始和終止時區",
+    "timezoneEditorTitle": "時區",
+    "timezoneEditorButton": "時區",
+    "timezoneTitle": "選擇時區",
+    "noTimezone": "無",
     "editorTitle": "事件"
   }
 });
@@ -382,7 +382,7 @@ if (kendo.ui.Slider) {
 kendo.ui.Slider.prototype.options =
 $.extend(true, kendo.ui.Slider.prototype.options,{
   "increaseButtonTitle": "增加",
-  "decreaseButtonTitle": "减少"
+  "decreaseButtonTitle": "減少"
 });
 }
 
@@ -391,9 +391,9 @@ $.extend(true, kendo.ui.Slider.prototype.options,{
 if (kendo.ui.TreeView) {
 kendo.ui.TreeView.prototype.options.messages =
 $.extend(true, kendo.ui.TreeView.prototype.options.messages,{
-  "loading": "加载中...",
-  "requestFailed": "加载失败",
-  "retry": "重试"
+  "loading": "加載中...",
+  "requestFailed": "加載失敗",
+  "retry": "重試"
 });
 }
 
@@ -402,17 +402,17 @@ $.extend(true, kendo.ui.TreeView.prototype.options.messages,{
 if (kendo.ui.Upload) {
 kendo.ui.Upload.prototype.options.localization =
 $.extend(true, kendo.ui.Upload.prototype.options.localization,{
-  "select": "选择...",
+  "select": "選擇...",
   "cancel": "取消",
-  "retry": "重试",
+  "retry": "重試",
   "remove": "移除",
-  "uploadSelectedFiles": "上传文件",
-  "dropFilesHere": "拖拽要上传的文件到此处",
-  "statusUploading": "上传中",
-  "statusUploaded": "已上传",
+  "uploadSelectedFiles": "上傳文件",
+  "dropFilesHere": "拖拽要上傳的文件到此處",
+  "statusUploading": "上傳中",
+  "statusUploaded": "已上傳",
   "statusWarning": "警告",
-  "statusFailed": "失败",
-  "headerStatusUploading": "上传...",
+  "statusFailed": "失敗",
+  "headerStatusUploading": "上傳...",
   "headerStatusUploaded": "完成"
 });
 }
@@ -422,12 +422,12 @@ $.extend(true, kendo.ui.Upload.prototype.options.localization,{
 if (kendo.ui.Validator) {
 kendo.ui.Validator.prototype.options.messages =
 $.extend(true, kendo.ui.Validator.prototype.options.messages,{
-  "required": "{0} 为必填项",
+  "required": "{0} 為必填項",
   "pattern": "{0} 非法",
-  "min": "{0} 应该大于或等于 {1}",
-  "max": "{0} 应该小于或等于 {1}",
+  "min": "{0} 應該大於或等於 {1}",
+  "max": "{0} 應該小於或等於 {1}",
   "step": "{0} 非法",
-  "email": "{0} 不是合法的邮件地址",
+  "email": "{0} 不是合法的郵件地址",
   "url": "{0} 不是合法的URL",
   "date": "{0} 不是合法的日期"
 });
