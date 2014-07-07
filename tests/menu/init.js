d92584fab3cc9fb9e03fdbe68ed7140e3aa0af54
Bundyo <bundyo@gmail.com>
2014-07-07 18:15:52 +0300

    Introduce .k-menu-group class, move all menu styling to it. This allows placing other group widgets (like a TreeView) inside the Menu. Add some tests.

---
+++ tests/menu/init.js
@@ -0,0 +1,69 @@
+(function() {
+
+function getRootItem(index) {
+   return $('#menu').find('> .k-item > .k-link').eq(index)
+}
+
+var menu;
+
+module("menu init", {
+    setup: function () {
+        QUnit.fixture.append(
+            '<ul id="menu">' +
+            '   <li>' +
+            '       Item 1' +
+            '       <ul>' +
+            '           <li>Sub Item 1</li>' +
+            '           <li>Sub Item 2</li>' +
+            '           <li>Sub Item 3</li>' +
+            '       </ul>' +
+            '   </li>' +
+            '   <li>' +
+            '       Item 2' +
+            '       <ul>' +
+            '           <li>Sub Item 1</li>' +
+            '           <li>Sub Item 2</li>' +
+            '           <li>Sub Item 3</li>' +
+            '       </ul>' +
+            '   </li>' +
+            '   <li>' +
+            '       Item 3' +
+            '       <ul>' +
+            '           <li>' +
+            '               <div>' +
+            '                   <ul>' +
+            '                       <li>Item 1</li>' +
+            '                       <li>Item 2</li>' +
+            '                   </ul>' +
+            '               </div>' +
+            '           </li>' +
+            '       </ul>' +
+            '   </li>' +
+            '</ul>'
+        );
+        menu = new kendo.ui.ContextMenu("#menu", { animation: false });
+    },
+    teardown: function() {
+        kendo.destroy(menu.element);
+    }
+});
+
+test('.k-menu is placed on root element', function() {
+    ok(menu.element.is(".k-menu"));
+});
+
+test('groups are both k-group and k-menu-group', function() {
+    ok(menu.element.find("ul").is(".k-menu-group.k-group"));
+});
+
+test('div inside menu is assigned .k-content class', function() {
+    ok(menu.element.find("div").is(".k-content"));
+});
+
+test('UL elements inside content don\'t get k-menu-group class', function() {
+    ok(!menu.element.find("div ul").is(".k-group"));
+    ok(!menu.element.find("div ul").is(".k-menu-group"));
+});
+
+})();
+
