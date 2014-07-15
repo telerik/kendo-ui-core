efa2c06160b51b225a08ae5a884d86e2473faf72
Daniel <karada@abv.bg>
2014-07-15 11:10:33 +0300

    Add diagram mvc api demos

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Diagram/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Kendo.Mvc.Examples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class DiagramController : Controller
+    {
+        public ActionResult Api()
+        {
+            return View();
+        }
+    }
+}
