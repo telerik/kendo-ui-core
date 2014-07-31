552ad9959c54bb6af6bc0389ebba95d409436185
Georgi Krustev <ggkrustev@yahoo.com>
2014-07-31 17:22:37 +0300

    Use tzTest instead test in date-formatting tests

--- tests/core/dateformatting.js
+++ tests/core/dateformatting.js
@@ -232,19 +232,19 @@ test('date formatting supports small year with four digits', function() {
     equal(toString(d, "s"), '0001-01-01T00:00:00');
 });
 
-test('date formatting supports "z" timezone offset specifier', function() {
+tzTest("Sofia", "date formatting supports 'z' timezone offset specifier", function() {
     var d = date(2000, 0, 1);
     equal(toString(d, "z"), '-2');
 });
 
-test('date formatting supports padded timezone offset specifier', function() {
+tzTest("Sofia", "date formatting supports padded timezone offset specifier", function() {
     var d = date(2000, 0, 1);
-    equal(toString(d, "zz"), '-02');
+    equal(toString(d, "zz"), "-02");
 });
 
-test('date formatting supports full timezone offset', function() {
+tzTest("Sofia", "date formatting supports full timezone offset", function() {
     var d = date(2000, 0, 1);
-    equal(toString(d, "zzz"), '-02:00');
+    equal(toString(d, "zzz"), "-02:00");
 });
 
 }());
