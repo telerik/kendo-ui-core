899de4b38db2c8e260439f238ad7eac7d6f4ef3e
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2014-07-31 18:40:30 +0300

    Add base tests file for the timeline view.

---
+++ tests/scheduler/timelineview.js
@@ -0,0 +1,23 @@
+(function() {
+    var TimelineView = kendo.ui.TimelineView,
+        SchedulerEvent = kendo.data.SchedulerEvent,
+        container;
+
+    module("Timeline View rendering", {
+        setup: function() {
+            container = $('<div class="k-scheduler" style="width:1000px;height:800px">');
+        },
+        teardown: function() {
+            if (container.data("kendoTimelineView")) {
+                container.data("kendoTimelineView").destroy();
+            }
+
+            kendo.destroy(QUnit.fixture);
+        }
+    });
+
+    test("title is read from the options", function () {
+        var view = setup({ title: "the title", date: new Date("2013/6/6") });
+        equal(view.title, "the title");
+    });
+})();
