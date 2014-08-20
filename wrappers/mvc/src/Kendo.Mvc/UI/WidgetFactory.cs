703874e4ddb43e3c9df58226ef651f4bdb653c08
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-08-20 10:12:49 +0300

    Add IntegerTextBox for longs. Closes #3555

--- wrappers/mvc/src/Kendo.Mvc/UI/WidgetFactory.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/WidgetFactory.cs
@@ -1769,6 +1769,19 @@ namespace Kendo.Mvc.UI.Fluent
         ///  &lt;%= Html.Kendo().IntegerTextBoxFor(m=>m.Property) %&gt;
         /// </code>
         /// </example>
+        public virtual NumericTextBoxBuilder<long> IntegerTextBoxFor(Expression<Func<TModel, Nullable<long>>> expression)
+        {
+            return NumericTextBoxFor<long>(expression).Format("n0").Decimals(0);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="NumericTextBox{T}"/>.
+        /// </summary>
+        /// <example>
+        /// <code lang="CS">
+        ///  &lt;%= Html.Kendo().IntegerTextBoxFor(m=>m.Property) %&gt;
+        /// </code>
+        /// </example>
         public virtual NumericTextBoxBuilder<int> IntegerTextBoxFor(Expression<Func<TModel, int>> expression)
         {
             return NumericTextBoxFor<int>(expression).Format("n0").Decimals(0);
