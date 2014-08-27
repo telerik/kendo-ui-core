d8e82f4b0604212230fad4aaa8aa5fea5f65b699
Atanas Korchev <akorchev@gmail.com>
2014-08-27 16:33:20 +0300

    Add jszip in src.

---
+++ src/kendo.xlsx.js
@@ -0,0 +1,19 @@
+(function(f, define){
+    define([ "./kendo.core" ], f);
+})(function(){
+
+var __meta__ = {
+    id: "xlsx",
+    name: "XLSX export",
+    category: "framework",
+    advanced: true,
+    depends: [ "core" ]
+};
+
+(function(JSZip){
+
+})(JSZip);
+
+return window.kendo;
+
+}, typeof define == 'function' && define.amd ? define : function(_, f){ f(); });
