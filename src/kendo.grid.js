229440f8e0fbc02dbf4521f8718c749a30d85d6d
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2014-09-03 11:01:29 +0300

    Reset selectable user events when content is recreated. Fixes #3589

--- src/kendo.grid.js
+++ src/kendo.grid.js
@@ -5157,6 +5157,10 @@ var __meta__ = {
                 that.touchScroller.contentResized();
             }
 
+            if (that.selectable) {
+                that.selectable.resetTouchEvents();
+            }
+
             that._angularItems("compile");
 
             that.trigger(DATABOUND);
