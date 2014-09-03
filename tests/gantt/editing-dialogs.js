bd4bd3cf47668d353a52c0c1037168a08cff16e7
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2014-09-03 11:35:29 +0300

    Add dependency delete confirmation tests

--- tests/gantt/editing-dialogs.js
+++ tests/gantt/editing-dialogs.js
@@ -2,7 +2,7 @@
     var Gantt = kendo.ui.Gantt;
     var element;
 
-    module("Delete confirmation", {
+    module("Task delete confirmation", {
         setup: function() {
             element = $("<div/>");
         },
@@ -66,11 +66,8 @@
 
     test("delete button rendered", function() {
         var gantt = setup();
-        var dataSource = gantt.dataSource;
         var taskUid = gantt.wrapper.find(".k-task").data("uid");
         var deleteButton;
-
-        stub(dataSource, "remove");
 
         gantt.removeTask(taskUid);
 
@@ -98,11 +95,8 @@
 
     test("cancel button rendered", function() {
         var gantt = setup();
-        var dataSource = gantt.dataSource;
         var taskUid = gantt.wrapper.find(".k-task").data("uid");
         var cancelButton;
-
-        stub(dataSource, "remove");
 
         gantt.removeTask(taskUid);
 
@@ -127,5 +121,118 @@
 
         equal(dataSource.calls("remove"), 0);
     });
+    
+    module("Dependency delete confirmation", {
+        setup: function() {
+            element = $("<div/>");
+        },
+        teardown: function() {
+            kendo.destroy(element);
+        }
+    });
+    
+    function setupDependency(options) {
+        return new Gantt(element, 
+            $.extend({
+                dataSource: {
+                    data: [{ id: 1, parentId: null, title: "foo", start: new Date("05/05/2014"), end: new Date("05/06/2014") },
+                        { id: 2, parentId: null, title: "bar", start: new Date("05/07/2014"), end: new Date("05/08/2014") }]
+                },
+                dependencies: {
+                    data: [{ id: 1, predecessorId: 1, successorId: 2, type: 1 }]
+                }
+            }, options)
+        );
+    }
+    
+    test("is shown on call to removeDependency()", function() {
+        var gantt = setupDependency();
+        var dependencyUid = gantt.wrapper.find(".k-line").data("uid");
 
+        equal($(".k-popup-edit-form").length, 0);
+
+        gantt.removeDependency(dependencyUid);
+
+        equal($(".k-popup-edit-form").length, 1);
+    });
+    
+    test("prevents call to datasource remove", function() {
+        var gantt = setup();
+        var dataSource = gantt.dependencies;
+        var dependencyUid = gantt.wrapper.find(".k-line").data("uid");
+
+        stub(dataSource, "remove");
+
+        gantt.removeDependency(dependencyUid);
+
+        equal(dataSource.calls("remove"), 0);
+    });
+    
+    test("default text rendered", function() {
+        var gantt = setup();
+        var dependencyUid = gantt.wrapper.find(".k-line").data("uid");
+        
+        gantt.removeDependency(dependencyUid);
+
+        equal($(".k-popup-message").text(), "Are you sure you want to delete this dependency?");
+    });
+    
+    test("delete button rendered", function() {
+        var gantt = setup();
+        var dependencyUid = gantt.wrapper.find(".k-line").data("uid");
+        var deleteButton;
+
+        gantt.removeDependency(dependencyUid);
+
+        deleteButton = $(".k-popup-edit-form .k-gantt-delete");
+
+        equal(deleteButton.length, 1);
+    });
+
+    test("delete button click calls datasource remove", function() {
+        var gantt = setup();
+        var dataSource = gantt.dependencies;
+        var dependencyUid = gantt.wrapper.find(".k-line").data("uid");
+        var deleteButton;
+
+        stub(dataSource, "remove");
+
+        gantt.removeDependency(dependencyUid);
+
+        deleteButton = $(".k-popup-edit-form .k-gantt-delete");
+
+        deleteButton.click();
+
+        equal(dataSource.calls("remove"), 1);
+    });
+    
+    test("cancel button rendered", function() {
+        var gantt = setup();
+        var dependencyUid = gantt.wrapper.find(".k-line").data("uid");
+        var cancelButton;
+
+        gantt.removeDependency(dependencyUid);
+
+        cancelButton = $(".k-popup-edit-form .k-gantt-cancel");
+
+        equal(cancelButton.length, 1);
+    });
+
+    test("cancel button click prevents call to  datasource remove", function() {
+        var gantt = setup();
+        var dataSource = gantt.dependencies;
+        var dependencyUid = gantt.wrapper.find(".k-line").data("uid");
+        var cancelButton;
+
+        gantt.removeDependency(dependencyUid);
+
+        gantt.removeTask(taskUid);
+
+        cancelButton = $(".k-popup-edit-form .k-gantt-cancel");
+
+        cancelButton.click();
+
+        equal(dataSource.calls("remove"), 0);
+    });
+    
 })();
