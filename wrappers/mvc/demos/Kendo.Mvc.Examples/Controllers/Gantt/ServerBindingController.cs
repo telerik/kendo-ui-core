ca8ffd647e2c35fc490f0c6ad3ef6e0aa4faec41
Dimtar Terziev <dimitar.terziev@telerik.com>
2014-10-02 14:05:26 +0300

    Add ServerBinding demo for the MVC Gantt

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Gantt/ServerBindingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class GanttController : Controller
+    {
+        public ActionResult ServerBinding()
+        {
+            ViewBag.Tasks = taskService.GetAll();
+            ViewBag.Dependencies = dependencyService.GetAll();
+
+            return View();
+        }
+
+    }
+}
