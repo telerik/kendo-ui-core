70c0f5ef37dba12e09ecb960863028d1be2ec586
Mihai Bazon <mihai@bazon.net>
2014-10-06 16:06:00 +0300

    [PDF] Update samples (WIP: currently toDataURL returns a Blob)

--- playground/saveas.js
+++ playground/saveas.js
@@ -1,18 +1,31 @@
-function saveAs(dataUrl, filename) {
+// function saveAs(url, filename) {
+//     if (navigator.msSaveBlob) {
+//         var parts = url.split(";base64,");
+//         var contentType = parts[0];
+//         var base64 = atob(parts[1]);
+//         var array = new Uint8Array(base64.length);
+
+//         for (var idx = 0; idx < base64.length; idx++) {
+//             array[idx] = base64.charCodeAt(idx);
+//         }
+
+//         navigator.msSaveBlob(new Blob([array.buffer], { type: contentType }), filename);
+//     } else {
+//         var a = document.createElement("a");
+//         a.href = url;
+//         a.download = filename;
+//         var e = document.createEvent("MouseEvents");
+//         e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+//         a.dispatchEvent(e);
+//     }
+// }
+
+function saveAs(blob, filename) {
     if (navigator.msSaveBlob) {
-        var parts = dataUrl.split(";base64,");
-        var contentType = parts[0];
-        var base64 = atob(parts[1]);
-        var array = new Uint8Array(base64.length);
-
-        for (var idx = 0; idx < base64.length; idx++) {
-            array[idx] = base64.charCodeAt(idx);
-        }
-
-        navigator.msSaveBlob(new Blob([array.buffer], { type: contentType }), filename);
+        navigator.msSaveBlob(blob, filename);
     } else {
         var a = document.createElement("a");
-        a.href = dataUrl;
+        a.href = URL.createObjectURL(blob);
         a.download = filename;
         var e = document.createEvent("MouseEvents");
         e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
