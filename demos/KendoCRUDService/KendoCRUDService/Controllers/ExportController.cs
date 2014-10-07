866f5218cdd63d5a3f547ab107f9d6920b03a365
Atanas Korchev <akorchev@gmail.com>
2014-10-07 11:15:16 +0300

    Add export controller which will be used in the export demos.

---
+++ demos/KendoCRUDService/KendoCRUDService/Controllers/ExportController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoCRUDService.Controllers
+{
+    public class ExportController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index(string contentType, string base64, string fileName)
+        {
+            if (Request.UrlReferrer.Host.EndsWith("telerik.com"))
+            {
+                var fileContents = Convert.FromBase64String(base64);
+
+                return File(fileContents, contentType, fileName);
+            }
+
+            return new HttpStatusCodeResult(403, "Available only for demos.telerik.com");
+        }
+    }
+}
