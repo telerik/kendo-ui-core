56e86558cb80910849f2f76c69594a1a8fafe5e0
Alexander Valchev <a.valchevbox@gmail.com>
2014-10-10 18:48:27 +0300

    Pass options. property to the popover pane.

--- src/kendo.mobile.popover.js
+++ src/kendo.mobile.popover.js
@@ -201,7 +201,8 @@ var __meta__ = {
     var PopOver = Widget.extend({
         init: function(element, options) {
             var that = this,
-                popupOptions;
+                popupOptions,
+                paneOptions;
 
             that.initialOpen = false;
 
@@ -219,7 +220,7 @@ var __meta__ = {
                 }
             });
 
-            that.pane = new ui.Pane(that.element, this.options.pane);
+            that.pane = new ui.Pane(that.element, $.extend(this.options.pane, { $angular: this.options.$angular }));
             that.pane.navigateToInitial();
 
             kendo.notify(that, ui);
