6a1ccd50ee17e5f08863fd9aef9d9c4836d2d214
Georgi Krustev <ggkrustev@yahoo.com>
2014-10-13 13:21:32 +0300

    Add a specific scheduler source binding required for the view data filtering. Fixes #3672

--- tests/scheduler/mvvm.js
+++ tests/scheduler/mvvm.js
@@ -36,4 +36,22 @@
 
         equal(dom.data("kendoScheduler").dataSource.at(0).title, "foo");
     });
+
+    test("filters data based on view range before bind", function() {
+        var today = kendo.date.today();
+        var yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
+
+        dom = $('<div data-role="scheduler" data-bind="source:events" />');
+
+        kendo.bind(dom, { events: [
+            { start: yesterday, end: yesterday, title: "foo1" },
+            { start: yesterday, end: yesterday, title: "foo2" },
+            { start: new Date(), end: new Date(), title: "foo3" },
+            { start: new Date(), end: new Date(), title: "foo4" }
+        ] } );
+
+        var events = dom.find(".k-event");
+
+        equal(events.length, 2);
+    });
 })();
