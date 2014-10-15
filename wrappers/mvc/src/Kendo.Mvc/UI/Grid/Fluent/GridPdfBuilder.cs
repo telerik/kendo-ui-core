d13b7e5cb449f7750e1d9be82f6e1f001b7fd06a
Atanas Korchev <akorchev@gmail.com>
2014-10-15 10:48:52 +0300

    Expose all PDF options in the MVC wrappers.

--- wrappers/mvc/src/Kendo.Mvc/UI/Grid/Fluent/GridPdfBuilder.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/Grid/Fluent/GridPdfBuilder.cs
@@ -28,12 +28,30 @@ namespace Kendo.Mvc.UI.Fluent
         }
 
         /// <summary>
-        /// Specifies the paper size
+        /// Specifies a predefiend paper size e.g. "A3", "A4" or "auto" (default).
         /// </summary>
         public GridPdfBuilder PaperSize(string paperSize)
         {
             excel.PaperSize = paperSize;
 
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies custom paper size in "pt" units.
+        /// </summary>
+        public GridPdfBuilder PaperSize(double width, double height)
+        {
+            excel.PaperSize = new[] { width, height };
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies custom paper size in custom units ("in", "mm", "pt", "cm")
+        /// </summary>
+        public GridPdfBuilder PaperSize(string width, string height)
+        {
+            excel.PaperSize = new[] { width, height };
             return this;
         }
 
@@ -44,6 +62,98 @@ namespace Kendo.Mvc.UI.Fluent
         public GridPdfBuilder ProxyUrl(string url)
         {
             excel.ProxyUrl = url;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the margins in "pt" units.
+        /// </summary>
+        public GridPdfBuilder Margin(double top, double right, double bottom, double left)
+        {
+            excel.Margin = new Dictionary<string, object>
+            {
+               { "top", top },
+               { "right", right },
+               { "bottom", bottom },
+               { "left", left }
+            };
+
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the margins in units ("in", "mm", "pt", "cm")
+        /// </summary>
+        public GridPdfBuilder Margin(string top, string right, string bottom, string left)
+        {
+            excel.Margin = new Dictionary<string, object>
+            {
+               { "top", top },
+               { "right", right },
+               { "bottom", bottom },
+               { "left", left }
+            };
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the title of the PDF document.
+        /// </summary>
+        public GridPdfBuilder Title(string title)
+        {
+            excel.Title = title;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the author of the PDF document.
+        /// </summary>
+        public GridPdfBuilder Author(string author)
+        {
+            excel.Author = author;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the subject of the PDF document.
+        /// </summary>
+        public GridPdfBuilder Subject(string subject)
+        {
+            excel.Subject = subject;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the keywords of the PDF document.
+        /// </summary>
+        public GridPdfBuilder Keywords(string keywords)
+        {
+            excel.Keywords = keywords;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the creator of the PDF document.
+        /// </summary>
+        public GridPdfBuilder Creator(string creator)
+        {
+            excel.Creator = creator;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the date of the PDF document.
+        /// </summary>
+        public GridPdfBuilder Date(DateTime date)
+        {
+            excel.Date = date;
 
             return this;
         }
--- wrappers/mvc/src/Kendo.Mvc/UI/Grid/Settings/GridPdfSettings.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/Grid/Settings/GridPdfSettings.cs
@@ -1,28 +1,33 @@
 using Kendo.Mvc.Extensions;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Kendo.Mvc.UI
 {    
     public class GridPdfSettings : JsonObject
     {
-        public string ProxyUrl
-        {
-            get;
-            set;
-        }
+        public string ProxyUrl { get; set; }
 
+        public object PaperSize { get; set; }
 
-        public string PaperSize
-        {
-            get;
-            set;
-        }
+        public string FileName { get; set; }
 
-        public string FileName
-        {
-            get;
-            set;
-        }
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Keywords { get; set; }
+
+        public string Creator { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public bool Landscape { get; set; }
+
+        public IDictionary<string, object> Margin { get; set; }
 
         protected override void Serialize(IDictionary<string, object> json)
         {
@@ -36,9 +41,49 @@ namespace Kendo.Mvc.UI
                 json["fileName"] = FileName;
             }
 
-            if (PaperSize.HasValue())
+            if (PaperSize != null)
             {
                 json["paperSize"] = PaperSize;
+            }
+
+            if (Landscape)
+            {
+                json["lanscape"] = Landscape;
+            }
+
+            if (Margin != null && Margin.Any())
+            {
+                json["margin"] = Margin;
+            }
+
+            if (Title.HasValue())
+            {
+                json["title"] = Title;
+            }
+
+            if (Author.HasValue())
+            {
+                json["author"] = Author;
+            }
+
+            if (Subject.HasValue())
+            {
+                json["subject"] = Subject;
+            }
+
+            if (Keywords.HasValue())
+            {
+                json["keywords"] = Keywords;
+            }
+
+            if (Creator.HasValue())
+            {
+                json["creator"] = Creator;
+            }
+
+            if (Date != null)
+            {
+                json["date"] = Date;
             }
         }
     }
