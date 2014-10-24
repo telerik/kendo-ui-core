c3fadf68acd501ab75e784a50e94c93019e0ff54
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-10-24 19:09:54 +0300

    Do not animate child Text of TextBox

--- src/kendo.dataviz.core.js
+++ src/kendo.dataviz.core.js
@@ -1469,7 +1469,7 @@ var __meta__ = {
             var id = options.id;
             var rows = (textbox.content + "").split(textbox.ROWS_SPLIT_REGEX);
             var floatElement = new FloatElement({vertical: true, align: options.align, wrap: false});
-            var textOptions = deepExtend({ }, options, { opacity: 1 });
+            var textOptions = deepExtend({ }, options, { opacity: 1, animation: null });
             var hasBox = textbox.hasBox();
             var text;
             var rowIdx;
