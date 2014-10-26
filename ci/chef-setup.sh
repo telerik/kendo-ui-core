862bfec28dccf0987f8e2364243572d3bccb7a2f
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-10-26 07:26:00 +0200

    Install ruby1.9.3 from Ubuntu repos

--- ci/chef-setup.sh
+++ ci/chef-setup.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+set -e
+
 if grep -q 'Ubuntu' /proc/version
 then
     apt-get install -y python-software-properties
-    apt-add-repository -y ppa:brightbox/ruby-ng
     apt-get update -y
 
     apt-get install -y ruby1.9.3 build-essential
