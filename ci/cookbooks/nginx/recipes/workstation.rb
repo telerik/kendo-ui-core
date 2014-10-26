d9c801d82736a7b68c011dd47acbb41e290369e8
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-10-26 07:27:34 +0200

    Don't overwrite system-wide nginx.conf, use a site file

--- ci/cookbooks/nginx/recipes/workstation.rb
+++ ci/cookbooks/nginx/recipes/workstation.rb
@@ -1,12 +1,16 @@
 
 package "nginx"
 
-template "/etc/nginx/nginx.conf" do
+template "/etc/nginx/sites-enabled/kendo" do
     source "workstation.conf.erb"
     variables({
         :home_dir => node['home_dir'] || ENV['HOME'],
         :kendo_dir => node['kendo_dir'] || File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', '..'))
     })
+end
+
+file "/etc/nginx/sites-enabled/default" do
+  action :delete
 end
 
 service "nginx" do
