bc0471cb0a06f52a87bd556015d3359960cbd4e1
germanov <hristo.germanov@gmail.com>
2014-10-29 10:08:26 +0200

    Update diagram events

--- wrappers/mvc/src/Kendo.Mvc/UI/Diagram/Fluent/DiagramEventBuilder.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/Diagram/Fluent/DiagramEventBuilder.cs
@@ -16,6 +16,72 @@ namespace Kendo.Mvc.UI.Fluent
         //>> Handlers
         
         /// <summary>
+        /// Fired when the widget is bound to data from dataDource and connectionsDataSource.The event handler function context (available via the this keyword) will be set to the widget instance.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the dataBound event.</param>
+        public DiagramEventBuilder DataBound(string handler)
+        {
+            Handler("dataBound", handler);
+
+            return this;
+        }
+        
+        /// <summary>
+        /// Fired when the user add new shape or connection.The event handler function context (available via the this keyword) will be set to the widget instance.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the Add event.</param>
+        public DiagramEventBuilder Add(string handler)
+        {
+            Handler("Add", handler);
+
+            return this;
+        }
+        
+        /// <summary>
+        /// Fired when the user edits a shape or connection.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the Edit event.</param>
+        public DiagramEventBuilder Edit(string handler)
+        {
+            Handler("Edit", handler);
+
+            return this;
+        }
+        
+        /// <summary>
+        /// Fired when the user a shape or connection da is saved.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the Save event.</param>
+        public DiagramEventBuilder Save(string handler)
+        {
+            Handler("Save", handler);
+
+            return this;
+        }
+        
+        /// <summary>
+        /// Fired when the user clicks the "cancel" button in the popup window.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the cancel event.</param>
+        public DiagramEventBuilder Cancel(string handler)
+        {
+            Handler("cancel", handler);
+
+            return this;
+        }
+        
+        /// <summary>
+        /// Fired when the user delete a shape or connection.
+        /// </summary>
+        /// <param name="handler">The name of the JavaScript function that will handle the remove event.</param>
+        public DiagramEventBuilder Remove(string handler)
+        {
+            Handler("remove", handler);
+
+            return this;
+        }
+        
+        /// <summary>
         /// Fired when an item is added or removed to/from the diagram.
         /// </summary>
         /// <param name="handler">The name of the JavaScript function that will handle the change event.</param>
