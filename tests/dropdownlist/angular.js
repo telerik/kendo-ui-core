ce9e6a10a2614dd6e53bb160a7039807c3729f7c
Petyo Ivanov <underlog@gmail.com>
2014-11-03 17:52:07 +0200

    Fix happy testing paths

--- tests/dropdownlist/angular.js
+++ tests/dropdownlist/angular.js
@@ -8,46 +8,46 @@
     ngTest("dropdown recognizes selected primitive items with k-ng-model", 1, function() {
         angular.module("kendo.tests").controller("mine", function($scope) {
             $scope.colors = [ "red", "green", "blue" ];
-            $scope.selectedColor = "red";
+            $scope.selectedColor = "green";
         });
 
         QUnit.fixture.html('<div ng-controller=mine><select kendo-drop-down-list k-ng-model=selectedColor k-data-source=colors></select></div>');
     },
 
     function() {
-        equal(QUnit.fixture.find("select").getKendoDropDownList().value(), "red");
+        equal(QUnit.fixture.find("select").getKendoDropDownList().value(), "green");
     });
 
     ngTest("dropdown recognizes selected object items with k-ng-model", 1, function() {
         angular.module("kendo.tests").controller("mine", function($scope) {
             $scope.colors = [ { color: "red" }, { color: "green" }, { color: "blue" } ];
-            $scope.selectedColor = $scope.colors[0];
+            $scope.selectedColor = $scope.colors[1];
         });
 
         QUnit.fixture.html('<div ng-controller=mine><select kendo-drop-down-list k-ng-model=selectedColor k-data-source=colors k-data-value-field="\'color\'"></select></div>');
     },
 
     function() {
-        equal(JSON.stringify(QUnit.fixture.find("select").getKendoDropDownList().dataItem()), JSON.stringify({ color: "red" }));
+        equal(JSON.stringify(QUnit.fixture.find("select").getKendoDropDownList().dataItem()), JSON.stringify({ color: "green" }));
     });
 
     ngTest("dropdown works with data-value-primitive in k-ng-model", 1, function() {
         angular.module("kendo.tests").controller("mine", function($scope) {
             $scope.colors = [ { color: "red" }, { color: "green" }, { color: "blue" } ];
-            $scope.selectedColor = "red";
+            $scope.selectedColor = "green";
         });
 
         QUnit.fixture.html('<div ng-controller=mine><select kendo-drop-down-list k-value-primitive="true" k-ng-model=selectedColor k-data-source=colors k-data-value-field="\'color\'"></select></div>');
     },
 
     function() {
-        equal(QUnit.fixture.find("select").getKendoDropDownList().value(), "red");
+        equal(QUnit.fixture.find("select").getKendoDropDownList().value(), "green");
     });
 
     ngTest("dropdown returns primitives to model", 1, function() {
         angular.module("kendo.tests").controller("mine", function($scope) {
             $scope.colors = [ { color: "red" }, { color: "green" }, { color: "blue" } ];
-            $scope.selectedColor = $scope.colors[0];
+            $scope.selectedColor = $scope.colors[1];
         });
 
         QUnit.fixture.html('<div ng-controller=mine><select kendo-drop-down-list k-ng-model=selectedColor k-data-source=colors k-data-value-field="\'color\'"></select></div>');
@@ -64,7 +64,7 @@
     ngTest("dropdown returns primitives to model with primitive", 1, function() {
         angular.module("kendo.tests").controller("mine", function($scope) {
             $scope.colors = [ { color: "red" }, { color: "green" }, { color: "blue" } ];
-            $scope.selectedColor = "red";
+            $scope.selectedColor = "green";
         });
 
         QUnit.fixture.html('<div ng-controller=mine><select kendo-drop-down-list k-value-primitive="true" k-ng-model=selectedColor k-data-source=colors k-data-value-field="\'color\'"></select></div>');
