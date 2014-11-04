602b3eca7bc03f4d88ec15c070e245ea58d17c52
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2014-11-04 15:09:50 +0200

    Fix GanttToolbarItem PHP object

--- wrappers/php/lib/Kendo/UI/GanttToolbarItem.php
+++ wrappers/php/lib/Kendo/UI/GanttToolbarItem.php
@@ -3,6 +3,9 @@
 namespace Kendo\UI;
 
 class GanttToolbarItem extends \Kendo\SerializableObject {
+    function __construct($name = null) {
+        $this->name($name);
+    }
 //>> Properties
 
     /**
