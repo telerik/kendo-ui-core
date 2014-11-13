628bb7104a33ca846f7d4a2d68b00b6eae255bff
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2014-11-13 14:40:59 +0200

    Add PDF Export settings to Gantt MVC Demos

--- wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Gantt/Pdf_ExportController.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Gantt/Pdf_ExportController.cs
@@ -16,5 +16,13 @@ namespace Kendo.Mvc.Examples.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Pdf_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
     }
 }
