3801938fd1f6a8f58a31924d564d9e813dc626be
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-11-14 14:39:10 +0200

    Add Diagram export demos in MVC

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Diagram/Pdf_ExportController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Kendo.Mvc.Examples.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System.Linq;
+using System;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class DiagramController : Controller
+    {
+        public ActionResult Pdf_Export()
+        {
+            return View();
+        }
+
+        public ActionResult Pdf_Export_Read()
+        {
+            return Json(DiagramDataRepository.OrgChart(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Pdf_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
+    }
+}
