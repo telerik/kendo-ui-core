722fdb5ed75221616ee9616748d6e5960f6d2a3b
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-11-14 16:51:30 +0200

    Add Stock Chart PDF export demo in MVC

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Financial/Pdf_ExportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class FinancialController : Controller
+    {
+        public ActionResult Pdf_Export()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Pdf_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
+    }
+}
