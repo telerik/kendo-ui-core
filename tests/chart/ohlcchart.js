e4c7b808d5a4b1e5b40da449f9b198fd026e7ffa
Daniel <karada@abv.bg>
2014-11-17 18:44:56 +0200

    Migrate ohlc chart tests

--- tests/chart/ohlcchart.js
+++ tests/chart/ohlcchart.js
@@ -1,5 +1,4 @@
 (function() {
-    return;
 
     var dataviz = kendo.dataviz,
         Box2D = dataviz.Box2D,
@@ -7,22 +6,23 @@
         chartBox = new Box2D(0, 0, 800, 600),
         ohlcChart,
         root,
-        view,
         firstPoint,
+        firstLine,
         TOLERANCE = 1;
 
     function setupOHLCChart(plotArea, options) {
-        view = new ViewStub();
-
         ohlcChart = new dataviz.OHLCChart(plotArea, options);
 
         root = new dataviz.RootElement();
         root.append(ohlcChart);
 
-        ohlcChart.reflow();
-        ohlcChart.getViewElements(view);
+        root.reflow();
+        root.renderVisual();
 
         firstPoint = ohlcChart.points[0];
+        var visual = firstPoint.visual;
+        var mainVisual = visual.children[0];
+        firstLine = mainVisual.children[0];
     }
 
     function stubPlotArea(getCategorySlot, getValueSlot, options) {
@@ -190,51 +190,27 @@
         });
 
         test("sets line width", function() {
-            equal(view.log.path[0].style.strokeWidth, 4);
+            equal(firstLine.options.stroke.width, 4);
         });
 
         test("sets line color", function() {
-            equal(view.log.path[0].style.stroke, "lineColor");
+            equal(firstLine.options.stroke.color, "lineColor");
         });
 
         test("sets line opacity", function() {
-            equal(view.log.path[0].style.strokeOpacity, 0.5);
+            equal(firstLine.options.stroke.opacity, 0.5);
         });
 
         test("sets line dashType", function() {
-            equal(view.log.path[0].style.dashType, "dot");
+            equal(firstLine.options.stroke.dashType, "dot");
         });
 
-        test("overlay rect has same model id as its segment", function() {
-            equal(view.log.rect[0].style.data.modelId, ohlcChart.points[0].modelId);
-        });
-
-        test("generates unique id", function() {
-            ok(ohlcChart.id);
-        });
-
-        test("renders group with OhlcChart id and no animations", function() {
-            var group = view.findInLog("group", function(item) {
-                return item.options.id === ohlcChart.id;
-            });
-
-            ok(group && !group.options.animation);
-            equal(group.options.id, ohlcChart.id);
-        });
-
-        test("renders chart group", function() {
-            var group = view.findInLog("group", function(item) {
-                return item.options.animation;
-            });
-            ok(group);
-        });
-
-        test("sets group animation", function() {
-            var group = view.findInLog("group", function(item) {
-                return item.options.animation;
-            });
-            equal(group.options.animation.type, "clip");
-        });
+        // test("sets group animation", function() {
+            // var group = view.findInLog("group", function(item) {
+                // return item.options.animation;
+            // });
+            // equal(group.options.animation.type, "clip");
+        // });
 
         // ------------------------------------------------------------
         module("OHLC Chart / Rendering / Highlight", {
@@ -245,44 +221,44 @@
             }
         });
 
-        test("highlightOverlay renders default line width", function() {
-            var line = firstPoint.highlightOverlay(view).children[0];
+        test("createHighlight renders default line width", function() {
+            var line = firstPoint.createHighlight().children[0];
 
-            equal(line.options.strokeWidth, 1);
+            equal(line.options.stroke.width, 1);
         });
 
-        test("highlightOverlay renders custom line width", function() {
+        test("createHighlight renders custom line width", function() {
             firstPoint.options.highlight.line.width = 2;
-            var line = firstPoint.highlightOverlay(view).children[0];
+            var line = firstPoint.createHighlight().children[0];
 
-            equal(line.options.strokeWidth, 2);
+            equal(line.options.stroke.width, 2);
         });
 
-        test("highlightOverlay renders default line color", function() {
+        test("createHighlight renders default line color (computed)", function() {
             firstPoint.color = "#ffffff";
-            var line = firstPoint.highlightOverlay(view).children[0];
+            var line = firstPoint.createHighlight().children[0];
 
-            equal(line.options.stroke, "#ffffff");
+            equal(line.options.stroke.color, "#cccccc");
         });
 
-        test("highlightOverlay renders custom line color", function() {
+        test("createHighlight renders custom line color", function() {
             firstPoint.options.highlight.line.color = "red";
-            var line = firstPoint.highlightOverlay(view).children[0];
+            var line = firstPoint.createHighlight().children[0];
 
-            equal(line.options.stroke, "red");
+            equal(line.options.stroke.color, "red");
         });
 
-        test("highlightOverlay renders default line opacity", function() {
-            var line = firstPoint.highlightOverlay(view).children[0];
+        test("createHighlight renders default line opacity", function() {
+            var line = firstPoint.createHighlight().children[0];
 
-            equal(line.options.strokeOpacity, 1);
+            equal(line.options.stroke.opacity, 1);
         });
 
-        test("highlightOverlay renders custom line opacity", function() {
+        test("createHighlight renders custom line opacity", function() {
             firstPoint.options.highlight.line.opacity = 0.5;
-            var line = firstPoint.highlightOverlay(view).children[0];
+            var line = firstPoint.createHighlight().children[0];
 
-            equal(line.options.strokeOpacity, 0.5);
+            equal(line.options.stroke.opacity, 0.5);
         });
 
     })();
@@ -439,10 +415,6 @@
             }
         });
 
-        test("is discoverable", function() {
-            ok(point.modelId);
-        });
-
         test("sets point border width", function() {
             createPoint({ border: { width: 4 } });
             equal(point.options.border.width, 4);
@@ -451,17 +423,6 @@
         test("sets point opacity", function() {
             createPoint({ opacity: 0.5 });
             deepEqual(point.options.opacity, 0.5);
-        });
-
-        test("sets point id", function() {
-            ok(point.id.length > 0);
-        });
-
-        test("point has same model id", function() {
-            view = new ViewStub();
-
-            point.getViewElements(view);
-            equal(view.log.rect[0].style.data.modelId, point.modelId);
         });
 
         test("tooltipAnchor is at top right of point", function() {
