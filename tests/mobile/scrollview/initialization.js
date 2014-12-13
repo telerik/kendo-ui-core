cc4b283a8199e68a367bdd6a691df0dbb07d756d
Petyo Ivanov <underlog@gmail.com>
2014-12-13 17:19:58 +0200

    Fix scrollview error when last item is reached
    Fix telerik/kendo-ui-core#462
    
    Conflicts:
    	tests/mobile/scrollview/initialization.js
    
    Conflicts:
    	tests/mobile/scrollview/initialization.js

--- tests/mobile/scrollview/initialization.js
+++ tests/mobile/scrollview/initialization.js
@@ -1,5 +1,6 @@
 (function() {
     module("scrollview utils")
+
     test("items returns the current items", function() {
         var div = $("<div />");
 
@@ -9,6 +10,19 @@
         });
 
         equal(scrollview.items().length, 3);
+        scrollview.destroy();
+    });
+
+    test("does not render items beyond the last one", function() {
+        var div = $("<div />");
+
+        var scrollview = new kendo.mobile.ui.ScrollView(div, {
+            dataSource: [1],
+            template: "#: data #",
+            emptyTemplate: "empty"
+        });
+
+        equal(scrollview.items()[2].innerHTML, "empty");
         scrollview.destroy();
     });
 })();
