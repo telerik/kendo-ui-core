16359af224912372daf5880ff0124c139cdba1bc
Petyo Ivanov <underlog@gmail.com>
2015-01-07 16:26:00 +0200

    Fix ng-model/k-delay combo
    Fix telerik/kendo-ui-core#477

--- tests/angular/rebind.js
+++ tests/angular/rebind.js
@@ -27,6 +27,74 @@
         }, 100);
     });
 
+    ngTest("ng delay works with an assigned kmodel", 2, function(){
+        angular.module('kendo.tests').controller('mine', function($scope) {
+            $scope.productsDataSource = new kendo.data.ObservableArray([
+              { ProductID: 1, ProductName: "ProductName1" },
+              { ProductID: 2, ProductName: "ProductName2" },
+              { ProductID: 3, ProductName: "ProductName3" },
+              { ProductID: 4, ProductName: "ProductName4" },
+              { ProductID: 5, ProductName: "ProductName5" }
+            ]);
+        });
+
+        QUnit.fixture.html('<div ng-controller=mine><select kendo-drop-down-list k-options="productsOptions" k-ng-model="selectedProduct" k-ng-delay="productsOptions"></select></div>'); },
+    function() {
+        var scope = QUnit.fixture.find("select").scope();
+
+        scope.$apply(function(){
+            scope.productsOptions = {
+                dataSource: scope.productsDataSource,
+                dataTextField: "ProductName",
+                dataValueField: "ProductID"
+            };
+
+            scope.selectedProduct = scope.productsDataSource[2];
+        });
+
+        stop();
+
+        setTimeout(function() {
+            start();
+            ok(QUnit.fixture.find("select").getKendoDropDownList());
+            equal(QUnit.fixture.find("select").getKendoDropDownList().value(), 3);
+        }, 100);
+    });
+
+    ngTest("ng delay works with an assigned kmodel", 2, function(){
+        angular.module('kendo.tests').controller('mine', function($scope) {
+            $scope.productsDataSource = new kendo.data.ObservableArray([
+              { ProductID: 1, ProductName: "ProductName1" },
+              { ProductID: 2, ProductName: "ProductName2" },
+              { ProductID: 3, ProductName: "ProductName3" },
+              { ProductID: 4, ProductName: "ProductName4" },
+              { ProductID: 5, ProductName: "ProductName5" }
+            ]);
+        });
+
+        QUnit.fixture.html('<div ng-controller=mine><select kendo-drop-down-list k-options="productsOptions" ng-model="selectedProduct" k-ng-delay="productsOptions"></select></div>'); },
+    function() {
+        var scope = QUnit.fixture.find("select").scope();
+
+        scope.$apply(function(){
+            scope.productsOptions = {
+                dataSource: scope.productsDataSource,
+                dataTextField: "ProductName",
+                dataValueField: "ProductID"
+            };
+
+            scope.selectedProduct = 3;
+        });
+
+        stop();
+
+        setTimeout(function() {
+            start();
+            ok(QUnit.fixture.find("select").getKendoDropDownList());
+            equal(QUnit.fixture.find("select").getKendoDropDownList().value(), 3);
+        }, 100);
+    });
+
     ngTest("ng delay will not delay if value is set", 1, function(){
         angular.module('kendo.tests').controller('mine', function($scope) {
             $scope.foo = "foo";
