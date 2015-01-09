1106a92c7a125f222cf99f398f62b3652d98f004
Petyo Ivanov <underlog@gmail.com>
2015-01-09 17:10:39 +0200

    Fix the broken overlay transition
    Fix telerik/kendo-ui-core#497

--- src/kendo.mobile.navbar.js
+++ src/kendo.mobile.navbar.js
@@ -27,13 +27,14 @@ var __meta__ = {
     function toggleTitle(centerElement) {
         var siblings = centerElement.siblings(),
             noTitle = !!centerElement.children("ul")[0],
-            showTitle = (!!siblings[0] && $.trim(centerElement.text()) === "");
+            showTitle = (!!siblings[0] && $.trim(centerElement.text()) === ""),
+            android = kendo.mobile.application && kendo.mobile.application.element.is(".km-android");
 
         centerElement.prevAll().toggleClass("km-absolute", noTitle);
         centerElement.toggleClass("km-show-title", showTitle);
         centerElement.toggleClass("km-fill-title", showTitle && !$.trim(centerElement.html()));
         centerElement.toggleClass("km-no-title", noTitle);
-        centerElement.toggleClass("km-hide-title", centerElement.css("visibility") == "hidden" && !siblings.children().is(":visible"));
+        centerElement.toggleClass("km-hide-title", android && !siblings.children().is(":visible"));
     }
 
     var NavBar = Widget.extend({
