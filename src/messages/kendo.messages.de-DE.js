b258035dcbe3dd58828ea1ecdbe653efa5f5fb95
Pascal Berger <pascal.berger@ioz.ch>
2015-02-16 09:52:00 +0100

    Additional German translations for the scheduler widget.

--- src/messages/kendo.messages.de-DE.js
+++ src/messages/kendo.messages.de-DE.js
@@ -344,7 +344,7 @@ $.extend(true, kendo.ui.Scheduler.prototype.options.messages,{
     "timezoneEditorButton": "Zeitzone",
     "timezoneEditorTitle": "Zeitzonen",
     "title": "Titel",
-    "noTimezone": "No timezone"
+    "noTimezone": "Keine Zeitzone"
   },
   "event": "Termin",
   "recurrenceMessages": {
@@ -365,7 +365,7 @@ $.extend(true, kendo.ui.Scheduler.prototype.options.messages,{
     "day": "Tag",
     "month": "Monat",
     "week": "Woche",
-    "workWeek": "Work Week"
+    "workWeek": "Arbeitswoche"
   },
   "deleteWindowTitle": "Termin löschen",
   "showFullDay": "Ganzen Tag anzeigen",
