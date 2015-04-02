b3f8060980d065490056b78fcc0be05bc16ddaac
Nikolay Rusev <nikolay.rusev@telerik.com>
2015-04-02 09:43:00 +0300

    Do not prevent default for button.k-button elements. Fixes telerik/kendo-ui-core#688

--- src/kendo.selectable.js
+++ src/kendo.selectable.js
@@ -23,7 +23,7 @@ var __meta__ = {
         CHANGE = "change",
         NS = ".kendoSelectable",
         UNSELECTING = "k-state-unselecting",
-        INPUTSELECTOR = "input,a,textarea,.k-multiselect-wrap,select,button,a.k-button>.k-icon,span.k-icon.k-i-expand,span.k-icon.k-i-collapse",
+        INPUTSELECTOR = "input,a,textarea,.k-multiselect-wrap,select,button,a.k-button>.k-icon,button.k-button>.k-icon,span.k-icon.k-i-expand,span.k-icon.k-i-collapse",
         msie = kendo.support.browser.msie,
         supportEventDelegation = false;
 
