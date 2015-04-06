4d5cf2eb8d4f38fbb09736c46679f0ca59e0b388
Dimo Dimov <dimo.dimov@telerik.com>
2015-04-06 18:39:37 +0300

    Change test padding check implementation

--- tests/multiselect/initialization.js
+++ tests/multiselect/initialization.js
@@ -255,7 +255,7 @@
 
         var padding = multiselect.list.find(".k-group-header").css("padding-right");
 
-        ok(parseFloat(padding) > 15);
+        ok(parseFloat(padding) >= kendo.support.scrollbar());
     });
 
     test("MultiSelect does not add scrollbar width to the fixed group header padding if popup has not scroll", function() {
