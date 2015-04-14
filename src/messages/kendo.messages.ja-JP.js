82976453d73deaafb9ddbe50f1da76ae62cc57ce
Pascal Berger <pascal.berger@ioz.ch>
2015-04-14 12:01:59 +0200

    Remove during build autogenerated code

--- src/messages/kendo.messages.ja-JP.js
+++ src/messages/kendo.messages.ja-JP.js
@@ -1,16 +1,4 @@
-﻿/*
-* Kendo UI v2014.3.1411 (http://www.telerik.com/kendo-ui)
-* Copyright 2015 Telerik AD. All rights reserved.
-*
-* Kendo UI commercial licenses may be obtained at
-* http://www.telerik.com/purchase/license-agreement/kendo-ui-complete
-* If you do not own a commercial license, this file shall be governed by the trial license terms.
-*/
-(function(f, define){
-    define([], f);
-})(function(){
-
-/* FlatColorPicker messages */
+﻿/* FlatColorPicker messages */
 
 if (kendo.ui.FlatColorPicker) {
 kendo.ui.FlatColorPicker.prototype.options.messages =
@@ -545,8 +533,3 @@ $.extend(true, kendo.ui.Validator.prototype.options.messages,{
   "date": "{0} は無効な日付です"
 });
 }
-
-
-return window.kendo;
-
-}, typeof define == 'function' && define.amd ? define : function(_, f){ f(); });
