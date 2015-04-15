768dc458114bd41a20a23b2fc8e96720b71277a3
Petyo Ivanov <underlog@gmail.com>
2015-04-15 17:00:35 +0300

    SplitView initializes header/footer
    Fix telerik/kendo-ui-core#610

--- src/kendo.mobile.splitview.js
+++ src/kendo.mobile.splitview.js
@@ -61,6 +61,10 @@ var __meta__ = {
                     pane = kendo.compileMobileDirective($(this), options.$angular[0]);
                     that.panes.push(pane);
                 });
+
+                that.element.children(kendo.directiveSelector("header footer")).each(function() {
+                    kendo.compileMobileDirective($(this), options.$angular[0]);
+                });
             }
 
             that.expandedPaneShim = $(EXPANED_PANE_SHIM).appendTo(that.element);
