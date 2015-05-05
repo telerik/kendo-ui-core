2bb644a71b5828b64cf9e3f14235e9ad6988e6b7
Georgi Krustev <ggkrustev@yahoo.com>
2015-05-05 14:30:57 +0300

    Show combobox placeholder when value is cleared in IE9. Fixes telerik/kendo-ui-core#775

--- src/kendo.combobox.js
+++ src/kendo.combobox.js
@@ -580,6 +580,7 @@ var __meta__ = {
                     if (that.selectedIndex === -1) {
                         that._accessor(value);
                         that.input.val(value);
+                        that._placeholder(true);
                     }
 
                     that._old = that._accessor();
