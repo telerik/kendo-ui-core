f957a4fcea8479b1b91342ab8003aa770cb41cea
Alex Gyoshev <alex@gyoshev.net>
2015-05-05 15:53:29 +0300

    Append stylesheet prior to modifying it
    Suppresses JS error in IE8.

--- src/kendo.responsivepanel.js
+++ src/kendo.responsivepanel.js
@@ -72,13 +72,13 @@ var __meta__ = {
             var head = $("head,body")[0];
             var style = document.createElement('style');
 
+            head.appendChild(style);
+
             if (style.styleSheet){
                 style.styleSheet.cssText = cssText;
             } else {
                 style.appendChild(document.createTextNode(cssText));
             }
-
-            head.appendChild(style);
         },
         options: {
             name: "ResponsivePanel",
