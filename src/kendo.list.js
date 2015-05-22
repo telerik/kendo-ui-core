ab8ec22277fedc8f3200d15d99d7418cab1fa8bb
Georgi Krustev <ggkrustev@yahoo.com>
2015-05-22 18:04:35 +0300

    Fix incorrect value expression generation when values are integers

--- src/kendo.list.js
+++ src/kendo.list.js
@@ -1555,7 +1555,7 @@ var __meta__ = {
                         selectedValue = '""';
                     } else if (selectedValue !== null) {
                         if ((type !== "boolean" && type !== "number") || typeof selectedValue === "object") {
-                            selectedValue = '"' + selectedValue.replace(/"/g, "\\\"") + '"';
+                            selectedValue = '"' + selectedValue.toString().replace(/"/g, "\\\"") + '"';
                         } else if (type === "number" && isNaN(selectedValue)) {
                             continue;
                         }
