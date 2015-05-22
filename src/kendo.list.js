3c834b5b728509f58e82272c34c2d814be1ee4db
Georgi Krustev <ggkrustev@yahoo.com>
2015-05-22 14:07:26 +0300

    Do not set 'null' value to the input element (applicable to IE9 or lower). Related to telerik/kendo-ui-core#775

--- src/kendo.list.js
+++ src/kendo.list.js
@@ -727,6 +727,9 @@ var __meta__ = {
             if (value === undefined) {
                 return element.value;
             } else {
+                if (value === null) {
+                    value = "";
+                }
                 element.value = value;
             }
         },
