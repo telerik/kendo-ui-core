3afaf0179dfaa9978c2af9c1107d6b50ec8975ad
Petyo Ivanov <underlog@gmail.com>
2015-06-08 11:06:17 +0300

    Fix erratic backbutton behavior

--- src/kendo.mobile.pane.js
+++ src/kendo.mobile.pane.js
@@ -273,8 +273,10 @@ var __meta__ = {
             var that = this,
                 linkRoles = "tab",
                 pressedButtonSelector = "[data-" + kendo.ns + "navigate-on-press]",
-                buttonSelector = roleSelector("button") + ":not(" + pressedButtonSelector + ")",
-                buttonSelectors = roleSelector("backbutton detailbutton listview-link") + "," + buttonSelector;
+
+                buttonSelectors = $.map(["button", "backbutton", "detailbutton", "listview-link"] , function(role) {
+                    return roleSelector(role) + ":not(" + pressedButtonSelector + ")";
+                }).join(",");
 
             this.element.handler(this)
                 .on("down", roleSelector(linkRoles) + "," + pressedButtonSelector, "_mouseup")
