9b91ab810c90b579e6318eb5d796cd71d8884bb6
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2015-06-25 11:00:31 +0300

    Do not trigger save event is validation fails. Fixes #893

--- src/kendo.listview.js
+++ src/kendo.listview.js
@@ -524,48 +524,40 @@ var __meta__ = {
            return this.dataSource.getByUid(uid);
        },
 
-       _closeEditable: function(validate) {
+       _closeEditable: function() {
            var that = this,
                editable = that.editable,
                data,
                item,
                index,
-               template = that.template,
-               valid = true;
+               template = that.template;
 
            if (editable) {
-               if (validate) {
-                   valid = editable.end();
+               if (editable.element.index() % 2) {
+                   template = that.altTemplate;
                }
 
-               if (valid) {
-                   if (editable.element.index() % 2) {
-                       template = that.altTemplate;
-                   }
+               that.angular("cleanup", function() {
+                   return { elements: [ editable.element ]};
+               });
 
-                   that.angular("cleanup", function() {
-                       return { elements: [ editable.element ]};
-                   });
+               data = that._modelFromElement(editable.element);
+               that._destroyEditable();
 
-                   data = that._modelFromElement(editable.element);
-                   that._destroyEditable();
+               index = editable.element.index();
+               editable.element.replaceWith(template(data));
+               item = that.items().eq(index);
+               item.attr(kendo.attr("uid"), data.uid);
 
-                   index = editable.element.index();
-                   editable.element.replaceWith(template(data));
-                   item = that.items().eq(index);
-                   item.attr(kendo.attr("uid"), data.uid);
-
-                   if (that._hasBindingTarget()) {
-                        kendo.bind(item, data);
-                   }
-
-                   that.angular("compile", function() {
-                       return { elements: [ item ], data: [ { dataItem: data } ]};
-                   });
+               if (that._hasBindingTarget()) {
+                   kendo.bind(item, data);
                }
+
+               that.angular("compile", function() {
+                   return { elements: [ item ], data: [ { dataItem: data } ]};
+               });
            }
-
-           return valid;
+           return true;
        },
 
        edit: function(item) {
@@ -600,10 +592,11 @@ var __meta__ = {
                return;
            }
 
-           editable = editable.element;
-           model = that._modelFromElement(editable);
+           var container = editable.element;
+           model = that._modelFromElement(container);
 
-           if (!that.trigger(SAVE, { model: model, item: editable }) && that._closeEditable(true)) {
+           if (editable.end() && !that.trigger(SAVE, { model: model, item: container }))  {
+               that._closeEditable();
                that.dataSource.sync();
            }
        },
@@ -615,7 +608,7 @@ var __meta__ = {
 
            if (that.editable) {
                dataSource.cancelChanges(that._modelFromElement(that.editable.element));
-               that._closeEditable(false);
+               that._closeEditable();
            }
 
            if (!that.trigger(REMOVE, { model: data, item: item })) {
@@ -649,7 +642,7 @@ var __meta__ = {
 
                if (!that.trigger(CANCEL, { model: model, container: container})) {
                    dataSource.cancelChanges(model);
-                   that._closeEditable(false);
+                   that._closeEditable();
                }
            }
        },
