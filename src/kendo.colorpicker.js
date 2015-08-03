86c3cc553553dc4d228da0955f036c9ac5517c4e
Alex Gyoshev <alex@gyoshev.net>
2015-08-03 13:11:31 +0300

    Handle clicks on k-select instead of just the icon

--- src/kendo.colorpicker.js
+++ src/kendo.colorpicker.js
@@ -708,7 +708,7 @@ var __meta__ = {
                     .on("focus" + NS, function () { innerWrapper.addClass("k-state-focused"); })
                     .on("blur" + NS, function () { innerWrapper.removeClass("k-state-focused"); })
                     .on(KEYDOWN_NS, bind(that._keydown, that))
-                    .on(CLICK_NS, ".k-icon", bind(that.toggle, that))
+                    .on(CLICK_NS, ".k-select", bind(that.toggle, that))
                     .on(CLICK_NS, that.options.toolIcon ? ".k-tool-icon" : ".k-selected-color", function(){
                         that.trigger("activate");
                     });
