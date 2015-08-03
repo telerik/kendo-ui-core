6304eacf2c43cf3ae37ab10a573c930dc4fe144c
KirilNN <kiril.nikolov@gmail.com>
2015-08-03 18:47:44 +0300

    Add detection for Edge browser.Closes #933

--- src/kendo.core.js
+++ src/kendo.core.js
@@ -2023,6 +2023,7 @@ function pad(number, digits, end) {
                     webkit: /(chrome)[ \/]([\w.]+)/i,
                     safari: /(webkit)[ \/]([\w.]+)/i,
                     opera: /(opera)(?:.*version|)[ \/]([\w.]+)/i,
+                    edge: /(edge)[ \/]([\w.]+)/i,
                     msie: /(msie\s|trident.*? rv:)([\w.]+)/i,
                     mozilla: /(mozilla)(?:.*? rv:([\w.]+)|)/i
                 };
