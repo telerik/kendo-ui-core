852da32b9a88ba4a6a38b4f640a3702c0eabf544
Atanas Korchev <akorchev@gmail.com>
2015-08-28 15:34:26 +0300

    Allow code snippet editing and running.

--- docs/_assets/javascripts/dojo.js
+++ docs/_assets/javascripts/dojo.js
@@ -1,11 +1,5 @@
 jQuery(function() {
    $("body").find(".prettyprint").not("[lang='pseudo']").each(function() {
-       dojoApi.showHintButton(this, "Edit this example");
-   });
-
-   $("body").on("click", ".dojo-submit-button", function(e) {
-       e.preventDefault();
-       var snippet = $(this).next().text();
-       dojoApi.post(snippet);
+        dojoApi.addButtons(this);
    });
 });
