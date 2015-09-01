16f9a05133770486e0a1b16ce3d7b655c31a7746
Petyo Ivanov <underlog@gmail.com>
2015-09-01 11:57:29 +0300

    Reset the userevents instance
    Fix telerik/kendo-ui-core#1081

--- src/kendo.mobile.listview.js
+++ src/kendo.mobile.listview.js
@@ -715,6 +715,7 @@ var __meta__ = {
                 groupedMode = groups && groups[0],
                 item;
 
+
             if (action === "itemchange" && !listView._hasBindingTarget()) {
                 item = listView.findByDataItem(dataItems)[0];
                 if (item) {
@@ -1087,6 +1088,7 @@ var __meta__ = {
             this.options.type = "flat";
             this._angularItems("cleanup");
             this.element.empty();
+            this._userEvents.cancel();
             this._style();
             return this.insertAt(dataItems, 0);
         },
