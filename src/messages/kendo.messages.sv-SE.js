9c87c7be7e79aa3ac048fdb687eca5df6f2fbadc
Pher0x <aronflodin@gmail.com>
2015-10-19 12:02:01 +0200

    still trying (=/

--- src/messages/kendo.messages.sv-SE.js
+++ src/messages/kendo.messages.sv-SE.js
@@ -309,8 +309,4 @@
                 deleteWindowTitle: "Ta bort tillfälle",
                 showFullDay: "Visa heldag",
                 showWorkDay: "Visa för arbetsdag"
-            }))
-    }(window.kendo.jQuery), window.kendo;
-}, "function" == typeof define && define.amd ? define : function (e, t) {
-    t();
-})
+            }))}(window.kendo.jQuery), window.kendo;}, "function" == typeof define && define.amd ? define : function (e, t) {t();})
