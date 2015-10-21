cbb4e6c03f7399e3ea167d363abc20d532392ab3
Alexander Popov <alexander.popov@telerik.com>
2015-10-21 19:09:22 +0300

    Set selectedIndex to -1 if value is null Workaround for Firefox

--- src/kendo.binder.js
+++ src/kendo.binder.js
@@ -731,7 +731,12 @@ var __meta__ = { // jshint ignore:line
                         that.render();
                         if(that.bindings.value){
                             if (that.bindings.value) {
-                                that.element.value = retrievePrimitiveValues(that.bindings.value.get(), $(that.element).data("valueField"));
+                                var val = retrievePrimitiveValues(that.bindings.value.get(), $(that.element).data("valueField"));
+                                if(val === null) {
+                                    that.element.selectedIndex = -1;
+                                } else {
+                                    that.element.value = val;
+                                }
                             }
                         }
                     }
