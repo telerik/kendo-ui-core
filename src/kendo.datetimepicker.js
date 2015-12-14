85ca9732534c0b5456dabdf191658f342f785c23
KirilNN <kiril.nikolov@gmail.com>
2015-12-14 13:38:21 +0200

    Raise change event for updating ng-model

--- src/kendo.datetimepicker.js
+++ src/kendo.datetimepicker.js
@@ -319,21 +319,28 @@ var __meta__ = { // jshint ignore:line
         },
 
         _change: function(value) {
-            var that = this;
+            var that = this,
+            oldValue = that.element.val(),
+            dateChanged;
 
             value = that._update(value);
+            dateChanged = +that._old != +value;
 
-            if (+that._old != +value) {
+            var valueUpdated = dateChanged && !that._typing;
+            var textFormatted = oldValue !== that.element.val();
+
+            if (valueUpdated || textFormatted) {
+                that.element.trigger(CHANGE);
+            }
+
+            if (dateChanged) {
                 that._old = value;
                 that._oldText = that.element.val();
 
                 that.trigger(CHANGE);
-
-                if (!that._typing) {
-                    // trigger the DOM change event so any subscriber gets notified
-                    that.element.trigger(CHANGE);
-                }
             }
+
+            that._typing = false;
         },
 
         _option: function(option, value) {
