3dfb4b73cbc80dab85990a825036e3562d64f47c
Alexander Valchev <a.valchevbox@gmail.com>
2016-02-02 10:31:07 +0200

    Merge pull request #1404 from madaleno/messages
    Updated translations for pt-PT

--- src/messages/kendo.messages.pt-PT.js
+++ src/messages/kendo.messages.pt-PT.js
@@ -175,7 +175,7 @@ $.extend(true, kendo.ui.FilterCell.prototype.options.operators,{
 if (kendo.ui.FilterMenu) {
 kendo.ui.FilterMenu.prototype.options.messages =
 $.extend(true, kendo.ui.FilterMenu.prototype.options.messages,{
-  "info": "Mostrar items com valor que:",
+  "info": "Mostrar itens com valor que:",
   "isTrue": "é verdadeiro",
   "isFalse": "é falso",
   "filter": "Filtrar",
@@ -200,7 +200,11 @@ $.extend(true, kendo.ui.FilterMenu.prototype.options.operators,{
     "startswith": "Começa com",
     "contains": "Contém",
     "doesnotcontain": "Não contém",
-    "endswith": "Termina com"
+    "endswith": "Termina com",
+    "isnull": "É nulo",
+    "isnotnull": "É não nulo",
+    "isempty": "É vazio",
+    "isnotempty": "É não vazio"
   },
   "number": {
     "eq": "É igual a",
@@ -208,7 +212,9 @@ $.extend(true, kendo.ui.FilterMenu.prototype.options.operators,{
     "gte": "É maior ou igual a",
     "gt": "É maior que",
     "lte": "É menor ou igual a",
-    "lt": "É menor que"
+    "lt": "É menor que",
+    "isnull": "É nulo",
+    "isnotnull": "É não nulo"
   },
   "date": {
     "eq": "É igual a",
@@ -216,12 +222,27 @@ $.extend(true, kendo.ui.FilterMenu.prototype.options.operators,{
     "gte": "É posterior ou igual a",
     "gt": "É posterior a",
     "lte": "É anterior ou igual a",
-    "lt": "É anterior a"
+    "lt": "É anterior a",
+    "isnull": "É nulo",
+    "isnotnull": "É não nulo"
   },
   "enums": {
     "eq": "É igual a",
-    "neq": "Não é igual a"
+    "neq": "Não é igual a",
+    "isnull": "É nulo",
+    "isnotnull": "É não nulo"
   }
+});
+}
+
+/* FilterMultiCheck messages */
+
+if (kendo.ui.FilterMultiCheck) {
+kendo.ui.FilterMultiCheck.prototype.options.messages =
+$.extend(true, kendo.ui.FilterMultiCheck.prototype.options.messages,{
+  "checkAll": "Selecionar Tudo",
+  "clear": "Limpar",
+  "filter": "Filtrar"
 });
 }
 
@@ -276,8 +297,8 @@ $.extend(true, kendo.ui.Grid.prototype.options.messages,{
     "create": "Adicionar novo registo",
     "destroy": "Eliminar",
     "edit": "Editar",
-    "excel": "Export to Excel",
-    "pdf": "Export to PDF",
+    "excel": "Exportar para Excel",
+    "pdf": "Exportar para PDF",
     "save": "Guardar alterações",
     "select": "Selecionar",
     "update": "Atualizar"
@@ -286,7 +307,8 @@ $.extend(true, kendo.ui.Grid.prototype.options.messages,{
     "cancelDelete": "Cancelar",
     "confirmation": "Tem a certeza que pretende eliminar este registo?",
     "confirmDelete": "Eliminar"
-  }
+  },
+  "noRecords": "Nenhum registo disponível."
 });
 }
 
@@ -295,7 +317,7 @@ $.extend(true, kendo.ui.Grid.prototype.options.messages,{
 if (kendo.ui.Groupable) {
 kendo.ui.Groupable.prototype.options.messages =
 $.extend(true, kendo.ui.Groupable.prototype.options.messages,{
-  "empty": "Arraste uma coluna para este espaço para agrupar pelo valor da mesma..."
+  "empty": "Arraste uma coluna para este espaço para agrupar pelo valor da mesma"
 });
 }
 
@@ -314,7 +336,7 @@ $.extend(true, kendo.ui.NumericTextBox.prototype.options,{
 if (kendo.ui.Pager) {
 kendo.ui.Pager.prototype.options.messages =
 $.extend(true, kendo.ui.Pager.prototype.options.messages,{
-  "allPages": "All",
+  "allPages": "Tudo",
   "display": "Registos {0} - {1} de {2}",
   "empty": "Sem registos para apresentar.",
   "page": "Página",
@@ -483,6 +505,318 @@ $.extend(true, kendo.ui.Scheduler.prototype.options.messages,{
 });
 }
 
+/* Spreadsheet messages */
+
+if (kendo.spreadsheet && kendo.spreadsheet.messages.borderPalette) {
+kendo.spreadsheet.messages.borderPalette =
+$.extend(true, kendo.spreadsheet.messages.borderPalette,{
+  "allBorders": "Todas as bordas",
+  "insideBorders": "Bordas interiores",
+  "insideHorizontalBorders": "Bordas interiores horizontais",
+  "insideVerticalBorders": "Bordas interiores verticais",
+  "outsideBorders": "Bordas exteriores",
+  "leftBorder": "Borda esquerda",
+  "topBorder": "Borda superior",
+  "rightBorder": "Borda direita",
+  "bottomBorder": "Borda inferior",
+  "noBorders": "Sem bordas",
+  "reset": "Reiniciar cor",
+  "customColor": "Personalizar cor...",
+  "apply": "Aplicar",
+  "cancel": "Cancelar"
+});
+}
+
+if (kendo.spreadsheet && kendo.spreadsheet.messages.dialogs) {
+kendo.spreadsheet.messages.dialogs =
+$.extend(true, kendo.spreadsheet.messages.dialogs,{
+  "apply": "Aplicar",
+  "save": "Guardar",
+  "cancel": "Cancelar",
+  "remove": "Remover",
+  "okText": "OK",
+  "formatCellsDialog": {
+    "title": "Formatar",
+    "categories": {
+      "number": "Número",
+      "currency": "Moeda",
+      "date": "Data"
+      }
+  },
+  "fontFamilyDialog": {
+    "title": "Tipo de Letra"
+  },
+  "fontSizeDialog": {
+    "title": "Tamanho da Letra"
+  },
+  "bordersDialog": {
+    "title": "Bordas"
+  },
+  "alignmentDialog": {
+    "title": "Alinhamento de texto",
+    "buttons": {
+     "justtifyLeft": "Alinhar à esquerda",
+     "justifyCenter": "Centro",
+     "justifyRight": "Alinhar à direita",
+     "justifyFull": "Justificar",
+     "alignTop": "Alinhar ao topo",
+     "alignMiddle": "Alinhar ao centro",
+     "alignBottom": "Alinhar ao fundo"
+    }
+  },
+  "mergeDialog": {
+    "title": "Unir Células",
+    "buttons": {
+      "mergeCells": "Unir tudo",
+      "mergeHorizontally": "Unir horizontalmente",
+      "mergeVertically": "Unir verticalmente",
+      "unmerge": "Desunir"
+    }
+  },
+  "freezeDialog": {
+    "title": "Congelar painéis",
+    "buttons": {
+      "freezePanes": "Congelar painéis",
+      "freezeRows": "Congelar linhas",
+      "freezeColumns": "Congelar colunas",
+      "unfreeze": "Descongelar painéis"
+    }
+  },
+  "validationDialog": {
+    "title": "Validação de Dados",
+    "hintMessage": "Por favor indique um valor {1} válido {0}.",
+    "hintTitle": "Validação {0}",
+    "criteria": {
+      "any": "Qualquer valor",
+      "number": "Número",
+      "text": "Texto",
+      "date": "Data",
+      "custom": "Personalizar Fórmula",
+      "list": "Lista"
+    },
+    "comparers": {
+      "greaterThan": "maior que",
+      "lessThan": "menor que",
+      "between": "entre",
+      "notBetween": "não entre",
+      "equalTo": "igual a",
+      "notEqualTo": "não igual a",
+      "greaterThanOrEqualTo": "maior que ou igual a",
+      "lessThanOrEqualTo": "menor que ou igual a"
+    },
+    "comparerMessages": {
+      "greaterThan": "maior que {0}",
+      "lessThan": "menor que {0}",
+      "between": "entre {0} e {1}",
+      "notBetween": "não entre {0} e {1}",
+      "equalTo": "igual a {0}",
+      "notEqualTo": "não igual a {0}",
+      "greaterThanOrEqualTo": "maior que ou igual a {0}",
+      "lessThanOrEqualTo": "menor que ou igual a {0}",
+      "custom": "que satisfaça a fórmula: {0}"
+    },
+    "labels": {
+      "criteria": "Critério",
+      "comparer": "Comparador",
+      "min": "Min",
+      "max": "Max",
+      "value": "Valor",
+      "start": "Inicio",
+      "end": "Fim",
+      "onInvalidData": "Quando dados inválidos",
+      "rejectInput": "Rejeitar entrada",
+      "showWarning": "Mostrar aviso",
+      "showHint": "Mostrar dica",
+      "hintTitle": "Título da dica",
+      "hintMessage": "Mensagem da dica",
+      "ignoreBlank": "Ignorar brancos"
+    },
+    "placeholders": {
+      "typeTitle": "Digite o título",
+      "typeMessage": "Digite a mensagem"
+    }
+  },
+  "saveAsDialog": {
+    "title": "Guardar Como...",
+    "labels": {
+      "fileName": "Nome do ficheiro",
+      "saveAsType": "Guardar como tipo"
+    }
+  },
+  "exportAsDialog": {
+    "title": "Exportar...",
+    "labels": {
+      "fileName": "Nome do ficheiro",
+      "saveAsType": "Guardar como tipo",
+      "exportArea": "Exportar",
+      "paperSize": "Dimensão do papel",
+      "margins": "Margens",
+      "orientation": "Orientação",
+      "print": "Imprimir",
+      "guidelines": "Guias",
+      "center": "Centrar",
+      "horizontally": "Horizontalmente",
+      "vertically": "Verticalmente"
+    }
+  },
+  "modifyMergedDialog": {
+    "errorMessage": "Não pode fazer parte de uma célula unida."
+  },
+  "useKeyboardDialog": {
+    "title": "Copiar e colar",
+    "errorMessage": "Estas ações não podem ser invocadas pelo menu. Por favor use antes os atalhos do teclado:",
+    "labels": {
+      "forCopy": "para copiar",
+      "forCut": "para cortar",
+      "forPaste": "para colar"
+    }
+  },
+  "unsupportedSelectionDialog": {
+    "errorMessage": "Esta ação não pode ser efectuada em seleções múltiplas."
+  }
+});
+}
+
+if (kendo.spreadsheet && kendo.spreadsheet.messages.filterMenu) {
+kendo.spreadsheet.messages.filterMenu =
+$.extend(true, kendo.spreadsheet.messages.filterMenu,{
+  "sortAscending": "Ordenar intervalo de A a Z",
+  "sortDescending": "Ordenar intervalo de Z a A",
+  "filterByValue": "Filtrar por valor",
+  "filterByCondition": "Filtrar por condição",
+  "apply": "Aplicar",
+  "search": "Pesquisar",
+  "addToCurrent": "Adicionar à seleção atual",
+  "clear": "Limpar",
+  "blanks": "(Brancos)",
+  "operatorNone": "Nenhum",
+  "and": "E",
+  "or": "OU",
+  "operators": {
+    "string": {
+      "contains": "Texto contém",
+      "doesnotcontain": "Texto não contém",
+      "startswith": "Texto começa com",
+      "endswith": "Texto termina com"
+    },
+    "date": {
+      "eq":  "Data é",
+      "neq": "Data não é",
+      "lt":  "Data é anterior",
+      "gt":  "Data é posterior"
+    },
+    "number": {
+      "eq": "É igual a",
+      "neq": "Não é igual a",
+      "gte": "É maior que ou igual a",
+      "gt": "É maior que",
+      "lte": "É menor que ou igual a",
+      "lt": "É menor que"
+    }
+  }
+});
+}
+
+if (kendo.spreadsheet && kendo.spreadsheet.messages.toolbar) {
+kendo.spreadsheet.messages.toolbar =
+$.extend(true, kendo.spreadsheet.messages.toolbar,{
+  "addColumnLeft": "Adicionar coluna à esquerda",
+  "addColumnRight": "Adicionar coluna à direita",
+  "addRowAbove": "Adicionar linha acima",
+  "addRowBelow": "Adicionar linha abaixo",
+  "alignment": "Alinhmento",
+  "alignmentButtons": {
+    "justtifyLeft": "Alinhar à esquerda",
+    "justifyCenter": "Centrar",
+    "justifyRight": "Alinhar à direita",
+    "justifyFull": "Justificar",
+    "alignTop": "Alinhar ao topo",
+    "alignMiddle": "Alinhar ao meio",
+    "alignBottom": "Alinhar ao fundo"
+  },
+  "backgroundColor": "Fundo",
+  "bold": "Negrito",
+  "borders": "Bordas",
+  "colorPicker": {
+    "reset": "Repôr cor",
+    "customColor": "Personalizar cor..."
+  },
+  "copy": "Copiar",
+  "cut": "Cortar",
+  "deleteColumn": "Eliminar coluna",
+  "deleteRow": "Eliminar linha",
+  "excelImport": "Importar de Excel...",
+  "filter": "Filtrar",
+  "fontFamily": "Tipo de letra",
+  "fontSize": "Tamanho da letra",
+  "format": "Personalizar formatação...",
+  "formatTypes": {
+    "automatic": "Automático",
+    "number": "Número",
+    "percent": "Percentagem",
+    "financial": "Financeiro",
+    "currency": "Moeda",
+    "date": "Data",
+    "time": "Hora",
+    "dateTime": "Data e hora",
+    "duration": "Duração",
+    "moreFormats": "Mais formatos..."
+  },
+  "formatDecreaseDecimal": "Decrementar decimais",
+  "formatIncreaseDecimal": "Incrementar decimais",
+  "freeze": "Congelar painéis",
+  "freezeButtons": {
+    "freezePanes": "Congelar paineis",
+    "freezeRows": "Congelar linhas",
+    "freezeColumns": "Congelar colunas",
+    "unfreeze": "Descongelar painéis"
+  },
+  "italic": "Itálico",
+  "merge": "Unir células",
+  "mergeButtons": {
+    "mergeCells": "Unir tudo",
+    "mergeHorizontally": "Unir horizontalmente",
+    "mergeVertically": "Unir verticalmente",
+    "unmerge": "Desunir"
+  },
+  "open": "Abrir...",
+  "paste": "Colar",
+  "quickAccess": {
+    "redo": "Refazer",
+    "undo": "Desfazer"
+  },
+  "saveAs": "Guardar como...",
+  "sortAsc": "Ordenação ascendente",
+  "sortDesc": "Ordenação descendente",
+  "sortButtons": {
+    "sortSheetAsc": "Ordenar folha de A a Z",
+    "sortSheetDesc": "Ordenar folha de Z a A",
+    "sortRangeAsc": "Ordenar folha de A a Z",
+    "sortRangeDesc": "Ordenar folha de Z a A"
+  },
+  "textColor": "Cor do Texto",
+  "textWrap": "Quebrar texto",
+  "underline": "Sublinhado",
+  "validation": "Validação de dados..."
+});
+}
+
+if (kendo.spreadsheet && kendo.spreadsheet.messages.view) {
+kendo.spreadsheet.messages.view =
+$.extend(true, kendo.spreadsheet.messages.view,{
+  "errors": {
+    "shiftingNonblankCells": "Não é possível inserir células devido a possível perda de dados. Escolha outra localização para inserir ou elimine os dados do final da sua folha de dados.",
+    "filterRangeContainingMerges": "Não é possível criar um filtro dentro de um intervalo contendo uniões",
+    "validationError": "O valor que indicou viola as regras de validação definidas na célula."
+  },
+  "tabs": {
+    "home": "Início",
+    "insert": "Inserir",
+    "data": "Dados"
+  }
+});
+}
+
 /* Slider messages */
 
 if (kendo.ui.Slider) {
@@ -490,6 +824,40 @@ kendo.ui.Slider.prototype.options =
 $.extend(true, kendo.ui.Slider.prototype.options,{
   "increaseButtonTitle": "Incrementar",
   "decreaseButtonTitle": "Decrementar"
+});
+}
+
+/* TreeList messages */
+
+if (kendo.ui.TreeList) {
+kendo.ui.TreeList.prototype.options.messages =
+$.extend(true, kendo.ui.TreeList.prototype.options.messages,{
+  "noRows": "Sem registos para mostrar",
+  "loading": "A carregar...",
+  "requestFailed": "Pedido falhado.",
+  "retry": "Repetir",
+  "commands": {
+      "edit": "Editar",
+      "update": "Atualizar",
+      "canceledit": "Cancelar",
+      "create": "Adicionar novo registo",
+      "createchild": "Adicionar registo descendente",
+      "destroy": "Eliminar",
+      "excel": "Exportar para Excel",
+      "pdf": "Exportar para PDF"
+  }
+});
+}
+
+if (kendo.ui.TreeList) {
+kendo.ui.TreeList.prototype.options.columnMenu =
+$.extend(true, kendo.ui.TreeList.prototype.options.columnMenu, {
+    "messages": {
+        "columns": "Escolher colunas",
+        "filter": "Aplicar filtro",
+        "sortAscending": "Ordenar (asc)",
+        "sortDescending": "Ordenar (desc)"
+    }
 });
 }
 
@@ -518,7 +886,7 @@ $.extend(true, kendo.ui.Upload.prototype.options.localization,{
   "statusUploading": "a enviar",
   "statusUploaded": "submetido",
   "statusWarning": "aviso",
-  "statusFailed": "failhou",
+  "statusFailed": "falhou",
   "headerStatusUploading": "Enviando...",
   "headerStatusUploaded": "Feito"
 });
@@ -536,7 +904,8 @@ $.extend(true, kendo.ui.Validator.prototype.options.messages,{
   "step": "{0} não é válido",
   "email": "{0} não é um email válido",
   "url": "{0} não é um endereço web válido",
-  "date": "{0} não é uma data válida"
+  "date": "{0} não é uma data válida",
+  "dateCompare": "A data final deve ser posterior à data inicial"
 });
 }
 })(window.kendo.jQuery);
