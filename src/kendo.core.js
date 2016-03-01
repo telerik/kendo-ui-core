074a2de46500ee15005f550e34f77600d14209a1
Georgi Krustev <ggkrustev@yahoo.com>
2016-03-01 17:28:55 +0200

    Revert changes made in  related to  formatting. Related to telerik/kendo-ui-core#1474

--- src/kendo.core.js
+++ src/kendo.core.js
@@ -1052,7 +1052,7 @@ function pad(number, digits, end) {
             }
         }
 
-        return value !== undefined && value !== null ? value : "";
+        return value !== undefined ? value : "";
     };
 
     kendo.format = function(fmt) {
