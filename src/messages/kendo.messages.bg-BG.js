0fe127d9b7e99f8f5d9fd300025bd9002ab897c3
Alex Gyoshev <alex@gyoshev.net>
2016-03-22 11:24:28 +0200

    Add TreeList localization messages

--- src/messages/kendo.messages.bg-BG.js
+++ src/messages/kendo.messages.bg-BG.js
@@ -615,6 +615,28 @@ $.extend(true, kendo.ui.Grid.prototype.options.messages,{
 });
 }
 
+/* TreeList messages */
+
+if (kendo.ui.TreeList) {
+kendo.ui.TreeList.prototype.options.messages =
+$.extend(true, kendo.ui.TreeList.prototype.options.messages,{
+    "noRows": "Няма налични записи.",
+    "loading": "Зареждане...",
+    "requestFailed": "Грешка при заявка",
+    "retry": "Опитай отново",
+    "commands": {
+        "edit": "Редактиране",
+        "update": "Обнови",
+        "canceledit": "Отказ",
+        "create": "Добави",
+        "createchild": "Добави подзапис",
+        "destroy": "Изтриване",
+        "excel": "Експорт към Excel",
+        "pdf": "Експорт към PDF"
+    }
+});
+}
+
 /* Pager messages */
 
 if (kendo.ui.Pager) {
