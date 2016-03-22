db413b97db6de5ca1eb15bc1d54dfbf65d690aea
Georgi Krustev <ggkrustev@yahoo.com>
2016-03-22 16:11:55 +0200

    Widget resets model when source is set asynchronously. Fixes telerik/kendo-ui-core#1545

--- src/kendo.angular.js
+++ src/kendo.angular.js
@@ -373,6 +373,9 @@ var __meta__ = { // jshint ignore:line
         }
 
         var value;
+        // Some widgets trigger "change" on the input field
+        // and this would result in two events sent (#135)
+        var haveChangeOnElement = false;
 
         if (isForm(element)) {
             value = function() {
@@ -399,7 +402,9 @@ var __meta__ = { // jshint ignore:line
                 val = null;
             }
 
+            haveChangeOnElement = true;
             setTimeout(function(){
+                haveChangeOnElement = false;
                 if (widget) { // might have been destroyed in between. :-(
                     var kNgModel = scope[widget.element.attr("k-ng-model")];
 
@@ -417,10 +422,6 @@ var __meta__ = { // jshint ignore:line
                 }
             }, 0);
         };
-
-        // Some widgets trigger "change" on the input field
-        // and this would result in two events sent (#135)
-        var haveChangeOnElement = false;
 
         if (isForm(element)) {
             element.on("change", function() {
