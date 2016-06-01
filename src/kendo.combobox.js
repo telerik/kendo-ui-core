d0ea9d15b47dfebc29f89acaa91e2cf1f5246a83
Alexander Popov <alexander.popov@telerik.com>
2016-06-01 12:13:32 +0300

    ComboBox input inherits maxlength attribute

--- src/kendo.combobox.js
+++ src/kendo.combobox.js
@@ -716,7 +716,8 @@ var __meta__ = { // jshint ignore:line
                 wrapper = that.wrapper,
                 SELECTOR = "input.k-input",
                 name = element.name || "",
-                input;
+                input,
+                maxLength;
 
             if (name) {
                 name = 'name="' + name + '_input" ';
@@ -734,8 +735,9 @@ var __meta__ = { // jshint ignore:line
             input[0].style.cssText = element.style.cssText;
             input[0].title = element.title;
 
-            if (element.maxLength > -1) {
-                input[0].maxLength = element.maxLength;
+            maxLength = parseInt(this.element.prop("maxlength") || this.element.attr("maxlength"));
+            if (maxLength > -1) {
+                input[0].maxLength = maxLength;
             }
 
             input.addClass(element.className)
--- tests/combobox/angular.js
+++ tests/combobox/angular.js
@@ -197,4 +197,22 @@
         equal(widget.listView.bound(), false);
         equal(widget.value(), "");
     });
+
+
+    ngTest("combobox's input should inherit maxlength attribute", 1, function() {
+        angular.module("kendo.tests").controller("mine", function($scope) {
+            var colors = new kendo.data.ObservableArray([ { color: "red", value: 1 }, { color: "green", value: 2 }, { color: "blue", value: 3 } ]);
+
+            $scope.colors = new kendo.data.DataSource({ data: colors }),
+            $scope.selectedColor = 2;
+        });
+
+        QUnit.fixture.html('<div ng-controller=mine><select maxlength="3" kendo-combo-box k-auto-bind="false" k-value-primitive="true" k-text="\'custom text\'" k-ng-model=selectedColor k-data-source=colors k-data-text-field="\'color\'" k-data-value-field="\'value\'"></select></div>');
+    },
+
+    function() {
+        var widget = QUnit.fixture.find("select").getKendoComboBox();
+
+        equal(widget.input.attr("maxlength"), 3);
+    });
 })();
