62d07ad4ff333d099fd7492890881c5faa123ca1
Nikodim Lazarov <nlazarov@gmail.com>
2016-07-22 19:08:30 +0300

    Change global scope function definition in kendo.dialog

--- src/kendo.dialog.js
+++ src/kendo.dialog.js
@@ -633,4 +633,4 @@
 
     return window.kendo;
 
-}, typeof define == 'function' && define.amd ? define : function(_, f) { f(); });
+}, typeof define == 'function' && define.amd ? define : function(a1, a2, a3){ (a3 || a2)(); });
