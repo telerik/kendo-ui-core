a4150883ab3c3ba0e8c3ec041326d5211245492e
joneff <joneff@gmail.com>
2016-10-04 11:08:11 +0300

    fix: remove text from refresh button in pager

--- src/kendo.pager.js
+++ src/kendo.pager.js
@@ -164,7 +164,7 @@ var __meta__ = { // jshint ignore:line
             if (options.refresh) {
                 if (!that.element.find(".k-pager-refresh").length) {
                     that.element.append('<a href="#" class="k-pager-refresh k-link" title="' + options.messages.refresh +
-                        '"><span class="k-icon k-i-refresh">' + options.messages.refresh + "</span></a>");
+                        '"><span class="k-icon k-i-refresh"></span></a>');
                 }
 
                 that.element.on(CLICK + NS, ".k-pager-refresh", proxy(that._refreshClick, that));
