e0bf5c64c7bd7601d46b55f40a920fa3637c1f39
Bozhidar Ruzhenov <ruzhenov@telerik.com>
2016-11-22 17:10:44 +0200

    chore(font-icons): remove debugger tests/window/api.js

--- tests/window/api.js
+++ tests/window/api.js
@@ -529,7 +529,6 @@
             animation: false,
             actions: ["Pin"]
         });
-debugger;
         equal(dialog.wrapper.find(".k-i-pin").length, 1);
         equal(dialog.wrapper.find(".k-i-unpin").length, 0);
 
