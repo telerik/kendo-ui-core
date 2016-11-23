8a38ecbd1d8ec28c1c116eb4aa2a345a3539fdf8
Bozhidar Ruzhenov <ruzhenov@telerik.com>
2016-11-23 17:20:51 +0200

    fix: (font-icons) merge conflicts - Integrate new unified font in editors' group widgets

--- tests/window/api.js
+++ tests/window/api.js
@@ -529,6 +529,7 @@
             animation: false,
             actions: ["Pin"]
         });
+
         equal(dialog.wrapper.find(".k-i-pin").length, 1);
         equal(dialog.wrapper.find(".k-i-unpin").length, 0);
 
