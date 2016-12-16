fcf521ecef4d00e15f42e574e046d26fa1308d7b
Dimo Dimov <dimo.dimov@telerik.com>
2016-12-16 16:40:28 +0200

    docs: fix build uses wrong sitemap template

--- docs/_plugins/sitemap_generator.rb
+++ docs/_plugins/sitemap_generator.rb
@@ -1,16 +1,13 @@
 require 'jekyll-sitemap'
 
 module Jekyll
-    class SitemapGenerator < JekyllSitemap
-        def source_path
-             File.expand_path('sitemap.xml', File.dirname(__FILE__))
-        end
 
-        def sitemap_content
-            site_map = Page.new(@site, File.dirname(__FILE__), '', 'sitemap.xml')
-            site_map.content = File.read(source_path)
-            site_map.render(Hash.new, @site.site_payload)
-            site_map.output
-        end
-    end
+	#override the method from the original Jekyll-sitemap gem so it always uses our template instead of using their built-in template
+	class JekyllSitemap < Jekyll::Generator
+		def source_path
+			File.expand_path('sitemap.xml', File.dirname(__FILE__))
+		end
+	end
+	JekyllSitemap.new.source_path
+
 end
