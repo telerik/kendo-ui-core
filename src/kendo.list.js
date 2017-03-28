e2e7aee8a919854ebdeed188e59a590ba1eab5c8
Ianko Djemerenov <iankodj@gmail.com>
2017-03-28 16:08:13 +0300

    fix(dropdownlist): optionLabel tab key trows JS error
    Related to: telerik/kendo-ui-core#2961

--- src/kendo.list.js
+++ src/kendo.list.js
@@ -1155,7 +1155,11 @@ var __meta__ = { // jshint ignore:line
 
                 if (current) {
                     dataItem = listView.dataItemByIndex(listView.getElementIndex(current));
-                    var shouldTrigger = that._value(dataItem) !==  List.unifyType(that.value(), typeof that._value(dataItem));
+                    var shouldTrigger = true;
+
+                    if(dataItem){
+                        shouldTrigger = that._value(dataItem) !==  List.unifyType(that.value(), typeof that._value(dataItem));
+                    }
 
                     if (shouldTrigger && that.trigger(SELECT, { dataItem: dataItem, item: current })) {
                         return;
