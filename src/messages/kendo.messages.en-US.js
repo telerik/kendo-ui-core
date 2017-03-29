b63c1f80c3b0404217c1082880b2f0ff737579f2
dbalinov <dragomir.balinov@progress.com>
2017-03-29 11:08:59 +0300

    fix(messages): sync status fails

--- src/messages/kendo.messages.en-US.js
+++ src/messages/kendo.messages.en-US.js
@@ -361,7 +361,8 @@ $.extend(true, kendo.ui.Grid.prototype.options.messages,{
     "confirmation": "Are you sure you want to delete this record?",
     "confirmDelete": "Delete"
   },
-  "noRecords": "No records available."
+  "noRecords": "No records available.",
+  "expandCollapseColumnHeader": ""
 });
 }
 
