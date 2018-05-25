3bfe2f660bb4e95ae68433996098ce638bdbc30a
IKKI2000 <ikki2002@qq.com>
2018-05-25 10:27:47 +0800

    Add DropDownTree Messages~

--- src/messages/kendo.messages.zh-CN.js
+++ src/messages/kendo.messages.zh-CN.js
@@ -1208,6 +1208,16 @@
                 "dayperiod": "上午/下午"
             });
     }
+    
+    /* DropDownTree messages */
+    if (kendo.ui.DropDownTree) {
+        kendo.ui.DropDownTree.prototype.options.messages =
+            $.extend(true, kendo.ui.DropDownTree.prototype.options.messages, {
+                clear: "清空",
+                deleteTag: "删除",
+                singleTag: "项已选择"
+            });
+    }
 
     /* Mobile Messages ------------------------------ */
 
