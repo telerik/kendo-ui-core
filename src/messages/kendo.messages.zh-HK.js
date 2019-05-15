7d6b3f71221f4d6910c2664f310afab6cd791f11
IKKI2000 <ikki2002@qq.com>
2019-05-15 09:49:49 +0800

    🌌 Update Calendar weekColumnHeader Messages~
    * Update Calendar "weekColumnHeader" Messages

--- src/messages/kendo.messages.zh-HK.js
+++ src/messages/kendo.messages.zh-HK.js
@@ -1198,7 +1198,7 @@
     if (kendo.ui.Calendar) {
         kendo.ui.Calendar.prototype.options.messages =
             $.extend(true, kendo.ui.Calendar.prototype.options.messages, {
-                "weekColumnHeader": ""
+                "weekColumnHeader": "週"
             });
     }
 
