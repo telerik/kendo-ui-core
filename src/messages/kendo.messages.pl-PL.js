7f12d05c3606a84d9660f1c112f33d616cd3e3b1
Anna Kosieradzka <a.wrochna@gmail.com>
2019-09-26 15:08:09 +0200

    Missing PL localizations

--- src/messages/kendo.messages.pl-PL.js
+++ src/messages/kendo.messages.pl-PL.js
@@ -173,7 +173,7 @@ $.extend(true, kendo.ui.Grid.prototype.options.messages,{
 if (kendo.ui.Pager) {
 kendo.ui.Pager.prototype.options.messages =
 $.extend(true, kendo.ui.Pager.prototype.options.messages,{
-  "allPages": "All",
+  "allPages": "Wszystkie",
   "page": "Strona",
   "display": "Wyświetlanie elementów {0} - {1} z {2}",
   "of": "z {0}",
@@ -193,7 +193,7 @@ $.extend(true, kendo.ui.Pager.prototype.options.messages,{
 if (kendo.ui.TreeListPager) {
 kendo.ui.TreeListPager.prototype.options.messages =
 $.extend(true, kendo.ui.TreeListPager.prototype.options.messages,{
-  "allPages": "All",
+  "allPages": "Wszystkie",
   "page": "Strona",
   "display": "Wyświetlanie elementów {0} - {1} z {2}",
   "of": "z {0}",
@@ -364,7 +364,7 @@ $.extend(true, kendo.ui.Upload.prototype.options.localization,{
   "statusUploaded": "załadowane",
   "statusUploading": "trwa ładowanie",
   "uploadSelectedFiles": "Załaduj pliki",
-  "headerStatusUploaded": "Done",
+  "headerStatusUploaded": "OK",
   "headerStatusUploading": "Uploading..."
 });
 }
@@ -374,53 +374,53 @@ $.extend(true, kendo.ui.Upload.prototype.options.localization,{
 if (kendo.ui.Scheduler) {
 kendo.ui.Scheduler.prototype.options.messages =
 $.extend(true, kendo.ui.Scheduler.prototype.options.messages,{
-  "allDay": "all day",
+  "allDay": "cały dzień",
   "cancel": "Anuluj",
   "editable": {
-    "confirmation": "Are you sure you want to delete this event?"
+    "confirmation": "Czy na pewno chcesz usunąć to wydarzenie?"
   },
-  "date": "Date",
-  "destroy": "Delete",
+  "date": "Data",
+  "destroy": "Usuń",
   "editor": {
-    "allDayEvent": "All day event",
-    "description": "Description",
-    "editorTitle": "Event",
-    "end": "End",
-    "endTimezone": "End timezone",
-    "repeat": "Repeat",
-    "separateTimezones": "Use separate start and end time zones",
-    "start": "Start",
-    "startTimezone": "Start timezone",
-    "timezone": " ",
-    "timezoneEditorButton": "Time zone",
-    "timezoneEditorTitle": "Timezones",
-    "title": "Title",
-    "noTimezone": "No timezone"
+    "allDayEvent": "Wydarzenie całodniowe",
+    "description": "Opis",
+    "editorTitle": "Wydarzenie",
+    "end": "Koniec",
+    "endTimezone": "Strefa czasowa końca",
+    "repeat": "Cykl",
+    "separateTimezones": "Osobne strefy czasowe dla początku i końca",
+    "start": "Początek",
+    "startTimezone": "Strefa czasowa początku",
+    "timezone": "Strefa czasowa",
+    "timezoneEditorButton": "Strefa czasowa",
+    "timezoneEditorTitle": "Strefy czasowe",
+    "title": "Tytuł",
+    "noTimezone": "Brak strefy czasowej"
   },
-  "event": "Event",
+  "event": "Wydarzenie",
   "recurrenceMessages": {
-    "deleteRecurring": "Do you want to delete only this event occurrence or the whole series?",
-    "deleteWindowOccurrence": "Delete current occurrence",
-    "deleteWindowSeries": "Delete the series",
-    "deleteWindowTitle": "Delete Recurring Item",
-    "editRecurring": "Do you want to edit only this event occurrence or the whole series?",
-    "editWindowOccurrence": "Edit current occurrence",
-    "editWindowSeries": "Edit the series",
-    "editWindowTitle": "Edit Recurring Item"
+    "deleteRecurring": "Czy chcesz usunąć pojedyńcze wydarzenie, czy cały cykl?",
+    "deleteWindowOccurrence": "Usuń bieżące wydarzenie",
+    "deleteWindowSeries": "Usuń cykl",
+    "deleteWindowTitle": "Usuń wydarzenie cykliczne",
+    "editRecurring": "Czy chcesz edytować pojedyńcze wydarzenie, czy cały cykl?",
+    "editWindowOccurrence": "Edytuj bieżące wydarzenie",
+    "editWindowSeries": "Edytuj cykl",
+    "editWindowTitle": "Edytuj wydarzenie cykliczne"
   },
-  "save": "Save",
-  "time": "Time",
-  "today": "Today",
+  "save": "Zapisz",
+  "time": "Czas",
+  "today": "Dziś",
   "views": {
     "agenda": "Agenda",
-    "day": "Day",
-    "month": "Month",
-    "week": "Week",
-    "workWeek": "Work Week"
+    "day": "Dzień",
+    "month": "Miesiąc",
+    "week": "Tydzień",
+    "workWeek": "Tydzień roboczy"
   },
-  "deleteWindowTitle": "Delete event",
-  "showFullDay": "Show full day",
-  "showWorkDay": "Show business hours"
+  "deleteWindowTitle": "Usuń wydarzenie",
+  "showFullDay": "Pokaż cały dzień",
+  "showWorkDay": "Pokaż godziny pracy"
 });
 }
 
