7a9985926603a62cccd03752996cdd92fd85146d
SWH321 <75625256+SWH321@users.noreply.github.com>
2020-12-17 12:50:57 +0100

    Update kendo.messages.hu-HU.js

--- src/messages/kendo.messages.hu-HU.js
+++ src/messages/kendo.messages.hu-HU.js
@@ -707,6 +707,9 @@ $.extend(true, kendo.spreadsheet.messages.dialogs,{
     "text": "Biztos, hogy törli ezt a munkalapot?",
     "title": "Munkalap törlése"
   },
+  "overflowDialog": {
+    "errorMessage": 'Nem illeszthető be, mert a másolási és a beillesztési terület nem azonos méretű és alakú.'
+  },
   "validationDialog": {
     "title": "Adatellenőrzés",
     "hintMessage": "Kérem, írjon be egy érvényes {0} értéket {1}.",
@@ -935,7 +938,8 @@ $.extend(true, kendo.spreadsheet.messages.view,{
   "errors": {
     "shiftingNonblankCells": "Adatvesztés nélkül nem lehet a cellákat beszúrni. Válasszon másik beszúrási pozíciót, vagy törölje az adatokat a munkalap végéről.",
     "filterRangeContainingMerges": "Nem lehet szűrőt készíteni összevonásokat tartalmazó tartományon belül",
-    "validationError": "A beírt érték megsérti a cellára beállított adatellenőrzési szabályokat."
+    "validationError": "A beírt érték megsérti a cellára beállított adatellenőrzési szabályokat.",
+	"cannotModifyDisabled": 'Nem lehet módosítani a letiltott cellákat.'
   },
   "tabs": {
     "home": "Elejére",
@@ -969,6 +973,22 @@ $.extend(true, kendo.ui.ListBox.prototype.options.messages,{
     "transferAllTo": "Összes felvétele",
     "transferAllFrom": "Összes eltávolítása"
   }
+});
+}
+
+/* DateTimePicker messages */
+if (kendo.ui.DateTimePicker) {
+kendo.ui.DateTimePicker.prototype.options.messages =
+$.extend(true, kendo.ui.DateTimePicker.prototype.options.messages, {
+  "set": "Beállít",
+  "cancel": "Mégse",
+  "hour": "Óra",
+  "minute": "Perc",
+  "second": "Másodperc",
+  "milisecond": "Millisec",
+  "now": "Most",
+  "date": "Dátum",
+  "time": "Idő"
 });
 }
 
@@ -1067,7 +1087,8 @@ $.extend(true, kendo.ui.Dialog.prototype.options.localization, {
 if (kendo.ui.Calendar) {
 kendo.ui.Calendar.prototype.options.messages =
 $.extend(true, kendo.ui.Calendar.prototype.options.messages, {
-  "weekColumnHeader": ""
+  "weekColumnHeader": "",
+  "today": "Ma"
 });
 }
 
