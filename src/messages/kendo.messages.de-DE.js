1e1de1040ebd42873289d7a33d0a0c11f450bfb8
James Patterson <james@m1dst.co.uk>
2021-01-07 10:50:58 +0000

    Added some filter translations for the de-DE locale

--- src/messages/kendo.messages.de-DE.js
+++ src/messages/kendo.messages.de-DE.js
@@ -1,7 +1,63 @@
 (function ($, undefined) {
   /* Filter cell operator messages */
 
-  if (kendo.ui.FilterCell) {
+  if (kendo.ui.Filter) {
+    kendo.ui.Filter.prototype.options.messages =
+      $.extend(true, kendo.ui.Filter.prototype.options.messages, {
+        "addExpression": "Ausdruck hinzufügen",
+        "addGroup": "Gruppe hinzufügen",
+        "and": "Und",
+        "apply": "Anwenden",
+        "close": "Schließen",
+        "fields": "Felder",
+        "operators": "Operatoren",
+        "or": "Oder"
+      });
+
+    kendo.ui.Filter.prototype.options.operators =
+      $.extend(true, kendo.ui.Filter.prototype.options.operators, {
+        "date": {
+          "eq": "gleich ist",
+          "gt": "später ist als",
+          "gte": "gleich oder später ist als",
+          "lt": "früher ist als",
+          "lte": "früher oder gleich ist als",
+          "neq": "nicht gleich ist",
+          "isnull": "Null ist",
+          "isnotnull": "nicht Null ist"
+        },
+        "boolean": {
+          "eq": "gleich ist",
+          "neq": "nicht gleich ist"
+        },
+        "number": {
+          "eq": "gleich ist",
+          "gt": "größer ist als",
+          "gte": "größer als oder gleich ist",
+          "lt": "kleiner ist",
+          "lte": "kleiner als oder gleich ist",
+          "neq": "nicht gleich ist",
+          "isnull": "Null ist",
+          "isnotnull": "nicht Null ist"
+        },
+        "string": {
+          "contains": "beinhaltet",
+          "doesnotcontain": "nicht beinhaltet",
+          "endswith": "endet mit",
+          "eq": "gleich ist",
+          "neq": "nicht gleich ist",
+          "startswith": "beginnt mit",
+          "isnull": "Null ist",
+          "isnotnull": "nicht Null ist",
+          "isempty": "leer ist",
+          "isnotempty": "nicht leer ist",
+          "isnullorempty": "Besitzt keinen Wert",
+          "isnotnullorempty": "Besitzt einen Wert"
+        }
+      });
+  }
+
+ if (kendo.ui.FilterCell) {
     kendo.ui.FilterCell.prototype.options.operators =
       $.extend(true, kendo.ui.FilterCell.prototype.options.operators, {
         "date": {
