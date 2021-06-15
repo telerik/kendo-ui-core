3598e564018c9500ca571e327a7b521d8df907fd
User Jenkins <jenkins@kendobuild-vsph.dev.progress.com>
2021-06-15 04:58:23 +0300

    Sync with Kendo UI Professional

--- typescript/kendo.all.d.ts
+++ typescript/kendo.all.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Kendo UI Professional v2021.2.511
+// Type definitions for Kendo UI Professional v2021.2.616
 // Project: http://www.telerik.com/kendo-ui
 // Definitions by: Telerik <https://github.com/telerik>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
