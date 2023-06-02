017eebc4cc4cf2812e59f8f7a220d93e80f04f93
ag-petrov <angel.petrov@progress.com>
2023-06-02 17:55:01 +0300

    chore: mark editable and listview as non-synced

--- src/kendo.listview.js
+++ src/kendo.listview.js
@@ -27,6 +27,10 @@ var __meta__ = {
     } ]
 };
 
+// The current file development is no longer in active phase but the code will be kept as is.
+// Please be advised that we have discontinued the sync for this file with the commercial version of Kendo UI for jQuery.
+// To take advantage of new features please visit (https://www.telerik.com/kendo-jquery-ui) and consider upgrading to a commercial license. 
+
 (function($, undefined) {
     var kendo = window.kendo,
         CHANGE = "change",
